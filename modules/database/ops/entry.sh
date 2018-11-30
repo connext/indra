66bd25f9715279ff81ef4015539f4de03a3dc3d9
Bo Henderson <bohende@gmail.com>
2018-11-30 17:43:42 +0530

    db: fix password handling during db initialization

--- modules/database/ops/entry.sh
+++ modules/database/ops/entry.sh
@@ -4,18 +4,19 @@ set -e
 # Print env then add database password
 echo "=> Starting Postgres in env:"
 env
-export POSTGRES_PASSWORD="`cat $POSTGRES_PASSWORD_FILE`"
 
 # Start temporary database in background to run migrations
 /docker-entrypoint.sh postgres &
 PID=$!
-bash ops/wait-for-it.sh 127.0.0.1:5432
+ops/wait-for-it.sh 127.0.0.1:5432
 echo "=> Good morning, Postgres! Running migrations..."
- 
+
 # Run migrations
 migrate=./node_modules/.bin/db-migrate
+POSTGRES_PASSWORD="`cat $POSTGRES_PASSWORD_FILE`"
 $migrate up all --verbose --config ops/config.json --migrations-dir node_modules/machinomy/migrations
 $migrate up all --verbose --config ops/config.json --migrations-dir migrations
+unset POSTGRES_PASSWORD
 
 # Start database in foreground to serve requests from hub
 echo "=> Migrations completed successfully, restarting database"
