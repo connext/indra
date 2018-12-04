fa621478206c545837712763ac96a747ab60648d
Bo Henderson <bohende@gmail.com>
2018-12-04 19:48:42 +0530

    ops: overhaul deploy scripts

--- ops/deploy.dev.sh
+++ ops/deploy.dev.sh
@@ -3,57 +3,69 @@
 ####################
 # ENV VARS
 
-DOMAINNAME=localhost
+project=connext
+
+# set defaults for some core env vars
+MODE=$MODE; [[ -n "$MODE" ]] || MODE=development
+DOMAINNAME=$DOMAINNAME; [[ -n "$DOMAINNAME" ]] || DOMAINNAME=localhost
+EMAIL=$EMAIL; [[ -n "$EMAIL" ]] || EMAIL=noreply@gmail.com
+
+# ethereum settings
+ETH_RPC_URL="http://ethprovider:8545"
 ETH_NETWORK_ID="4447"
 ETH_MNEMONIC="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
 WALLET_ADDRESS="0xfb482f8f779fd96a857f1486471524808b97452d"
 CHANNEL_MANAGER_ADDRESS="0xa8c50098f6e144bf5bae32bdd1ed722e977a0a42"
 HOT_WALLET_ADDRESS="0xfb482f8f779fd96a857f1486471524808b97452d"
 TOKEN_CONTRACT_ADDRESS="0xd01c08c7180eae392265d8c7df311cf5a93f1b73"
-SERVICE_USER_KEY="foo"
-ETH_RPC_URL="http://ethprovider:8545"
+
+# database settings
+POSTGRES_HOST="database"
+POSTGRES_PORT="5432"
+POSTGRES_USER="$project"
+POSTGRES_DB="$project"
+POSTGRES_PASSWORD_FILE="/run/secrets/database_dev"
 REDIS_URL="redis://redis:6379"
 
 ####################
+# Deploy according to above configuration
 
-project=connext
 hub_image=${project}_hub:dev
 ethprovider_image=${project}_ethprovider:dev
 database_image=${project}_database:dev
 redis_image=redis:5-alpine
 
+# turn on swarm mode if it's not already on
 docker swarm init 2> /dev/null
 
 function pull_if_unavailable {
-    if [[ -z "`docker image ls | grep ${1%:*} | grep ${1#*:}`" ]]
-    then
-        docker pull $1
-    fi
+  if [[ -z "`docker image ls | grep ${1%:*} | grep ${1#*:}`" ]]
+  then docker pull $1
+  fi
 }
 
 pull_if_unavailable $redis_image
 
 function new_secret {
-    secret=$2
-    if [[ -z "$secret" ]]
-    then
-        secret=`head -c 32 /dev/urandom | xxd -plain -c 32 | tr -d '\n\r'`
-    fi
-    if [[ -z "`docker secret ls -f name=$1 | grep -w $1`" ]]
-    then
-        id=`echo $secret | tr -d '\n\r' | docker secret create $1 -`
-        echo "Created secret called $1 with id $id"
-    fi
+  secret=$2
+  if [[ -z "$secret" ]]
+  then secret=`head -c 32 /dev/urandom | xxd -plain -c 32 | tr -d '\n\r'`
+  fi
+  if [[ -z "`docker secret ls -f name=$1 | grep -w $1`" ]]
+  then
+    id=`echo $secret | tr -d '\n\r' | docker secret create $1 -`
+    echo "Created secret called $1 with id $id"
+  fi
 }
 
-new_secret database_dev
+new_secret connext_database_dev
 
 mkdir -p /tmp/$project
 cat - > /tmp/$project/docker-compose.yml <<EOF
 version: '3.4'
 
 secrets:
-  database_dev:
+  connext_database_dev:
     external: true
 
 volumes:
@@ -67,7 +79,7 @@ services:
     ports:
       - "8080:8080"
     secrets:
-      - database_dev
+      - connext_database_dev
     environment:
       WALLET_ADDRESS: $WALLET_ADDRESS
       CHANNEL_MANAGER_ADDRESS: $CHANNEL_MANAGER_ADDRESS
@@ -75,11 +87,13 @@ services:
       TOKEN_CONTRACT_ADDRESS: $TOKEN_CONTRACT_ADDRESS
       ETH_RPC_URL: $ETH_RPC_URL
       SERVICE_USER_KEY: $SERVICE_USER_KEY
-      POSTGRES_USER: $project
-      POSTGRES_DB: $project
-      POSTGRES_PASSWORD_FILE: /run/secrets/database_dev
+      POSTGRES_USER: $POSTGRES_USER
+      POSTGRES_PASSWORD_FILE: /run/secrets/connext_database_dev
+      POSTGRES_HOST: $POSTGRES_HOST
+      POSTGRES_PORT: $POSTGRES_PORT
+      POSTGRES_DB: $POSTGRES_DB
       REDIS_URL: $REDIS_URL
-    
+
   redis:
     image: $redis_image
     ports:
@@ -90,13 +104,13 @@ services:
     environment:
       POSTGRES_USER: $project
       POSTGRES_DB: $project
-      POSTGRES_PASSWORD_FILE: /run/secrets/database_dev
+      POSTGRES_PASSWORD_FILE: /run/secrets/connext_database_dev
     deploy:
       mode: global
     ports:
       - "5432:5432"
     secrets:
-      - database_dev
+      - connext_database_dev
     volumes:
       - database_dev:/var/lib/postgresql/data
 
@@ -119,12 +133,13 @@ rm -rf /tmp/$project
 
 echo -n "Waiting for the $project stack to wake up."
 while true
-do num_awake="`docker container ls | grep $project | wc -l | sed 's/ //g'`"
-   sleep 3
-   if [[ "$num_awake" == "4" ]]
-   then break
-   else echo -n "."
-   fi
+do
+    num_awake="`docker container ls | grep $project | wc -l | sed 's/ //g'`"
+    sleep 3
+    if [[ "$num_awake" == "4" ]]
+    then break
+    else echo -n "."
+    fi
 done
 echo " Good Morning!"
 sleep 2
--- ops/deploy.prod.sh
+++ ops/deploy.prod.sh
@@ -1,92 +1,116 @@
 #!/usr/bin/env bash
 
-# eth settings
-CONTRACT_ADDRESS=0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF
-ETH_NETWORK_ID=4447
-ETH_NODE_PROTOCOL=http://
-ETH_NODE_HOST=host.docker.internal
-ETH_NODE_PORT=8545
-ETH_NODE_URL=${ETH_NODE_PROTOCOL}${ETH_NODE_HOST}:${ETH_NODE_PORT}
-HUB_ACCOUNT=0x627306090abab3a6e1400e9345bc60c78a8bef57
+####################
+# ENV VARS
 
-# image settings
-repository=connextproject
 project=connext
-hub_image=${repository}/${project}_hub:latest
-chainsaw_image=${repository}/${project}_chainsaw:latest
-database_image=postgres:10
 
-# typeorm
-TYPEORM_HOST=postgres
-TYPEORM_PORT=5432
-TYPEORM_DATABASE=$project
-TYPEORM_USERNAME=$project
-TYPEORM_PASSWORD_FILE=/run/secrets/connext_db_dev
+# set defaults for some core env vars
+MODE=$MODE; [[ -n "$MODE" ]] || MODE=development
+DOMAINNAME=$DOMAINNAME; [[ -n "$DOMAINNAME" ]] || DOMAINNAME=localhost
+EMAIL=$EMAIL; [[ -n "$EMAIL" ]] || EMAIL=noreply@gmail.com
+
+# misc settings
+SERVICE_USER_KEY="foo"
+
+# ethereum settings
+ETH_RPC_URL="http://ethprovider:8545"
+ETH_NETWORK_ID="4447"
+ETH_MNEMONIC="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+WALLET_ADDRESS="0xfb482f8f779fd96a857f1486471524808b97452d"
+CHANNEL_MANAGER_ADDRESS="0xa8c50098f6e144bf5bae32bdd1ed722e977a0a42"
+HOT_WALLET_ADDRESS="0xfb482f8f779fd96a857f1486471524808b97452d"
+TOKEN_CONTRACT_ADDRESS="0xd01c08c7180eae392265d8c7df311cf5a93f1b73"
+
+# database settings
+POSTGRES_HOST="database"
+POSTGRES_PORT="5432"
+POSTGRES_USER="$project"
+POSTGRES_DB="$project"
+POSTGRES_PASSWORD_FILE="/run/secrets/database_dev"
+REDIS_URL="redis://redis:6379"
+
+# docker image settings
+registry=docker.io
+repository=connextproject
+
+####################
+# Deploy according to above configuration
+
+if [[ "$MODE" == "live" ]]
+then version="`cat package.json | jq .version | tr -d '"'`"
+else version="latest"
+fi
+
+chainsaw_image="$registry/$repository/chainsaw:$version"
+database_image="$registry/$repository/database:$version"
+hub_image="$registry/$repository/hub:$version"
+redis_image="redis:5-alpine"
 
 # turn on swarm mode if it's not already on
 docker swarm init 2> /dev/null
 
 function pull_if_unavailable {
-    if [[ -z "`docker image ls | grep ${1%:*} | grep ${1#*:}`" ]]
-    then
-        docker pull $1
-    fi
+  if [[ -z "`docker image ls | grep ${1%:*} | grep ${1#*:}`" ]]
+  then docker pull $1
+  fi
 }
 
+pull_if_unavailable $chainsaw_image
 pull_if_unavailable $database_image
 pull_if_unavailable $hub_image
-pull_if_unavailable $chainsaw_image
+pull_if_unavailable $redis_image
 
 function new_secret {
-    secret=$2
-    if [[ -z "$secret" ]]
-    then
-        secret=`head -c 32 /dev/urandom | xxd -plain -c 32 | tr -d '\n\r'`
-    fi
-    if [[ -z "`docker secret ls -f name=$1 | grep -w $1`" ]]
-    then
-        id=`echo $secret | tr -d '\n\r' | docker secret create $1 -`
-        echo "Created secret called $1 with id $id"
-    fi
+  secret=$2
+  if [[ -z "$secret" ]]
+  then secret=`head -c 32 /dev/urandom | xxd -plain -c 32 | tr -d '\n\r'`
+  fi
+  if [[ -z "`docker secret ls -f name=$1 | grep -w $1`" ]]
+  then
+    id=`echo $secret | tr -d '\n\r' | docker secret create $1 -`
+    echo "Created secret called $1 with id $id"
+  fi
 }
 
-new_secret connext_db_dev
-
-mkdir -p ~/ethereum
+new_secret connext_database
 
 mkdir -p /tmp/$project
 cat - > /tmp/$project/docker-compose.yml <<EOF
 version: '3.4'
 
 secrets:
-  connext_db_dev:
+  connext_database:
     external: true
 
 volumes:
-  data:
+  database:
 
 services:
+
   hub:
     image: $hub_image
     ports:
       - '3000:3000'
     depends_on:
-      - postgres
-      - ganache
+      - database
       - chainsaw
     secrets:
-      - connext_db_dev
+      - connext_database
     environment:
-      NODE_ENV: development
-      ETH_NODE_URL: $ETH_NODE_URL
-      HUB_ACCOUNT: $HUB_ACCOUNT
-      CONTRACT_ADDRESS: $CONTRACT_ADDRESS
-      TYPEORM_HOST: $TYPEORM_HOST
-      TYPEORM_PORT: $TYPEORM_PORT
-      TYPEORM_DATABASE: $TYPEORM_DATABASE
-      TYPEORM_USERNAME: $TYPEORM_USERNAME
-      TYPEORM_PASSWORD_FILE: $TYPEORM_PASSWORD_FILE
-      TYPEORM_LOGGING: "true"
+      NODE_ENV: production
+      SERVICE_USER_KEY: $SERVICE_USER_KEY
+      ETH_RPC_URL: $ETH_RPC_URL
+      WALLET_ADDRESS: $WALLET_ADDRESS
+      HOT_WALLET_ADDRESS: $HOT_WALLET_ADDRESS
+      CHANNEL_MANAGER_ADDRESS: $CHANNEL_MANAGER_ADDRESS
+      TOKEN_CONTRACT_ADDRESS: $TOKEN_CONTRACT_ADDRESS
+      POSTGRES_USER: $POSTGRES_USER
+      POSTGRES_PASSWORD_FILE: /run/secrets/connext_database
+      POSTGRES_HOST: $POSTGRES_HOST
+      POSTGRES_PORT: $POSTGRES_PORT
+      POSTGRES_DB: $POSTGRES_DB
+      REDIS_URL: $REDIS_URL
 
   chainsaw:
     image: $chainsaw_image
@@ -94,54 +118,55 @@ services:
       - '3001:3001'
     depends_on:
       - postgres
-      - ganache
     secrets:
       - connext_db_dev
     environment:
-      NODE_ENV: development
-      ETH_NETWORK_ID: $ETH_NETWORK_ID
-      ETH_NODE_PROTOCOL: $ETH_NODE_PROTOCOL
-      ETH_NODE_HOST: $ETH_NODE_HOST
-      ETH_NODE_PORT: $ETH_NODE_PORT
+      NODE_ENV: production
+      SERVICE_USER_KEY: $SERVICE_USER_KEY
+      ETH_RPC_URL: $ETH_RPC_URL
+      WALLET_ADDRESS: $WALLET_ADDRESS
+      HOT_WALLET_ADDRESS: $HOT_WALLET_ADDRESS
+      CHANNEL_MANAGER_ADDRESS: $CHANNEL_MANAGER_ADDRESS
+      TOKEN_CONTRACT_ADDRESS: $TOKEN_CONTRACT_ADDRESS
+      POSTGRES_USER: $POSTGRES_USER
+      POSTGRES_PASSWORD_FILE: /run/secrets/connext_database
+      POSTGRES_HOST: $POSTGRES_HOST
+      POSTGRES_PORT: $POSTGRES_PORT
+      POSTGRES_DB: $POSTGRES_DB
+      REDIS_URL: $REDIS_URL
       POLLING_INTERVAL: 2000
-      CONTRACT_ADDRESS: $CONTRACT_ADDRESS
-      TYPEORM_HOST: $TYPEORM_HOST
-      TYPEORM_PORT: $TYPEORM_PORT
-      TYPEORM_DATABASE: $TYPEORM_DATABASE
-      TYPEORM_USERNAME: $TYPEORM_USERNAME
-      TYPEORM_PASSWORD_FILE: $TYPEORM_PASSWORD_FILE
-      TYPEORM_LOGGING: "true"
 
-  postgres:
-    image: postgres:10
+  redis:
+    image: $redis_image
     ports:
-      - '5432:5432'
+      - "6379:6379"
+
+  database:
+    image: $database_image
     deploy:
       mode: global
     secrets:
-      - connext_db_dev
+      - connext_database
     environment:
-      POSTGRES_USER: $TYPEORM_USERNAME
-      POSTGRES_DB: $TYPEORM_DATABASE
-      POSTGRES_PASSWORD_FILE: $TYPEORM_PASSWORD_FILE
+      POSTGRES_USER: $POSTGRES_USER
+      POSTGRES_DB: $POSTGRES_DB
+      POSTGRES_PASSWORD_FILE: /run/secrets/connext_database
     volumes:
-      - data:/var/lib/postgresql/data
+      - database:/var/lib/postgresql/data
 EOF
 
 docker stack deploy -c /tmp/$project/docker-compose.yml $project
 rm -rf /tmp/$project
 
 echo -n "Waiting for the $project stack to wake up."
-number_of_services=3
 while true
 do
+    num_awake="`docker container ls | grep $project | wc -l | sed 's/ //g'`"
     sleep 3
-    if [[ "`docker container ls | grep $project | wc -l | sed 's/ //g'`" == "$number_of_services" ]]
-    then
-        echo " Good Morning!"
-        break
-    else
-        echo -n "."
+    if [[ "$num_awake" == "4" ]]
+    then break
+    else echo -n "."
     fi
 done
-
+echo " Good Morning!"
+sleep 2
