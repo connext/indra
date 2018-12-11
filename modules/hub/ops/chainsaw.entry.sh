7f47a8cc590219412ca701c1fddbb3397e5badfa
Bo Henderson <bohende@gmail.com>
2018-12-11 12:38:58 +0530

    hub/ops: fix bug while waiting for redis

--- modules/hub/ops/chainsaw.entry.sh
+++ modules/hub/ops/chainsaw.entry.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+REDIS=${REDIS_URL#*://}
 DATABASE=database:5432
 export DATABASE_URL="postgresql://$POSTGRES_USER:`cat $POSTGRES_PASSWORD_FILE`@$DATABASE/$POSTGRES_DB"
 
-echo "Waiting for $DATABASE and $REDIS_URL to wake up..."
+echo "Waiting for $DATABASE and $REDIS to wake up..."
 bash ops/wait-for-it.sh $DATABASE 2> /dev/null
-bash ops/wait-for-it.sh $REDIS_URL 2> /dev/null
+bash ops/wait-for-it.sh $REDIS 2> /dev/null
 
 echo "Starting Chainsaw!"
 node ./dist/spankchain/main.js chainsaw
