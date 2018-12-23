3261dbc48cfbcad167e4180bf6b581151ebe8749
Bo Henderson <bohende@gmail.com>
2018-12-23 11:40:05 +0530

    contracts: expose migration-finished flag port

--- modules/contracts/ops/entry.sh
+++ modules/contracts/ops/entry.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ganache_rpc_port=8545
+migration_flag_port=8546
+
 mkdir -p /data build/contracts
 ganache=./node_modules/.bin/ganache-cli
 truffle=./node_modules/.bin/truffle
@@ -11,7 +14,7 @@ mnemonic=$ETH_MNEMONIC
 [[ -n "$mnemonic" ]] || mnemonic="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
 
 echo "Starting Ganache with options: netid=$netid, mnemonic=$mnemonic..."
-$ganache --host="0.0.0.0" --port="8545" --db="/data" --mnemonic="$mnemonic" --networkId="$netid" --blockTime=3 &
+$ganache --host="0.0.0.0" --port="$ganache_rpc_port" --db="/data" --mnemonic="$mnemonic" --networkId="$netid" --blockTime=3 &
 sleep 5
 echo 'lets go'
 
@@ -40,6 +43,15 @@ then
     echo "Watching contracts/migrations for changes (`getHash`)"
 fi
 
+function migrations_done_flag {
+  echo "===> Signalling the completion of migrations..."
+  while true # unix.stackexchange.com/a/37762
+  do echo 'eth migrations complete' | nc -lk -p $migration_flag_port
+  done > /dev/null
+}
+migrations_done_flag &
+migrations_flag_pid=$!
+
 while true;
 do
     if [[ "`getHash`" == "`cat build/state-hash`" ]]
@@ -48,6 +60,9 @@ do
     else
         echo 
         echo "Changes detected! Re-migrating (`getHash`)"
+        kill $migrations_flag_pid
         migrate
+        migrations_done_flag &
+        migrations_flag_pid=$!
     fi
 done
