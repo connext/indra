0b4b84145016dda17fdac2b38cc4ac0d21dd2746
Rahul <rksethuram9@gmail.com>
2018-12-26 15:02:42 -0800

    Add function to create new wallet

--- modules/wallet/src/walletGen.js
+++ modules/wallet/src/walletGen.js
@@ -1,38 +1,42 @@
-import { store } from './App';
-import * as ethers from 'ethers';
+import { store } from "./App";
+import * as ethers from "ethers";
 
-export function createWallet(){
-  let mnemonic = localStorage.getItem('mnemonic')
-  let wallet
+export function createWallet() {
+  console.log("Creating new random wallet");
+  let wallet = new ethers.Wallet.createRandom();
+  localStorage.setItem("mnemonic", wallet.signingKey.mnemonic);
+}
+
+export function findOrCreateWallet() {
+  let mnemonic = localStorage.getItem("mnemonic");
+  let wallet;
   if (mnemonic) {
-    console.log('found existing wallet')
-    wallet = new ethers.Wallet.fromMnemonic(mnemonic)
+    console.log("found existing wallet");
+    wallet = new ethers.Wallet.fromMnemonic(mnemonic);
   } else {
-    console.log('Creating new random wallet')
-    wallet = new ethers.Wallet.createRandom()
-    localStorage.setItem('mnemonic', wallet.signingKey.mnemonic)
+    createWallet();
   }
   store.dispatch({
-    type: 'SET_WALLET',
+    type: "SET_WALLET",
     text: wallet //Buffer.from(String(privKey.private),'hex')
   });
   return wallet;
-} 
-export function createWalletFromKey(privKey){
-    const wallet = new ethers.Wallet(privKey)
-    console.log(wallet);
-    store.dispatch({
-        type: 'SET_WALLET',
-        text: wallet //Buffer.from(String(privKey.private),'hex')
-        });
-    return wallet;
-} 
+}
 
-export function getStore(){
-  if(store){
-    return store
-  }else{
-    console.log( "no store found")
+export function createWalletFromKey(privKey) {
+  const wallet = new ethers.Wallet(privKey);
+  console.log(wallet);
+  store.dispatch({
+    type: "SET_WALLET",
+    text: wallet //Buffer.from(String(privKey.private),'hex')
+  });
+  return wallet;
+}
+
+export function getStore() {
+  if (store) {
+    return store;
+  } else {
+    console.log("no store found");
   }
-};
-
+}
