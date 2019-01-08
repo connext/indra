d5dddacc599b90a98afeaf311c363b3726f4f7ff
Rahul <rksethuram9@gmail.com>
2019-01-07 20:07:03 -0800

    Updated yarn locks, update DB

--- modules/database/templates/channel-manager.ejs.sql
+++ modules/database/templates/channel-manager.ejs.sql
@@ -100,6 +100,19 @@ To be enforced by DB:
 - Can't update cm_*_updates
 */
 
+CREATE DOMAIN csw_eth_address as citext
+CHECK ( value ~* '^0x[a-f0-9]{40}$' );
+
+CREATE DOMAIN csw_sha3_hash AS citext
+CHECK ( value ~* '^0x[a-f0-9]{64}$' );
+
+CREATE DOMAIN eth_signature AS VARCHAR(132)
+CHECK ( value ~* '^0x[a-f0-9]{130}$' );
+
+CREATE DOMAIN token_amount AS NUMERIC(78,0);
+
+CREATE DOMAIN wei_amount AS NUMERIC(78,0);
+
 --
 -- Chainsaw
 --
@@ -113,6 +126,18 @@ create type cm_event_type as enum (
     'DidEmptyThread',
     'DidNukeThread'
 );
+
+CREATE TABLE chainsaw_poll_events (
+    block_number BIGINT NOT NULL,
+    polled_at BIGINT NOT NULL,
+    contract csw_eth_address NOT NULL,
+    poll_type varchar not null default 'FETCH_EVENTS',
+    add column tx_idx integer
+);
+
+alter table chainsaw_poll_events
+add constraint chainsaw_poll_events_block_number_tx_id_unique
+unique (block_number, tx_idx);
 
 create table chainsaw_events (
     id bigserial primary key,
@@ -1197,14 +1222,6 @@ exception when unique_violation then
 end
 $pgsql$;
 
-alter table chainsaw_poll_events add column poll_type varchar not null default 'FETCH_EVENTS';
-alter table chainsaw_poll_events add column tx_idx integer;
-alter table chainsaw_poll_events
-drop constraint chainsaw_poll_events_block_number_key;
-alter table chainsaw_poll_events
-add constraint chainsaw_poll_events_block_number_tx_id_unique
-unique (block_number, tx_idx);
-
 create or replace function cm_chainsaw_events_since(
   _contract csw_eth_address,
   _block_num integer,
@@ -1232,8 +1249,6 @@ $pgsql$;
 --
 -- payments
 --
-
-drop view payments;
 
 create table _payments (
   id bigserial primary key,
@@ -1301,3 +1316,24 @@ create view payments as (
 );
 
 -- vim: set shiftwidth=2 tabstop=2 softtabstop=2 :vim
+
+CREATE TABLE gas_estimates (
+    id BIGSERIAL PRIMARY KEY,
+    retrieved_at BIGINT,
+
+    speed DOUBLE PRECISION,
+    block_num BIGINT UNIQUE,
+    block_time DOUBLE PRECISION,
+
+    fastest DOUBLE PRECISION,
+    fastest_wait DOUBLE PRECISION,
+
+    fast DOUBLE PRECISION,
+    fast_wait DOUBLE PRECISION,
+
+    average DOUBLE PRECISION,
+    avg_wait DOUBLE PRECISION,
+
+    safe_low DOUBLE PRECISION,
+    safe_low_wait DOUBLE PRECISION
+);
