d3e59ff2013e7f5c71a8e996d868c42d4f3d5237
Rahul <rksethuram9@gmail.com>
2019-01-24 16:50:42 -0800

    Clean up

--- modules/wallet/src/components/depositCard.js
+++ modules/wallet/src/components/depositCard.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import ArchiveIcon from "@material-ui/icons/Archive";
 import TextField from "@material-ui/core/TextField";
-import { withStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
 import Switch from "@material-ui/core/Switch";
 import HelpIcon from "@material-ui/icons/Help";
 import IconButton from "@material-ui/core/IconButton";
@@ -18,9 +13,6 @@ const Web3 = require("web3");
 const eth = require("ethers");
 
 class DepositCard extends Component {
-  constructor(props) {
-    super(props);
-  }
   state = {
     checkedA: true,
     checkedB: false,
