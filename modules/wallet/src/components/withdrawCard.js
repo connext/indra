a11041afb4dd90ddb59c3dee7c20c1db052a52bc
Rahul <rksethuram9@gmail.com>
2019-01-28 18:31:46 -0800

    Wrong variable was being referenced

--- modules/wallet/src/components/withdrawCard.js
+++ modules/wallet/src/components/withdrawCard.js
@@ -44,7 +44,7 @@ class WithdrawCard extends Component {
   };
 
   handleChange = name => event => {
-    var withdrawalValWei = this.state.withdrawalVal.weiToSell;
+    var withdrawalValWei = this.state.withdrawalVal.withdrawalWeiUser;
     var withdrawalValToken = this.state.withdrawalVal.tokensToSell;
     this.setState({ [name]: event.target.checked });
     if (this.state.checkedB) {
