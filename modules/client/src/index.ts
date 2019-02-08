45de6c7d080b23346a94fd1de2a3f72af46845df
LayneHaber <layne.haber@gmail.com>
2019-02-08 12:15:27 -0700

    delete

--- modules/client/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { StateGenerator } from './StateGenerator'
-import { Utils } from './Utils'
-import { ConnextClient } from './Connext'
-
-export { StateGenerator, Utils }
-export default ConnextClient
