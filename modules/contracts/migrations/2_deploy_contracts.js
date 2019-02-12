fef9fe0d8c47ea766954c6c82f4db2b19b045b09
Rahul <rksethuram9@gmail.com>
2019-02-12 14:29:52 -0800

    Use deployed version of ECTools where applicable

--- modules/contracts/migrations/2_deploy_contracts.js
+++ modules/contracts/migrations/2_deploy_contracts.js
@@ -2,9 +2,8 @@ const EC = artifacts.require("./ECTools.sol");
 const CM = artifacts.require("./ChannelManager.sol");
 const HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
 
+// TODO: TEST ALL THIS, ESP ECTOOLS STUFF, but we should definitely have it
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(EC);
-
   const data = require('../data.json')
 
   let tokenAddress
@@ -13,11 +12,17 @@ module.exports = async function(deployer, network, accounts) {
     // TODO change to BOOTY address for mainnet spanks
     tokenAddress = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359" // DAI
 
-  } else if (network === "rinkeby") {
+    // TODO: use deployed ECTools
+    await deployer.deploy(EC);
+    await deployer.link(EC, CM);
+  } else if (network === "rinkeby" || network === "rinkebyLive") {
     tokenAddress = "0xc778417e063141139fce010982780140aa0cd5ab" // Rinkeby WETH
 
+    // use deployed version of ECTools
+    const deployedEC = await EC.at('0xf6B5eed0b9cC2948cBD95C788Db7457B54d80c44')
+    await deployer.link(deployedEC, CM);
   } else if (network !== "mainnet" && network !== "rinkeby") {
-    const supply = web3.utils.toBN(web3.utils.toWei("696969", "ether"));
+    web3.utils.toBN(web3.utils.toWei("696969", "ether"));
     await deployer.deploy(
       HumanStandardToken,
       data.token.supply,
@@ -27,8 +32,10 @@ module.exports = async function(deployer, network, accounts) {
     );
     const hst = await HumanStandardToken.deployed();
     tokenAddress = hst.address;
+
+    await deployer.deploy(EC);
+    await deployer.link(EC, CM);
   }
 
-  await deployer.link(EC, CM);
   await deployer.deploy(CM, accounts[0], data.channelManager.challengePeriod, tokenAddress);
 };
