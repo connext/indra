29651f85e5e59d084cc7b73fff3ef7e6cc605c9e
Bo Henderson <bohende@gmail.com>
2019-02-18 13:56:54 +0530

    remove unnecessary code from ethprovider entrypoint

--- modules/contracts/ops/entry.sh
+++ modules/contracts/ops/entry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ########################################
 # env setup
@@ -6,12 +7,6 @@
 ganache_net_id=4447
 ganache_rpc_port=8545
 migration_flag_port=8544
-
-#export ETH_MNEMONIC="$ETH_MNEMONIC"
-#export ETH_NETWORK="$ETH_NETWORK"
-#export ETH_PROVIDER="$ETH_PROVIDER"
-#export INFURA_KEY=$INFURA_KEY
-#export PRIVATE_KEY_FILE="$PRIVATE_KEY_FILE"
 
 ########################################
 # Setup some helper functions
@@ -24,6 +19,20 @@ function curleth {
     $ETH_PROVIDER \
       | jq .result \
       | tr -d '"\n\r'
+}
+
+function signal_migrations_complete {
+  echo "===> Signalling the completion of migrations..."
+  while true # unix.stackexchange.com/a/37762
+  do sleep 2 && echo 'eth migrations complete' | nc -lk -p $migration_flag_port
+  done > /dev/null
+}
+
+function watch {
+  echo "Watching contract src & artifacts for changes.."
+  while true
+  do node ops/migrate.js && sleep 5
+  done
 }
 
 ########################################
@@ -53,112 +62,6 @@ echo "Running migration script.."
 echo
 
 node ops/migrate.js
-
-exit
-
-########################################
-# TODO: delete this
-
-function checkNetwork {
-  if [[ "$1" == "mainnet" && "$2" == "1" ]]
-  then true
-  elif [[ "$1" == "ropsten" && "$2" == "3" ]]
-  then true
-  elif [[ "$1" == "rinkeby" && "$2" == "4" ]]
-  then true
-  elif [[ "$1" == "kovan" && "$2" == "42" ]]
-  then true
-  elif [[ "$1" == "ganache" && "$2" == "4447" ]]
-  then true
-  else
-    echo "Network $ETH_NETWORK doesn't match chain id ($netid) of provider: $ETH_PROVIDER"
-    exit 1
-  fi
-}
-
-function getAddress {
-  cat ops/addresses.json | jq ".$1.networks[\"$2\"].address"
-}
-
-function getHash {
-  find contracts migrations ops/addresses.json -type f -not -name "*.swp" \
-    | xargs cat | sha256sum | tr -d ' -'
-}
-
-function migrate {
-  echo && echo "Migrating contracts to network $ETH_NETWORK" 
-  ./node_modules/.bin/truffle migrate --verbose-rpc --reset --network=$ETH_NETWORK
-  echo "Migrations complete, exit code: $?"
-}
-
-function signal_migrations_complete {
-  echo "===> Signalling the completion of migrations..."
-  while true # unix.stackexchange.com/a/37762
-  do sleep 2 && echo 'eth migrations complete' | nc -lk -p $migration_flag_port
-  done > /dev/null
-}
-
-function watch {
-  echo "Watching contract src & artifacts for changes.."
-  while true
-  do migrate && sleep 2
-  done
-}
-
-########################################
-# Make some sanity checks
-
-if [[ -z "$ETH_PROVIDER" ]]
-then echo "ETH_PROVIDER env var not available, aborting" && exit
-else echo "Preparing migrations for $ETH_NETWORK against provider: $ETH_PROVIDER"
-fi
-
-netid="`curleth net_version '[]'`"
-
-checkNetwork $ETH_NETWORK $netid
-
-########################################
-# Decide whether or not to migrate
-
-ECToolsAddress="`getAddress ECTools $netid`"
-ChannelManagerAddress="`getAddress ChannelManager $netid`"
-
-ExpectedECToolsCode="`cat build/contracts/ECTools.json | jq .deployedBytecode | tr -d '"'`"
-ExpectedChannelManagerCode="`cat build/contracts/ChannelManager.json | jq .deployedBytecode | tr -d '"'`"
-
-shouldMigrate="no"
-
-# If this network doesn't have addresses for either of these contracts, then migrate
-
-# If this network ha addresses for these contracts but the code is different, them re-migrate
-
-if [[ "${#ECToolsAddress}" == "42" ]]
-then
-  ECToolsCode="`curleth eth_getCode '[\"'"$ECToolsAddress"'\",\"latest\"]'`"
-fi
-
-if [[ "${#ChannelManagerAddress}" == "42" ]]
-then
-  ChannelManagerCode="`curleth eth_getCode '[\"'"$ChannelManagerAddress"'\",\"latest\"]'`"
-fi
-
-echo "ECToolsAddress=$ECToolsAddress"
-echo "ChannelManagerAddress=$ChannelManagerAddress"
-echo "ECToolsCode=$ECToolsCode"
-echo "ChannelManagerCode=$ChannelManagerCode"
-
-if [[ "${#ECToolsAddress}" != "42" || "${#ChannelManagerAddress}" != "42" ]]
-then shouldMigrate="yes"
-fi
-
-########################################
-# Migrate
-
-echo "eth_accounts returns: `curleth 'eth_accounts' '[]'`"
-
-if [[ "$shouldMigrate" == "yes" ]]
-then echo "Migrations activated!" && migrate
-fi
 
 ########################################
 # In dev-mode, signal completion & start watchers
