fcdd201fb71ea3f5e84e1281e55a7c1418c1d1d7
LayneHaber <layne.haber@gmail.com>
2019-02-25 13:50:03 -0700

    add hub migrations to db dir

---
+++ modules/database/migrations/sqls/20190222054946-add-payment-secret-up.sql
@@ -0,0 +1,43 @@
+/* Replace with your SQL commands */
+ALTER TABLE _payments
+  ADD COLUMN secret text NULL;
+ALTER TABLE _payments ADD CONSTRAINT _unique_secret UNIQUE (secret);
+
+CREATE OR REPLACE VIEW payments AS
+SELECT
+  p.id,
+  up.created_on,
+  up.contract,
+  p.purchase_id,
+  up. "user" AS sender,
+  p.recipient,
+  p.amount_wei,
+  p.amount_token,
+  p.meta,
+  CASE WHEN p.secret IS NOT NULL
+  	   THEN 'PT_LINK'::text 
+  	   ELSE 'PT_CHANNEL'::text
+  	   END AS payment_type,
+  up.recipient AS custodian_address,
+  p.secret
+FROM (
+  _payments p
+  JOIN cm_channel_updates up ON ((up.id = p.channel_update_id)))
+UNION ALL
+SELECT
+  p.id,
+  up.created_on,
+  up.contract,
+  p.purchase_id,
+  up.sender,
+  p.recipient,
+  p.amount_wei,
+  p.amount_token,
+  p.meta,
+  'PT_THREAD'::text AS payment_type,
+  NULL::citext AS custodian_address,
+  p.secret
+FROM (
+  _payments p
+  JOIN cm_thread_updates up ON ((up.id = p.thread_update_id)))
+
