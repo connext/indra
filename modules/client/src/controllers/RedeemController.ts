c35a08fca15c4402451be528fd7fd147cd29c1f7
LayneHaber <layne.haber@gmail.com>
2019-02-25 14:47:57 -0700

    connext redeem returns purchase id

--- modules/client/src/controllers/RedeemController.ts
+++ modules/client/src/controllers/RedeemController.ts
@@ -2,14 +2,15 @@ import { AbstractController } from "./AbstractController";
 import Web3 = require('web3')
 
 export class RedeemController extends AbstractController {
-  public redeem = async (secret: string) => {
+  public redeem = async (secret: string): Promise<{ purchaseId: string }> => {
     // check that the secret was generated as a hex
     if (!Web3.utils.isHex(secret)) {
       throw new Error(`The secret provided is not a hex string. Was it generated using the 'generateSecret' method of connext? Secret: ${secret}`)
     }
     try {
-      const sync = await this.hub.redeem(secret)
-      this.connext.syncController.handleHubSync(sync)
+      const res = await this.hub.redeem(secret)
+      this.connext.syncController.handleHubSync(res.sync)
+      return { purchaseId: res.purchaseId }
     } catch (e) {
       throw new Error(`Error redeeming payment with secret: ${secret}` + e.message)
     }
