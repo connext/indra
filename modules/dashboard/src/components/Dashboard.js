8ad6aa98695bd0a1b0709ae9086a651a7a1a1874
Rahul <rksethuram9@gmail.com>
2019-02-26 16:24:46 -0800

    dashboard fixes

--- modules/dashboard/src/components/Dashboard.js
+++ modules/dashboard/src/components/Dashboard.js
@@ -13,8 +13,9 @@ import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import { mainListItems, secondaryListItems } from "./listItems";
+import { mainListItems } from "./listItems";
 import SimpleTable from "./SimpleTable";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -141,7 +142,7 @@ const ContractInfoCardStyled = withStyles(styles)(ContractInfoCard);
 
 class Dashboard extends React.Component {
   state = {
-    open: true,
+    open: false,
     wei: {
       raw: 0,
       formatted: 0
@@ -192,9 +193,13 @@ class Dashboard extends React.Component {
     this.setState({ open: false });
   };
 
+  toggleDrawer = () => {
+    this.setState({ open: !this.state.open });
+  }
+
   render() {
     const { classes } = this.props;
-    const { wei, token, loadingContract } = this.state;
+    const { wei, token, loadingContract, open } = this.state;
 
     return (
       <div className={classes.root}>
@@ -222,14 +227,12 @@ class Dashboard extends React.Component {
           open={this.state.open}
         >
           <div className={classes.toolbarIcon}>
-            <IconButton onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon />
+            <IconButton onClick={this.toggleDrawer}>
+              {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
             </IconButton>
           </div>
           <Divider />
           <List>{mainListItems}</List>
-          <Divider />
-          <List>{secondaryListItems}</List>
         </Drawer>
         <main className={classes.content}>
           <div className={classes.appBarSpacer} />
