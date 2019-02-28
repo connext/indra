3f251b8fe2de971ceb1f61406639c7e76754b715
Bo Henderson <bohende@gmail.com>
2019-02-28 14:50:19 +0530

    merge remote configuration into db backup script

--- modules/database/ops/backup.sh
+++ modules/database/ops/backup.sh
@@ -3,6 +3,7 @@ set -e
 
 project="indra"
 bucket_name=backups.hub.connext.network
+lifecycle=ops/backup-lifecycle.json
 if [[ -n "$ETH_NETWORK" ]]
 then network=$ETH_NETWORK
 else network=ganache
@@ -15,16 +16,33 @@ backup_path=$dir/snapshots/$backup_file
 mkdir -p "`dirname $backup_path`"
 
 echo "Creating database snapshot..."
-
 pg_dump --username=$project $project > $backup_path
+echo "Done backing up database, snapshot saved to: $backup_path"
 
 if [[ -n "$AWS_ACCESS_KEY_ID" || -n "$AWS_SECRET_ACCESS_KEY" ]]
 then
-  echo "Uploading db snapshot to remote storage..."
-  S3_KEY=$bucket_name/backups/$backup_file
-  aws s3 cp $backup_path s3://$S3_KEY --sse AES256
+
+  # Create bucket if it doesn't exist
+  if [[ -z "`aws s3api list-buckets | grep '"Name":' | grep "$bucket_name"`" ]]
+  then
+    echo "Creating bucket $bucket_name"
+    aws s3api create-bucket --bucket $bucket_name
+    if [[ ! -f "$lifecycle" ]]
+    then
+      echo "Setting bucke's lifecycle config..."
+      aws s3api put-bucket-lifecycle-configuration \
+        --bucket $bucket_name \
+        --lifecycle-configuration file://$lifecycle
+    else echo "Couldn't find lifecycle config file, skipping setup: $lifecycle"
+    fi
+  else
+    echo "AWS S3 bucket $bucket_name already exists"
+  fi
+
+  echo "Uploading db snapshot to $bucket_name"
+  aws s3 cp $backup_path s3://$bucket_name/backups/$backup_file --sse AES256
+  echo "Done, snapshot has been stored remotely"
+
 else
   echo "No access keys found, couldn't backup to remote storage"
 fi
-
-echo "Done backing up db, snapshot saved to $backup_path"
--- modules/database/ops/entry.sh
+++ modules/database/ops/entry.sh
@@ -6,69 +6,79 @@ backup_frequency=1800
 should_restore_backup="no"
 backup_file="snapshots/`ls snapshots | sort -r | head -n 1`"
 
-echo "Starting database in env:"
+echo "[ENTRY] Starting database in env:"
 env
 
 # Is this a fresh database? Should we restore data from a snapshot?
 if [[ ! -f "/var/lib/postgresql/data/PG_VERSION" && -f "$backup_file" ]]
 then 
-  echo "Fresh postgres db started w backup present, we'll restore: $backup_file"
+  echo "[ENTRY] Fresh postgres db started w backup present, we'll restore: $backup_file"
   should_restore_backup="yes"
-else echo "Database exists or no snapshots found"
+else echo "[ENTRY] Database exists or no snapshots found"
 fi
 
 # Start temporary database in background to run migrations
 while [[ -f "/var/lib/postgresql/data/postmaster.pid" ]]
-do echo "Waiting for lock to be released..." && sleep 2
+do echo "[ENTRY] Waiting for lock to be released..." && sleep 2
 done
 /docker-entrypoint.sh postgres &
 PID=$!
 
 # Wait for database to wake up
-while ! pg_isready; do sleep 2; done
+while ! pg_isready; do echo '[ENTRY] Waiting...' && sleep 2; done
+sleep 2 # wake up for real
+while ! pg_isready; do echo '[ENTRY] Waiting...' && sleep 2; done
 sleep 2
-echo "Good morning, Postgres!"
-
-# Run migrations
-echo "Running migrations..."
-if [[ -z "POSTGRES_PASSWORD" ]]
-then POSTGRES_PASSWORD="`cat $POSTGRES_PASSWORD_FILE`"
-fi
-./node_modules/.bin/db-migrate up --config ops/config.json --verbose all
-echo "Migrations complete!"
-sleep 2
-if [[ -n "POSTGRES_PASSWORD_FILE" ]]
-then unset POSTGRES_PASSWORD
-fi
+echo "[ENTRY] Good morning, Postgres!"
 
 # Maybe restore data from snapshot
 if [[ "$should_restore_backup" == "yes" ]]
 then
-  echo "Restoring db snapshot from file $backup_file"
-  #psql --username=$POSTGRES_USER $POSTGRES_DB < $backup_file
-  echo "Done restoring db snapshot"
+  echo "[ENTRY] Restoring db snapshot from file $backup_file"
+  sleep 2
+  psql --username=$POSTGRES_USER $POSTGRES_DB < $backup_file
+  echo "[ENTRY] Done restoring db snapshot"
+fi
+
+# Run migrations
+echo "[ENTRY] Running migrations..."
+if [[ -n "POSTGRES_PASSWORD_FILE" ]]
+then export POSTGRES_PASSWORD="`cat $POSTGRES_PASSWORD_FILE`"
+fi
+./node_modules/.bin/db-migrate up --config ops/config.json --verbose all
+echo "[ENTRY] Migrations complete!"
+if [[ -n "POSTGRES_PASSWORD_FILE" ]]
+then unset POSTGRES_PASSWORD
 fi
 
 # Stop the db instance used to run migrations & restore snapshot
-echo "Stopping old database.."
+echo "[ENTRY] Stopping old database.."
 kill $PID
 while [[ -f "/var/lib/postgresql/data/postmaster.pid" ]]
-do echo "Waiting for lock to be released..." && sleep 2
+do echo "[ENTRY] Waiting for lock to be released..." && sleep 2
 done
 
 # Start migrations signaller
-echo "===> Starting migrations signaller"
+echo "[ENTRY] ===> Starting migrations signaller"
 while true # unix.stackexchange.com/a/37762
 do echo 'db migrations complete' | nc -lk -p 5431
 done > /dev/null &
 
 # Setup remote backups & start backing up the db periodically
 bash ops/configure-remote-backup.sh
-echo "===> Starting backer upper"
+echo "[ENTRY] ===> Starting backer upper"
 while true
-do sleep 5 && bash ops/backup.sh && sleep $backup_frequency 
+do sleep $backup_frequency && bash ops/backup.sh
 done &
 
+# Set an exit trap so that the database will do one final backup before shutting down
+function finish {
+  echo "[ENTRY] Database exiting, doing one final backup"
+  bash ops/backup.sh
+  echo "[ENTRY] Done, database exiting.."
+}
+trap finish EXIT
+
 # Start database in foreground to serve requests from hub
-echo "===> Starting new database.."
+echo "[ENTRY] ===> Starting new database.."
 exec /docker-entrypoint.sh postgres
