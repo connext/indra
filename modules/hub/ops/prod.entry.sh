5a55ba9bb9dcea6c734ccc22d04f0242f7ebb292
Bo Henderson <bohende@gmail.com>
2019-03-01 13:57:03 +0530

    hub: clean up waiters in entry

--- modules/hub/ops/prod.entry.sh
+++ modules/hub/ops/prod.entry.sh
@@ -5,12 +5,12 @@ migrations=database:5431
 database=database:5432
 redis=${REDIS_URL#*://}
 
-echo "Waiting for $redis to wake up..."
+echo "Waiting for redis ($redis) to wake up..."
 bash ops/wait-for.sh -t 60 $redis 2> /dev/null
-echo "Waiting for $database to wake up..."
-bash ops/wait-for.sh -t 60 $database 2> /dev/null
-echo "Waiting for $migrations to wake up..."
+echo "Waiting for db migrations ($migrations) to finish..."
 bash ops/wait-for.sh -t 60 $migrations 2> /dev/null
+echo "Waiting for database ($database) to wake up..."
+bash ops/wait-for.sh -t 60 $database 2> /dev/null
 
 export DATABASE_URL="postgresql://$POSTGRES_USER:`cat $POSTGRES_PASSWORD_FILE`@$database/$POSTGRES_DB"
 
