a090f9539f7a90fd49caeb9aecd11c02da11a860
LayneHaber <layne.haber@gmail.com>
2019-02-28 18:29:29 -0800

    update return

--- modules/client/src/controllers/RedeemController.ts
+++ modules/client/src/controllers/RedeemController.ts
@@ -1,17 +1,19 @@
 import { AbstractController } from "./AbstractController";
 import Web3 = require('web3')
+import { UpdateRequest, PaymentArgs, Payment } from "@src/types";
 
 export class RedeemController extends AbstractController {
-  public redeem = async (secret: string): Promise<{ purchaseId: string }> => {
+  public redeem = async (secret: string): Promise<{ purchaseId: string, amount: Payment }> => {
     // check that the secret was generated as a hex
     if (!Web3.utils.isHex(secret)) {
       throw new Error(`The secret provided is not a hex string. Was it generated using the 'generateSecret' method of connext? Secret: ${secret}`)
     }
 
     try {
-      const res = await this.hub.redeem(secret)
-      this.connext.syncController.handleHubSync(res.sync)
-      return { purchaseId: res.purchaseId }
+      const { purchaseId, sync, amount } = await this.hub.redeem(secret)
+      this.connext.syncController.handleHubSync(sync)
+      // get amount of purchase
+      return { purchaseId, amount }
     } catch (e) {
       throw new Error(`Error redeeming payment with secret: ${secret}. ` + e.message)
     }
