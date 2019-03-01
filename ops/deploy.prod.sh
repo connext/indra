930644d15f1ecbd9c144ecba83059d235ffbd2d3
Bo Henderson <bohende@gmail.com>
2019-03-01 16:32:15 +0530

    fix syntax error in prod deploy script

--- ops/deploy.prod.sh
+++ ops/deploy.prod.sh
@@ -9,11 +9,11 @@ registry="connextproject"
 number_of_services=5
 
 # set defaults for some core env vars
-MODE=$MODE; [[ -n "$MODE" ]] || MODE=development
-DOMAINNAME=$DOMAINNAME; [[ -n "$DOMAINNAME" ]] || DOMAINNAME=localhost
-EMAIL=$EMAIL; [[ -n "$EMAIL" ]] || EMAIL=noreply@gmail.com
-INFURA_KEY=$INFURA_KEY; [[ -n "$INFURA_KEY" ]] || INFURA_KEY="abc123"
-INGESTION_KEY=$INGESTION_KEY; [[ -n "$INGESTION_KEY" ]] || INGESTION_KEY="abc123"
+MODE="${MODE:-development}"
+DOMAINNAME="${DOMAINNAME:-localhost}"
+EMAIL="${EMAIL:-noreply@gmail.com}"
+INFURA_KEY="${INFURA_KEY:-abc123}"
+INGESTION_KEY="${INGESTION_KEY:-abc123}"
 
 # misc settings
 SERVICE_USER_KEY="foo"
@@ -110,8 +110,8 @@ services:
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
     environment:
-      - LOGDNA_KEY: $INGESTION_KEY
-      - TAGS: logdna
+      LOGDNA_KEY: $INGESTION_KEY
+      TAGS: logdna
 
   proxy:
     image: $proxy_image
