dab4af8ae0fdac9e52f7a87dda5bfe15acd95469
Bo Henderson <bohende@gmail.com>
2019-03-01 15:32:41 +0530

    db: harden entry script & make sure exits are cleaner

--- modules/database/ops/entry.sh
+++ modules/database/ops/entry.sh
@@ -1,86 +1,110 @@
 #!/bin/bash
 set -e
 
-# backup every half hour
-backup_frequency=1800
+########################################
+## Setup Env
+
+# 60 sec/min * 30 min = 1800
+backup_frequency="1800"
 should_restore_backup="no"
 backup_file="snapshots/`ls snapshots | sort -r | head -n 1`"
 
-echo "[ENTRY] Starting database in env:"
+########################################
+## Helper functions
+
+function log {
+  echo "[ENTRY] $1"
+}
+
+function unlock {
+  lock="/var/lib/postgresql/data/postmaster.pid"
+  sleep 2
+  while [[ -f "$lock" ]]
+  do
+    mode=${1:-fast}
+    postmaster="`head -n1 $lock`"
+    log "Waiting on lock for pid $postmaster to be released..."
+    if [[ -n "`ps -o pid | grep $postmaster`" ]]
+    then log "Process $postmaster is running, killing it now.." && kill $postmaster
+    else log "Process $postmaster is NOT running, removing the lock now.." && rm $lock
+    fi
+    sleep 2
+  done
+}
+
+# Set an exit trap so that the database will do one final backup before shutting down
+function cleanup {
+  log "Database exiting, creating final snapshot"
+  bash ops/backup.sh
+  log "Shutting the database down"
+  kill "$db_pid"
+  unlock smart
+  log "Clean exit."
+}
+
+trap cleanup SIGTERM
+
+########################################
+## Execute
+
+log "Starting database in env:"
 env
 
 # Is this a fresh database? Should we restore data from a snapshot?
 if [[ ! -f "/var/lib/postgresql/data/PG_VERSION" && -f "$backup_file" ]]
 then 
-  echo "[ENTRY] Fresh postgres db started w backup present, we'll restore: $backup_file"
+  log "Fresh postgres db started w backup present, we'll restore: $backup_file"
   should_restore_backup="yes"
-else echo "[ENTRY] Database exists or no snapshots found"
+else log "Database exists or no snapshots found"
 fi
 
-# Start temporary database in background to run migrations
-while [[ -f "/var/lib/postgresql/data/postmaster.pid" ]]
-do echo "[ENTRY] Waiting for lock to be released..." && sleep 5
-done
+# Start temp database & wait until it wakes up
+log "Starting temp database for migrations/recovery.."
+unlock fast
 /docker-entrypoint.sh postgres &
 PID=$!
-
-# Wait for database to wake up
-while ! pg_isready; do echo '[ENTRY] Waiting...' && sleep 2; done
-sleep 2 # wake up for real
-while ! pg_isready; do echo '[ENTRY] Waiting...' && sleep 2; done
-sleep 2
-echo "[ENTRY] Good morning, Postgres!"
+while ! psql -U $POSTGRES_USER -d $POSTGRES_DB -c "select 1" > /dev/null 2>&1
+do log "Waiting for db to wake up.." && sleep 1
+done
+log "Good morning, Postgres!"
 
 # Maybe restore data from snapshot
 if [[ "$should_restore_backup" == "yes" ]]
 then
-  echo "[ENTRY] Restoring db snapshot from file $backup_file"
-  sleep 2
+  log "Restoring db snapshot from file $backup_file"
   psql --username=$POSTGRES_USER $POSTGRES_DB < $backup_file
-  echo "[ENTRY] Done restoring db snapshot"
+  log "Done restoring db snapshot"
 fi
 
 # Run migrations
-echo "[ENTRY] Running migrations..."
+log "Running migrations..."
 if [[ -n "POSTGRES_PASSWORD_FILE" ]]
 then export POSTGRES_PASSWORD="`cat $POSTGRES_PASSWORD_FILE`"
 fi
-./node_modules/.bin/db-migrate up --config ops/config.json --verbose all
-echo "[ENTRY] Migrations complete!"
+./node_modules/.bin/db-migrate up --config ops/config.json
+log "Migrations complete!"
 if [[ -n "POSTGRES_PASSWORD_FILE" ]]
 then unset POSTGRES_PASSWORD
 fi
 
-# Stop the db instance used to run migrations & restore snapshot
-echo "[ENTRY] Stopping old database.."
+log "Stopping old database.."
 kill $PID
-while [[ -f "/var/lib/postgresql/data/postmaster.pid" ]]
-do echo "[ENTRY] Waiting for lock to be released..." && sleep 2
-done
+unlock smart
 
 # Start migrations signaller
-echo "[ENTRY] ===> Starting migrations signaller"
+log "===> Starting migrations signaller"
 while true # unix.stackexchange.com/a/37762
 do echo 'db migrations complete' | nc -lk -p 5431
 done > /dev/null &
 
-# Setup remote backups & start backing up the db periodically
-echo "[ENTRY] ===> Starting backer upper"
+# Start backing up the db periodically
+log "===> Starting backer upper"
 while true
 do sleep $backup_frequency && bash ops/backup.sh
 done &
 
-# Set an exit trap so that the database will do one final backup before shutting down
-function finish {
-  echo "[ENTRY] Database exiting, doing one final backup"
-  bash ops/backup.sh
-  kill "$db_pid"
-  echo "[ENTRY] Done, database exiting.."
-}
-trap finish SIGTERM
-
 # Start database to serve requests from clients
-echo "[ENTRY] ===> Starting new database.."
+log "===> Starting new database.."
 /docker-entrypoint.sh postgres &
 db_pid=$!
 wait "$db_pid"
