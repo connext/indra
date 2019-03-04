10b796ae4f52db06d49dd6c240524fd9ec0697c7
Bo Henderson <bohende@gmail.com>
2019-03-04 11:59:47 +0530

    fix some warnings in imports

--- modules/dashboard/src/components/Withdrawals.js
+++ modules/dashboard/src/components/Withdrawals.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+//import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
+//import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import axios from "axios";
