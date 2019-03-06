d7af69c4a58868a9e2ee4a071b16e068137e0222
Bo Henderson <bohende@gmail.com>
2019-03-06 13:28:17 +0530

    add comments to pre-commit hook

--- ops/pre-commit.sh
+++ ops/pre-commit.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-wegood="yes"
+# Thanks to https://stackoverflow.com/a/5180310 for getting staged changes
 version="`git show :package.json | grep '"version":' | awk -F '"' '{print $4}'`"
 registry="https://index.docker.io/v1/repositories/connextproject"
-
+wegood="yes"
 branch="`git symbolic-ref HEAD | sed -e 's|.*/\(.*\)|\1|'`"
 
 if [[ "$branch" != "master" ]]
@@ -15,6 +15,7 @@ echo "Pre-commit hook activated for $branch branch (staged package.json version:
 echo
 for image in database hub proxy
 do
+  # Thanks to https://stackoverflow.com/a/39731444 for url to query
   if curl -sflL "$registry/indra_$image/tags/$version" > /dev/null
   then echo "connextproject/$image:$version already exists on docker hub" && wegood="no"
   else echo "connextproject/$image:$version does not exist on docker hub yet"
@@ -24,7 +25,7 @@ echo
 
 if [[ "$wegood" == "no" ]]
 then
-  exec < /dev/tty
+  exec < /dev/tty # Thanks to https://stackoverflow.com/a/10015707
   echo "It's recommended you increment the version in package.json before commiting to master"
   read -p "Are you sure you want CI to override version $version docker images? (y/n) " -n 1 -r
   echo
@@ -33,4 +34,3 @@ then
   else echo "Hope you know what you're doing";echo
   fi
 fi
-
