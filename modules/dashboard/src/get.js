81e2202ce29fc0b59c5f9987c433abf5e02882cc
LayneHaber <layne.haber@gmail.com>
2019-03-23 12:25:52 -0700

    simplify getter

--- modules/dashboard/src/get.js
+++ modules/dashboard/src/get.js
@@ -5,18 +5,9 @@ const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:9999`
 const get = async (url) => {
     console.log(`Getting ${url}...`)
     const res = await axios.get(`${apiUrl}/${url}`)
-    console.log(res);
-    if (res.data && res.data.rows && res.data.rows.length === 0 && !res.data.fields) {
-      console.log(`Got zero rows from ${url}`)
-      return []
-    } else if (res.data && res.data.rows && res.data.rows.length === 1) {
-      console.log(`Got one row from ${url}: ${JSON.stringify(res.data.rows[0])}`)
-      return res.data.rows[0]
-    } else if (res.data && res.data.rows && res.data.rows.length >= 1) {
-      console.log(`Got ${res.data.rows.length} rows from ${url}: ${JSON.stringify(res.data.rows)}`)
-      return res.data.rows
+    if (res.data) {
+      return res.data
     } else {
-      console.warn(`Couldn't get ${url}: ${JSON.stringify(res)}`)
       return null
     }
 }
