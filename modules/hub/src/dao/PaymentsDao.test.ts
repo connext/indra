023f9bf05c498f5bf1a036333d145439bdd9ea7a
LayneHaber <layne.haber@gmail.com>
2019-04-12 20:01:46 -0700

    add optimistic insert test

--- modules/hub/src/dao/PaymentsDao.test.ts
+++ modules/hub/src/dao/PaymentsDao.test.ts
@@ -72,4 +72,40 @@ describe('PaymentsDao', () => {
       'sender': '0xa000000000000000000000000000000000000000',
     })
   })
+
+  it('should create an optimistic payment', async () => {
+    // save a payment for this update
+    const paymentId = await paymentMetaDao.save('abc123', {
+      type: 'PT_OPTIMISTIC',
+      amount: {
+        amountToken: tokenVal(2),
+        amountWei: '0',
+      },
+      recipient: mkAddress('0xb'),
+      meta: {
+        foo: 42,
+      },
+    })
+
+    const d = await channelUpdateFactory(registry, { user: mkAddress('0xa') })
+    await paymentsDao.createOptimisticPayment(paymentId, d.update.id)
+
+    const db = registry.get('DBEngine')
+    assert.containSubset((await db.queryOne('SELECT * FROM payments_optimistic')), {
+      payment_id: '' + paymentId,
+      channel_update_id: '' + d.update.id,
+    })
+
+    assert.containSubset((await db.queryOne(`select * from payments where id = ${paymentId}`)), { 'amount_token': '2000000000000000000',
+      'amount_wei': '0',
+      'contract': '0xCCC0000000000000000000000000000000000000',
+      'meta': {
+        'foo': 42
+      },
+      'payment_type': 'optimistic',
+      'purchase_id': 'abc123',
+      'recipient': '0xb000000000000000000000000000000000000000',
+      'sender': '0xa000000000000000000000000000000000000000',
+    })
+  })
 })
