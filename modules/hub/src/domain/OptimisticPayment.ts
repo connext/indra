74a3e5bb7a8d5df4ddc1fc3bf79235dc1543a2ee
LayneHaber <layne.haber@gmail.com>
2019-04-14 14:27:06 -0700

    omit type

--- modules/hub/src/domain/OptimisticPayment.ts
+++ modules/hub/src/domain/OptimisticPayment.ts
@@ -4,7 +4,7 @@ import { PurchasePaymentRow } from "./Purchase";
 
 export type OptimisticPaymentStatus = "new" | "custodial" | "completed" | "failed"
 
-export type OptimisticPurchasePaymentRow = Omit<PurchasePaymentRow, "id"> & {
+export type OptimisticPurchasePaymentRow = Omit<PurchasePaymentRow, "type" | "id"> & {
   status: OptimisticPaymentStatus
   channelUpdateId: number
   paymentId: number
