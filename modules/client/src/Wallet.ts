2b72c7f69e4c67e5913ded2c16e2f42a007e1279
Bo Henderson <bohende@gmail.com>
2019-04-16 19:13:51 +0530

    client: clean up wallet methods

--- modules/client/src/Wallet.ts
+++ modules/client/src/Wallet.ts
@@ -1,57 +1,26 @@
-import eth from 'ethers';
+import { ethers as eth } from 'ethers';
 import Web3 from 'web3';
 import { ConnextClientOptions } from './Connext';
-import {
-  Address,
-} from './types';
-
-// Conforms to the ethers signer API so we can use a web3 signer to create writable contracts
-// More info: https://github.com/ethereum/web3.js/issues/2675
+import { Address, } from './types';
 
 export interface IWallet {
   address: Address
   provider: any
-  getAddress: () => Address
+  signer: any
   signTransaction: (tx: any) => Promise<string>
+  sendTransaction: (tx: any) => Promise<any>
   signMessage: (message: string) => Promise<string>
+  getAddress: () => Address // Needed to conform to ethers signer API
 }
 
 export default class Wallet implements IWallet {
   address: Address
   provider: any
+  signer: any
   private password?: string
-  private wallet?: any
-  private web3?: any
 
   constructor(opts: ConnextClientOptions) {
     this.password = opts.password
-
-    ////////////////////////////////////////
-    // Setup our a signer
-
-    // First choice: Sign w private key
-    if (opts.privateKey) {
-      this.wallet = eth.Wallet.fromMnemonic(opts.mnemonic || '')
-      this.address = this.wallet.address.toLowerCase()
-
-    // Second choice: Sign w mnemonic
-    } else if (opts.mnemonic) {
-      this.wallet = eth.Wallet.fromMnemonic(opts.mnemonic || '')
-      this.address = this.wallet.address.toLowerCase()
-
-    // Third choice: Sign w web3
-    } else if (
-      (opts.user && opts.web3 && opts.web3.eth) &&
-      (opts.web3.eth.sign || (opts.web3.eth.personal && opts.web3.eth.personal.sign))
-    ) {
-      this.web3 = opts.web3
-      this.address = opts.user.toLowerCase()
-
-    // Fallback: create new random wallet
-    } else {
-      this.wallet = eth.Wallet.createRandom()
-      this.address = this.wallet.address.toLowerCase()
-    }
 
     ////////////////////////////////////////
     // Connect to an eth provider
@@ -61,46 +30,56 @@ export default class Wallet implements IWallet {
       this.provider = new eth.providers.JsonRpcProvider(opts.ethUrl)
 
     // Second choice: use provided web3
-    } else if (opts.web3) {
-      this.provider = new eth.providers.Web3Provider(opts.ethUrl)
+    } else if (opts.web3 && opts.web3.currentProvider) {
+      this.provider = new eth.providers.Web3Provider((opts.web3.currentProvider as any))
 
     // Third choice: use hub's ethprovider (derived from hubUrl)
-    } else if (opts.hubUrl.substring(opts.hubUrls.length - 4) === '/hub') {
-      const ethUrl = `${opts.hubUrl.substring(opts.hubUrls.length - 3)}/eth`
+    } else if (opts.hubUrl.substring(opts.hubUrl.length - 4) === '/hub') {
+      const ethUrl = `${opts.hubUrl.substring(opts.hubUrl.length - 3)}/eth`
       this.provider = new eth.providers.JsonRpcProvider(ethUrl)
 
-    // Fallback: use ethers default provider (uses chainId=1 if ethNetworkId is null)
+    // Fallback: use default provider for this chain id
+    } else if (opts.ethNetworkId)  {
+      this.provider = eth.getDefaultProvider(eth.utils.getNetwork(parseInt(opts.ethNetworkId,10)))
+
+    // If no chainid, default to rinkeby
     } else {
-      this.provider = eth.getDefaultProvider(ethers.providers.getNetwork(opts.ethNetworkId))
+      this.provider = eth.getDefaultProvider('rinkeby')
     }
+
+    ////////////////////////////////////////
+    // Setup our a signer
+
+    // First choice: Sign w private key
+    if (opts.privateKey) {
+      this.signer = eth.Wallet.fromMnemonic(opts.mnemonic || '')
+
+    // Second choice: Sign w mnemonic
+    } else if (opts.mnemonic) {
+      this.signer = eth.Wallet.fromMnemonic(opts.mnemonic || '')
+
+    // Third choice: Sign w web3
+    } else if (
+      (opts.user && opts.web3 && opts.web3.eth) &&
+      (opts.web3.eth.sign || (opts.web3.eth.personal && opts.web3.eth.personal.sign))
+    ) {
+      this.signer = (new eth.providers.Web3Provider((opts.web3.currentProvider as any))).getSigner();
+
+    // Fallback: create new random mnemonic
+    } else {
+      this.signer = eth.Wallet.createRandom()
+      console.log(`Created new wallet: ${this.signer.address}`)
+    }
+
+    this.signer.connect(this.provider)
+    this.address = this.signer.address.toLowerCase()
   }
 
-  getAddress() {
-    return this.address
-  }
+  // Methods
+  getAddress = () => this.address
+  signTransaction = (tx: any) => this.signer.sign(tx)
+  sendTransaction = (tx: any) => this.signer.sendTransaction(tx)
+  signMessage = (message: string) => this.signer.signMessage(message)
 
-  async signTransaction(tx: any) {
-    if (this.wallet) {
-      return await this.wallet.sign(tx)
-    } else if (this.web3) {
-      return await (
-        this.web3.eth.personal
-          ? this.web3.eth.personal.signTransaction(tx, this.address, this.password)
-          : this.web3.eth.signTransaction(tx, this.address, this.password)
-      )
-    }
-  }
-
-  async signMessage(message: string) {
-    if (this.wallet) {
-      return await this.wallet.signMessage(message)
-    } else if (this.web3) {
-      return await (
-        this.web3.eth.personal
-          ? this.web3.eth.personal.sign(message, this.address, this.password)
-          : this.web3.eth.sign(message, this.address, this.password)
-      )
-    }
-  }
 }
 
