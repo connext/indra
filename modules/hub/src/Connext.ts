96c3e1ec11265930dce96eadb01b1072cfa5948c
Bo Henderson <bohende@gmail.com>
2019-04-22 14:17:11 +0530

    hub: init connext import wrapper

---
+++ modules/hub/src/Connext.ts
@@ -0,0 +1,15 @@
+import { Poller } from './vendor/connext/lib/poller/Poller'
+import * as getters from './vendor/connext/state/getters';
+import { StateGenerator } from './vendor/connext/StateGenerator'
+import * as types from './vendor/connext/types'
+import { Utils, emptyAddress, emptyRootHash } from './vendor/connext/Utils'
+import { Validator } from './vendor/connext/validator';
+
+export {
+  Poller,
+  getters,
+  StateGenerator,
+  types,
+  Utils,
+  Validator,
+}
