80a7adb3b528f993208800d3f363b7cbd68a13dd
LayneHaber <layne.haber@gmail.com>
2019-04-24 16:06:41 -0700

    test all signing methods (TODO: personal sign locally??)

--- modules/client/src/Utils.test.ts
+++ modules/client/src/Utils.test.ts
@@ -9,7 +9,6 @@ import { MerkleUtils } from './helpers/merkleUtils'
 import MerkleTree from './helpers/merkleTree'
 import * as t from './testing/index'
 import Web3 from 'web3';
-import Wallet from './Wallet';
 
 const hub = t.mkAddress("0xaa")
 const utils = new Utils(hub)
@@ -17,8 +16,10 @@ const utils = new Utils(hub)
 describe('Utils', () => {
   let web3: Web3
   let accounts: string[]
-  let partyA: string
-  let wallet: Wallet // OUR signing wallet
+  let partyA: string // web3.eth.sign
+  let wallet: eth.Wallet // signer.signMessage
+  let walletAddr: string
+  let personal: string // web3.eth.personal
   
   beforeEach('instantiate wallets with pk and with web3', async function () {
     // instantiate web3
@@ -29,80 +30,107 @@ describe('Utils', () => {
       'http://localhost:3000/api/eth'
     ]
 
-    let errored = true
-    for (const provider of providers) {
-      web3 = new Web3(new HttpProvider(provider))
+    let provider
+    for (const p of providers) {
+      web3 = new Web3(new HttpProvider(p))
       try {
         accounts = await web3.eth.getAccounts()
-        errored = false
+        provider = p
         break
       } catch (e) {
-        console.log(`No web3 HTTP provider found at ${provider}. Error: `,'' +e)
-        errored = true
+        console.log(`No web3 HTTP provider found at ${p}. Error: `,'' +e)
       }
     }
 
-    if (errored) {
+    if (!provider) {
       console.log(`skipping tests which require web3`)
       this.skip()
       return
     }
 
     partyA = accounts[1]
+    personal = await web3.eth.personal.newAccount("testing")
+    // PK for acct: 0x17b105bcb3f06b3098de6eed0497a3e36aa72471
+    // ganache mnemonic: refuse result toy bunker royal small story exhaust know piano base stand
+    wallet = new eth.Wallet("0x0aba2a064ba9dedf2eb7623e75b7701a72f21acbdad69f60ebaa728a8e00e5bb", new eth.providers.JsonRpcProvider(provider))
+    walletAddr = "0x17b105bcb3f06b3098de6eed0497a3e36aa72471"
   })
 
-  it('should properly recover the signer from the channel state update hash with a web3-enabled wallet, using web3.eth.sign', async () => {
+  it('should properly recover the signer from the channel state update hash', async () => {
+    // using web3.eth.sign
     const state = t.getChannelState('full', {
       user: partyA,
       balanceWei: [100, 200],
     })
     // generate hash
     const hash = utils.createChannelStateHash(state)
-    // sign
-    const sig = await web3.eth.sign(hash, partyA)
-    // recover signer
-    const signer = utils.recoverSignerFromChannelState(
-      state,
-      sig,
-      "user"
-    )
-    expect(signer).to.equal(partyA.toLowerCase())
+    // sign using all known methods
+    const sigs = [
+      { 
+        sig: await web3.eth.sign(hash, partyA), 
+        signer: partyA,
+        method: "web3.eth.sign"
+      },
+      // TODO: personal sign on ganache?
+      // { 
+      //   sig: await web3.eth.personal.sign(hash, personal, "testing"), 
+      //   signer: personal,
+      //   method: "web3.eth.personal.sign"
+      // },
+      { 
+        sig: await wallet.signMessage(hash),
+        signer: walletAddr,
+        method: "signer.signMessage"
+      }
+    ]
+
+    // recover signers
+    for (const s of sigs) {
+      const recovered = utils.recoverSigner(
+        hash,
+        s.sig,
+        s.signer.toLowerCase()
+      )
+      expect(recovered, "Testing with signing method: " + s.method).to.equal(s.signer.toLowerCase())
+    }
   })
 
-  it.skip('should recover the signer from the channel update when there are no threads', async () => {
-    // create and sign channel state update
-    const channelStateFingerprint = t.getChannelState('full', {
-      balanceWei: [100, 200],
-    })
-    // generate hash
-    const hash = utils.createChannelStateHash(channelStateFingerprint)
-    // sign
-    const sig = await web3.eth.personal.sign(hash, partyA, '')
-    // recover signer
-    const signer = utils.recoverSignerFromChannelState(
-      channelStateFingerprint,
-      sig,
-      "user"
-    )
-    expect(signer).to.equal(partyA.toLowerCase())
-  })
-
-  it.skip('should recover the signer from the thread state update', async () => {
+  it('should recover the signer from the thread state update', async () => {
     // create and sign channel state update
     const threadStateFingerprint = t.getThreadState('full', {
       balanceWei: [100, 200],
     })
     // generate hash
     const hash = utils.createThreadStateHash(threadStateFingerprint)
-    // sign
-    const sig = await web3.eth.sign(hash, partyA)
-    console.log(hash) // log harcode hash for other hash test
-    // recover signer
-    const signer = utils.recoverSignerFromThreadState(
-      threadStateFingerprint,
-      sig,
-    )
-    expect(signer).to.equal(partyA.toLowerCase())
+    // sign using all known methods
+    const sigs = [
+      { 
+        sig: await web3.eth.sign(hash, partyA), 
+        signer: partyA,
+        method: "web3.eth.sign"
+      },
+      // TODO: personal sign on ganache?
+      // { 
+      //   sig: await web3.eth.personal.sign(hash, personal, "testing"), 
+      //   signer: personal,
+      //   method: "web3.eth.personal.sign"
+      // },
+      { 
+        sig: await wallet.signMessage(hash),
+        signer: walletAddr,
+        method: "signer.signMessage"
+      }
+    ]
+
+    // recover signers
+    for (const s of sigs) {
+      const recovered = utils.recoverSigner(
+        hash,
+        s.sig,
+        s.signer.toLowerCase()
+      )
+      expect(recovered, "Testing with signing method: " + s.method).to.equal(s.signer.toLowerCase())
+    }
   })
 
   it('should return the correct root hash', async () => {
