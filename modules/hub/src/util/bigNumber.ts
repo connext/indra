970632c8227edcb7c338048656243ad8e5cf2865
LayneHaber <layne.haber@gmail.com>
2019-04-26 13:26:08 -0700

    user ethers utils

--- modules/hub/src/util/bigNumber.ts
+++ modules/hub/src/util/bigNumber.ts
@@ -1,13 +1,13 @@
-import { BigNumber } from 'bignumber.js'
+import { ethers } from 'ethers';
 
-export function Big(n: number | string): BigNumber {
-  return new BigNumber(n)
+export function Big(n: number | string): ethers.utils.BigNumber {
+  return ethers.utils.bigNumberify(n)
 }
 
-export function toWeiBigNum(amount: number | string): BigNumber {
-  return Big(amount).times(1e18)
+export function toWeiBigNum(amount: number | string): ethers.utils.BigNumber {
+  return ethers.utils.parseEther(amount.toString())
 }
 
 export function toWeiString(amount: number | string): string {
-  return Big(amount).times(1e18).toFixed()
+  return ethers.utils.parseEther(amount.toString()).toString()
 }
