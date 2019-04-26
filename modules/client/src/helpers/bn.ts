777915a420744431ec0b476e96312d2bab01d009
LayneHaber <layne.haber@gmail.com>
2019-04-26 15:21:31 -0700

    uncomment fn, in case needed later or by implementers

--- modules/client/src/helpers/bn.ts
+++ modules/client/src/helpers/bn.ts
@@ -25,24 +25,24 @@ export function minBN(a: BN, ...bs: BN[]): BN {
   return a
 }
 
-// // this function uses string manipulation
-// // to move the decimal point of the non-integer number provided
-// // multiplier is of base10
-// // this is added because of exchange rate issues with BN lib
-// export function mul(num: string, multiplier: number) {
-//   const n = Math.log10(multiplier)
-//   const decimalPos = num.indexOf('.') === -1 ? num.length : num.indexOf('.')
-//   const prefix = num.slice(0, decimalPos)
-//   let istr = prefix
-//   for (let i = 0; i < n; i++) {
-//     if (num.charAt(i + prefix.length + 1)) {
-//       istr += num.charAt(i + prefix.length + 1)
-//     } else {
-//       istr += '0'
-//     }
-//   }
-//   const newPos = decimalPos + 1 + n
-//   const suffix = num.substr(newPos) ? '.' + num.substr(newPos) : ''
-//   istr += suffix
-//   return istr
-// }
+// this function uses string manipulation
+// to move the decimal point of the non-integer number provided
+// multiplier is of base10
+// this is added because of exchange rate issues with BN lib
+export function mul(num: string, multiplier: number) {
+  const n = Math.log10(multiplier)
+  const decimalPos = num.indexOf('.') === -1 ? num.length : num.indexOf('.')
+  const prefix = num.slice(0, decimalPos)
+  let istr = prefix
+  for (let i = 0; i < n; i++) {
+    if (num.charAt(i + prefix.length + 1)) {
+      istr += num.charAt(i + prefix.length + 1)
+    } else {
+      istr += '0'
+    }
+  }
+  const newPos = decimalPos + 1 + n
+  const suffix = num.substr(newPos) ? '.' + num.substr(newPos) : ''
+  istr += suffix
+  return istr
+}
