0485c92fd1a1e6427fd047d721ba62330caf6da2
LayneHaber <layne.haber@gmail.com>
2019-04-26 12:48:08 -0700

    fix typings

--- modules/client/src/Wallet.ts
+++ modules/client/src/Wallet.ts
@@ -2,11 +2,11 @@ import { ethers as eth } from 'ethers';
 import Web3 from 'web3';
 import { ConnextClientOptions } from './Connext';
 import {
-  Transaction,
-  UnsignedTransaction,
   TransactionRequest,
   TransactionResponse,
 } from './types';
+import { objMapPromise } from './StateGenerator';
+import { TransactionConfig } from 'web3-core';
 
 export default class Wallet extends eth.Signer {
   address: string
@@ -81,7 +81,7 @@ export default class Wallet extends eth.Signer {
     if (this.web3) {
       let sig
 
-      // Modern versions of web3 will add the standard ethereum message prefix for us
+      // Modern versions of web3 will add the standard ethereum message prefix for us 
       sig = await this.web3.eth.sign(eth.utils.hexlify(bytes), this.address)
       if (this.address === eth.utils.verifyMessage(bytes, sig).toLowerCase()) {
         return sig
@@ -104,46 +104,43 @@ export default class Wallet extends eth.Signer {
       return await this.signer.sign(tx)
     }
     if (this.web3) {
-      return (await (this.web3.eth.signTransaction as any)(tx)).raw // TODO: fix type
+      // resolve any fields
+      const resolved = await objMapPromise(tx, async (k, v) => await v) as any
+      // convert to right object
+      const txObj: TransactionConfig = {
+        from: this.address,
+        to: resolved.to,
+        value: resolved.value,
+        gas: parseInt(resolved.gasLimit),
+        gasPrice: resolved.gasPrice,
+        data: resolved.data,
+      }
+      return (await this.web3.eth.signTransaction(txObj)).raw // TODO: fix type
     }
     throw new Error(`Could not sign transaction`)
   }
 
-  // TODO: This could return a TransactionResponse or whatever
-  // Not a huge deal right now, we only ever use the `.hash` property of the return value
-  async sendTransaction(tx: TransactionRequest): Promise<TransactionResponse> {
+  async sendTransaction(txReq: TransactionRequest): Promise<TransactionResponse> {
     // TransactionRequest properties can be promises, make sure they've all resolved
-    tx.to = await tx.to // TODO: replace this line w below
-    //for (let prop in tx) {
-    //  tx[prop] = tx[prop] && typeof tx[prop] === 'object' ? await tx[prop] : tx[prop]
-    //}
+    const tx = await objMapPromise(txReq, async (k, v) => await v) as any
 
     const signedTx = await this.signTransaction(tx)
     if (this.provider) {
       return await this.provider.sendTransaction(signedTx)
     } else if (this.web3) {
-      // TODO: properly convert to transaction response type
-      // const receipt = await this.web3.eth.sendSignedTransaction(signedTx)
-      // return {
-      //   blockHash: receipt.blockHash,
-      //   blockNumber: receipt.blockNumber,//3346463,
-      //   timestamp: 0, // best way to get?
-      //   creates: null,
-      //   to: receipt.to,
-      //   data: , // ??
-      //   from: receipt.from,
-      //   hash: receipt.transactionHash,
-      //   gasLimit:  , // ?? BigNumberify
-      //   gasPrice: , // ?? BigNumberify
-      //   nonce: , // ?? number
-      //   value: , // ?? string
-      //   r: , // ?? string
-      //   s: , // ?? string
-      //   v: , // ?? string
-      //   raw: , // ?? string
-      // }
-      // @ts-ignore
-      return await this.web3.eth.sendSignedTransaction(signedTx) as TransactionResponse
+      const receipt = await this.web3.eth.sendSignedTransaction(signedTx)
+      // cast receipt to object
+      // NOTE: THIS IS AN INCOMPLETE CASTING
+      // Not a huge deal right now, we only ever use the `.hash` property of the return value
+      // TODO: should be consistent
+      const response = {
+        blockHash: receipt.blockHash,
+        blockNumber: receipt.blockNumber,
+        to: receipt.to,
+        hash: receipt.transactionHash,
+        from: receipt.from,
+      } as TransactionResponse
+      return response
     }
     throw new Error("Could not send transaction")
   }
