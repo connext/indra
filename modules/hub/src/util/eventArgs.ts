7fed69c9966545772b117e9b45e0e015cde06d27
Bo Henderson <bohende@gmail.com>
2019-04-29 15:12:02 +0530

    hub: fix util/eventArgs + tests

--- modules/hub/src/util/eventArgs.ts
+++ modules/hub/src/util/eventArgs.ts
@@ -1,35 +1,28 @@
+import * as eth from 'ethers';
 import { BigNumber } from 'ethers/utils'
 
-const re = /^0+/gi
+const { arrayify, bigNumberify, hexDataLength, hexDataSlice, hexlify, stripZeros, } = eth.utils
+
+// Adds the 0x hex prefix if it doesn't already exist, otherwise noop
+const prefix = (hex: string): string => `0x${hex.replace('0x', '')}`
 
 export function eventArgToAddress(arg: string): string {
-  const strip0x = arg.replace('0x', '')
-  const slice = strip0x.substring(32 * 2 - 20 * 2, 32 * 2) // take last 20 bytes of 32 byte string
-  return `0x${slice}`
+  return hexDataSlice(prefix(arg), hexDataLength(prefix(arg)) - 20) // extract last 20 bytes
 }
 
 export function eventArgToBigNum(arg: string): BigNumber {
-  // add 0x prefix if it does not have it
-  if (!arg.startsWith('0x'))
-    arg = `0x${arg}`
-  return new BigNumber(arg)
+  return bigNumberify(prefix(arg))
 }
 
 // data arg is a series of 32 byte strings that represent non-indexed arguments
 export function dataArgToBytes32(dataArg: string, index: number): string {
-  const strip0x = dataArg.replace('0x', '')
-  const start = index * 32 * 2 // 2 hex chars = 1 byte, data is broken up into 32 byte chunks
-  const slice = strip0x.substring(start, start + 32 * 2)
-  return `0x${slice}`
+  return hexDataSlice(prefix(dataArg), index, index + 32)
 }
 
 export function dataArgToAddress(dataArg: string, index: number): string {
-  const data = dataArgToBytes32(dataArg, index)
-  const strip0x = data.replace('0x', '')
-  const slice = strip0x.substring(32 * 2 - 20 * 2, 32 * 2) // take last 20 bytes of 32 byte string
-  return `0x${slice}`
+  return hexDataSlice(prefix(dataArg), index + 12, index + 32)
 }
 
 export function dataArgToBigNumber(dataArg: string, index: number): BigNumber {
-  return new BigNumber(dataArgToBytes32(dataArg, index))
+  return eth.utils.bigNumberify(dataArgToBytes32(dataArg, index))
 }
