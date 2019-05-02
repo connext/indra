b8d1432062c0d1d8acb07101408693d92b6959c6
Bo Henderson <bohende@gmail.com>
2019-05-02 14:40:46 +0530

    client: lint & refactor Utils tests

--- modules/client/src/Utils.ts
+++ modules/client/src/Utils.ts
@@ -12,6 +12,7 @@ import {
   ChannelState,
   convertChannelState,
   convertThreadState,
+  ExchangeRates,
   Payment,
   SignedDepositRequestProposal,
   ThreadState,
@@ -23,46 +24,20 @@ import {
  *********** UTIL FNS ************
  *********************************/
 
-const { arrayify, isHexString, solidityKeccak256, toUtf8Bytes, verifyMessage, } = eth.utils
+const { arrayify, isHexString, solidityKeccak256, toUtf8Bytes, verifyMessage } = eth.utils
 
-// define the utils functions
+// Define the utils functions
 export class Utils {
-  public emptyAddress = eth.constants.AddressZero
-  public emptyRootHash = eth.constants.HashZero
-  public getExchangeRates = getters.getExchangeRates
-  public Poller = Poller
-  public assetToWei = bigUtils.assetToWei
-  public maxBN = bigUtils.maxBN
-  public minBN = bigUtils.minBN
-  public toWeiBig = bigUtils.toWeiBig
-  public toWeiString = bigUtils.toWeiString
-  public weiToAsset = bigUtils.weiToAsset
-
-  public hasPendingOps(stateAny: ChannelState<any>) {
-    const state = convertChannelState('str', stateAny)
-    for (let field in state) {
-      if (!field.startsWith('pending'))
-        continue
-      if ((state as any)[field] !== '0')
-        return true
-    }
-    return false
-  }
-
-  public createDepositRequestProposalHash(
-    req: Payment,
-  ): string {
-    return solidityKeccak256(
-      [
-        'uint256',
-        'uint256'
-      ],
-      [
-        req.amountToken,
-        req.amountWei
-      ]
-    )
-  }
+  public assetToWei: (fiat: BN, rate: string) => BN[] = bigUtils.assetToWei
+  public emptyAddress: string = eth.constants.AddressZero
+  public emptyRootHash: string = eth.constants.HashZero
+  public getExchangeRates: (state: ConnextState) => ExchangeRates = getters.getExchangeRates
+  public maxBN: (a: BN, b: BN) => BN = bigUtils.maxBN
+  public minBN: (a: BN, ...b: BN[]) => BN = bigUtils.minBN
+  public Poller: any = Poller
+  public toWeiBig: (amount: number | string | BN) => BN = bigUtils.toWeiBig
+  public toWeiString: (amount: number | string | BN) => string = bigUtils.toWeiString
+  public weiToAsset: (wei: BN, rate: string) => BN = bigUtils.weiToAsset
 
   public createChannelStateHash(
     channelState: UnsignedChannelState,
@@ -84,31 +59,51 @@ export class Utils {
         channelState.contractAddress,
         [
           channelState.user,
-          channelState.recipient
-        ], [
+          channelState.recipient,
+        ],
+        [
           channelState.balanceWeiHub,
-          channelState.balanceWeiUser
-        ], [
+          channelState.balanceWeiUser,
+        ],
+        [
           channelState.balanceTokenHub,
-          channelState.balanceTokenUser
-        ], [
+          channelState.balanceTokenUser,
+        ],
+        [
           channelState.pendingDepositWeiHub,
           channelState.pendingWithdrawalWeiHub,
           channelState.pendingDepositWeiUser,
           channelState.pendingWithdrawalWeiUser,
-        ], [
+        ],
+        [
           channelState.pendingDepositTokenHub,
           channelState.pendingWithdrawalTokenHub,
           channelState.pendingDepositTokenUser,
           channelState.pendingWithdrawalTokenUser,
-        ], [
+        ],
+        [
           channelState.txCountGlobal,
-          channelState.txCountChain
+          channelState.txCountChain,
         ],
         channelState.threadRoot,
         channelState.threadCount,
         channelState.timeout,
-      ]
+      ],
+    )
+  }
+
+  public createDepositRequestProposalHash(
+    req: Payment,
+  ): string {
+    return solidityKeccak256(
+      [
+        'uint256',
+        'uint256',
+      ],
+      [
+        req.amountToken,
+        req.amountWei,
+      ],
     )
   }
 
@@ -121,7 +116,7 @@ export class Utils {
         'uint256',
         'uint256[2]',
         'uint256[2]',
-        'uint256'
+        'uint256',
       ],
       [
         threadState.contractAddress,
@@ -130,30 +125,31 @@ export class Utils {
         threadState.threadId,
         [
           threadState.balanceWeiSender,
-          threadState.balanceWeiReceiver
-        ], [
-          threadState.balanceTokenSender,
-          threadState.balanceTokenReceiver
+          threadState.balanceWeiReceiver,
         ],
-        threadState.txCount
-      ]
+        [
+          threadState.balanceTokenSender,
+          threadState.balanceTokenReceiver,
+        ],
+        threadState.txCount,
+      ],
     )
   }
 
   public generateThreadMerkleTree(
     threadInitialStates: UnsignedThreadState[],
-  ): any {
-    // TO DO: should this just return emptyRootHash?
+  ): MerkleTree {
     if (threadInitialStates.length === 0) {
+      // should this just return emptyRootHash?
       throw new Error('Cannot create a Merkle tree with 0 leaves.')
     }
-    let merkle
-    let elems = threadInitialStates.map(threadInitialState => {
-      // hash each initial state and convert hash to buffer
-      const hash = this.createThreadStateHash(threadInitialState)
-      const buf = MerkleUtils.hexToBuffer(hash)
-      return buf
-    })
+    let merkle: MerkleTree
+    const elems: Buffer[] = threadInitialStates.map((
+        threadInitialState: UnsignedThreadState,
+      ): Buffer => {
+        const hash: string = this.createThreadStateHash(threadInitialState)
+        return MerkleUtils.hexToBuffer(hash)
+      })
     if (elems.length % 2 !== 0) {
       // cant have odd number of leaves
       elems.push(MerkleUtils.hexToBuffer(this.emptyRootHash))
@@ -162,91 +158,101 @@ export class Utils {
     return merkle
   }
 
-  public recoverSigner(hash: string, sig: string, signer: string) {
-    const bytes = isHexString(hash) ? arrayify(hash) : toUtf8Bytes(hash)
-    let recovered = verifyMessage(bytes, sig).toLowerCase()
-    if (recovered && recovered !== signer.toLowerCase()) {
-      return recovered
-    }
-    console.warn(`Signature doesn't match new scheme. Expected ${signer}, Recovered ${recovered}`)
-    // For backwards compatibility, TODO: remove until below
-    const fingerprint = util.toBuffer(String(hash))
-    const prefix = util.toBuffer('\x19Ethereum Signed Message:\n')
-    const prefixedMsg = util.keccak256(Buffer.concat(
-      [ prefix, util.toBuffer(String(fingerprint.length)), fingerprint ]
-    ))
-    const res = util.fromRpcSig(sig)
-    const pubKey = util.ecrecover( util.toBuffer(prefixedMsg), res.v, res.r, res.s,)
-    recovered = util.bufferToHex(util.pubToAddress(pubKey))
-    if (recovered && recovered !== signer.toLowerCase()) {
-      return recovered
-    }
-    console.warn(`Signature doesn't match old scheme. Expected ${signer}, Recovered ${recovered}`)
-    // TODO: remove until here
-    return null
-  }
-
-  public recoverSignerFromDepositRequest(
-    args: SignedDepositRequestProposal,
-    signer: string,
-  ): string | null {
-    const hash = this.createDepositRequestProposalHash(args)
-    return this.recoverSigner(hash, args.sigUser, signer)
-  }
-
-  public recoverSignerFromChannelState(
-    channelState: UnsignedChannelState,
-    sig: string,
-    signer: "user" | "hub",
-  ): string | null {
-    const hash: any = this.createChannelStateHash(channelState)
-    const signerAddress = signer == "user" ? channelState.user : channelState.recipient
-    return this.recoverSigner(hash, sig, signerAddress)
-  }
-
-  public recoverSignerFromThreadState(
-    threadState: UnsignedThreadState,
-    sig: string,
-  ): string | null {
-    // console.log('recovering signer from state:', threadState)
-    const hash: any = this.createThreadStateHash(threadState)
-    return this.recoverSigner(hash, sig, threadState.sender)
-  }
-
-  public generateThreadRootHash(
-    threadInitialStates: ThreadState[],
-  ): string {
-    let temp = []
-    let threadRootHash
-    if (threadInitialStates.length === 0) {
-      // reset to initial value -- no open VCs
-      threadRootHash = this.emptyRootHash
-    } else {
-      for(let i = 0; i < threadInitialStates.length; i++) {
-        temp[i] = convertThreadState("str-unsigned", threadInitialStates[i])
-      }
-      const merkle = this.generateThreadMerkleTree(temp)
-      threadRootHash = MerkleUtils.bufferToHex(merkle.getRoot())
-    }
-    return threadRootHash
-  }
-
   public generateThreadProof(
     thread: UnsignedThreadState,
     threads: UnsignedThreadState[],
   ): any {
-    // generate hash
-    const hash = this.createThreadStateHash(thread)
-    // generate merkle tree
-    let merkle = this.generateThreadMerkleTree(threads)
-    let mproof = merkle.proof(MerkleUtils.hexToBuffer(hash))
-    let proof = []
-    for (var i = 0; i < mproof.length; i++) {
+    const hash: string = this.createThreadStateHash(thread)
+    const merkle: MerkleTree = this.generateThreadMerkleTree(threads)
+    const mproof: any = merkle.proof(MerkleUtils.hexToBuffer(hash))
+    let proof: string[] = []
+    for (const i of mproof) {
       proof.push(MerkleUtils.bufferToHex(mproof[i]))
     }
     proof.unshift(hash)
     proof = MerkleUtils.marshallState(proof)
     return proof
+  }
+
+  public generateThreadRootHash(
+    threadInitialStates: ThreadState[],
+  ): string {
+    const temp: any[] = []
+    let threadRootHash: string
+    if (threadInitialStates.length === 0) {
+      // reset to initial value -- no open VCs
+      threadRootHash = this.emptyRootHash
+    } else {
+      for (const state of threadInitialStates) {
+        temp.push(convertThreadState('str-unsigned', state))
+      }
+      const merkle: any = this.generateThreadMerkleTree(temp)
+      threadRootHash = MerkleUtils.bufferToHex(merkle.getRoot())
+    }
+    return threadRootHash
+  }
+
+  public hasPendingOps(stateAny: ChannelState<any>): boolean {
+    const state: any = convertChannelState('str', stateAny)
+    for (const field in state) {
+      if (!field.startsWith('pending')) {
+        continue
+      }
+      if ((state as any)[field] !== '0') {
+        return true
+      }
+    }
+    return false
+  }
+
+  public recoverSigner(hash: string, sig: string, signer: string): string | undefined {
+    const bytes: Uint8Array = isHexString(hash) ? arrayify(hash) : toUtf8Bytes(hash)
+    let recovered: string = verifyMessage(bytes, sig).toLowerCase()
+    if (recovered && recovered === signer.toLowerCase()) {
+      return recovered
+    }
+    console.warn(`Signature doesn't match new scheme. Expected ${signer}, Recovered ${recovered}`)
+    // For backwards compatibility, TODO: remove until below
+    const fingerprint: any = util.toBuffer(String(hash))
+    const prefix: any = util.toBuffer('\x19Ethereum Signed Message:\n')
+    const prefixedMsg: any = util.keccak256(Buffer.concat(
+      [ prefix, util.toBuffer(String(fingerprint.length)), fingerprint ],
+    ))
+    const res: any = util.fromRpcSig(sig)
+    const pubKey: any = util.ecrecover(util.toBuffer(prefixedMsg), res.v, res.r, res.s)
+    recovered = util.bufferToHex(util.pubToAddress(pubKey))
+    if (recovered && recovered === signer.toLowerCase()) {
+      return recovered
+    }
+    console.warn(`Signature doesn't match old scheme. Expected ${signer}, Recovered ${recovered}`)
+    // TODO: remove until here
+    return undefined
+  }
+
+  public recoverSignerFromChannelState(
+    channelState: UnsignedChannelState,
+    sig: string,
+    signer: 'user' | 'hub',
+  ): string | undefined {
+    const hash: string = this.createChannelStateHash(channelState)
+    const signerAddress: string = signer === 'user' ? channelState.user : channelState.recipient
+    return this.recoverSigner(hash, sig, signerAddress)
+  }
+
+  public recoverSignerFromDepositRequest(
+    args: SignedDepositRequestProposal,
+    signer: string,
+  ): string | undefined {
+    const hash: string = this.createDepositRequestProposalHash(args)
+    return this.recoverSigner(hash, args.sigUser, signer)
+  }
+
+  public recoverSignerFromThreadState(
+    threadState: UnsignedThreadState,
+    sig: string,
+  ): string | undefined {
+    const hash: string = this.createThreadStateHash(threadState)
+    return this.recoverSigner(hash, sig, threadState.sender)
   }
 
 }
