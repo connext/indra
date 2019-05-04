6d19fa77d9f293678d62f817fd807e12f6153835
LayneHaber <layne.haber@gmail.com>
2019-05-03 20:05:19 -0700

    wds use parameterized tests

--- modules/client/src/controllers/WithdrawalController.test.ts
+++ modules/client/src/controllers/WithdrawalController.test.ts
@@ -1,6 +1,7 @@
-import { mkAddress } from '../testing';
+import { mkAddress, parameterizedTests } from '../testing';
 import { MockConnextInternal, MockStore } from '../testing/mocks';
-import { WithdrawalParameters } from '../types';
+import { WithdrawalParameters, convertChannelState } from '../types';
+import { Big } from '../lib/bn';
 // @ts-ignore
 global.fetch = require('node-fetch-polyfill');
 
@@ -8,50 +9,75 @@ describe('WithdrawalController: unit tests', () => {
   const user = mkAddress('0xAAA')
   let connext: MockConnextInternal
   const mockStore = new MockStore()
+  const exchangeRate = '5'
 
   beforeEach(async () => {
-    connext = new MockConnextInternal()
-  })
-
-  it('should withdraw all of users tokens', async () => {
-    // add channel with initial booty balance to exchange and withdraw
-    mockStore.setChannel({
+     // add channel with initial booty balance to exchange and withdraw
+     mockStore.setChannel({
       user,
       balanceWei: [10, 5],
       balanceToken: [0, 50],
     })
-    mockStore.setExchangeRate({ 'USD': '5' })
-    connext = new MockConnextInternal({ user, store: mockStore.createStore() })
+    mockStore.setExchangeRate({ 'USD': exchangeRate })
+    connext = new MockConnextInternal({ 
+      user, 
+      store: mockStore.createStore() 
+    })
+  })
 
-    await connext.start()
-
-    const params: WithdrawalParameters = {
-      exchangeRate: '5',
-      recipient: mkAddress('0xBBB'),
-      tokensToSell: '50',
-      withdrawalWeiUser: '5',
-    }
-
-    // wait to allow controller to set exchange rates
-    await new Promise(res => setTimeout(res, 20))
-
-    await connext.withdrawalController.requestUserWithdrawal(params)
-    await new Promise(res => setTimeout(res, 20))
-
-    connext.mockHub.assertReceivedUpdate({
-      reason: 'ProposePendingWithdrawal',
+  parameterizedTests([
+    {
+      name: "should withdraw all of users tokens",
       args: {
         exchangeRate: '5',
         recipient: mkAddress('0xBBB'),
         tokensToSell: '50',
-        weiToSell: '0',
-        targetWeiUser: '0',
-        targetWeiHub: '0',
-        targetTokenHub: '0',
-        additionalWeiHubToUser: '0',
+        withdrawalWeiUser: '5',
+        weiToSell: null,
+        withdrawalTokenUser: null,
       },
-      sigUser: true,
-      sigHub: false,
+    }
+  ], ({ name, args, }) => {
+
+    it(name, async () => {
+      await connext.start()
+
+      const preWdChan = convertChannelState("bn",
+        connext.store.getState().persistent.channel
+      )
+
+      // wait to allow controller to set exchange rates
+      await new Promise(res => setTimeout(res, 20))
+
+      // ignore args, should be Partial<Withdrawal> | SuccinctWithdrawal
+      // @ts-ignore
+      await connext.withdrawalController.requestUserWithdrawal(args)
+
+      await new Promise(res => setTimeout(res, 20))
+
+      const targetWeiUser = preWdChan.balanceWeiUser
+        .sub(Big(args.weiToSell || 0))
+        .sub(Big(args.withdrawalWeiUser || 0))
+        .toString()
+
+      const targetTokenUser = preWdChan.balanceTokenUser
+        .sub(Big(args.tokensToSell || 0))
+        .sub(Big(args.withdrawalTokenUser || 0))
+        .toString()
+
+      connext.mockHub.assertReceivedUpdate({
+        reason: 'ProposePendingWithdrawal',
+        args: {
+          exchangeRate: args.exchangeRate || exchangeRate,
+          recipient: args.recipient || user,
+          tokensToSell: args.tokensToSell || '0',
+          weiToSell: args.weiToSell || '0',
+          targetWeiUser,
+          targetTokenUser,
+        },
+        sigUser: true,
+        sigHub: false,
+      })
     })
   })
 
