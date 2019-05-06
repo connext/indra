1615ac4367e6ef71595b81fe8f9ba41cbb1c2772
LayneHaber <layne.haber@gmail.com>
2019-05-06 16:33:07 -0700

    fix tests

--- modules/client/src/controllers/WithdrawalController.test.ts
+++ modules/client/src/controllers/WithdrawalController.test.ts
@@ -11,32 +11,23 @@ const mockStore = new MockStore()
 const exchangeRate = '5'
 
 describe("createWithdrawalParameters", () => {
-  beforeEach(async () => {
-    // add channel with initial booty balance to exchange and withdraw
-    mockStore.setChannel({
-      user,
-      balanceWei: [10, 5],
-      balanceToken: [0, 50],
-    })
-    mockStore.setExchangeRate({ 'USD': exchangeRate })
-    connext = new MockConnextInternal({ 
-      user, 
-      store: mockStore.createStore() 
-    })
-  })
+  const user = mkAddress('0xAAA')
+  let connext: MockConnextInternal
+  const mockStore = new MockStore()
+  const exchangeRate = '5'
 
   parameterizedTests([
     {
-      name: "should work when supplied with only an amountWei",
+      name: "createWithdrawalParameters should work when supplied with only an amountWei",
       args: { 
-        amount: { amountWei: '1'}
+        amountWei: '1',
       },
       expected: { withdrawalWeiUser: '1' }
     },
     {
       name: "should work when supplied with only an amountToken",
       args: { 
-        amount: { amountToken: '1'}
+        amountToken: '1',
       },
       expected: { withdrawalTokenUser: '1' }
     },
@@ -45,35 +36,46 @@ describe("createWithdrawalParameters", () => {
       args: { 
         withdrawalWeiUser: '1',
       },
-      expected: { withdrawalTokenUser: '1' }
+      expected: { withdrawalWeiUser: '1', withdrawalTokenUser: '0' }
     },
     {
       name: "should correctly exchange on withdrawals when provided with an amount (native balance first)",
       args: { 
-        amount: { amountWei: '10' },
+        amountWei: '10'
       },
       expected: { withdrawalWeiUser: '5', tokensToSell: '25' }
     },
     {
       name: "should correctly calculate parameters if both wd amounts are supplied",
       args: { 
-        amount: { amountWei: '10', amountToken: '15' },
+        amountWei: '10', 
+        amountToken: '15'
       },
       expected: { withdrawalWeiUser: '5', tokensToSell: '25', withdrawalTokenUser: '15' }
     },
-  ], async ({ name, args, expected }) => {
+  ], async ({ args, expected }) => {
+    mockStore.setChannel({
+      user,
+      balanceWei: [10, 5],
+      balanceToken: [0, 50],
+    })
+    mockStore.setExchangeRate({ 'USD': exchangeRate })
+    connext = new MockConnextInternal({ 
+      store: mockStore.createStore(),
+    })
 
-    it(name, async () => {
-      const ans = connext.withdrawalController.createWithdrawalParameters(args)
-      assert.containSubset(ans, {
-        withdrawalWeiUser: '0',
-        tokensToSell: '0',
-        withdrawalTokenUser: '0',
-        weiToSell: '0',
-        exchangeRate: '5',
-        recipient: user,
-        ...expected
-      })
+    await connext.start()
+
+    const ans = connext.withdrawalController.createWithdrawalParameters(args)
+    console.log('************ ans', ans)
+    assert.containSubset(ans, {
+      withdrawalWeiUser: '0',
+      tokensToSell: '0',
+      withdrawalTokenUser: '0',
+      weiToSell: '0',
+      exchangeRate: '5',
+      recipient: connext.wallet.address,
+      ...expected
     })
   })
 })
@@ -97,14 +99,13 @@ describe('WithdrawalController: unit tests', () => {
     {
       name: "should withdraw all of users tokens as wei",
       args: {
-        exchangeRate: '5',
         recipient: mkAddress('0xBBB'),
         tokensToSell: '50',
         withdrawalWeiUser: '5',
         weiToSell: null,
         withdrawalTokenUser: null,
       },
-    }, 
+    },
     {
       name: "should fail if recipient is not a valid address",
       args: {
@@ -140,56 +141,53 @@ describe('WithdrawalController: unit tests', () => {
       },
       failsWith: /User exchanging wei at withdrawal is not permitted at this time./
     },
-  ], ({ name, args, failsWith }) => {
+  ], async ({ name, args, failsWith }) => {
+    await connext.start()
 
-    it(name, async () => {
-      await connext.start()
+    const preWdChan = convertChannelState("bn",
+      connext.store.getState().persistent.channel
+    )
 
-      const preWdChan = convertChannelState("bn",
-        connext.store.getState().persistent.channel
-      )
+    // wait to allow controller to set exchange rates
+    await new Promise(res => setTimeout(res, 20))
 
-      // wait to allow controller to set exchange rates
-      await new Promise(res => setTimeout(res, 20))
-
-      if (failsWith) {
-        // ignore args, should be Partial<Withdrawal> | SuccinctWithdrawal
-        await assert.isRejected(
-          // @ts-ignore
-          connext.withdrawalController.requestUserWithdrawal(args),
-          failsWith
-        )
-        return
-      }
+    if (failsWith) {
       // ignore args, should be Partial<Withdrawal> | SuccinctWithdrawal
-      // @ts-ignore
-      await connext.withdrawalController.requestUserWithdrawal(args)
+      await assert.isRejected(
+        // @ts-ignore
+        connext.withdrawalController.requestUserWithdrawal(args),
+        failsWith
+      )
+      return
+    }
+    // ignore args, should be Partial<Withdrawal> | SuccinctWithdrawal
+    // @ts-ignore
+    await connext.withdrawalController.requestUserWithdrawal(args)
 
-      await new Promise(res => setTimeout(res, 20))
+    await new Promise(res => setTimeout(res, 20))
 
-      const targetWeiUser = preWdChan.balanceWeiUser
-        .sub(Big(args.weiToSell || 0))
-        .sub(Big(args.withdrawalWeiUser || 0))
-        .toString()
+    const targetWeiUser = preWdChan.balanceWeiUser
+      .sub(Big(args.weiToSell || 0))
+      .sub(Big(args.withdrawalWeiUser || 0))
+      .toString()
 
-      const targetTokenUser = preWdChan.balanceTokenUser
-        .sub(Big(args.tokensToSell || 0))
-        .sub(Big(args.withdrawalTokenUser || 0))
-        .toString()
+    const targetTokenUser = preWdChan.balanceTokenUser
+      .sub(Big(args.tokensToSell || 0))
+      .sub(Big(args.withdrawalTokenUser || 0))
+      .toString()
 
-      connext.mockHub.assertReceivedUpdate({
-        reason: 'ProposePendingWithdrawal',
-        args: {
-          exchangeRate: args.exchangeRate || exchangeRate,
-          recipient: args.recipient || user,
-          tokensToSell: args.tokensToSell || '0',
-          weiToSell: args.weiToSell || '0',
-          targetWeiUser,
-          targetTokenUser,
-        },
-        sigUser: true,
-        sigHub: false,
-      })
+    connext.mockHub.assertReceivedUpdate({
+      reason: 'ProposePendingWithdrawal',
+      args: {
+        exchangeRate,
+        recipient: args.recipient || user,
+        tokensToSell: args.tokensToSell || '0',
+        weiToSell: args.weiToSell || '0',
+        targetWeiUser,
+        targetTokenUser,
+      },
+      sigUser: true,
+      sigHub: false,
     })
   })
 
