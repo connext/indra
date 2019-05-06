42b5f88f41c1f12ab293679baf272d7eb3f2a33c
LayneHaber <layne.haber@gmail.com>
2019-05-06 16:55:38 -0700

    fix mocks

--- modules/client/src/testing/mocks.ts
+++ modules/client/src/testing/mocks.ts
@@ -361,7 +361,7 @@ export class MockHub implements IHubAPIClient {
         console.log("TEST INCLUSION")
         this.receivedUpdateRequests.push(p.update as UpdateRequest)
       }
-      if (p.type == 'PT_CHANNEL' || p.type == 'PT_LINK' || p.type == 'PT_CUSTODIAL') {
+      if (p.type != 'PT_THREAD') {
         return {
           type: 'channel',
           update: {
