a6bb9cd7cfab4f4badac369967053893d60deb25
LayneHaber <layne.haber@gmail.com>
2019-05-06 14:51:19 -0700

    refactor to use parameterized

--- modules/client/src/state/middleware.test.ts
+++ modules/client/src/state/middleware.test.ts
@@ -1,45 +1,52 @@
-import { mkAddress, assert, mkHash } from "../testing";
+import { mkAddress, assert, mkHash, parameterizedTests } from "../testing";
 import { MockStore } from "../testing/mocks";
 import * as actions from './actions'
 
 describe("handleStateFlags", () => {
-  it("should work when user processes deposit", async () => {
-    const user = mkAddress('0xAAA')
-    const mock = new MockStore()
-    const store = mock.createStore()
-
-    mock.setChannel({
-      user,
-      balanceToken: [15, 15],
-      balanceWei: [15, 15],
-      txCountGlobal: 2,
-      txCountChain: 2,
-    })
-
-    store.dispatch(actions.setSortedSyncResultsFromHub([{
-      type: "channel",
-      update: {
-        reason: "ProposePendingDeposit",
-        sigHub: mkHash('0xas'),
-        args: {
-          depositTokenUser: '1',
-          depositWeiUser: '1',
-        },
-        txCount: 2,
-        createdOn: new Date(),
-        id: 1,
+  parameterizedTests([
+    {
+      name: "should work when user processes deposit",
+      channel: { 
+        balanceToken: [15, 15],
+        balanceWei: [15, 15],
+      } as any,
+      action: 'setSortedSyncResultsFromHub',
+      payload: [{
+        type: "channel",
+        update: {
+          reason: "ProposePendingDeposit",
+          sigHub: mkHash('0xas'),
+          args: {
+            depositTokenUser: '1',
+            depositWeiUser: '1',
+          },
+        }
+      }],
+      expected: {
+        deposit: {
+          transactionHash: null,
+          submitted: true,
+          detected: false,
+        }
       }
-    }]))
+    }
+  ], ({ name, channel, action, payload, expected }) => {
+    it(name, () => {
+      const user = mkAddress('0xAAA')
+      const mock = new MockStore()
+      const store = mock.createStore()
 
-    const state = store.getState()
+      mock.setChannel({
+        user,
+        ...channel,
+      })
 
-    assert.containSubset(state.runtime, {
-      deposit: {
-        transactionHash: null,
-        submitted: true,
-        detected: false,
-      }
+      store.dispatch((actions as any)[action](payload))
+
+      const state = store.getState()
+
+      assert.containSubset(state.runtime, expected)
+
     })
-
   })
 })
