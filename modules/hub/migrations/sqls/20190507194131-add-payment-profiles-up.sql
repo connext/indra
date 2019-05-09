3e3d4c85993141cf26ee3e020e4b706fc7a42149
LayneHaber <layne.haber@gmail.com>
2019-05-09 13:00:27 -0700

    migration to add payment profiles

--- modules/hub/migrations/sqls/20190507194131-add-payment-profiles-up.sql
+++ modules/hub/migrations/sqls/20190507194131-add-payment-profiles-up.sql
@@ -1,1 +1,16 @@
-create table payment_profiles
+
+/* Simple payment profile with min collateral and collateral target */
+create table payment_profiles (
+	id bigserial primary key,
+	minimum_maintained_collateral_wei wei_amount,
+  minimum_maintained_collateral_token wei_amount,
+	amount_to_collateralize_wei wei_amount,
+  amount_to_collateralize_token wei_amount
+);
+
+/* Add payment profile id to the channels column */
+alter table _cm_channels add column payment_profile_id bigint null;
+alter table _cm_channels add constraint payment_profile_id_fk foreign key (payment_profile_id) references payment_profiles(id);
+
+/* Update the channels view */
+create or replace view cm_channels as (select * from _cm_channels);
