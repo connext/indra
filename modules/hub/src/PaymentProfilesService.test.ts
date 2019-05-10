a970a731e57b7b5076000e187dd37715381c464c
LayneHaber <layne.haber@gmail.com>
2019-05-09 23:06:15 -0700

    clean imports and testing

--- modules/hub/src/PaymentProfilesService.test.ts
+++ modules/hub/src/PaymentProfilesService.test.ts
@@ -2,7 +2,6 @@ import { big, types } from 'connext';
 import { assert } from 'chai'
 import { getTestRegistry } from './testing'
 import { channelUpdateFactory } from './testing/factories';
-import { SQL } from './DBEngine';
 import { mkAddress } from './testing/stateUtils';
 import { PaymentProfileConfig } from 'connext/types/types';
 
@@ -13,7 +12,6 @@ const {
 describe('PaymentProfilesService', () => {
   const registry = getTestRegistry()
   const service = registry.get('PaymentProfilesService')
-  const db = registry.get('DBEngine')
 
   // **** helper functions
   const createAndAssertPaymentProfile = async (c: Partial<types.PaymentProfileConfig>, failsWith?: RegExp) => {
@@ -49,10 +47,8 @@ describe('PaymentProfilesService', () => {
     // verify all config entries
     await service.doAddProfileKey(config.id, addresses)
     channels.forEach(async chan => {
-      const row = await db.queryOne(SQL`
-        SELECT * FROM _cm_channels WHERE "user" = ${chan.user} AND 
-      `)
-      assert.equal(row.payment_profile_id, config.id.toString())
+      const retrieved = await service.doGetPaymentProfileByUser(chan.user)
+      assert.containSubset(config, retrieved)
     })
   }
 
