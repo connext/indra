e4685f41525270e3ec849d5bff4063e154dab341
LayneHaber <layne.haber@gmail.com>
2019-05-29 13:54:02 -0700

    clean config

--- modules/hub/src/Config.ts
+++ modules/hub/src/Config.ts
@@ -5,31 +5,31 @@ import camelize from './util/camelize'
 // required / expected environment variables
 // only variables in this array will be camelized
 const ENV_VARS = [
+  'ADMIN_ADDRESSES',
   'AUTH_DOMAIN_WHITELIST',
   'AUTH_REALM',
-  'CARD_IMAGE_URL',
-  'CARD_NAME',
+  'BEI_MIN_COLLATERALIZATION',
+  'BEI_MAX_COLLATERALIZATION',
+  'CHANNEL_BEI_DEPOSIT',
+  'CHANNEL_BEI_LIMIT',
   'CHANNEL_MANAGER_ADDRESS',
-  'COINPAYMENTS_API_KEY',
-  'COINPAYMENTS_API_SECRET',
-  'COINPAYMENTS_IPN_SECRET',
-  'COINPAYMENTS_MERCHANT_ID',
   'DATABASE_URL',
   'ETH_NETWORK_ID',
   'ETH_RPC_URL',
   'FORCE_SSL',
   'HOT_WALLET_ADDRESS',
+  'HOT_WALLET_MIN_BALANCE',
+  'HUB_PUBLIC_URL',
   'HTTPS_PORT',
   'HUB_PUBLIC_URL',
-  'MIN_SETTLEMENT_PERIOD',
+  'MAILGUN_API_KEY',
   'PORT',
   'PRIVATE_KEY_FILE',
-  'REALTIME_DB_SECRET',
-  'RECIPIENT_WHITELIST',
+  'RECENT_PAYMENTS_INTERVAL',
   'REDIS_URL',
-  'SERVICE_USER_KEY',
-  'SESSION_SECRET',
+  'SERVICE_KEY',
   'SHOULD_COLLATERALIZE_URL',
+  'STALE_CHANNEL_DAYS',
   'TOKEN_CONTRACT_ADDRESS'
 ]
 
@@ -62,20 +62,24 @@ export class Config {
     for (let key in (overrides || {}))
       instance[key] = overrides[key]
 
+    // transform any wei or bei values to BigNumbers, specifically
+    // looking for: channelBeiLimit, channelBeiDeposit, beiMinCollateralization,
+    // beiMaxCollateralization
+    for (let key in instance) {
+      if (key.toLowerCase().includes('bei') || key.toLowerCase().includes('wei')) {
+        instance[key] = toWei(instance[key])
+      }
+    }
+
     return instance
   }
 
-  // used in `BrandingApiServiceHandler` of the BrandingApiService
-  // address where this is the hubs wdal address. This value is
-  // set to `process.env.WALLET_ADDRESS!` in `main.ts`
-  // process.env.WALLET_ADDRESS does not exist
-  // TODO: remove or fix, that's a silly assignment
-  public recipientAddress: string = ''
+  // public recipientAddress: string = ''
+  // TODO: remove branding api service
   
   
   ////////////////////////////////////////
   // HUB GENERAL CONFIG
-
 
   // ETH CONFIG //
 
@@ -87,8 +91,7 @@ export class Config {
   public hotWalletAddress: string = ''
   // used in `WithdrawalsService`, will not withdraw if balance of
   // hot wallet will go below this threshold
-  // TODO: not in ENV_VARS
-  public hotWalletMinBalance: string = toWei('6.9').toString()
+  public hotWalletMinBalance: string = toWei('7').toString()
   // contract address of cm
   public channelManagerAddress: string = ''
   // node/provider URL for instantiating web3/ethers
@@ -98,41 +101,24 @@ export class Config {
   // TODO: keep this? client will use it on start
   public ethNetworkId: string = ''
 
-
   // WEB CONFIG //
 
   // used in `PaymentHub` optionally to create
   // a new service registry
-  // TODO: not in env_vars, remove?
   public registry?: Registry
-  // used in `BrandingApiService`, hardcoded in main
-  // TODO: not in env_vars, remove?
-  public branding: BrandingConfig
   // url in format 'postgresql://...' for db
   public databaseUrl: string = ''
   // url for redis when calling `createHandyClient` in `RedisClient`.ts
-  // TODO: is this set anywhere?
   public redisUrl: string = ''
-  // hardcoded in main as SpankChain
-  // TODO: remove?
-  public authRealm: string = ''
-  // auth whitelist, being hardcoded in main.ts as []
-  // TODO: remove?
-  public authDomainWhitelist: string[] = []
-  // hardcoded in main.ts
-  // as a null env var (should be hub addr?)
-  // TODO: not in env_vars, being used in auth middleware, remove?
-  public adminAddresses?: string[] = []
+  // admin users, not practically used atm so default to hub only reqs
+  public adminAddresses: string[] = [ process.env.HOT_WALLET_ADDRESS.toLowerCase() ]
   // service key, used in `AuthMiddleware`
-  // TODO: not in env_vars, instead refd as `SERVICE_USER_KEY`
-  public serviceKey: string = 'omqGMZzn90vFJskXFxzuO3gYHM6M989spw99f3ngRSiNSOUdB0PmmYTvZMByUKD'
-  // being hardcoded in main.ts
+  public serviceKey: string = 'foo' // delivered from env
+  // default port of hub
   public port: number = 8080
   // used in ApiServer.ts if forceSsl is true
-  // TODO: not in env_vars
   public httpsPort: number = 8443
   // used in ApiServer.ts
-  // TODO: not in env_vars
   public forceSsl: boolean | false = process.env.FORCE_SSL && process.env.FORCE_SSL.toLowerCase() === 'true'
 
   // NODE ENV CONFIG
@@ -141,37 +127,26 @@ export class Config {
   public isDev = env == 'development'
 
 
-
   ////////////////////////////////////////
   // HUB COLLATERAL CONFIG
 
 
   // amount users can have in any one channel for their balance
   // used in ChannelsService as exchange rate ceiling
-  // TODO: not in env_vars
-  public channelBeiLimit = toWei(process.env.CHANNEL_BEI_LIMIT || 69)
+  public channelBeiLimit = toWei(70)
   // minimum amount of bei the hub will put in as collateral
   // used in CloseChannelService, ChannelsService
-  // TODO: not in env_vars
-  public beiMinCollateralization = toWei(process.env.BEI_MIN_COLLATERALIZATION || 10)
-
+  public beiMinCollateralization = toWei(10)
   // max bei the hub will collateralize at any point
   // for receiving payments used in ChannelsService
-  // TODO: not in env_vars
-  public beiMaxCollateralization = toWei(process.env.BEI_MAX_COLLATERALIZATION || 169)
+  public beiMaxCollateralization = toWei(170)
   // used as sliding window to calculate collateral based on
   // recent payments
   // used in ChannelsService.ts
-  // TODO: not in env_vars
-  public recentPaymentsInterval  = (process.env.RECENT_PAYMENTS_INTERVAL || '10 minutes')
-  // max value going to any one thread
-  // TODO: not in env_vars, lines referencing are commented
-  // out, remove?
-  public threadBeiLimit = toWei(process.env.THREAD_BEI_LIMIT || 10)
+  public recentPaymentsInterval  = '10 minutes'
   // ceiling of what hub will deposit for exchange alongside
   // user deposit
-  // TODO: not in env_vars, lower to channelBeiLimit?
-  public channelBeiDeposit = toWei(process.env.CHANNEL_BEI_DEPOSIT || 1000)
+  public channelBeiDeposit = toWei(1000)
   
   // URL used to check whether a user should receive collateral.
   // Called by ChannelsService.shouldCollateralize:
@@ -189,49 +164,18 @@ export class Config {
   // number of days without activity until channel is 
   // considered "stale". used in autodispute
   // being hardcoded in main.ts
-  // TODO: not in env_vars, also no autodisputes, remove?
   public staleChannelDays?: number = process.env.STALE_CHANNEL_DAYS ? parseInt(process.env.STALE_CHANNEL_DAYS) : null // if null, will not dispute
 
   ////////////////////////////////////////
   // HUB API AND SERVICE KEYS
-  // being hardcoded in main.ts, not used anywhere
-  // TODO: remove?
-  public sessionSecret: string = ''
   // mailgun, used in email endpoint
-  public mailgunApiKey = process.env.MAILGUN_API_KEY || ""
+  public mailgunApiKey = ""
 
   // used in: coin payments service, mailgun email endpoint
-  // TODO: remove? is this being used anywhere else?
   public hubPublicUrl = envswitch({
     development: null, // will be generated by NgrokService
     staging: 'https://staging.hub.connext.network',
     production: 'https://hub.connext.network',
-  })
-
-  ////////////////////////////////////////
-  // used in: coinpayments service
-  public coinpaymentsMerchantId = envswitch({
-    development: '898d6ead05235f6081e97a58a6699289',
-    staging: '898d6ead05235f6081e97a58a6699289',
-    production: 'set by environment variable',
-  })
-
-  public coinpaymentsApiKey = envswitch({
-    development: '62eceb03e8fcb4f8ebdc1b8f43e1e6f4b9b120f0856061d228cf04b01ed5cf08',
-    staging: '62eceb03e8fcb4f8ebdc1b8f43e1e6f4b9b120f0856061d228cf04b01ed5cf08',
-    production: 'set by environment variable',
-  })
-
-  public coinpaymentsApiSecret = envswitch({
-    development: 'A78Dba053693985Fa8F9aad010352caa61a6e2ECb7E20E87AcfABc7ee37C3005',
-    staging: 'A78Dba053693985Fa8F9aad010352caa61a6e2ECb7E20E87AcfABc7ee37C3005',
-    production: 'set by environment variable',
-  })
-
-  public coinpaymentsIpnSecret = envswitch({
-    development: 'U1BC9v1s3l0zxdH3',
-    staging: 'U1BC9v1s3l0zxdH3',
-    production: 'set by environment variable',
   })
 }
 
