0626abf24c106b8b645dfcc149a10e3c5d0f2869
LayneHaber <layne.haber@gmail.com>
2019-05-29 14:41:00 -0700

    add admin only config endpoint

--- modules/hub/src/api/ConfigApiService.ts
+++ modules/hub/src/api/ConfigApiService.ts
@@ -4,6 +4,8 @@ import Config from '../Config'
 import log from '../util/log'
 
 import { ApiService } from './ApiService'
+import { isServiceOrAdmin } from '../util/ownedAddressOrAdmin';
+import { isBN } from '../util';
 
 const LOG = log('ConfigApiService')
 
@@ -11,6 +13,7 @@ export default class ConfigApiService extends ApiService<ConfigApiServiceHandler
   public namespace: string = 'config'
   public routes: any = {
     'GET /': 'doGetConfig',
+    'GET /admin': 'doGetAdminConfig'
   }
   public handler: any = ConfigApiServiceHandler
   public dependencies: any = {
@@ -21,6 +24,7 @@ export default class ConfigApiService extends ApiService<ConfigApiServiceHandler
 class ConfigApiServiceHandler {
   public config: Config
 
+  // unauthed endpoint
   public doGetConfig(req: express.Request, res: express.Response): any {
     return res.send({
       contractAddress: this.config.channelManagerAddress,
@@ -29,6 +33,24 @@ class ConfigApiServiceHandler {
       maxCollateralization: this.config.beiMaxCollateralization.toString(),
       tokenAddress: this.config.tokenContractAddress,
     })
+  }
+
+  // admin only config
+  public doGetAdminConfig(req: express.Request, res: express.Response): any {
+    if (!isServiceOrAdmin(req)) {
+      res.status(403)
+      return res.send({ error: 'Admin role not detected on request.' })
+    }
+    // cast bn values to string
+    let response
+    for (let key in this.config) {
+      if (isBN(this.config[key])) {
+        response[key] = this.config[key].toString()
+      } else {
+        response[key] = this.config[key]
+      }
+    }
+    return res.send(response)
   }
 
 }
