3a1fe79a317eb118d78669478eff3bae3c0e4c10
Rahul <rksethuram9@gmail.com>
2019-05-30 17:39:21 -0700

    Remove dependency that was injected twice

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,10 @@ import { AppService } from "./app.service";
 import { ConfigModule } from "./config/config.module";
 import { NodeController } from "./node/node.controller";
 import { NodeModule } from "./node/node.module";
-import { NodeProvider } from "./node/node.provider";
 
 @Module({
   imports: [ConfigModule, NodeModule],
   controllers: [AppController, NodeController],
-  providers: [AppService, NodeProvider],
+  providers: [AppService],
 })
 export class AppModule {}
