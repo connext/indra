e40243de4e04944781a02d4e953e159d1101b931
Rahul <rksethuram9@gmail.com>
2019-05-30 17:31:18 -0700

    Firebase stuff

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
+const FirebaseServer = require("firebase-server");
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const firebase = new FirebaseServer(5555, "localhost");
+  process.on("SIGINT", async () => {
+    console.log("Shutting down playground-server...");
+    firebase.close();
+    process.exit(0);
+  });
 }
 bootstrap();
