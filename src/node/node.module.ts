4e4c6a8919e3dc41a6d3b38e2ee741f670cc8b6a
Rahul <rksethuram9@gmail.com>
2019-06-03 12:16:02 -0700

    Add user dependency

--- src/node/node.module.ts
+++ src/node/node.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 
 import { ConfigModule } from "../config/config.module";
+import { UserModule } from "../user/user.module";
 
 import { NodeController } from "./node.controller";
 import { FirebaseProvider, NodeProvider } from "./node.provider";
 
 @Module({
   providers: [NodeProvider, FirebaseProvider],
-  imports: [ConfigModule],
+  imports: [ConfigModule, UserModule],
   controllers: [NodeController],
   exports: [NodeProvider, FirebaseProvider],
 })
