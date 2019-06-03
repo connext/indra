9805d991dcf91f467c05a2c13619b0a288234978
Rahul <rksethuram9@gmail.com>
2019-06-03 14:09:19 -0700

    Add route for get channel

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UserService } from "./user.service";
@@ -6,6 +6,10 @@ import { UserService } from "./user.service";
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @Get(":ethAddress")
+  async findByEthAddress(@Param("ethAddress") ethAddress: string) {
+    return (await this.userService.findByEthAddress(ethAddress)) || {};
+  }
 
   // TODO: validation
   @Post()
