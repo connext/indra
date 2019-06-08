f771925b9f4f4b930eaf0621f1b5be29295cb75c
Rahul <rksethuram9@gmail.com>
2019-06-07 19:26:36 -0700

    Nats provider

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const FirebaseProviderId = "FIREBASE";
 export const DatabaseProviderId = "DATABASE_CONNECTION";
 export const UserRepoProviderId = "USER_REPOSITORY";
 export const PostgresProviderId = "POSTGRES";
+export const NatsProviderId = "NATS";
