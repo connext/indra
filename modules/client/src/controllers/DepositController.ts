a63ea4f4f096f53659a45312ff4185f92c2a369f
LayneHaber <layne.haber@gmail.com>
2019-06-14 14:41:41 -0700

    update controller instantiation

--- modules/client/src/controllers/DepositController.ts
+++ modules/client/src/controllers/DepositController.ts
@@ -1,9 +1,19 @@
-import { DepositParameters } from "../types";
-
+import { DepositParameters, ChannelState } from "../types";
+import { Logger } from "../lib/logger";
+import { Node } from "@counterfactual/node";
 
 export class DepositController {
 
-  public deposit(params: DepositParameters) {
-    
+  private log: Logger;
+  private cfModule: Node;
+
+  public constructor(cfModule: Node, logLevel?: number) {
+    this.cfModule = cfModule;
+    this.log = new Logger("DepositController", logLevel)
+  }
+
+  public async deposit(params: DepositParameters): Promise<ChannelState> {
+    console.log("Deposit called, yay!")
+    return {} as ChannelState
   }
 }
