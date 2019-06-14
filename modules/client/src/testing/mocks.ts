a949f52c65c299ce17fa11581047ce6e6c00eda1
LayneHaber <layne.haber@gmail.com>
2019-06-13 23:21:41 -0700

    initial mocks push, WIP

---
+++ modules/client/src/testing/mocks.ts
@@ -0,0 +1,79 @@
+import { INodeApiClient } from "../node"
+import { NodeConfig, NodeInitializationParameters, ClientOptions } from "../types"
+import { Logger } from "../lib/logger";
+import { ethers } from "ethers";
+import { Address } from "@counterfactual/types";
+import { TransactionRequest, TransactionResponse } from "ethers/providers";
+import * as nats from "ts-nats";
+import { Wallet } from "../wallet";
+
+export const address: string = '0x627306090abab3a6e1400e9345bc60c78a8bef57'
+export const mnemonic: string = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+export const privateKey: string = '0x8339a8d4aa2aa5771f0230f50c725a4d6e6b7bc87bbf8b63b0c260285346eff6'
+export const ethUrl: string = process.env.ETH_RPC_URL || "http://localhost:8545"
+export const nodeUrl: string = process.env.NODE_URL || "nats://morecoolstuffs"
+
+export class MockNatsClient extends nats.Client {
+  public request(subject: string, timeout: number, body?: any) {
+    console.log(`Sending request to ${subject} ${body ? `with body: ${body}` : `without body`}`)
+    return {} as any // TODO: Msg type?
+  }
+}
+
+export class MockWallet extends Wallet {
+  public address: string;
+
+  public constructor(opts: Partial<ClientOptions> & { address?: string } = {}) {
+    // properly assign opts
+    super();
+    this.address = opts.address || address
+  }
+
+  public async getAddress(): Promise<string> {
+    return this.address
+  }
+
+  public async sendTransaction(txReq: TransactionRequest): Promise<TransactionResponse> {
+    console.log(`Sending transaction: ${JSON.stringify(txReq, null, 2)}`)
+    return {} as TransactionResponse
+  }
+  
+  public async signTransaction(txReq: TransactionRequest): Promise<string> {
+    return ""
+  }
+
+  public async signMessage(message: string): Promise<string> {
+    console.log(`Signing message: ${message}`)
+    return ""
+  }
+}
+
+export class MockNodeClientApi implements INodeApiClient {
+  // public receivedUpdateRequests: UpdateRequest[] = []
+  public log: Logger
+
+  private nodeUrl: string;
+  private nats: MockNatsClient; // TODO: rename to messaging?
+  public wallet: MockWallet;
+  private address: Address;
+  private nonce: string | undefined;
+  private signature: string | undefined;
+
+  public constructor(opts: Partial<NodeInitializationParameters> = {}) {
+    this.log = new Logger('MockNodeClientApi', opts.logLevel)
+    this.nodeUrl = opts.nodeUrl || nodeUrl;
+    this.nats = opts.nats || new MockNatsClient(); // TODO: rename to messaging?
+    this.wallet = opts.wallet || new MockWallet();
+    this.address = opts.wallet ? opts.wallet.address : address;
+    this.nonce = undefined;
+    this.signature = undefined;
+  }
+
+  public async config(): Promise<NodeConfig> {
+    return {
+      nodePublicIdentifier: "x-pubcooolstuffs", // x-pub of node
+      chainId: "mocks", // network that your channel is on
+      nodeUrl,
+    }
+  }
+}
