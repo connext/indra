47b32321284b5b1a54830fd7a07136891c86a4e5
Rahul <rksethuram9@gmail.com>
2019-06-14 16:58:06 +0200

    Add typing

--- modules/node/src/user/user.controller.ts
+++ modules/node/src/user/user.controller.ts
@@ -1,19 +1,20 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./user.entity";
 import { UserService } from "./user.service";
 
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get(":xpub")
-  async findByEthAddress(@Param("xpub") xpub: string) {
+  async findByXpub(@Param("xpub") xpub: string): Promise<User | {}> {
     return (await this.userService.findByXpub(xpub)) || {};
   }
 
   // TODO: validation
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.userService.create(createUserDto);
   }
 }
