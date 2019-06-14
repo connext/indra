e6a1099972ce1dc13280a8583d4643cf02dd45ce
Rahul <rksethuram9@gmail.com>
2019-06-14 16:24:20 +0200

    Add forward refs

--- modules/node/src/channel/channel.module.ts
+++ modules/node/src/channel/channel.module.ts
@@ -1,14 +1,17 @@
 import { forwardRef, Module } from "@nestjs/common";
 
+import { DatabaseModule } from "../database/database.module";
+import { NodeModule } from "../node/node.module";
 import { UserModule } from "../user/user.module";
 
 import { ChannelController } from "./channel.controller";
+import { channelProvider } from "./channel.provider";
 import { ChannelService } from "./channel.service";
 
 @Module({
   controllers: [ChannelController],
   exports: [ChannelService],
-  imports: [UserModule],
-  providers: [ChannelService],
+  imports: [DatabaseModule, UserModule, forwardRef(() => NodeModule)],
+  providers: [ChannelService, channelProvider],
 })
 export class ChannelModule {}
