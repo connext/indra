94cb92bde1c3ee7ab8fcc3a8c97ccebd9a2f530f
LayneHaber <layne.haber@gmail.com>
2019-06-17 15:18:55 -0700

    type has all columns

--- modules/hub/src/domain/ChannelDispute.ts
+++ modules/hub/src/domain/ChannelDispute.ts
@@ -1,8 +1,13 @@
+import { DisputeStatus } from "connext/types";
+
 export interface ChannelDisputeRow {
   id: number,
   channelId: number,
   startedOn: string,
   reason: string,
   onchainTxIdStart: number | null,
-  onchainTxIdEmpty: number | null
+  onchainTxIdEmpty: number | null,
+  status: DisputeStatus,
+  originator: string,
+  disputePeriodEnds: number | null,
 }
