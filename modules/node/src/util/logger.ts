e989ea6884a3c7b4dd044f413dffeca0061f72be
Rahul <rksethuram9@gmail.com>
2019-06-17 16:24:34 +0200

    Custom logger

---
+++ modules/node/src/util/logger.ts
@@ -0,0 +1,24 @@
+import { Logger } from "@nestjs/common";
+
+export class CLogger extends Logger {
+  private cxt: string;
+  constructor(context: string) {
+    super();
+    this.cxt = context;
+  }
+  log(message: string) {
+    super.log(message, this.cxt);
+  }
+  error(message: string, trace?: string) {
+    super.error(message, trace, this.cxt);
+  }
+  warn(message: string) {
+    super.warn(message, this.cxt);
+  }
+  debug(message: string) {
+    super.debug(message, this.cxt);
+  }
+  verbose(message: string) {
+    super.verbose(message, this.cxt);
+  }
+}
