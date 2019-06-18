abe19ec268fe3b77a94d689c736d74d4519989af
LayneHaber <layne.haber@gmail.com>
2019-06-18 13:01:08 -0700

    WIP, abandon, comment out for ts stuff

---
+++ modules/client/src/controllers/DepositController.spec.ts
@@ -0,0 +1,12 @@
+// TODO: unit testing!!
+
+// describe("DepositController.deposit", () => {
+  
+//   beforeEach(async () => {
+//     // create new deposit controller
+//     const cfModule = new MockCfModule()
+//   })
+//   test("should work", async () => {
+
+//   })
+// })
