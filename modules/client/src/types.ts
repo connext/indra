77c0521b5d4a6d165401475e08d07c7caf5727ac
LayneHaber <layne.haber@gmail.com>
2019-06-18 15:10:18 -0700

    ts-lint

--- modules/client/src/types.ts
+++ modules/client/src/types.ts
@@ -1,17 +1,18 @@
-import { BigNumber } from "ethers/utils";
-import { Address, Node as NodeTypes } from "@counterfactual/types";
 import { Node } from "@counterfactual/node";
-import { Client as NatsClient } from "ts-nats"
-import { Wallet } from "./wallet";
+import { Address, Node as NodeTypes } from "@counterfactual/types";
+import { BigNumber } from "ethers/utils";
+import { Client as NatsClient } from "ts-nats";
+
 import { INodeApiClient } from "./node";
+import { Wallet } from "./wallet";
 
 // types for the connext client package
 
 // shared with node
 
 /**
- * Type for instantiating the client. To properly instantiate the client, you 
- * will need to provide one of the following: 
+ * Type for instantiating the client. To properly instantiate the client, you
+ * will need to provide one of the following:
  * - mnemonic
  * - privateKey
  * - externalWallet
@@ -37,21 +38,22 @@ export type ClientOptions = {
   keyGen?: () => Promise<string>; // TODO: what will the type look like?
   safeSignHook?: (state: ChannelState | AppState) => Promise<string>;
   loadState?: (key: string) => Promise<string | null>;
-  saveState?: (pairs: {
-    key: string;
-    value: any;
-  }[]) => Promise<void>; 
+  saveState?: (
+    pairs: {
+      key: string;
+      value: any;
+    }[],
+  ) => Promise<void>;
   // TODO: state: string?
   logLevel?: number; // see logger.ts for meaning, optional
-
 
   // TODO: should be used in internal options? --> only if hardcoded
   // nats communication config, client must provide
   natsClusterId?: string;
   natsToken?: string;
-}
+};
 
-export type InternalClientOptions = ClientOptions &  {
+export type InternalClientOptions = ClientOptions & {
   // Optional, useful for dependency injection
   // TODO: can nats, node, wallet be optional?
   nats: NatsClient; // converted to nats-client in ConnextInternal constructor
@@ -63,22 +65,22 @@ export type InternalClientOptions = ClientOptions &  {
   // counterfactual node
   cfModule: Node;
   multisigAddress: string;
-}
+};
 
 // TODO: define properly!!
-export type ChannelProvider = {}
+export type ChannelProvider = {};
 
 export type ChannelState<T = string> = {
   apps: AppState<T>[];
   // TODO: CF types should all be generic, this will be
   // a BigNumber
   freeBalance: NodeTypes.GetFreeBalanceStateResult;
-}
-export type ChannelStateBigNumber = ChannelState<BigNumber>
+};
+export type ChannelStateBigNumber = ChannelState<BigNumber>;
 
-export type INodeAPIClient = {}
-export type ConnextStore = {}
-export type IMultisig = {}
+export type INodeAPIClient = {};
+export type ConnextStore = {};
+export type IMultisig = {};
 
 ///////////////////////////////////
 ////////// NODE TYPES ////////////
@@ -86,44 +88,47 @@ export type IMultisig = {}
 
 ////// General typings
 export type NodeInitializationParameters = {
-  nodeUrl: string,
-  nats: NatsClient,
-  wallet: Wallet,
-  logLevel?: number,
-}
+  nodeUrl: string;
+  nats: NatsClient;
+  wallet: Wallet;
+  logLevel?: number;
+};
 
 ///// Specific response types
 export type NodeConfig = {
-  nodePublicIdentifier: string // x-pub of node
-  chainId: string // network that your channel is on
-  nodeUrl: string
-}
+  nodePublicIdentifier: string; // x-pub of node
+  chainId: string; // network that your channel is on
+  nodeUrl: string,
+};
 
 /////////////////////////////////
 ////////// APP TYPES ////////////
 /////////////////////////////////
 
 // all the types of counterfactual app states
-export type AppState<T=string> = EthUnidirectionalTransferAppState<T>
-export type AppStateBigNumber = AppState<BigNumber>
+export type AppState<T = string> = EthUnidirectionalTransferAppState<T>;
+export type AppStateBigNumber = AppState<BigNumber>;
 
 // all the types of counterfactual app actions
-export type AppAction<T=string> = EthUnidirectionalTransferAppAction<T>
-export type AppActionBigNumber = AppAction<BigNumber>
+export type AppAction<T = string> = EthUnidirectionalTransferAppAction<T>;
+export type AppActionBigNumber = AppAction<BigNumber>;
 
 ////// ETHUnidirectionalTransferApp.sol typings
-export type EthUnidirectionalTransferAppState<T=string> = {
+export type EthUnidirectionalTransferAppState<T = string> = {
   transfers: [Transfer<T>, Transfer<T>];
   finalized: boolean;
-}
-export type EthUnidirectionalTransferAppStateBigNumber = EthUnidirectionalTransferAppState<BigNumber>
+};
+export type EthUnidirectionalTransferAppStateBigNumber = EthUnidirectionalTransferAppState<
+  BigNumber
+>;
 
-export type EthUnidirectionalTransferAppAction<T=string> = {
+export type EthUnidirectionalTransferAppAction<T = string> = {
   transferAmount: T;
   finalize: boolean;
-}
-export type EthUnidirectionalTransferAppActionBigNumber = EthUnidirectionalTransferAppAction<BigNumber>
-
+};
+export type EthUnidirectionalTransferAppActionBigNumber = EthUnidirectionalTransferAppAction<
+  BigNumber
+>;
 
 /////////////////////////////////
 ///////// INPUT TYPES ///////////
@@ -131,16 +136,16 @@ export type EthUnidirectionalTransferAppActionBigNumber = EthUnidirectionalTrans
 
 ////// Deposit types
 // TODO: we should have a way to deposit multiple things
-export type DepositParameters<T = string> = AssetAmount<T>
-export type DepositParametersBigNumber = DepositParameters<BigNumber>
+export type DepositParameters<T = string> = AssetAmount<T>;
+export type DepositParametersBigNumber = DepositParameters<BigNumber>;
 
 ////// Transfer types
 // TODO: would we ever want to pay people in the same app with multiple currencies?
 export type TransferParameters<T = string> = AssetAmount<T> & {
   recipient: Address;
   meta?: any; // TODO: meta types? should this be a string
-}
-export type TransferParametersBigNumber = TransferParameters<BigNumber>
+};
+export type TransferParametersBigNumber = TransferParameters<BigNumber>;
 
 ////// Exchange types
 // TODO: would we ever want to pay people in the same app with multiple currencies?
@@ -149,112 +154,114 @@ export type ExchangeParameters<T = string> = {
   toAssetId: Address;
   fromAssetId: Address; // TODO: do these assets have to be renamed?
   // make sure they are consistent with CF stuffs
-}
-export type ExchangeParametersBigNumber = ExchangeParameters<BigNumber>
+};
+export type ExchangeParametersBigNumber = ExchangeParameters<BigNumber>;
 
 ////// Withdraw types
 export type WithdrawParameters<T = string> = AssetAmount<T> & {
   recipient?: Address; // if not provided, will default to signer addr
-}
-export type WithdrawParametersBigNumber = WithdrawParameters<BigNumber>
-
+};
+export type WithdrawParametersBigNumber = WithdrawParameters<BigNumber>;
 
 /////////////////////////////////
 /////// LOW LEVEL TYPES /////////
 /////////////////////////////////
 // transfer types
-export type Transfer<T=string> = AssetAmount<T> & {
+export type Transfer<T = string> = AssetAmount<T> & {
   to: Address;
-}
-export type TransferBigNumber = Transfer<BigNumber>
+};
+export type TransferBigNumber = Transfer<BigNumber>;
 
 // asset types
-export type AssetAmount<T=string> = {
+export type AssetAmount<T = string> = {
   amount: T;
   assetId?: Address; // undefined if eth
-}
-export type AssetAmountBigNumber = AssetAmount<BigNumber>
-
+};
+export type AssetAmountBigNumber = AssetAmount<BigNumber>;
 
 /////////////////////////////////
 //////// CONVERSION FNS /////////
 /////////////////////////////////
 export type NumericTypes = {
-  'str': string;
-  'bignumber': BigNumber;
-  'number': number;
-}
+  str: string;
+  bignumber: BigNumber;
+  number: number;
+};
 
 export type NumericTypeName = keyof NumericTypes;
 
 const getType = (input: any): NumericTypeName => {
-  if (typeof input === 'string') return 'str'
-  if (BigNumber.isBigNumber(input)) return 'bignumber'
-  if (typeof input === 'number') return 'number' // used for testing purposes
-  throw new Error(`Unknown input type: ${typeof input}, value: ${JSON.stringify(input)}`)
-}
+  if (typeof input === "string") return "str";
+  if (BigNumber.isBigNumber(input)) return "bignumber";
+  if (typeof input === "number") return "number"; // used for testing purposes
+  throw new Error(
+    `Unknown input type: ${typeof input}, value: ${JSON.stringify(input)}`,
+  );
+};
 
 const castFunctions: any = {
-  'bignumber-str': (x: BigNumber): string => x.toString(),
-  'number-bignumber': (x: number): BigNumber => new BigNumber(x),
-  'number-str': (x: number): string => x.toString(),
-  'str-bignumber': (x: string): BigNumber => new BigNumber(x),
-}
+  "bignumber-str": (x: BigNumber): string => x.toString(),
+  "number-bignumber": (x: number): BigNumber => new BigNumber(x),
+  "number-str": (x: number): string => x.toString(),
+  "str-bignumber": (x: string): BigNumber => new BigNumber(x),
+};
 
 export const convertFields = (
-  fromType: NumericTypeName, toType: NumericTypeName, fields: string[], input: any,
+  fromType: NumericTypeName,
+  toType: NumericTypeName,
+  fields: string[],
+  input: any,
 ): any => {
-  if (fromType === toType) return input
+  if (fromType === toType) return input;
 
-  if (toType === 'number') throw new Error('Should not convert fields to numbers')
+  if (toType === "number") {
+    throw new Error("Should not convert fields to numbers");
+  }
 
-  let key
-  if (fromType === 'number' && toType === 'str') {
-    key = `bignumber-str`
-  } else if (fromType === 'number') {
-    key = `str-${toType}`
+  let key;
+  if (fromType === "number" && toType === "str") {
+    key = `bignumber-str`;
+  } else if (fromType === "number") {
+    key = `str-${toType}`;
   }
 
   // casting functions same for strs and number types
-  const cast = castFunctions[key || [fromType, toType].join('-')]
-  if (!cast) throw new Error(`No castFunc for ${fromType} -> ${toType}`)
+  const cast = castFunctions[key || [fromType, toType].join("-")];
+  if (!cast) throw new Error(`No castFunc for ${fromType} -> ${toType}`);
 
-  const res = { ...input }
+  const res = { ...input };
   for (const field of fields) {
-    const name = field.split('?')[0]
-    const isOptional = field.endsWith('?')
-    if (isOptional && !(name in input)) continue
-    res[name] = cast(input[name])
+    const name = field.split("?")[0];
+    const isOptional = field.endsWith("?");
+    if (isOptional && !(name in input)) continue;
+    res[name] = cast(input[name]);
   }
 
-  return res
-}
+  return res;
+};
 
 /**
  * Conversion function for AssetAmount or Transfer types. More generally, will
  * work for any types with only the numeric field "amount" with properly added
- * overloading definitions 
+ * overloading definitions
  */
-export function convertAssetAmount <
-To extends NumericTypeName,
->(
-  to: To, obj: AssetAmount<any>,
+export function convertAssetAmount<To extends NumericTypeName>(
+  to: To,
+  obj: AssetAmount<any>,
 ): AssetAmount<NumericTypes[To]>;
-export function convertAssetAmount <
-To extends NumericTypeName,
->(
-  to: To, obj: Transfer<any>,
+export function convertAssetAmount<To extends NumericTypeName>(
+  to: To,
+  obj: Transfer<any>,
 ): Transfer<NumericTypes[To]>;
-export function convertAssetAmount <
-  To extends NumericTypeName,
->(
-  to: To, obj: AssetAmount<any> | Transfer<any>,
+export function convertAssetAmount<To extends NumericTypeName>(
+  to: To,
+  obj: AssetAmount<any> | Transfer<any>,
 ) {
-  const fromType = getType(obj.amount)
-  return convertFields(fromType, to, ["amount"], obj)
+  const fromType = getType(obj.amount);
+  return convertFields(fromType, to, ["amount"], obj);
 }
 
 export const convert: any = {
   Asset: convertAssetAmount,
   Transfer: convertAssetAmount,
-}
+};
