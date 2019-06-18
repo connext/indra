2f9367fa7bd0ba48724b164cb712a22da8c49e74
LayneHaber <layne.haber@gmail.com>
2019-06-18 15:02:11 -0700

    initial exports

---
+++ modules/client/src/index.ts
@@ -0,0 +1,4 @@
+import { connect, ConnextChannel } from "./connext";
+import { ClientOptions, DepositParameters } from "./types";
+
+export { connect, ClientOptions, ConnextChannel, DepositParameters };
