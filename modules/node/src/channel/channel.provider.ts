400d00a1134648c50ce52d12c8f0e09faa729eb0
Rahul <rksethuram9@gmail.com>
2019-06-19 19:53:44 +0200

    WIP

--- modules/node/src/channel/channel.provider.ts
+++ modules/node/src/channel/channel.provider.ts
@@ -1,11 +1,24 @@
+import { NatsMessagingService } from "@connext/nats-messaging-client";
 import { FactoryProvider } from "@nestjs/common/interfaces";
+import { RpcException } from "@nestjs/microservices";
+import { Client } from "ts-nats";
 
-import { NatsServiceFactory } from "../../../nats-messaging-client"; // FIXME
 import { ChannelMessagingProviderId, NatsProviderId } from "../constants";
+
+async function setupSubscriptions(natsClient: Client) {
+  await natsClient.subscribe("greeter", (err, msg) => {
+    if (err) {
+      throw new RpcException("")
+    } else if (msg.reply) {
+      natsClient.publish(msg.reply, `hello there ${msg.data}`);
+    }
+  });
+}
 
 export const channelProvider: FactoryProvider = {
   inject: [NatsProviderId],
   provide: ChannelMessagingProviderId,
-  useFactory: (natsProvider: NatsServiceFactory) => {
-  }
-}
+  useFactory: (nats: NatsMessagingService) => {
+    const client = nats.getConnection();
+  },
+};
