ae513c6c738b46d8786901e5a0cbe54555a6d73e
Rahul <rksethuram9@gmail.com>
2019-06-20 16:02:00 +0200

    Slowly remove channel controller

--- modules/node/src/channel/channel.controller.ts
+++ modules/node/src/channel/channel.controller.ts
@@ -1,39 +1,13 @@
 import { Node as NodeTypes } from "@counterfactual/types";
-import {
-  Body,
-  Controller,
-  NotFoundException,
-  Param,
-  Post,
-} from "@nestjs/common";
+import { Body, Controller, Param, Post } from "@nestjs/common";
 import { parseEther } from "ethers/utils";
 
-import { UserRepository } from "../user/user.repository";
-
 import { ChannelService } from "./channel.service";
-import { CreateChannelDto } from "./dto/create-channel.dto";
 import { DepositDto } from "./dto/deposit-dto";
 
 @Controller("channels")
 export class ChannelController {
-  constructor(
-    private readonly channelService: ChannelService,
-    private readonly userRepository: UserRepository,
-  ) {}
-
-  @Post()
-  async create(
-    @Body() createChannelDto: CreateChannelDto,
-  ): Promise<{ transactionHash: string }> {
-    const user = await this.userRepository.findByXpub(
-      createChannelDto.counterpartyXpub,
-    );
-    if (!user) {
-      throw new NotFoundException();
-    }
-    const { transactionHash } = await this.channelService.create(user.xpub);
-    return { transactionHash };
-  }
+  constructor(private readonly channelService: ChannelService) {}
 
   @Post("deposit/:multisigAddress")
   async deposit(
