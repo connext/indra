b73eebede0d5d3b08ac6c08eef45538f8b89ccad
Rahul <rksethuram9@gmail.com>
2019-06-20 21:11:31 +0200

    Add extra repositories

--- modules/node/src/channel/channel.repository.ts
+++ modules/node/src/channel/channel.repository.ts
@@ -1,6 +1,12 @@
 import { EntityRepository, Repository } from "typeorm";
 
-import { Channel } from "./channel.entity";
+import { Channel, ChannelUpdate, NodeChannel } from "./channel.entity";
 
 @EntityRepository(Channel)
 export class ChannelRepository extends Repository<Channel> {}
+
+@EntityRepository(ChannelUpdate)
+export class ChannelUpdateRepository extends Repository<ChannelUpdate> {}
+
+@EntityRepository(NodeChannel)
+export class NodeChannelRepository extends Repository<NodeChannel> {}
