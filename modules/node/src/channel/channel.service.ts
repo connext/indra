081c1df239db6a72f8bfe5b40507f3db713393a7
LayneHaber <layne.haber@gmail.com>
2019-06-20 11:56:33 -0700

    prettier

--- modules/node/src/channel/channel.service.ts
+++ modules/node/src/channel/channel.service.ts
@@ -40,7 +40,9 @@ export class ChannelService implements OnModuleInit {
         }
 
         if (user.channels.length > 0) {
-          throw new RpcException(`Channel already exists for user ${counterpartyXpub}`);
+          throw new RpcException(
+            `Channel already exists for user ${counterpartyXpub}`,
+          );
         }
 
         const createChannelResponse = (await this.node.router.dispatch(
@@ -65,7 +67,8 @@ export class ChannelService implements OnModuleInit {
           }),
         )) as JsonRpcResponse;
 
-        const multisigResult = multisigResponse.result as NodeTypes.GetStateDepositHolderAddressResult;
+        const multisigResult: NodeTypes.GetStateDepositHolderAddressResult =
+          multisigResponse.result;
 
         const channel = new Channel();
         channel.counterpartyXpub = counterpartyXpub;
@@ -101,24 +104,29 @@ export class ChannelService implements OnModuleInit {
       requestId: generateUUID(),
       type: NodeTypes.MethodName.DEPOSIT,
     });
-    logger.log(`depositResponse.result: ${JSON.stringify(depositResponse.result)}`);
+    logger.log(
+      `depositResponse.result: ${JSON.stringify(depositResponse.result)}`,
+    );
     return depositResponse.result as NodeTypes.DepositResult;
   }
 
   // initialize CF Node with methods from this service to avoid circular dependency
   onModuleInit(): void {
-    this.node.on(NodeTypes.EventName.DEPOSIT_CONFIRMED, (res: DepositConfirmationMessage) => {
-      if (!res || !res.data) {
-        return;
-      }
-      logger.log("DEPOSIT_CONFIRMED event fired");
-      logger.log(`Deposit detected: ${JSON.stringify(res)}, matching`);
-      this.deposit(
-        res.data.multisigAddress,
-        res.data.amount as any, // FIXME
-        !!res.data.notifyCounterparty,
-      );
-    });
+    this.node.on(
+      NodeTypes.EventName.DEPOSIT_CONFIRMED,
+      (res: DepositConfirmationMessage) => {
+        if (!res || !res.data) {
+          return;
+        }
+        logger.log("DEPOSIT_CONFIRMED event fired");
+        logger.log(`Deposit detected: ${JSON.stringify(res)}, matching`);
+        this.deposit(
+          res.data.multisigAddress,
+          res.data.amount as any, // FIXME
+          !!res.data.notifyCounterparty,
+        );
+      },
+    );
 
     this.node.on(NodeTypes.EventName.CREATE_CHANNEL, (
       res: any, // FIXME
