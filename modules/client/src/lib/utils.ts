50ffc711c81aca89e7a89a13ba656faa3883dea0
LayneHaber <layne.haber@gmail.com>
2019-06-20 23:30:10 -0700

    another lil note for rahul

--- modules/client/src/lib/utils.ts
+++ modules/client/src/lib/utils.ts
@@ -53,6 +53,8 @@ export const delay = (ms: number): Promise<void> =>
   new Promise((res: any): any => setTimeout(res, ms));
 
 // TODO: Temporary - this eventually should be exposed at the top level and retrieve from store
+// @rahul: will this eventually be a node api client method or always
+// called through the cf node?
 export async function getFreeBalance(
   node: Node,
   multisigAddress: string,
