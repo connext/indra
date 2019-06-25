83295d906b8de9373afc82dbb69e43d68c4cfc99
LayneHaber <layne.haber@gmail.com>
2019-06-24 23:51:51 -0700

    show listener structure

--- modules/client/src/listener.ts
+++ modules/client/src/listener.ts
@@ -13,14 +13,63 @@ import { EventEmitter } from "events";
 
 import { Logger } from "./lib/logger";
 
+type CallbackStruct = {
+  [index in keyof typeof NodeTypes.EventName]: (data: NodeTypes.EventData) => Promise<any> | void;
+};
 export class ConnextListener extends EventEmitter {
   private log: Logger;
   private cfModule: Node;
+  private registeredCallbacks: Partial<CallbackStruct> = {};
+
+  // TODO: should have some constant "event parsing" functions
+  // in an obj keyed by the cf module event names
+  // that way we can emit different payloads based on the
+  // cf events
+
+  // TODO: finish out these callbacks
+  private defaultCallbacks: CallbackStruct = {
+    "PROPOSE_INSTALL": (data: NodeTypes.EventData): void => {
+      this.emit(EventName.PROPOSE_INSTALL_VIRTUAL, data);
+    },
+    // @ts-ignore
+    "CREATE_CHANNEL": (data: CreateChannelMessage): void => {
+      this.emit(EventName.CREATE_CHANNEL, data);
+    }
+  };
 
   constructor(cfModule: Node, logLevel: number) {
     super();
     this.cfModule = cfModule;
     this.log = new Logger("ConnextListener", logLevel);
+  }
+
+  // @rahul --> thinking its best to have default callbacks, and easy ways for
+  // the controllers to update what the handler is for their purposes
+  // not sure if this is the best way to do this though...
+
+  // also, if i expose the event listener here do you know the best way
+  // to emit events so users can listen like connext.on() while still
+  // keeping all the listener bits modular? could have an event emitter
+  // at the client level as well but would rather not
+  public registerCfListener(event: NodeTypes.EventName, cb: (data: any) => Promise<void>): void {
+    // remove existing listener
+    // check if its previously registered with a callbacl
+    if (this.registeredCallbacks[event]) {
+      this.removeListener(event, this.registeredCallbacks[event]);
+    } else {
+      // remove the default
+      this.removeListener(event, this.defaultCallbacks[event]);
+    }
+
+    // replace with new fn
+    // TODO: type res by obj with event as keys
+    this.cfModule.on(event, async (res: any) => {
+      await cb(res);
+      this.emit(event, res);
+    });
+
+    // save reference to event to remove later
+    this.registeredCallbacks[event] = cb;
   }
 
   // TODO: make more generic
@@ -102,4 +151,4 @@ export class ConnextListener extends EventEmitter {
 
     console.info(`CF Node handlers connected`);
   }
-}
+};
