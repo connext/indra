9bae0b84b310ffcce19e0226f06c0e8d056e525c
Rahul <rksethuram9@gmail.com>
2019-07-03 17:21:49 +0100

    Lint

--- modules/node/src/validator/isEthAddress.ts
+++ modules/node/src/validator/isEthAddress.ts
@@ -3,15 +3,15 @@ import { registerDecorator, ValidationOptions } from "class-validator";
 import { EthAddressRegex } from "../constants";
 
 // tslint:disable-next-line:function-name
-export function IsEthAddress(validationOptions?: ValidationOptions) {
-  return function(object: Object, propertyName: string) {
+export function IsEthAddress(validationOptions?: ValidationOptions): Function {
+  return function(object: Object, propertyName: string): void {
     registerDecorator({
       name: "isEthAddress",
       options: validationOptions,
       propertyName,
       target: object.constructor,
       validator: {
-        validate(value: any) {
+        validate(value: any): boolean {
           return typeof value === "string" && EthAddressRegex.test(value);
         },
       },
--- modules/node/src/validator/isXpub.ts
+++ modules/node/src/validator/isXpub.ts
@@ -3,18 +3,22 @@ import { registerDecorator, ValidationOptions } from "class-validator";
 import { XpubRegex } from "../constants";
 
 // tslint:disable-next-line:function-name
-export function IsXpub(validationOptions?: ValidationOptions) {
-  return function(object: Object, propertyName: string) {
+export function IsXpub(validationOptions?: ValidationOptions): Function {
+  return function(object: Object, propertyName: string): void {
     registerDecorator({
       name: "isXpub",
       options: validationOptions,
       propertyName,
       target: object.constructor,
       validator: {
-        validate(value: any) {
+        validate(value: any): boolean {
           return typeof value === "string" && XpubRegex.test(value);
         },
       },
     });
   };
+}
+
+export function isXpub(xpub: string): boolean {
+  return XpubRegex.test(xpub);
 }
