e75dfaef4f4447ab210e0ba54c3e5c0249c1de41
Rahul <rksethuram9@gmail.com>
2019-07-03 16:15:30 +0100

    Move things into DB

--- modules/node/src/database/database.service.ts
+++ modules/node/src/database/database.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 
-import { entities, viewEntites } from "../app.module";
+import { App, AppUpdate } from "../app/app.entity";
+import { AppRegistry } from "../appRegistry/appRegistry.entity";
+import { Channel, ChannelUpdate, NodeChannel } from "../channel/channel.entity";
 import { ConfigService } from "../config/config.service";
+import { PaymentProfile } from "../paymentProfile/paymentProfile.entity";
+import { User } from "../user/user.entity";
+
+export const entities = [App, AppRegistry, AppUpdate, Channel, ChannelUpdate, PaymentProfile, User];
+export const viewEntites = [NodeChannel];
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
@@ -11,6 +18,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     return {
       ...this.config.getPostgresConfig(),
       entities: [...entities, ...viewEntites],
+      logging: ["error"],
       synchronize: true,
       type: "postgres",
     };
