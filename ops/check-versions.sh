090b69f6114631dc947d67eec4374084c8a69b1d
Bo Henderson <bohende@gmail.com>
2019-07-04 12:27:19 +0530

    ignore create-react-app's version checking & use latest versions of everything

--- ops/check-versions.sh
+++ ops/check-versions.sh
@@ -11,7 +11,7 @@ do
   echo "module: $module"
   cd $module
   npm outdated -S | tail -n +2 | awk '$3 != $4'
-  npm outdated -D | tail -n +2 | sed '/^jest/d' | awk '$3 != $4'
+  npm outdated -D | tail -n +2 | awk '$3 != $4'
   cd ..
   echo
 done
