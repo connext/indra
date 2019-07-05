e5f005e972e2115991e32559bed9a1cb37fce58f
Rahul <rksethuram9@gmail.com>
2019-07-05 12:30:36 +0100

    Test hello nats

--- modules/node/test-nats.ts
+++ modules/node/test-nats.ts
@@ -2,7 +2,9 @@ import { connect } from "ts-nats";
 
 async function start(): Promise<void> {
   const nc = await connect({ servers: ["nats://localhost:4222"] });
-  await nc.subscribe("exchange-rate", (err, msg) => {
+  const msg = await nc.request("hello", 1000, JSON.stringify({ data: "Hello, Indra", id: "myid" }));
+  console.log("msg: ", msg);
+  await nc.subscribe("exchange-rate", (err: any, msg: any) => {
     if (err) {
       console.log(err);
     } else {
