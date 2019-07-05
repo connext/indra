dfb763575f0b3fb50cfea2d21ae60d481c4d212a
Rahul <rksethuram9@gmail.com>
2019-07-05 12:19:11 +0100

    Test exchange rate sub

--- modules/node/test-nats.ts
+++ modules/node/test-nats.ts
@@ -2,8 +2,14 @@ import { connect } from "ts-nats";
 
 async function start(): Promise<void> {
   const nc = await connect({ servers: ["nats://localhost:4222"] });
-  const msg = await nc.request("config.get", 1000);
-  console.log("msg: ", msg);
+  await nc.subscribe("exchange-rate", (err, msg) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(msg);
+      console.log(msg.data);
+    }
+  });
 }
 
 start();
