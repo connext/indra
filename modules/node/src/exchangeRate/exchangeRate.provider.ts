e32bb94c6b86601d29eae4e667d417c8b42fe2d4
Rahul <rksethuram9@gmail.com>
2019-07-05 12:20:31 +0100

    Add exchange rate provider to use service

--- modules/node/src/exchangeRate/exchangeRate.provider.ts
+++ modules/node/src/exchangeRate/exchangeRate.provider.ts
@@ -1,28 +1,45 @@
 import { FactoryProvider } from "@nestjs/common/interfaces";
-import { ClientProxy } from "@nestjs/microservices";
 import { Contract, ethers } from "ethers";
+import { BigNumber } from "ethers/utils";
+import interval from "interval-promise";
 
 import { medianizerAbi } from "../abi/medianizer.abi";
-import { MedianizerProviderId, NatsClientProviderId } from "../constants";
+import { ExchangeRateProviderId } from "../constants";
+import { CLogger } from "../util";
 
+import { ExchangeRateService } from "./exchangeRate.service";
+
+// mainnet
 const MEDIANIZER_ADDRESS = "0x729D19f657BD0614b4985Cf1D82531c67569197B";
+// TODO: handle on ganache?
 
-async function getExchangeRate(medianizer: Contract) {
-  return (await medianizer.compute())[0];
-}
+const logger = new CLogger("exchangeRateProvider");
+
+// async function getExchangeRate(medianizer: Contract): Promise<BigNumber> {
+//   return (await medianizer.compute())[0];
+// }
+
+// export const exchangeRateProvider: FactoryProvider = {
+//   inject: [NatsClientProviderId],
+//   provide: ExchangeRateProviderId,
+//   useFactory: (natsClient: ClientProxy): void => {
+//     const provider = ethers.getDefaultProvider();
+//     const medianizer = new ethers.Contract(MEDIANIZER_ADDRESS, medianizerAbi, provider);
+//     interval(async () => {
+//       const rate = await getExchangeRate(medianizer);
+//       logger.log(`Got new rate ${rate}`);
+//       natsClient.send("exchange-rate", rate);
+//       natsClient.emit("exchange-rate", rate);
+//     }, 5000);
+//   },
+// };
 
 export const exchangeRateProvider: FactoryProvider = {
-  inject: [NatsClientProviderId],
-  provide: NatsClientProviderId,
-  useFactory: (natsClient: ClientProxy): any => {
-    // add interval promise
-  },
-};
-
-export const medianizerProvider: FactoryProvider = {
-  provide: MedianizerProviderId,
-  useFactory: (): Contract => {
-    const provider = ethers.getDefaultProvider();
-    return new ethers.Contract(MEDIANIZER_ADDRESS, medianizerAbi, provider);
+  inject: [ExchangeRateService],
+  provide: ExchangeRateProviderId,
+  useFactory: (exchangeRateService: ExchangeRateService): void => {
+    interval(async () => {
+      await exchangeRateService.publishRate();
+    }, 5000);
   },
 };
