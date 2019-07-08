175b9d02122dc87a65c86b67df9e75f7ea7892d2
Bo Henderson <bohende@gmail.com>
2019-07-08 18:30:46 +0530

    daicard: fix localStorage wrapper

--- modules/daicard/src/App.js
+++ modules/daicard/src/App.js
@@ -128,11 +128,27 @@ class App extends React.Component {
 
     const store = {
       get: (key) => {
-        try {
-          return JSON.parse(localStorage.getItem(`CF_NODE:${key}`));
-        } catch {
-          return localStorage.getItem(`CF_NODE:${key}`);
+        const raw = localStorage.getItem(`CF_NODE:${key}`)
+        if (raw) {
+          try {
+            return JSON.parse(raw);
+          } catch {
+            return raw;
+          }
         }
+        // Handle partial matches so the following line works -.-
+        // https://github.com/counterfactual/monorepo/blob/master/packages/node/src/store.ts#L54
+        const partialMatches = {}
+        for (const k of Object.keys(localStorage)) {
+          if (k.includes(`${key}/`)) {
+            try {
+              partialMatches[k.replace('CF_NODE:', '').replace(`${key}/`, '')] = JSON.parse(localStorage.getItem(k))
+            } catch {
+              partialMatches[k.replace('CF_NODE:', '').replace(`${key}/`, '')] = localStorage.getItem(k)
+            }
+          }
+        }
+        return partialMatches;
       },
       set: (pairs, allowDelete) => {
         for (const pair of pairs) {
