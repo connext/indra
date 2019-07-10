f635290ed4eae79978d00df719cb1208fb05b205
LayneHaber <layne.haber@gmail.com>
2019-07-10 11:42:59 -0700

    user default message patterns

--- modules/node/src/appRegistry/appRegistry.controller.ts
+++ modules/node/src/appRegistry/appRegistry.controller.ts
@@ -1,19 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
 import { MessagePattern } from "@nestjs/microservices";
 
-import { AppRegistry, Network } from "./appRegistry.entity";
+import { Network } from "./appRegistry.entity";
 import { AppRegistryRepository } from "./appRegistry.repository";
 
 @Controller()
 export class AppRegistryController {
   constructor(private readonly appRegistryRepository: AppRegistryRepository) {}
-  // @MessagePattern("app-registry")
-  // async get(data: { name: string; network: Network } | undefined): Promise<any> {
-  //   console.log("recieved message with data:", JSON.stringify(data, null, 2));
-  //   if (!data || !data.network || !data.name) {
-  //     console.log("returning:", await this.appRegistryRepository.find());
-  //     return JSON.stringify(await this.appRegistryRepository.find());
-  //   }
-  //   return [await this.appRegistryRepository.findByNameAndNetwork(data.name, data.network)];
-  // }
+  @MessagePattern("app-registry")
+  async get(data: { name: string; network: Network } | undefined): Promise<any> {
+    if (!data || !data.network || !data.name) {
+      return await this.appRegistryRepository.find();
+    }
+    return [await this.appRegistryRepository.findByNameAndNetwork(data.name, data.network)];
+  }
 }
