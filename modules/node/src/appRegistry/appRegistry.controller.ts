55c3c8152d8ac6ff53e94fd5e3dca3b7f722e4de
LayneHaber <layne.haber@gmail.com>
2019-07-09 22:37:27 -0700

    tmp remove

--- modules/node/src/appRegistry/appRegistry.controller.ts
+++ modules/node/src/appRegistry/appRegistry.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { MessagePattern } from "@nestjs/microservices";
 
 import { AppRegistry, Network } from "./appRegistry.entity";
@@ -7,11 +7,13 @@ import { AppRegistryRepository } from "./appRegistry.repository";
 @Controller()
 export class AppRegistryController {
   constructor(private readonly appRegistryRepository: AppRegistryRepository) {}
-  @MessagePattern("app-registry")
-  async get(data: { name: string; network: Network } | undefined): Promise<AppRegistry[]> {
-    if (!data || !data.network || !data.name) {
-      return await this.appRegistryRepository.find();
-    }
-    return [await this.appRegistryRepository.findByNameAndNetwork(data.name, data.network)];
-  }
+  // @MessagePattern("app-registry")
+  // async get(data: { name: string; network: Network } | undefined): Promise<any> {
+  //   console.log("recieved message with data:", JSON.stringify(data, null, 2));
+  //   if (!data || !data.network || !data.name) {
+  //     console.log("returning:", await this.appRegistryRepository.find());
+  //     return JSON.stringify(await this.appRegistryRepository.find());
+  //   }
+  //   return [await this.appRegistryRepository.findByNameAndNetwork(data.name, data.network)];
+  // }
 }
