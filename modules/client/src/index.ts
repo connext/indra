b0f3c6571d421fb1866cd3cf7d59ef0e526716ac
LayneHaber <layne.haber@gmail.com>
2019-07-15 12:48:25 -0700

    free balance exposed at top level in client

--- modules/client/src/index.ts
+++ modules/client/src/index.ts
@@ -1,5 +1,5 @@
 import { connect, ConnextInternal } from "./connext";
-import { getFreeBalance, logEthFreeBalance } from "./lib/utils";
+import { logEthFreeBalance } from "./lib/utils";
 import { ClientOptions } from "./types";
 
-export { ClientOptions, ConnextInternal, connect, getFreeBalance, logEthFreeBalance };
+export { ClientOptions, ConnextInternal, connect, logEthFreeBalance };
