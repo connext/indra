0d8d052cbc7d58c6b1245902928e1b43f0333e48
LayneHaber <layne.haber@gmail.com>
2019-07-21 23:18:43 -0700

    rename

--- modules/client/src/validation/appProposals.ts
+++ modules/client/src/validation/appProposals.ts
@@ -63,7 +63,7 @@ export const validateTransferApp = async (
 };
 
 export const appProposalValidation: ProposalValidator = {
-  EthUnidirectionalTransferApp: validateTransferApp,
+  UnidirectionalTransferApp: validateTransferApp,
   SimpleTwoPartySwapApp: validateSwapApp,
 };
 
