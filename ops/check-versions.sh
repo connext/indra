378d657913eb7571302b23c1e6d72f94bb163be1
Bo Henderson <bohende@gmail.com>
2019-07-25 12:33:49 +0530

    check-versions: prettier format

--- ops/check-versions.sh
+++ ops/check-versions.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "module: project root"
-npm outdated -D | tail -n +2 | awk '$3 != $4'
+format='{printf("| %-32s|%8s  ->  %-8s|\n", $1, $3, $4)}'
+
+echo "==== Module: project root"
+npm outdated -D | tail -n +2 | awk '$3 != $4' | awk "$format"
 echo
 
 cd modules
@@ -11,10 +13,10 @@ do
   if [[ "$module" == "proxy" ]]
   then continue
   fi
-  echo "module: $module"
+  echo "===== Module: $module"
   cd $module
-  npm outdated -S | tail -n +2 | awk '$3 != $4'
-  npm outdated -D | tail -n +2 | awk '$3 != $4'
+  npm outdated -D | tail -n +2 | awk '$3 != $4' | awk "$format"
+  npm outdated -S | tail -n +2 | awk '$3 != $4' | awk "$format"
   cd ..
   echo
 done
