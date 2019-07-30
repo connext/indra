58f7909a2e55ada45d36cac477fee4c00bf4a36b
LayneHaber <layne.haber@gmail.com>
2019-07-30 16:22:53 -0700

    reorg conditional logic

--- modules/client/src/controllers/ConditionalTransferController.ts
+++ modules/client/src/controllers/ConditionalTransferController.ts
@@ -1,10 +1,14 @@
 import {
+  ConditionalTransferInitialStateBigNumber,
   ConditionalTransferParameters,
   ConditionalTransferResponse,
   convert,
+  LinkedTransferParameters,
+  LinkedTransferResponse,
   RegisteredAppDetails,
   SupportedApplication,
   SupportedApplications,
+  TransferCondition,
   UnidirectionalLinkedTransferAppActionBigNumber,
   UnidirectionalLinkedTransferAppStage,
   UnidirectionalLinkedTransferAppStateBigNumber,
@@ -12,14 +16,18 @@ import {
 import { RejectInstallVirtualMessage } from "@counterfactual/node";
 import { Node as NodeTypes } from "@counterfactual/types";
 import { Zero } from "ethers/constants";
-import { BigNumber, solidityKeccak256 } from "ethers/utils";
-import { fromExtendedKey } from "ethers/utils/hdnode";
-import { v4 as uuidV4 } from "uuid";
+import { BigNumber, hexlify, randomBytes, solidityKeccak256 } from "ethers/utils";
 
-import { falsy, invalidAddress, invalidXpub, notLessThanOrEqualTo } from "../validation";
+import { freeBalanceAddressFromXpub } from "../lib/utils";
+import { falsy, invalidAddress, notLessThanOrEqualTo } from "../validation";
 
 import { AbstractController } from "./AbstractController";
 
+type ConditionalExecution = {
+  [index in TransferCondition]: (
+    params: ConditionalTransferParameters,
+  ) => Promise<ConditionalTransferResponse>;
+};
 export class ConditionalTransferController extends AbstractController {
   private appId: string;
 
@@ -28,79 +36,35 @@ export class ConditionalTransferController extends AbstractController {
   public conditionalTransfer = async (
     params: ConditionalTransferParameters,
   ): Promise<ConditionalTransferResponse> => {
-    this.log.info(`Transfer called with parameters: ${JSON.stringify(params, null, 2)}`);
+    this.log.info(
+      `Conditional transfer called with parameters: ${JSON.stringify(params, null, 2)}`,
+    );
 
+    const res = await this.conditionalExecutors[params.conditionType](params);
+    return res;
+  };
+
+  /////////////////////////////////
+  ////// PRIVATE METHODS
+  private handleLinkedTransfers = async (
+    params: LinkedTransferParameters,
+  ): Promise<LinkedTransferResponse> => {
     // convert params + validate
-    const { recipient, amount, assetId } = convert.TransferParameters("bignumber", params);
-    this.log.info(`********** assetId: ${assetId}`);
-    const invalid = await this.validate(recipient, amount, assetId);
+    const { amount, assetId } = convert.LinkedTransfer("bignumber", params);
+    const invalid = await this.validateLinked(amount, assetId);
+    if (invalid) {
+      throw new Error(invalid);
+    }
 
     const appInfo = this.connext.getRegisteredAppDetails(
       SupportedApplications.UnidirectionalLinkedTransferApp as SupportedApplication,
     );
 
     // generate random payment id
-    const paymentId = uuidV4();
-    // generate hex string
-    // TODO: pad?
-    const preImage = `0x${crypto.getRandomValues(new Uint32Array(1))[0].toString(16)}`;
+    const paymentId = hexlify(randomBytes(32));
+    // generate random preimage
+    const preImage = hexlify(randomBytes(32));
     // install the transfer application
-    const appId = await this.conditionalTransferAppInstalled(
-      amount,
-      recipient,
-      assetId,
-      paymentId,
-      preImage,
-      appInfo,
-    );
-    if (!appId) {
-      throw new Error(`App was not installed`);
-    }
-
-    if (invalid) {
-      throw new Error(invalid.toString());
-    }
-    return { paymentId, preImage, freeBalance: await this.connext.getFreeBalance(assetId) };
-  };
-
-  /////////////////////////////////
-  ////// PRIVATE METHODS
-  private validate = async (
-    recipient: string,
-    amount: BigNumber,
-    assetId: string,
-  ): Promise<undefined | string> => {
-    // check that there is sufficient free balance for amount
-    const freeBalance = await this.connext.getFreeBalance(assetId);
-    const preTransferBal = freeBalance[this.connext.freeBalanceAddress];
-    const errs = [
-      invalidXpub(recipient),
-      invalidAddress(assetId),
-      notLessThanOrEqualTo(amount, preTransferBal),
-    ];
-    return errs ? errs.filter(falsy)[0] : undefined;
-  };
-
-  private createLinkedHash(action: UnidirectionalLinkedTransferAppActionBigNumber): string {
-    return solidityKeccak256(
-      ["uint256", "address", "string", "string"],
-      [action.amount, action.assetId, action.paymentId, action.preImage],
-    );
-  }
-
-  // creates a promise that is resolved once the app is installed
-  // and rejected if the virtual application is rejected
-  private conditionalTransferAppInstalled = async (
-    amount: BigNumber,
-    recipient: string,
-    assetId: string,
-    paymentId: string,
-    preImage: string,
-    appInfo: RegisteredAppDetails,
-  ): Promise<string | undefined> => {
-    let boundResolve: (value?: any) => void;
-    let boundReject: (reason?: any) => void;
-
     const linkedHash = this.createLinkedHash({ amount, assetId, paymentId, preImage });
 
     const initialState: UnidirectionalLinkedTransferAppStateBigNumber = {
@@ -110,49 +74,97 @@ export class ConditionalTransferController extends AbstractController {
       transfers: [
         {
           amount: Zero,
-          to: this.wallet.address,
+          to: freeBalanceAddressFromXpub(this.connext.publicIdentifier),
           // TODO: replace? fromExtendedKey(this.publicIdentifier).derivePath("0").address
         },
         {
           amount,
-          to: fromExtendedKey(recipient).derivePath("0").address,
+          to: freeBalanceAddressFromXpub(this.connext.nodePublicIdentifier),
         },
       ],
       turnNum: Zero,
     };
 
+    const appId = await this.conditionalTransferAppInstalled(
+      amount,
+      assetId,
+      initialState,
+      appInfo,
+    );
+    if (!appId) {
+      throw new Error(`App was not installed`);
+    }
+
+    return {
+      freeBalance: await this.connext.getFreeBalance(assetId),
+      paymentId,
+      preImage,
+    };
+  };
+
+  private validateLinked = async (
+    amount: BigNumber,
+    assetId: string,
+  ): Promise<undefined | string> => {
+    // check that there is sufficient free balance for amount
+    const freeBalance = await this.connext.getFreeBalance(assetId);
+    const preTransferBal = freeBalance[this.connext.freeBalanceAddress];
+    const errs = [invalidAddress(assetId), notLessThanOrEqualTo(amount, preTransferBal)];
+    return errs ? errs.filter(falsy)[0] : undefined;
+  };
+
+  private createLinkedHash = (action: UnidirectionalLinkedTransferAppActionBigNumber): string => {
+    return solidityKeccak256(
+      ["uint256", "address", "bytes32", "bytes32"],
+      [action.amount, action.assetId, action.paymentId, action.preImage],
+    );
+  };
+
+  // creates a promise that is resolved once the app is installed
+  // and rejected if the virtual application is rejected
+  private conditionalTransferAppInstalled = async (
+    initiatorDeposit: BigNumber,
+    assetId: string,
+    initialState: ConditionalTransferInitialStateBigNumber,
+    appInfo: RegisteredAppDetails,
+  ): Promise<string | undefined> => {
+    let boundResolve: (value?: any) => void;
+    let boundReject: (reason?: any) => void;
+
     // note: intermediary is added in connext.ts as well
-    const { actionEncoding, appDefinitionAddress: appDefinition, stateEncoding } = appInfo;
-    const params: NodeTypes.ProposeInstallVirtualParams = {
+    const {
+      actionEncoding,
+      appDefinitionAddress: appDefinition,
+      outcomeType,
+      stateEncoding,
+    } = appInfo;
+    const params: NodeTypes.ProposeInstallParams = {
       abiEncodings: {
         actionEncoding,
         stateEncoding,
       },
       appDefinition,
       initialState,
-      initiatorDeposit: amount,
+      initiatorDeposit,
       initiatorDepositTokenAddress: assetId,
-      intermediaries: [this.connext.nodePublicIdentifier],
-      outcomeType: appInfo.outcomeType,
-      proposedToIdentifier: recipient,
+      outcomeType,
+      proposedToIdentifier: this.connext.nodePublicIdentifier,
       responderDeposit: Zero,
       responderDepositTokenAddress: assetId,
       timeout: Zero,
     };
 
-    const res = await this.connext.proposeInstallVirtualApp(params);
+    const res = await this.connext.proposeInstallApp(params);
     // set app instance id
     this.appId = res.appInstanceId;
 
     try {
-      await new Promise(
-        (res: () => any, rej: () => any): void => {
-          boundReject = this.rejectInstallTransfer.bind(null, rej);
-          boundResolve = this.resolveInstallTransfer.bind(null, res);
-          this.listener.on(NodeTypes.EventName.INSTALL_VIRTUAL, boundResolve);
-          this.listener.on(NodeTypes.EventName.REJECT_INSTALL_VIRTUAL, boundReject);
-        },
-      );
+      await new Promise((res: () => any, rej: () => any): void => {
+        boundReject = this.rejectInstallTransfer.bind(null, rej);
+        boundResolve = this.resolveInstallTransfer.bind(null, res);
+        this.listener.on(NodeTypes.EventName.INSTALL, boundResolve);
+        this.listener.on(NodeTypes.EventName.REJECT_INSTALL, boundReject);
+      });
       this.log.info(`App was installed successfully!: ${JSON.stringify(res)}`);
       return res.appInstanceId;
     } catch (e) {
@@ -167,12 +179,8 @@ export class ConditionalTransferController extends AbstractController {
   private resolveInstallTransfer = (res: (value?: unknown) => void, data: any): any => {
     if (this.appId !== data.params.appInstanceId) {
       this.log.info(
-        `Caught INSTALL_VIRTUAL event for different app ${JSON.stringify(data)}, expected ${
-          this.appId
-        }`,
+        `Caught INSTALL event for different app ${JSON.stringify(data)}, expected ${this.appId}`,
       );
-      // TODO: do we need to recreate the handler here?
-      res();
       return;
     }
     if (this.timeout) {
@@ -192,11 +200,15 @@ export class ConditionalTransferController extends AbstractController {
       return;
     }
 
-    return rej(`Install virtual failed. Event data: ${JSON.stringify(msg, null, 2)}`);
+    return rej(`Install failed. Event data: ${JSON.stringify(msg, null, 2)}`);
   };
 
   private cleanupInstallListeners = (boundResolve: any, boundReject: any): void => {
-    this.listener.removeListener(NodeTypes.EventName.INSTALL_VIRTUAL, boundResolve);
-    this.listener.removeListener(NodeTypes.EventName.REJECT_INSTALL_VIRTUAL, boundReject);
+    this.listener.removeListener(NodeTypes.EventName.INSTALL, boundResolve);
+    this.listener.removeListener(NodeTypes.EventName.REJECT_INSTALL, boundReject);
+  };
+
+  private conditionalExecutors: ConditionalExecution = {
+    LINKED_TRANSFER: this.handleLinkedTransfers,
   };
 }
