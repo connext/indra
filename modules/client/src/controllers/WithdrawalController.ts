41e58388180942fcda84d106d436b3aafae6a6f3
Rahul <rksethuram9@gmail.com>
2019-07-31 15:24:07 +0200

    Types were messed up, this may have been causing problems

--- modules/client/src/controllers/WithdrawalController.ts
+++ modules/client/src/controllers/WithdrawalController.ts
@@ -11,7 +11,8 @@ export class WithdrawalController extends AbstractController {
   public async withdraw(params: WithdrawParameters): Promise<ChannelState> {
     const myFreeBalanceAddress = this.connext.freeBalanceAddress;
 
-    const { amount, recipient, assetId } = convert.Withdraw("bignumber", params);
+    const { amount, assetId } = convert.Withdraw("bignumber", params);
+    const { recipient } = params;
 
     const invalid = await this.validateInputs(amount, assetId, recipient);
     if (invalid) {
