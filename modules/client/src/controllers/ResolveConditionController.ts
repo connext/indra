d04bf0fb09c4f6618571dc15ae06e264988eb5dc
LayneHaber <layne.haber@gmail.com>
2019-07-30 22:03:12 -0700

    rename

---
+++ modules/client/src/controllers/ResolveConditionController.ts
@@ -0,0 +1,209 @@
+import {
+  ChannelState,
+  ConditionalTransferInitialStateBigNumber,
+  ConditionalTransferParameters,
+  ConditionalTransferResponse,
+  convert,
+  RegisteredAppDetails,
+  ResolveConditionParameters,
+  ResolveConditionResponse,
+  ResolveLinkedTransferParameters,
+  ResolveLinkedTransferResponse,
+  SupportedApplication,
+  SupportedApplications,
+  TransferCondition,
+  UnidirectionalLinkedTransferAppStage,
+  UnidirectionalLinkedTransferAppState,
+  UnidirectionalLinkedTransferAppStateBigNumber,
+} from "@connext/types";
+import { RejectProposalMessage } from "@counterfactual/node";
+import { Node as NodeTypes } from "@counterfactual/types";
+import { Zero } from "ethers/constants";
+import { BigNumber } from "ethers/utils";
+
+import { freeBalanceAddressFromXpub } from "../lib/utils";
+
+import { AbstractController } from "./AbstractController";
+import { createLinkedHash } from "./ConditionalTransferController";
+
+type ConditionResolvers = {
+  [index in TransferCondition]: (
+    params: ResolveConditionParameters,
+  ) => Promise<ResolveConditionResponse>;
+};
+export class ResolveConditionController extends AbstractController {
+  private appId: string;
+
+  private timeout: NodeJS.Timeout;
+
+  public resolve = async (
+    params: ResolveConditionParameters,
+  ): Promise<ResolveConditionResponse> => {
+    this.log.info(
+      `Conditional transfer called with parameters: ${JSON.stringify(params, null, 2)}`,
+    );
+
+    const res = await this.conditionResolvers[params.conditionType](params);
+    return res;
+  };
+
+  /////////////////////////////////
+  ////// PRIVATE METHODS
+  private resolveLinkedTransfer = async (
+    params: ResolveLinkedTransferParameters,
+  ): Promise<ResolveLinkedTransferResponse> => {
+    // convert and validate
+    const { paymentId, preImage, amount, assetId } = convert.ResolveLinkedTransfer(
+      "bignumber",
+      params,
+    );
+
+    const invalid = await this.validateLinked(paymentId, preImage, amount, assetId);
+    if (invalid) {
+      throw new Error(invalid);
+    }
+
+    // get appInfo
+    const appInfo = this.connext.getRegisteredAppDetails(
+      SupportedApplications.UnidirectionalLinkedTransferApp as SupportedApplication,
+    );
+
+    // generate hash
+    const linkedHash = createLinkedHash({ amount, assetId, paymentId, preImage });
+
+    // create initial state
+    const initialState: UnidirectionalLinkedTransferAppStateBigNumber = {
+      finalized: false,
+      linkedHash,
+      stage: UnidirectionalLinkedTransferAppStage.POST_FUND,
+      transfers: [
+        {
+          amount,
+          to: freeBalanceAddressFromXpub(this.connext.publicIdentifier),
+          // TODO: replace? fromExtendedKey(this.publicIdentifier).derivePath("0").address
+        },
+        {
+          amount: Zero,
+          to: freeBalanceAddressFromXpub(this.connext.nodePublicIdentifier),
+        },
+      ],
+      turnNum: Zero,
+    };
+
+    const appId = await this.conditionalTransferAppInstalled(
+      amount,
+      assetId,
+      initialState,
+      appInfo,
+    );
+    if (!appId) {
+      throw new Error(`App was not installed`);
+    }
+
+    return {
+      freeBalance: await this.connext.getFreeBalance(assetId),
+      paymentId,
+    };
+  };
+
+  // creates a promise that is resolved once the app is installed
+  // and rejected if the virtual application is rejected
+  private conditionalTransferAppInstalled = async (
+    initiatorDeposit: BigNumber,
+    assetId: string,
+    initialState: ConditionalTransferInitialStateBigNumber,
+    appInfo: RegisteredAppDetails,
+  ): Promise<string | undefined> => {
+    let boundResolve: (value?: any) => void;
+    let boundReject: (reason?: any) => void;
+
+    // note: intermediary is added in connext.ts as well
+    const {
+      actionEncoding,
+      appDefinitionAddress: appDefinition,
+      outcomeType,
+      stateEncoding,
+    } = appInfo;
+    const params: NodeTypes.ProposeInstallParams = {
+      abiEncodings: {
+        actionEncoding,
+        stateEncoding,
+      },
+      appDefinition,
+      initialState,
+      initiatorDeposit,
+      initiatorDepositTokenAddress: assetId,
+      outcomeType,
+      proposedToIdentifier: this.connext.nodePublicIdentifier,
+      responderDeposit: Zero,
+      responderDepositTokenAddress: assetId,
+      timeout: Zero,
+    };
+
+    const res = await this.connext.proposeInstallApp(params);
+    // set app instance id
+    this.appId = res.appInstanceId;
+
+    try {
+      await new Promise((res: () => any, rej: () => any): void => {
+        boundReject = this.rejectInstallTransfer.bind(null, rej);
+        boundResolve = this.resolveInstallTransfer.bind(null, res);
+        this.listener.on(NodeTypes.EventName.INSTALL, boundResolve);
+        this.listener.on(NodeTypes.EventName.REJECT_INSTALL, boundReject);
+      });
+      this.log.info(`App was installed successfully!: ${JSON.stringify(res)}`);
+      return res.appInstanceId;
+    } catch (e) {
+      this.log.error(`Error installing app: ${e.toString()}`);
+      return undefined;
+    } finally {
+      this.cleanupInstallListeners(boundResolve, boundReject);
+    }
+  };
+
+  // TODO: fix type of data
+  private resolveInstallTransfer = (res: (value?: unknown) => void, data: any): any => {
+    if (this.appId !== data.params.appInstanceId) {
+      this.log.info(
+        `Caught INSTALL event for different app ${JSON.stringify(data)}, expected ${this.appId}`,
+      );
+      return;
+    }
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    res(data);
+    return data;
+  };
+
+  // TODO: fix types of data
+  private rejectInstallTransfer = (
+    rej: (reason?: any) => void,
+    msg: RejectProposalMessage, // reject install??
+  ): any => {
+    // check app id
+    if (this.appId !== msg.data.appInstanceId) {
+      return;
+    }
+
+    return rej(`Install failed. Event data: ${JSON.stringify(msg, null, 2)}`);
+  };
+
+  private cleanupInstallListeners = (boundResolve: any, boundReject: any): void => {
+    this.listener.removeListener(NodeTypes.EventName.INSTALL, boundResolve);
+    this.listener.removeListener(NodeTypes.EventName.REJECT_INSTALL, boundReject);
+  };
+
+  private validateLinked = async (
+    paymentId: string,
+    preImage: string,
+    amount: BigNumber,
+    assetId: string,
+  ): Promise<string | undefined> => {
+    return undefined;
+  };
+
+  private conditionResolvers: ConditionResolvers = {
+    LINKED_TRANSFER: this.resolveLinkedTransfer,
+  };
+}
