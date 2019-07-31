d8271d3e09edefc27b628f9039556f250cdae349
Rahul <rksethuram9@gmail.com>
2019-07-31 23:19:50 +0200

    Move lots of stuff out of node factory provider

--- modules/node/src/node/node.provider.ts
+++ modules/node/src/node/node.provider.ts
@@ -1,110 +1,15 @@
 import { IMessagingService, MessagingServiceFactory } from "@connext/messaging";
-import {
-  CreateChannelMessage,
-  DepositConfirmationMessage,
-  InstallMessage,
-  InstallVirtualMessage,
-  MNEMONIC_PATH,
-  Node,
-  ProposeMessage,
-  ProposeVirtualMessage,
-  RejectInstallVirtualMessage,
-  UninstallMessage,
-  UninstallVirtualMessage,
-  UpdateStateMessage,
-  WithdrawMessage,
-} from "@counterfactual/node";
+import { MNEMONIC_PATH, Node } from "@counterfactual/node";
 import { PostgresServiceFactory } from "@counterfactual/postgresql-node-connector";
-import { Node as NodeTypes } from "@counterfactual/types";
 import { Provider } from "@nestjs/common";
 import { FactoryProvider } from "@nestjs/common/interfaces";
 import { ethers as eth } from "ethers";
 
 import { ConfigService } from "../config/config.service";
 import { MessagingProviderId, NodeProviderId, PostgresProviderId } from "../constants";
-import { CLogger, registerCfNodeListener } from "../util";
+import { CLogger } from "../util";
 
 const logger = new CLogger("NodeProvider");
-
-type CallbackStruct = {
-  [index in keyof typeof NodeTypes.EventName]: (data: any) => Promise<any> | void;
-};
-
-function logEvent(event: NodeTypes.EventName, res: NodeTypes.NodeMessage & { data: any }): void {
-  logger.log(
-    `${event} event fired from ${res && res.from ? res.from : null}, data: ${
-      res ? JSON.stringify(res.data) : "event did not have a result"
-    }`,
-  );
-}
-
-const defaultCallbacks: CallbackStruct = {
-  COUNTER_DEPOSIT_CONFIRMED: (data: DepositConfirmationMessage): void => {
-    logEvent(NodeTypes.EventName.COUNTER_DEPOSIT_CONFIRMED, data);
-  },
-  CREATE_CHANNEL: async (data: CreateChannelMessage): Promise<void> => {
-    logEvent(NodeTypes.EventName.CREATE_CHANNEL, data);
-  },
-  DEPOSIT_CONFIRMED: (data: DepositConfirmationMessage): void => {
-    logEvent(NodeTypes.EventName.DEPOSIT_CONFIRMED, data);
-  },
-  DEPOSIT_FAILED: (data: any): void => {
-    logEvent(NodeTypes.EventName.DEPOSIT_FAILED, data);
-  },
-  DEPOSIT_STARTED: (data: any): void => {
-    logEvent(NodeTypes.EventName.DEPOSIT_STARTED, data);
-  },
-  INSTALL: (data: InstallMessage): void => {
-    logEvent(NodeTypes.EventName.INSTALL, data);
-  },
-  // TODO: make cf return app instance id and app def?
-  INSTALL_VIRTUAL: (data: InstallVirtualMessage): void => {
-    logEvent(NodeTypes.EventName.INSTALL_VIRTUAL, data);
-  },
-  PROPOSE_INSTALL: (data: ProposeMessage): void => {
-    logEvent(NodeTypes.EventName.PROPOSE_INSTALL, data);
-  },
-  PROPOSE_INSTALL_VIRTUAL: (data: ProposeVirtualMessage): void => {
-    logEvent(NodeTypes.EventName.PROPOSE_INSTALL_VIRTUAL, data);
-  },
-  PROPOSE_STATE: (data: any): void => {
-    // TODO: need to validate all apps here as well?
-    logEvent(NodeTypes.EventName.PROPOSE_STATE, data);
-  },
-  PROTOCOL_MESSAGE_EVENT: (data: any): void => {
-    logEvent(NodeTypes.EventName.PROTOCOL_MESSAGE_EVENT, data);
-  },
-  REJECT_INSTALL: (data: any): void => {
-    logEvent(NodeTypes.EventName.REJECT_INSTALL, data);
-  },
-  REJECT_INSTALL_VIRTUAL: (data: RejectInstallVirtualMessage): void => {
-    logEvent(NodeTypes.EventName.REJECT_INSTALL_VIRTUAL, data);
-  },
-  REJECT_STATE: (data: any): void => {
-    logEvent(NodeTypes.EventName.REJECT_STATE, data);
-  },
-  UNINSTALL: (data: UninstallMessage): void => {
-    logEvent(NodeTypes.EventName.UNINSTALL, data);
-  },
-  UNINSTALL_VIRTUAL: (data: UninstallVirtualMessage): void => {
-    logEvent(NodeTypes.EventName.UNINSTALL_VIRTUAL, data);
-  },
-  UPDATE_STATE: (data: UpdateStateMessage): void => {
-    logEvent(NodeTypes.EventName.UPDATE_STATE, data);
-  },
-  WITHDRAW_EVENT: (data: any): void => {
-    logEvent(NodeTypes.EventName.WITHDRAW_EVENT, data);
-  },
-  WITHDRAWAL_CONFIRMED: (data: WithdrawMessage): void => {
-    logEvent(NodeTypes.EventName.WITHDRAWAL_CONFIRMED, data);
-  },
-  WITHDRAWAL_FAILED: (data: any): void => {
-    logEvent(NodeTypes.EventName.WITHDRAWAL_FAILED, data);
-  },
-  WITHDRAWAL_STARTED: (data: any): void => {
-    logEvent(NodeTypes.EventName.WITHDRAWAL_STARTED, data);
-  },
-};
 
 export const nodeProviderFactory: Provider = {
   inject: [ConfigService, MessagingProviderId, PostgresProviderId],
@@ -140,11 +45,6 @@ export const nodeProviderFactory: Provider = {
       `Free balance address ${JSON.stringify(
         eth.utils.HDNode.fromExtendedKey(node.publicIdentifier).derivePath("0").address,
       )}`,
-    );
-    Object.entries(defaultCallbacks).forEach(
-      ([event, callback]: [NodeTypes.EventName, () => any]): void => {
-        registerCfNodeListener(node, NodeTypes.EventName[event], callback, "DefaultListener");
-      },
     );
     return node;
   },
