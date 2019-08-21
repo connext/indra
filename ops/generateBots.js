fd5448b60c64d038bec7678bad9cbcbae41ee78c
LayneHaber <layne.haber@gmail.com>
2019-08-20 17:07:31 -0700

    add funding to bots script

--- ops/generateBots.js
+++ ops/generateBots.js
@@ -1,15 +1,62 @@
 const ethers = require("ethers");
 const fs = require('fs');
+const tokenAbi = require("human-standard-token-abi");
 
-function generateBots(number) {
+
+const generateBots = async (number, funderMnemonic, ethRpc, tokenAddress) => {
+  // some basic error handling
+  if (!number) {
+    throw new Error("No number of bots to generate provided");
+  }
+  if (!funderMnemonic) {
+    throw new Error("No funder mnemonic provided");
+  }
+  if (!ethRpc) {
+    throw new Error("No eth rpc url provided");
+  }
+  if (!tokenAddress) {
+    throw new Error("No token address provided");
+  }
+
+  console.log("Called with opts:")
+  console.log("   - number", number)
+  console.log("   - funderMnemonic", funderMnemonic)
+  console.log("   - ethRpc", ethRpc)
+  console.log("   - tokenAddress", tokenAddress)
+
+
+  // make the funder account and wallet
+  const ethGift = '3'
+  const tokenGift = '1000000'
+  const provider = new ethers.providers.JsonRpcProvider(ethRpc)
+  const funder = new ethers.Wallet.fromMnemonic(funderMnemonic).connect(provider)
+  const token = new ethers.Contract(tokenAddress, tokenAbi, funder)
+
+
   let obj = {}
   for (let i = 0; i < number; i++) {
-    const mnemonic = ethers.Wallet.createRandom().mnemonic;
-    const node = ethers.utils.HDNode.fromMnemonic(mnemonic);
+    const bot = ethers.Wallet.createRandom()
+    
+    // send eth
+    console.log(`\nSending ${ethGift} eth to ${bot.address}`)
+    const ethTx = await funder.sendTransaction({
+      to: bot.address,
+      value: ethers.utils.parseEther(ethGift)
+    })
+    await funder.provider.waitForTransaction(ethTx.hash)
+    console.log(`Transaction mined! Hash: ${ethTx.hash}`)
+
+    // send tokens
+    console.log(`Minting ${tokenGift} tokens for ${bot.address}`)
+    const tokenTx = await token.mint(bot.address, parseEther(tokenGift))
+    await funder.provider.waitForTransaction(tokenTx.hash)
+    console.log(`Transaction mined! Hash: ${tokenTx.hash}`)
+
+    const node = ethers.utils.HDNode.fromMnemonic(bot.mnemonic);
     const xpub = node.neuter().extendedKey;
-    obj[i+1] = { mnemonic, xpub };
+    obj[i+1] = { mnemonic: bot.mnemonic, xpub };
   }
   fs.writeFileSync("bots.json", JSON.stringify(obj, null, 2));
 }
 
-generateBots(process.argv[2])
+generateBots(process.argv[2], process.argv[3], process.argv[4], process.argv[5]).then(() => console.log("Completed"))
