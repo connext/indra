e2fad92ae14490e879eaa4dcd04f3ee8a5da261c
LayneHaber <layne.haber@gmail.com>
2019-08-22 17:56:10 -0700

    fix for building

--- modules/client/src/controllers/WithdrawalController.ts
+++ modules/client/src/controllers/WithdrawalController.ts
@@ -32,7 +32,7 @@ export class WithdrawalController extends AbstractController {
     try {
       if (!userSubmitted) {
         this.log.info(`Calling ${CFModuleTypes.RpcMethodName.WITHDRAW_COMMITMENT}`);
-        const withdrawResponse = await this.connext.cfWithdrawCommitment(
+        const withdrawResponse = await this.connext.providerWithdrawCommitment(
           amount,
           assetId,
           recipient,
