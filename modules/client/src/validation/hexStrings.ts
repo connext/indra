53ef60e75de475d67cf3c7e72a52dce4f97e952e
LayneHaber <layne.haber@gmail.com>
2019-08-23 18:43:32 -0700

    validation on hex strings

---
+++ modules/client/src/validation/hexStrings.ts
@@ -0,0 +1,14 @@
+import { utils } from "ethers";
+import { hexDataLength } from "ethers/utils";
+
+export function invalid32ByteHexString(value: any): string | undefined {
+  if (typeof value !== "string" || !utils.isHexString(value)) {
+    return `Value ${value.toString()} is not a valid hex string`;
+  }
+  // check that its 32 bytes
+  if (hexDataLength(value) !== 32) {
+    return `Value ${value.toString()} is not a valid 32 byte hex string`;
+  }
+
+  return undefined;
+}
