9f338e8dd19e4121655efb118ffbb8fdfb4097be
Bo Henderson <bohende@gmail.com>
2019-08-29 17:15:43 +0200

    node: run db migrations on startup in prod-mode

--- modules/node/ops/entry.sh
+++ modules/node/ops/entry.sh
@@ -39,25 +39,6 @@ wait_for "database" "$INDRA_PG_HOST:$INDRA_PG_PORT"
 wait_for "nats" "$INDRA_NATS_SERVERS"
 wait_for "ethprovider" "$INDRA_ETH_RPC_URL"
 
-if [[ "$NODE_ENV" == "production" ]]
-then
-  ls
-  # Create a .env file for typeorm to use
-  cat -> .env <<-EOF
-	TYPEORM_CONNECTION=postgres
-	TYPEORM_DATABASE=$INDRA_PG_DATABASE
-	TYPEORM_ENTITIES=src/**/*.entity.ts
-	TYPEORM_HOST=$INDRA_PG_HOST
-	TYPEORM_MIGRATIONS=migrations/*.ts
-	TYPEORM_MIGRATIONS_DIR=migrations
-	TYPEORM_PASSWORD=`cat $INDRA_PG_PASSWORD_FILE`
-	TYPEORM_PORT=$INDRA_PG_PORT
-	TYPEORM_SYNCHRONIZE=false
-	TYPEORM_USERNAME=$INDRA_PG_USERNAME
-	EOF
-  ./node_modules/.bin/ts-node ./node_modules/.bin/typeorm migration:run
-fi
-
 if [[ "$NODE_ENV" == "development" ]]
 then
   exec ./node_modules/.bin/nodemon \
--- modules/node/src/database/database.service.ts
+++ modules/node/src/database/database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 
+import { createTables1565695944514 } from "../../migrations/1565695944514-createTables";
 import { AppRegistry } from "../appRegistry/appRegistry.entity";
 import { Channel } from "../channel/channel.entity";
 import { ConfigService } from "../config/config.service";
@@ -18,7 +19,9 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       ...this.config.getPostgresConfig(),
       entities: [...entities, ...viewEntites],
       logging: ["error"],
-      synchronize: true, // TODO: this.config.isDevMode(),
+      migrations: [createTables1565695944514],
+      migrationsRun: !this.config.isDevMode(),
+      synchronize: this.config.isDevMode(),
       type: "postgres",
     };
   }
