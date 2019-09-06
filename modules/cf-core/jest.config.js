9268c3f523b91a63b0c0d383eba01c6e33f8381b
Bo Henderson <bohende@gmail.com>
2019-09-06 18:44:09 +0200

    tentative fix for .d.ts importing issue

--- modules/cf-core/jest.config.js
+++ modules/cf-core/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   "coveragePathIgnorePatterns": [ "test" ],
   "coverageThreshold": { "global": { "branches": 90, "functions": 90, "lines": 90, "statements": 90 } },
   "extraGlobals": ["Math"],
+  "globals": { "ts-jest": { "isolatedModules": true }, },
   "globalSetup": "<rootDir>/test/global-setup.jest.ts",
   "globalTeardown": "<rootDir>/test/global-teardown.jest.ts",
   "moduleFileExtensions": [ "ts", "js", "json" ],
