5476e54e6d820f246ac86c2df109fa39f0423823
Rahul <rksethuram9@gmail.com>
2019-09-16 15:02:45 +0200

    Add redis

---
+++ modules/node/src/redis/redis.provider.ts
@@ -0,0 +1,51 @@
+import { FactoryProvider } from "@nestjs/common/interfaces";
+import Redis from "ioredis";
+import Redlock from "redlock";
+
+import { ConfigService } from "../config/config.service";
+import { RedisProviderId, RedlockProviderId } from "../constants";
+
+export const redisClientFactory: FactoryProvider = {
+  inject: [ConfigService],
+  provide: RedisProviderId,
+  useFactory: (config: ConfigService): Redis.Redis => {
+    const redisClient = new Redis(config.getRedisUrl(), {
+      retryStrategy: (times: number): number => {
+        console.log("Lost connection to redis. Retrying to connect...");
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+      },
+    });
+    return redisClient;
+  },
+};
+
+export const redlockClientFactory: FactoryProvider = {
+  inject: [RedisProviderId],
+  provide: RedlockProviderId,
+  useFactory: (redis: Redis.Redis): Redlock => {
+    const redlockClient = new Redlock([redis], {
+      // the expected clock drift; for more details
+      // see http://redis.io/topics/distlock
+      driftFactor: 0.01, // time in ms
+
+      // the max number of times Redlock will attempt
+      // to lock a resource before erroring
+      retryCount: 100,
+
+      // the time in ms between attempts
+      retryDelay: 100, // time in ms
+
+      // the max time in ms randomly added to retries
+      // to improve performance under high contention
+      // see https://www.awsarchitectureblog.com/2015/03/backoff.html
+      retryJitter: 1000, // time in ms
+    });
+
+    redlockClient.on("clientError", (err: any) => {
+      console.error("A redis error has occurred:", err);
+    });
+
+    return redlockClient;
+  },
+};
