781428d65b0b6799897c87fd79d13359e87f1474
Bo Henderson <bohende@gmail.com>
2019-09-17 13:21:46 +0100

    fix listener module imports

--- modules/node/src/listener/listener.module.ts
+++ modules/node/src/listener/listener.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppRegistryModule } from "../appRegistry/appRegistry.module";
 import { CFCoreModule } from "../cfCore/cfCore.module";
 import { ChannelModule } from "../channel/channel.module";
 import { MessagingModule } from "../messaging/messaging.module";
-import { TransferModule } from "../transfer/transfer.module";
 import { LinkedTransferRepository } from "../transfer/transfer.repository";
 
 import ListenerService from "./listener.service";
@@ -17,7 +17,7 @@ import ListenerService from "./listener.service";
     CFCoreModule,
     AppRegistryModule,
     ChannelModule,
-    TransferModule,
+    MessagingModule,
     TypeOrmModule.forFeature([LinkedTransferRepository]),
     MessagingModule,
   ],
