73855b4e1bd0afb2ff20d6a075c52d4501ddbf9a
Rahul <rksethuram9@gmail.com>
2019-09-30 21:08:50 +0200

    Add migration

---
+++ modules/node/migrations/1569862328684-add-recipient-to-links.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddRecipientToLinks1569862328684 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+      ALTER TABLE linked_transfer
+      ADD COLUMN "recipientPublicIdentifier" text
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+      ALTER TABLE linked_transfer
+      DROP COLUMN "recipientPublicIdentifier"
+    `);
+  }
+}
