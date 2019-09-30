327808f5f2d1457af24d795821396d130c58987b
Bo Henderson <bohende@gmail.com>
2019-09-30 22:32:21 +0530

    ops & proxy bugfixes to get hasura working

--- ops/start-dev.sh
+++ ops/start-dev.sh
@@ -31,23 +31,24 @@ eth_contract_addresses="`cat address-book.json | tr -d ' \n\r'`"
 eth_mnemonic="candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
 
 # database connection settings
-postgres_db="$project"
-postgres_password_file="/run/secrets/${project}_database_dev"
-postgres_host="database"
-postgres_port="5432"
-postgres_user="$project"
+pg_db="$project"
+pg_password_file="/run/secrets/${project}_database_dev"
+pg_host="database"
+pg_port="5432"
+pg_user="$project"
 
 # docker images
 builder_image="${project}_builder"
+daicard_devserver_image="$builder_image"
 database_image="postgres:9-alpine"
 ethprovider_image="trufflesuite/ganache-cli:v6.4.5"
-node_image="$builder_image"
+hasura_image="hasura/graphql-engine:latest"
 nats_image="nats:2.0.0-linux"
+node_image="$builder_image"
 proxy_image="${project}_proxy:dev"
-daicard_devserver_image="$builder_image"
-relay_image="${project}_relay"
 redis_image=redis:5-alpine
 redis_url="redis://redis:6379"
+relay_image="${project}_relay"
 
 node_port=8080
 nats_port=4222
@@ -62,8 +63,9 @@ function pull_if_unavailable {
   fi
 }
 pull_if_unavailable "$database_image"
-pull_if_unavailable "$nats_image"
 pull_if_unavailable "$ethprovider_image"
+pull_if_unavailable "$hasura_image"
+pull_if_unavailable "$nats_image"
 
 # Initialize random new secrets
 function new_secret {
@@ -112,7 +114,7 @@ services:
       DAICARD_URL: http://daicard:3000
       ETH_RPC_URL: $eth_rpc_url
       MESSAGING_URL: http://relay:4223
-      HASURA_URL: http://hasura:8083
+      HASURA_URL: http://hasura:8080
       MODE: dev
     networks:
       - $project
@@ -151,11 +153,11 @@ services:
       INDRA_NATS_CLUSTER_ID:
       INDRA_NATS_SERVERS: nats://nats:$nats_port
       INDRA_NATS_TOKEN:
-      INDRA_PG_DATABASE: $postgres_db
-      INDRA_PG_HOST: $postgres_host
-      INDRA_PG_PASSWORD_FILE: $postgres_password_file
-      INDRA_PG_PORT: $postgres_port
-      INDRA_PG_USERNAME: $postgres_user
+      INDRA_PG_DATABASE: $pg_db
+      INDRA_PG_HOST: $pg_host
+      INDRA_PG_PASSWORD_FILE: $pg_password_file
+      INDRA_PG_PORT: $pg_port
+      INDRA_PG_USERNAME: $pg_user
       INDRA_PORT: $node_port
       INDRA_REDIS_URL: $redis_url
       NODE_ENV: development
@@ -184,12 +186,12 @@ services:
       mode: global
     environment:
       POSTGRES_DB: $project
-      POSTGRES_PASSWORD_FILE: $postgres_password_file
+      POSTGRES_PASSWORD_FILE: $pg_password_file
       POSTGRES_USER: $project
     networks:
       - $project
     ports:
-      - "5432:5432"
+      - "$pg_port:$pg_port"
     secrets:
       - ${project}_database_dev
     volumes:
@@ -204,14 +206,15 @@ services:
       - "$nats_port:$nats_port"
 
   hasura:
-    image: hasura/graphql-engine
+    image: $hasura_image
     environment:
-      HASURA_GRAPHQL_DATABASE_URL: "postgres://$postgres_user:$postgres_user@$postgres_host:5432/$project"
+      HASURA_GRAPHQL_DATABASE_URL: "postgres://$pg_user:$pg_user@$pg_host:$pg_port/$project"
       HASURA_GRAPHQL_ENABLE_CONSOLE: "true"
     networks:
       - $project
     ports:
       - "8083:8080"
+
   redis:
     image: $redis_image
     networks:
