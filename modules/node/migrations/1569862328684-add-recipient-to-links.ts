48f886d96ec973ab03b5ff16d8735ddd3740f8d4
Rahul <rksethuram9@gmail.com>
2019-10-01 16:15:26 +0200

    Add preimage column in DB

--- modules/node/migrations/1569862328684-add-recipient-to-links.ts
+++ modules/node/migrations/1569862328684-add-recipient-to-links.ts
@@ -4,14 +4,16 @@ export class AddRecipientToLinks1569862328684 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`
       ALTER TABLE linked_transfer
-      ADD COLUMN "recipientPublicIdentifier" text
+        ADD COLUMN "recipientPublicIdentifier" text,
+        ADD COLUMN "encryptedPreImage" text
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`
       ALTER TABLE linked_transfer
-      DROP COLUMN "recipientPublicIdentifier"
+        DROP COLUMN "recipientPublicIdentifier",
+        DROP COLUMN "encryptedPreImage"
     `);
   }
 }
