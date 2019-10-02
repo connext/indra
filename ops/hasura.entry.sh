f29b546607cee4f1280c4210668398046f4b8744
Hunter Hillman <hthillman@Hunters-MacBook-Pro.local>
2019-10-02 13:04:11 -0400

    fix capitalization

--- ops/hasura.entry.sh
+++ ops/hasura.entry.sh
@@ -1,2 +1,2 @@
-export HASURA_GRAPHQL_DATABASE_URL="postgres://$PG_USER:`cat $PG_PASSWORD_FILE`@$PG_HOST:$PG_PORT/$PG_DATABASE"
+export HASURA_GRAPHQL_DATABASE_URL="postgres://$pg_user:`cat $pg_password_file`@$pg_host:$pg_port/$pg_database"
 exec graphql-engine serve
