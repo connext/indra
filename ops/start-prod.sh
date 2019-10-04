a4963b7f35a91da0985e8f4c274c4e285a2d3cec
Chris Buckland <cpbuckland88@gmail.com>
2019-10-04 18:28:11 +0200

    Updated indentation in the prod pisa config

--- ops/start-prod.sh
+++ ops/start-prod.sh
@@ -120,23 +120,23 @@ then
   pisa_service="
   pisa:
     image: $pisa_image
-      ports:
-        - "5487:3000"
-      entrypoint: >-
-        node ./build/src/startUp.js 
-        --json-rpc-url $INDRA_ETH_PROVIDER 
-        --host-name 0.0.0.0 
-        --host-port 3000 
-        --responder-key 0x388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418 
-        --receipt-key 0x388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418
-        --db-dir ./db
-        --loglevel info
-        --pisa-contract-address 0x0000000000000000000000000000000000000000 
-        --instance-name connext-test
-        --rate-limit-user-window-ms 1000
-        --rate-limit-user-max 100
-        --rate-limit-global-window-ms 1000
-        --rate-limit-global-max 100
+    ports:
+      - 5487:3000
+    entrypoint: >-
+      node ./build/src/startUp.js 
+      --json-rpc-url $INDRA_ETH_PROVIDER 
+      --host-name 0.0.0.0 
+      --host-port 3000 
+      --responder-key 0x388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418 
+      --receipt-key 0x388c684f0ba1ef5017716adb5d21a053ea8e90277d0868337519f97bede61418
+      --db-dir ./db
+      --loglevel info
+      --pisa-contract-address 0x0000000000000000000000000000000000000000 
+      --instance-name connext-test
+      --rate-limit-user-window-ms 1000
+      --rate-limit-user-max 100
+      --rate-limit-global-window-ms 1000
+      --rate-limit-global-max 100
   "
   INDRA_ETH_PROVIDER="http://ethprovider:8545"
 else echo "Eth network \"$chainId\" is not supported for $INDRA_MODE-mode deployments" && exit 1
