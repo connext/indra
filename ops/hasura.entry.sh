51ee4d23beb218a04bac6fa1d721c6f3babffe03
Bo Henderson <bohende@gmail.com>
2019-10-07 12:31:29 +0900

    add db url log to hasura startup

--- ops/hasura.entry.sh
+++ ops/hasura.entry.sh
@@ -1,2 +1,4 @@
 export HASURA_GRAPHQL_DATABASE_URL="postgres://$pg_user:`cat $pg_password_file`@$pg_host:$pg_port/$pg_database"
+env
+echo;echo "Using DB URL: $HASURA_GRAPHQL_DATABASE_URL";echo;
 exec graphql-engine serve
