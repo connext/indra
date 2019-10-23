c32b1427ff6cc6319bb5b2b5e845d8b1b0d15082
LayneHaber <layne.haber@gmail.com>
2019-10-23 16:31:49 -0700

    error handling + clean up

--- modules/daicard/src/utils/clientWalletConnectMapping.js
+++ modules/daicard/src/utils/clientWalletConnectMapping.js
@@ -5,39 +5,16 @@ export let walletConnector = null;
 
 export async function initWalletConnect(uri, client) {
   walletConnector = new WalletConnectBrowser({ uri });
-  console.log("INITING");
 
   walletConnector.on("session_request", (error, payload) => {
-    console.log("Received session request");
     if (error) {
       throw error;
     }
     displaySessionApproval(payload);
-
-    // Handle Session Request
-
-    /* payload:
-        {
-            id: 1,
-            jsonrpc: '2.0'.
-            method: 'session_request',
-            params: [{
-            peerId: '15d8b6a3-15bd-493e-9358-111e3a4e6ee4',
-            peerMeta: {
-                name: "WalletConnect Example",
-                description: "Try out WalletConnect v1.0.0-beta",
-                icons: ["https://example.walletconnect.org/favicon.ico"],
-                url: "https://example.walletconnect.org",
-                ssl: true
-            }
-            }]
-        }
-        */
   });
 
   // Subscribe to call requests
   walletConnector.on("call_request", async (error, payload) => {
-    console.log(`Received call request: ${JSON.stringify({ error, payload }, null, 2)}`);
     if (error) {
       throw error;
     }
@@ -47,19 +24,6 @@ export async function initWalletConnect(uri, client) {
     } else {
       walletConnector.rejectRequest({ id: payload.id });
     }
-    // Handle Call Request
-
-    /* payload:
-        {
-            id: 1,
-            jsonrpc: '2.0'.
-            method: 'eth_sign',
-            params: [
-            "0xbc28ea04101f03ea7a94c1379bc3ab32e65e62d3",
-            "My email is john@doe.com - 1537836206101"
-            ]
-        }
-        */
   });
 
   walletConnector.on("disconnect", (error, payload) => {
@@ -72,10 +36,8 @@ export async function initWalletConnect(uri, client) {
 }
 
 export function displaySessionApproval(payload) {
-  console.log(`displaySessionApproval()`)
   walletConnector.approveSession({ accounts: [], chainId: 4447 });
   //TODO: proc modal that approves the walletconnection from the wallet
-  console.log(`called walletConnector.approveSession()`)
 }
 
 function verifyFields(params, keys) {
@@ -88,97 +50,114 @@ function verifyFields(params, keys) {
 
   const naStr = keys.filter(k => !!!params[k]);
   if (naStr.length !== 0) {
-    throw new Error(`[verifyFields] Params missing needed keys. Params: ${JSON.stringify(params, null, 2)}, keys: ${JSON.stringify(keys, null, 2)}`)
+    throw new Error(`[verifyFields] Params missing needed keys. Params: ${prettyPrint(params)}, keys: ${prettyPring(keys)}`)
   }
   return;
 }
 
+function prettyPrint(obj) {
+  return JSON.stringify(obj, null, 2)
+}
+
 async function mapPayloadToClient(payload, channel) {
+  const { params, id, method } = payload;
+  if (!params || typeof params !== "object") {
+    throw new Error(`Invalid payload params. Payload: ${prettyPrint(payload)}`)
+  }
+
+  if (!id) {
+    throw new Error(`Invalid payload id. Payload: ${prettyPrint(payload)}`)
+  }
+
+  if (!method || typeof method !== "string") {
+    throw new Error(`Invalid payload method. Payload: ${prettyPrint(payload)}`)
+  }
+
   let result;
   try {
-    switch (payload.method) {
+    switch (method) {
       case "chan_store_set":
-        verifyFields(payload.params, ["pairs"])
-        const { pairs } = payload.params
+        verifyFields(params, ["pairs"])
+        const { pairs } = params
         result = await channel.channelRouter.set(pairs)
         break;
 
       case "chan_store_get":
-        verifyFields(payload.params, ["path"])
-        const { path } = payload.params
+        verifyFields(params, ["path"])
+        const { path } = params
         result = await channel.channelRouter.get(path)
         break;
 
       case "chan_node_auth":
-        verifyFields(payload.params, ["message"])
-        const { message } = payload.params
+        verifyFields(params, ["message"])
+        const { message } = params
         result = await channel.channelRouter.signMessage(message)
         break;
 
       case "chan_config":
-        result = await channel.channelProviderConfig(payload.params)
+        result = await channel.channelProviderConfig(params)
         break;
 
       case NodeTypes.RpcMethodName.DEPOSIT:
-        result = await channel.providerDeposit(payload.params);
+        result = await channel.providerDeposit(params);
         break;
       case NodeTypes.RpcMethodName.GET_STATE:
-        result = await channel.getState(payload.params);
+        result = await channel.getState(params);
         break;
       case NodeTypes.RpcMethodName.GET_APP_INSTANCES:
-        result = await channel.getAppInstances(payload.params);
+        result = await channel.getAppInstances(params);
         break;
       case NodeTypes.RpcMethodName.GET_FREE_BALANCE_STATE:
-        verifyFields(payload.params, ["tokenAddress", "multisigAddress"])
-        const { tokenAddress } = payload.params;
+        verifyFields(params, ["tokenAddress", "multisigAddress"])
+        const { tokenAddress } = params;
         result = await channel.getFreeBalance(tokenAddress);
         break;
 
       case NodeTypes.RpcMethodName.GET_PROPOSED_APP_INSTANCES:
-        result = await channel.getProposedAppInstances(payload.params);
+        result = await channel.getProposedAppInstances(params);
         break;
       case NodeTypes.RpcMethodName.GET_APP_INSTANCE_DETAILS:
-        result = await channel.getAppInstanceDetails(payload.params);
+        result = await channel.getAppInstanceDetails(params);
         break;
       case NodeTypes.RpcMethodName.TAKE_ACTION:
-        result = await channel.takeAction(payload.params);
+        result = await channel.takeAction(params);
         break;
       case NodeTypes.RpcMethodName.UPDATE_STATE:
-        result = await channel.updateState(payload.params);
+        result = await channel.updateState(params);
         break;
       case NodeTypes.RpcMethodName.PROPOSE_INSTALL_VIRTUAL:
-        result = await channel.proposeInstallVirtualApp(payload.params);
+        result = await channel.proposeInstallVirtualApp(params);
         break;
       case NodeTypes.RpcMethodName.PROPOSE_INSTALL:
-        result = await channel.proposeInstallApp(payload.params);
+        result = await channel.proposeInstallApp(params);
         break;
       case NodeTypes.RpcMethodName.INSTALL_VIRTUAL:
-        result = await channel.installVirtualApp(payload.params);
+        result = await channel.installVirtualApp(params);
         break;
       case NodeTypes.RpcMethodName.INSTALL:
-        result = await channel.installApp(payload.params);
+        result = await channel.installApp(params);
         break;
       case NodeTypes.RpcMethodName.UNINSTALL:
-        result = await channel.uninstallApp(payload.params);
+        result = await channel.uninstallApp(params);
         break;
       case NodeTypes.RpcMethodName.UNINSTALL_VIRTUAL:
-        result = await channel.uninstallVirtualApp(payload.params);
+        result = await channel.uninstallVirtualApp(params);
         break;
       case NodeTypes.RpcMethodName.REJECT_INSTALL:
-        result = await channel.rejectInstallApp(payload.params);
+        result = await channel.rejectInstallApp(params);
         break;
       case NodeTypes.RpcMethodName.WITHDRAW:
-        result = await channel.providerWithdraw(payload.params);
+        result = await channel.providerWithdraw(params);
         break;
       case NodeTypes.RpcMethodName.WITHDRAW_COMMITMENT:
-        result = await channel.withdrawCommitment(payload.params);
+        result = await channel.withdrawCommitment(params);
         break;
       default:
-        console.log(`WALLET CONNECT MAPPING ERROR: unknown method: ${payload.method}`);
+        console.error(`Wallet connect mapping error, unknown method. Payload: ${JSON.stringify(payload, null, 2)}`);
         break;
     }
   } catch (e) {
-    console.log("AHIOFEJOIFEJIOFEAJ: ", e);
+    console.error(`Wallet connect error: ${JSON.stringify(e, null, 2)}`);
   }
-  walletConnector.approveRequest({ id: payload.id, result });
+  walletConnector.approveRequest({ id, result });
 }
