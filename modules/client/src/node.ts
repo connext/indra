6e168cf463dde806d5d37dbaae1c21f61490ecaa
Rahul <rksethuram9@gmail.com>
2019-10-26 10:58:41 +0200

    Fix name

--- modules/client/src/node.ts
+++ modules/client/src/node.ts
@@ -236,8 +236,7 @@ export class NodeApiClient implements INodeApiClient {
     await this.messaging.unsubscribe(`swap-rate.${from}.${to}`);
   }
 
-  // TODO: need to add auth for this!
-  public async restoreStates(publicIdentifier: string): Promise<{ path: string; value: object }[]> {
+  public async getStateForRestore(publicIdentifier: string): Promise<{ data: any }> {
     return this.send(`channel.restore-states.${publicIdentifier}`);
   }
 
