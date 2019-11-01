8401c0cbf4633268cb4607b79549f6e76042cc87
LayneHaber <layne.haber@gmail.com>
2019-10-31 21:24:45 -0700

    add useful utils

--- modules/node/src/util/cfCore.ts
+++ modules/node/src/util/cfCore.ts
@@ -1,3 +1,5 @@
+import MinimumViableMultisig from "@counterfactual/cf-funding-protocol-contracts/expected-build-artifacts/MinimumViableMultisig.json";
+import Proxy from "@counterfactual/cf-funding-protocol-contracts/expected-build-artifacts/Proxy.json";
 import { ethers as eth } from "ethers";
 export {
   CreateChannelMessage,
@@ -21,4 +23,54 @@ export {
 
 export function freeBalanceAddressFromXpub(xpub: string): string {
   return eth.utils.getAddress(eth.utils.HDNode.fromExtendedKey(xpub).derivePath("0").address);
+}
+
+export function xkeyKthHDNode(xkey: string, k: number): eth.utils.HDNode.HDNode {
+  return eth.utils.HDNode.fromExtendedKey(xkey).derivePath(`${k}`);
+}
+
+export function xkeyKthAddress(xkey: string, k: number): string {
+  return eth.utils.computeAddress(xkeyKthHDNode(xkey, k).publicKey);
+}
+
+export function sortAddresses(addrs: string[]): string[] {
+  return addrs.sort((a, b) => (parseInt(a, 16) < parseInt(b, 16) ? -1 : 1));
+}
+
+export function xkeysToSortedKthAddresses(xkeys: string[], k: number): string[] {
+  return sortAddresses(xkeys.map(xkey => xkeyKthAddress(xkey, k)));
+}
+
+// TODO: this should be imported from the counterfactual utils
+export function getMultisigAddressfromXpubs(
+  owners: string[],
+  proxyFactoryAddress: string,
+  minimumViableMultisigAddress: string,
+): string {
+  return eth.utils.getAddress(
+    eth.utils
+      .solidityKeccak256(
+        ["bytes1", "address", "uint256", "bytes32"],
+        [
+          "0xff",
+          proxyFactoryAddress,
+          eth.utils.solidityKeccak256(
+            ["bytes32", "uint256"],
+            [
+              eth.utils.keccak256(
+                new eth.utils.Interface(MinimumViableMultisig.abi).functions.setup.encode([
+                  xkeysToSortedKthAddresses(owners, 0),
+                ]),
+              ),
+              0,
+            ],
+          ),
+          eth.utils.solidityKeccak256(
+            ["bytes", "uint256"],
+            [`0x${Proxy.evm.bytecode.object}`, minimumViableMultisigAddress],
+          ),
+        ],
+      )
+      .slice(-40),
+  );
 }
