88e0ee8bf4df607d4d8a229b9d224bf40070ef34
LayneHaber <layne.haber@gmail.com>
2019-10-31 21:22:46 -0700

    add find all incorrect channels admin endpoint

--- modules/node/src/admin/admin.service.ts
+++ modules/node/src/admin/admin.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 import { CFCoreService } from "../cfCore/cfCore.service";
 import { ChannelService } from "../channel/channel.service";
-import { CLogger, stringify } from "../util";
+import { CLogger } from "../util";
 
 const logger = new CLogger("AdminService");
 
@@ -29,7 +29,7 @@ export class AdminService {
    */
   async getNoFreeBalance(): Promise<{ multisigAddress: string; userXpub: string; error: any }[]> {
     // get all available channels, meaning theyre deployed
-    const channels = await this.channelService.findAll(true);
+    const channels = await this.channelService.findAll();
     const corrupted = [];
     for (const channel of channels) {
       // try to get the free balance of eth
@@ -46,5 +46,64 @@ export class AdminService {
       }
     }
     return corrupted;
+  }
+
+  /**
+   * October 31, 2019
+   *
+   * For some as of yet unidentified reason, the multisig address for some
+   * channels has changed. This results in 2 channel entries for affected
+   * channels. The first was generated around channel creation in from the
+   * event data. The second was used to store any subsequent updates.
+   *
+   * The store entry for `channel/{multisigAddr1}` will have a free balance
+   * instance, while the store entry for `channel/{multisigAddr2}` will have
+   * all subsequent information regarding channel updates (i.e proposed apps).
+   *
+   * While this may not be a problem for the functioning of *all* channels,
+   * it will be a problem for *any* channel that is restored from the hub.
+   * This is because the channels restored by the hub search for the outdated
+   * channel state stored under the path `channel/{multisigAddr1}`. It is likely
+   * that other, older channels are also affected by this bug.
+   *
+   * This method will pull out any channel that has a multisigAddress in the
+   * channel table, that is different from the expected multisigAddress.
+   *
+   * Related to bug described in `getNoFreeBalance`.
+   */
+  async getIncorrectMultisigAddresses(): Promise<
+    {
+      oldMultisigAddress: string;
+      expectedMultisigAddress: string;
+      userXpub: string;
+      channelId: number;
+    }[]
+  > {
+    const channels = await this.channelService.findAll();
+    const ans = [];
+    for (const channel of channels) {
+      const {
+        multisigAddress: oldMultisigAddress,
+        userPublicIdentifier: userXpub,
+        id: channelId,
+      } = channel;
+      // calculate the expected multsig address
+      const expectedMultisigAddress = await this.cfCoreService.getExpectedMultisigAddressFromUserXpub(
+        userXpub,
+      );
+
+      // if it matches, return
+      if (expectedMultisigAddress === expectedMultisigAddress) {
+        continue;
+      }
+
+      ans.push({
+        channelId,
+        expectedMultisigAddress,
+        oldMultisigAddress,
+        userXpub,
+      });
+    }
+    return ans;
   }
 }
