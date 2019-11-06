bc19955c95e17bac43e7ed3d505be308fd53d8e5
Rahul <rksethuram9@gmail.com>
2019-11-05 17:01:07 -0800

    Split up DB entries

--- modules/node/src/cfCore/cfCore.repository.ts
+++ modules/node/src/cfCore/cfCore.repository.ts
@@ -3,10 +3,15 @@ import { EntityRepository, Like, Repository } from "typeorm";
 import { CLogger, stringify } from "../util";
 
 import { CFCoreRecord } from "./cfCore.entity";
+import { LATEST_CF_STORE_VERSION } from "./cfCore.provider";
 
 type StringKeyValue = { [path: string]: StringKeyValue };
 
 const logger = new CLogger("CFCoreRecordRepository");
+
+const COMMITMENT_PATH = "commitments";
+const VERSION_PATH = "version";
+const WITHDRAWAL_PATH = "withdrawals";
 
 @EntityRepository(CFCoreRecord)
 export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
@@ -15,6 +20,95 @@ export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
   }
 
   async get(path: string): Promise<StringKeyValue | string | undefined> {
+    logger.debug(`Getting path from store: ${path}`);
+
+    // get all the channel records
+    const res = await this.find({ path: Like(`%channel%/version/${LATEST_CF_STORE_VERSION}`) });
+
+    // put them together into a big blob
+    const stateChannelsMap = res.reduce((stateChannels, channelRecord) => {
+      const multisigAddress = channelRecord.path.split("/").pop();
+      stateChannels[multisigAddress] = channelRecord.value;
+      return stateChannels;
+    }, {});
+
+    const commitments = await this.findOne(
+      `${path}/${COMMITMENT_PATH}/version/${LATEST_CF_STORE_VERSION}`,
+    );
+    const version = await this.findOne(
+      `${path}/${VERSION_PATH}/version/${LATEST_CF_STORE_VERSION}`,
+    );
+    const withdrawals = await this.findOne(
+      `${path}/${WITHDRAWAL_PATH}/version/${LATEST_CF_STORE_VERSION}`,
+    );
+
+    // this corresponds to version 1 of the store spec
+    return {
+      commitments: commitments || {},
+      stateChannelsMap: stateChannelsMap || {},
+      version: LATEST_CF_STORE_VERSION,
+      withdrawals: withdrawals || {},
+    } as any;
+  }
+
+  async set(pairs: { path: string; value: any }[]): Promise<void> {
+    for (const pair of pairs) {
+      logger.debug(`Saving record for path: ${pair.path}`);
+      // Wrapping the value into an object is necessary for Postgres bc the JSON column breaks
+      // if you use anything other than JSON (i.e. a raw string).
+      // In some cases, the cf core code is inserting strings as values instead of objects :(
+      const { stateChannelsMap } = pair.value;
+
+      Object.entries(stateChannelsMap);
+
+      const savePromises = Object.entries(stateChannelsMap).map(
+        async ([multisigAddress, channelData]: [string, StringKeyValue]): Promise<CFCoreRecord> => {
+          const newPath = `${pair.path}/channel/${multisigAddress}/version/${LATEST_CF_STORE_VERSION}`;
+          let record = await this.findOne({ where: { path: newPath } });
+          if (record) {
+            record.value = channelData;
+          } else {
+            record = { path: newPath, value: channelData } as CFCoreRecord;
+          }
+          return this.save(record);
+        },
+      );
+
+      const commitmentPath = `${pair.path}/${COMMITMENT_PATH}/version/${LATEST_CF_STORE_VERSION}`;
+      let commitments = await this.findOne({
+        where: { path: commitmentPath },
+      });
+      if (commitments) {
+        commitments.value = pair.value.commitments;
+      } else {
+        commitments = {
+          path: commitmentPath,
+          value: pair.value.commitments || {},
+        } as CFCoreRecord;
+      }
+      const saveCommitments = this.save(commitments);
+
+      const withdrawalsPath = `${pair.path}/${WITHDRAWAL_PATH}/version/${LATEST_CF_STORE_VERSION}`;
+      let withdrawals = await this.findOne({
+        where: { path: withdrawalsPath },
+      });
+      if (withdrawals) {
+        withdrawals.value = pair.value.withdrawals;
+      } else {
+        withdrawals = {
+          path: withdrawalsPath,
+          value: pair.value.withdrawals || {},
+        } as CFCoreRecord;
+      }
+      const saveWithdrawals = this.save(withdrawals);
+
+      savePromises.concat([saveCommitments, saveWithdrawals]);
+
+      await Promise.all(savePromises);
+    }
+  }
+
+  async getV0(path: string): Promise<StringKeyValue | string | undefined> {
     // logger.debug(`Getting path from store: ${path}`);
     let res: any;
     // FIXME: this queries for all channels or proposed app instances, which
@@ -54,17 +148,6 @@ export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
     }
     // logger.log(`Got value: ${JSON.stringify(res.value[path])}`);
     return res.value[path];
-  }
-
-  async set(pairs: { path: string; value: any }[]): Promise<void> {
-    for (const pair of pairs) {
-      // Wrapping the value into an object is necessary for Postgres bc the JSON column breaks
-      // if you use anything other than JSON (i.e. a raw string).
-      // In some cases, the cf core code is inserting strings as values instead of objects :(
-      const record = { path: pair.path, value: { [pair.path]: pair.value } };
-      // logger.log(`Saving record: ${JSON.stringify(record)}`);
-      await this.save(record);
-    }
   }
 
   async getLegacyCFCoreRecord(multisigAddress: string): Promise<CFCoreRecord> {
