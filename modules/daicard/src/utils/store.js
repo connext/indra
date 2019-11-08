5013398035f05044c0246a7003baf2b5eec1b6bf
Rahul <rksethuram9@gmail.com>
2019-11-08 19:21:29 +0100

    Fix store implementations

--- modules/daicard/src/utils/store.js
+++ modules/daicard/src/utils/store.js
@@ -13,26 +13,6 @@ export const storeFactory = options => {
           return raw;
         }
       }
-      // Handle partial matches so the following line works -.-
-      // https://github.com/counterfactual/monorepo/blob/master/packages/node/src/store.ts#L54
-      if (path.endsWith("channel") || path.endsWith("appInstanceIdToProposedAppInstance")) {
-        const partialMatches = {};
-        for (const k of Object.keys(localStorage)) {
-          if (k.includes(`${path}/`)) {
-            try {
-              partialMatches[
-                k.replace(`${ConnextClientStorePrefix}:`, "").replace(`${path}/`, "")
-              ] = JSON.parse(localStorage.getItem(k));
-            } catch {
-              partialMatches[
-                k.replace(`${ConnextClientStorePrefix}:`, "").replace(`${path}/`, "")
-              ] = localStorage.getItem(k);
-            }
-          }
-        }
-        return partialMatches;
-      }
-      return raw;
     },
 
     set: async (pairs, shouldBackup) => {
