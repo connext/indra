6bcf41d7b125386d56cea77dc0f42d77f73f9011
Bo Henderson <bohende@gmail.com>
2019-11-11 12:23:45 +0900

    add cf-core and cf-types to npm-publish script

--- ops/npm-publish.sh
+++ ops/npm-publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 # This is the order they'll be published in
-packages="messaging,types,client"
+packages="messaging,cf-types,cf-core,types,client"
 project="indra"
 
 ########################################
