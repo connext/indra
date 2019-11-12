1412d7f86e261456fb50297356541c38214c00a6
Bo Henderson <bohende@gmail.com>
2019-11-12 10:25:35 +0900

    get tests building again

--- modules/cf-core/src/index.ts
+++ modules/cf-core/src/index.ts
@@ -1,5 +1,15 @@
+export * from "rpc-server";
+export * from "./methods/errors";
+export { getNetworkEnum, EthereumNetworkName } from "./network-configuration";
 export * from "./node";
 export * from "./private-keys-generator";
-export * from "./methods/errors";
-export * from "rpc-server";
-export { getNetworkEnum, EthereumNetworkName } from "./network-configuration";
+export {
+  CreateChannelMessage,
+  InstallMessage,
+  InstallVirtualMessage,
+  NODE_EVENTS,
+  ProposeMessage,
+  UninstallMessage,
+  UninstallVirtualMessage,
+  UpdateStateMessage,
+} from "./types";
