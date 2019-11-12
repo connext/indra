f5df8f7cadc4c15ecaae1d01acc5b6977cb8b462
Bo Henderson <bohende@gmail.com>
2019-11-12 12:16:08 +0900

    fix npm publish order

--- ops/npm-publish.sh
+++ ops/npm-publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 # This is the order they'll be published in
-packages="cf-types,cf-core,messaging,types,client"
+packages="cf-types,types,cf-core,messaging,client"
 project="indra"
 
 ########################################
