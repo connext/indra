254522fbf1e2bd1aa8569c251028f48a43bc25dd
Bo Henderson <bohende@gmail.com>
2019-11-13 12:08:45 +0900

    node: use xpubToAddress to print proper signing address

--- modules/node/src/cfCore/cfCore.provider.ts
+++ modules/node/src/cfCore/cfCore.provider.ts
@@ -7,7 +7,7 @@ import { fromMnemonic } from "ethers/utils/hdnode";
 import { ConfigService } from "../config/config.service";
 import { CFCoreProviderId, CF_PATH, MessagingProviderId } from "../constants";
 import { LockService } from "../lock/lock.service";
-import { CLogger, freeBalanceAddressFromXpub } from "../util";
+import { CLogger, xpubToAddress } from "../util";
 import { CFCore } from "../util/cfCore";
 
 import { CFCoreRecordRepository } from "./cfCore.repository";
@@ -29,12 +29,6 @@ export const cfCoreProviderFactory: Provider = {
 
     // test that provider works
     const { chainId, name: networkName } = await config.getEthNetwork();
-    const signerAddr = hdNode.neuter().address;
-    const provider = config.getEthProvider();
-    const balance = (await provider.getBalance(signerAddr)).toString();
-    logger.log(
-      `Balance of signer address ${signerAddr} on ${networkName} (chainId ${chainId}): ${balance}`,
-    );
     const cfCore = await CFCore.create(
       messaging as any, // TODO: FIX
       store,
@@ -48,11 +42,15 @@ export const cfCoreProviderFactory: Provider = {
         return Promise.resolve(hdNode.derivePath(uniqueId).privateKey);
       },
     );
+    const signerAddr = xpubToAddress(cfCore.publicIdentifier);
+    const provider = config.getEthProvider();
+    const balance = (await provider.getBalance(signerAddr)).toString();
+    logger.log(
+      `Balance of signer address ${signerAddr} on ${networkName} (chainId ${chainId}): ${balance}`,
+    );
     logger.log("CFCore created");
     logger.log(`Public Identifier ${JSON.stringify(cfCore.publicIdentifier)}`);
-    logger.log(
-      `Free balance address ${JSON.stringify(freeBalanceAddressFromXpub(cfCore.publicIdentifier))}`,
-    );
+    logger.log(`Free balance address ${xpubToAddress(cfCore.publicIdentifier)}`);
     return cfCore;
   },
 };
