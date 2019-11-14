9ceebeea0ee75833f0a756aaea93c910d2d16ac3
Rahul <rksethuram9@gmail.com>
2019-11-14 16:37:59 +0100

    Remove check since we will recollateralize

--- modules/client/src/controllers/ResolveConditionController.ts
+++ modules/client/src/controllers/ResolveConditionController.ts
@@ -84,8 +84,6 @@ export class ResolveConditionController extends AbstractController {
 
     const freeBal = await this.connext.getFreeBalance(assetId);
     const preTransferBal = freeBal[this.connext.freeBalanceAddress];
-    const nodeFreeBal = freeBal[xpubToAddress(this.connext.nodePublicIdentifier)];
-    validate(notLessThan(amountBN, nodeFreeBal));
 
     // TODO: dont listen to linked transfer app in default listener, only listen for it here
 
