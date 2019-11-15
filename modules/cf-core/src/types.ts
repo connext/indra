437c22a2d6b4ec676e34ed8c11f61117ff8036d0
LayneHaber <layne.haber@gmail.com>
2019-11-15 12:24:34 -0800

    correct types used

--- modules/cf-core/src/types.ts
+++ modules/cf-core/src/types.ts
@@ -28,6 +28,7 @@ export {
 } from "@connext/types";
 
 import { ProtocolMessage } from "./machine";
+import { ProposeInstallProtocolParams } from "./machine/types";
 
 export type NodeEvents = Node.EventName;
 export const NODE_EVENTS = Node.EventName;
@@ -38,7 +39,7 @@ export interface NodeMessageWrappedProtocolMessage extends Node.NodeMessage {
 
 export interface ProposeMessage extends Node.NodeMessage {
   data: {
-    params: Node.ProposeInstallParams;
+    params: ProposeInstallProtocolParams;
     appInstanceId: string;
   };
 }
