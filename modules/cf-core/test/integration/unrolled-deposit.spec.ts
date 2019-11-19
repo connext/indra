5a099273453904f6fec9327fe841dc73e3c7a274
Rahul <rksethuram9@gmail.com>
2019-11-19 15:27:27 +0100

    Fix up tests

--- modules/cf-core/test/integration/unrolled-deposit.spec.ts
+++ modules/cf-core/test/integration/unrolled-deposit.spec.ts
@@ -15,9 +15,10 @@ import {
   getBalances,
   getInstalledAppInstances,
   getProposedAppInstances,
-  getFreeBalanceState
+  transferERC20Tokens
 } from "./utils";
 import { xkeyKthAddress } from "../../src/machine";
+import { NetworkContextForTestSuite } from "@counterfactual/local-ganache-server";
 
 expect.extend({ toBeLt, toBeEq });
 
@@ -27,74 +28,167 @@ describe("Node method follows spec - install balance refund", () => {
   let nodeB: Node;
   let provider: JsonRpcProvider;
 
-  describe("Balance refund app is installed by node A", () => {
-    beforeEach(async () => {
-      const context: SetupContext = await setup(global);
-      provider = new JsonRpcProvider(global["ganacheURL"]);
-      nodeA = context["A"].node;
-      nodeB = context["B"].node;
+  beforeEach(async () => {
+    const context: SetupContext = await setup(global);
+    provider = new JsonRpcProvider(global["ganacheURL"]);
+    nodeA = context["A"].node;
+    nodeB = context["B"].node;
 
-      multisigAddress = await createChannel(nodeA, nodeB);
-    });
+    multisigAddress = await createChannel(nodeA, nodeB);
+  });
 
-    it("install app with ETH, sending ETH should increase free balance", async done => {
-      nodeB.on(NODE_EVENTS.INSTALL, async () => {
-        const [appInstanceNodeA] = await getInstalledAppInstances(nodeA);
-        const [appInstanceNodeB] = await getInstalledAppInstances(nodeB);
-        expect(appInstanceNodeA).toBeDefined();
-        expect(appInstanceNodeA).toEqual(appInstanceNodeB);
-        expect(
-          (appInstanceNodeA.latestState as CoinBalanceRefundState).recipient
-        ).toBe(xkeyKthAddress(nodeA.publicIdentifier, 0));
+  it("install app with ETH, sending ETH should increase free balance", async done => {
+    nodeB.on(NODE_EVENTS.INSTALL, async () => {
+      const [appInstanceNodeA] = await getInstalledAppInstances(nodeA);
+      const [appInstanceNodeB] = await getInstalledAppInstances(nodeB);
+      expect(appInstanceNodeA).toBeDefined();
+      expect(appInstanceNodeA).toEqual(appInstanceNodeB);
+      expect(
+        (appInstanceNodeA.latestState as CoinBalanceRefundState).recipient
+      ).toBe(xkeyKthAddress(nodeA.publicIdentifier, 0));
 
-        const proposedAppsA = await getProposedAppInstances(nodeA);
-        expect(proposedAppsA.length).toBe(0);
+      const proposedAppsA = await getProposedAppInstances(nodeA);
+      expect(proposedAppsA.length).toBe(0);
 
-        const [preSendBalA, preSendBalB] = await getBalances(
-          nodeA,
-          nodeB,
-          multisigAddress,
-          AddressZero
-        );
-        expect(preSendBalA).toBeEq(0);
-        expect(preSendBalB).toBeEq(0);
+      const [preSendBalA, preSendBalB] = await getBalances(
+        nodeA,
+        nodeB,
+        multisigAddress,
+        AddressZero
+      );
+      expect(preSendBalA).toBeEq(0);
+      expect(preSendBalB).toBeEq(0);
 
-        const tx = await provider.getSigner().sendTransaction({
-          to: multisigAddress,
-          value: One
-        });
-        await provider.waitForTransaction(tx.hash!);
-        const multisigBalance = await provider.getBalance(multisigAddress);
-        expect(multisigBalance).toBeEq(1);
-
-        await nodeA.rpcRouter.dispatch({
-          id: Date.now(),
-          methodName: NodeTypes.RpcMethodName.UNINSTALL_BALANCE_REFUND,
-          parameters: {
-            multisigAddress
-          } as NodeTypes.UninstallBalanceRefundParams
-        });
-
-        const [postSendBalA, postSendBalB] = await getBalances(
-          nodeA,
-          nodeB,
-          multisigAddress,
-          AddressZero
-        );
-        expect(postSendBalA).toBeEq(1);
-        expect(postSendBalB).toBeEq(0);
-
-        done();
+      const tx = await provider.getSigner().sendTransaction({
+        to: multisigAddress,
+        value: One
       });
+      await provider.waitForTransaction(tx.hash!);
+      const multisigBalance = await provider.getBalance(multisigAddress);
+      expect(multisigBalance).toBeEq(1);
 
       await nodeA.rpcRouter.dispatch({
         id: Date.now(),
-        methodName: NodeTypes.RpcMethodName.INSTALL_BALANCE_REFUND,
+        methodName: NodeTypes.RpcMethodName.UNINSTALL_BALANCE_REFUND,
         parameters: {
-          multisigAddress,
-          tokenAddress: AddressZero
-        } as NodeTypes.InstallBalanceRefundParams
+          multisigAddress
+        } as NodeTypes.UninstallBalanceRefundParams
       });
+
+      const [postSendBalA, postSendBalB] = await getBalances(
+        nodeA,
+        nodeB,
+        multisigAddress,
+        AddressZero
+      );
+      expect(postSendBalA).toBeEq(1);
+      expect(postSendBalB).toBeEq(0);
+
+      done();
     });
+
+    await nodeA.rpcRouter.dispatch({
+      id: Date.now(),
+      methodName: NodeTypes.RpcMethodName.INSTALL_BALANCE_REFUND,
+      parameters: {
+        multisigAddress,
+        tokenAddress: AddressZero
+      } as NodeTypes.InstallBalanceRefundParams
+    });
+  });
+
+  it("install app with tokens, sending tokens should increase free balance", async done => {
+    const erc20TokenAddress = (global[
+      "networkContext"
+    ] as NetworkContextForTestSuite).DolphinCoin;
+
+    nodeB.on(NODE_EVENTS.INSTALL, async () => {
+      const [appInstanceNodeA] = await getInstalledAppInstances(nodeA);
+      const [appInstanceNodeB] = await getInstalledAppInstances(nodeB);
+      expect(appInstanceNodeA).toBeDefined();
+      expect(appInstanceNodeA).toEqual(appInstanceNodeB);
+      expect(
+        (appInstanceNodeA.latestState as CoinBalanceRefundState).recipient
+      ).toBe(xkeyKthAddress(nodeA.publicIdentifier, 0));
+
+      const proposedAppsA = await getProposedAppInstances(nodeA);
+      expect(proposedAppsA.length).toBe(0);
+
+      const [preSendBalA, preSendBalB] = await getBalances(
+        nodeA,
+        nodeB,
+        multisigAddress,
+        erc20TokenAddress
+      );
+      expect(preSendBalA).toBeEq(0);
+      expect(preSendBalB).toBeEq(0);
+
+      await transferERC20Tokens(multisigAddress, erc20TokenAddress);
+
+      await nodeA.rpcRouter.dispatch({
+        id: Date.now(),
+        methodName: NodeTypes.RpcMethodName.UNINSTALL_BALANCE_REFUND,
+        parameters: {
+          multisigAddress
+        } as NodeTypes.UninstallBalanceRefundParams
+      });
+
+      const [postSendBalA, postSendBalB] = await getBalances(
+        nodeA,
+        nodeB,
+        multisigAddress,
+        erc20TokenAddress
+      );
+      expect(postSendBalA).toBeEq(1);
+      expect(postSendBalB).toBeEq(0);
+
+      done();
+    });
+
+    await nodeA.rpcRouter.dispatch({
+      id: Date.now(),
+      methodName: NodeTypes.RpcMethodName.INSTALL_BALANCE_REFUND,
+      parameters: {
+        multisigAddress,
+        tokenAddress: erc20TokenAddress
+      } as NodeTypes.InstallBalanceRefundParams
+    });
+  });
+
+  it("cannot install when already installed", async done => {
+    nodeB.on(NODE_EVENTS.INSTALL, async () => {
+      await expect(
+        nodeA.rpcRouter.dispatch({
+          id: Date.now(),
+          methodName: NodeTypes.RpcMethodName.INSTALL_BALANCE_REFUND,
+          parameters: {
+            multisigAddress,
+            tokenAddress: AddressZero
+          } as NodeTypes.InstallBalanceRefundParams
+        })
+      ).rejects.toThrow(/balance refund app is installed/i);
+      done();
+    });
+
+    await nodeA.rpcRouter.dispatch({
+      id: Date.now(),
+      methodName: NodeTypes.RpcMethodName.INSTALL_BALANCE_REFUND,
+      parameters: {
+        multisigAddress,
+        tokenAddress: AddressZero
+      } as NodeTypes.InstallBalanceRefundParams
+    });
+  });
+
+  it("cannot uninstall if not installed", async () => {
+    await expect(
+      nodeA.rpcRouter.dispatch({
+        id: Date.now(),
+        methodName: NodeTypes.RpcMethodName.UNINSTALL_BALANCE_REFUND,
+        parameters: {
+          multisigAddress
+        } as NodeTypes.InstallBalanceRefundParams
+      })
+    ).rejects.toThrow(/balance refund app is not installed/i);
   });
 });
