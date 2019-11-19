6ec0fec62296a0c3797bfbc0cef6f3e959da3177
Bo Henderson <bohende@gmail.com>
2019-11-19 15:22:29 +0900

    extract AddressInput component

--- modules/daicard/src/components/cashOutCard.js
+++ modules/daicard/src/components/cashOutCard.js
@@ -54,7 +54,7 @@ const useDebounce = (value, delay) => {
   return debouncedValue;
 }
 
-const useAddress = (address, ethProvider, network, setScan) => {
+const useAddress = (address, ethProvider, network) => {
   const [recipientDisplay, setRecipientDisplay] = useState(null);
   const [recipientValue, setRecipientValue] = useState(null);
   const [recipientError, setRecipientError] = useState(null);
@@ -79,19 +79,79 @@ const useAddress = (address, ethProvider, network, setScan) => {
       }
       setRecipientValue(error ? undefined : newVal);
       setRecipientError(error);
-      setScan(false);
     })()
   }, [debouncedRecipient]);
-  const setAddress = setRecipientDisplay;
-  return ([{ display: recipientDisplay, value: recipientValue, error: recipientError }, setAddress]);
+  return [
+    { display: recipientDisplay, value: recipientValue, error: recipientError },
+    setRecipientDisplay,
+  ];
+}
+
+const AddressInput = ({ address, setAddress }) => {
+  const [scan, setScan] = useState(false);
+  return (
+    <div>
+      <TextField
+        style={{ width: "100%" }}
+        id="outlined-with-placeholder"
+        label="Address"
+        placeholder="0x0..."
+        value={address.display || ""}
+        onChange={evt => setAddress(evt.target.value)}
+        margin="normal"
+        variant="outlined"
+        required
+        helperText={address.error}
+        error={!!address.error}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Tooltip disableFocusListener disableTouchListener title="Scan with QR code">
+                <Button
+                  disableTouchRipple
+                  variant="contained"
+                  color="primary"
+                  style={{ color: "primary" }}
+                  onClick={() => setScan(true)}
+                >
+                  <QRIcon />
+                </Button>
+              </Tooltip>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Modal
+        id="qrscan"
+        open={scan}
+        onClose={() => setScan(false)}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          position: "absolute",
+          top: "10%",
+          width: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <QRScan handleResult={(res) => {
+          setAddress(res);
+          setScan(false);
+        }} />
+      </Modal>
+    </div>
+  );
 }
 
 export const CashoutCard = style(({
   balance, channel, classes, ethProvider, history, machine, network, refreshBalances, swapRate, token,
 }) => {
-    const [scan, setScan] = useState(false);
     const [withdrawing, setWithdrawing] = useState(false);
-    const [recipient, setRecipient] = useAddress(null, ethProvider, network, setScan);
+    const [recipient, setRecipient] = useAddress(null, ethProvider, network);
 
     const cashoutTokens = async () => {
       const value = recipient.value;
@@ -185,56 +245,8 @@ export const CashoutCard = style(({
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            style={{ width: "100%" }}
-            id="outlined-with-placeholder"
-            label="Address"
-            placeholder="0x0..."
-            value={recipient.display || ""}
-            onChange={evt => setRecipient(evt.target.value)}
-            margin="normal"
-            variant="outlined"
-            required
-            helperText={recipient.error}
-            error={!!recipient.error}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Tooltip disableFocusListener disableTouchListener title="Scan with QR code">
-                    <Button
-                      disableTouchRipple
-                      variant="contained"
-                      color="primary"
-                      style={{ color: "primary" }}
-                      onClick={() => setScan(true)}
-                    >
-                      <QRIcon />
-                    </Button>
-                  </Tooltip>
-                </InputAdornment>
-              ),
-            }}
-          />
+          <AddressInput address={recipient} setAddress={setRecipient} />
         </Grid>
-        <Modal
-          id="qrscan"
-          open={scan}
-          onClose={() => setScan(false)}
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            position: "absolute",
-            top: "10%",
-            width: "375px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: "0",
-            right: "0",
-          }}
-        >
-          <QRScan handleResult={setRecipient} history={history} />
-        </Modal>
         <Grid item xs={12}>
           <Grid container spacing={8} direction="row" alignItems="center" justify="center">
             <Grid item xs={6}>
