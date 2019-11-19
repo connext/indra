ca5da28f7a9507027523f58646615fc0249d55ec
Bo Henderson <bohende@gmail.com>
2019-11-19 16:02:51 +0900

    extract useXpub and XpubInput into the input file

--- modules/daicard/src/components/input.js
+++ modules/daicard/src/components/input.js
@@ -18,41 +18,134 @@ const useDebounce = (value, delay) => {
       setDebouncedValue(value);
     }, delay)
     return () => clearTimeout(handler);
-  }, [value]);
+  }, [value, delay]);
   return debouncedValue;
 }
 
-export const useAddress = (address, ethProvider, network) => {
-  const [recipientDisplay, setRecipientDisplay] = useState(null);
-  const [recipientValue, setRecipientValue] = useState(null);
-  const [recipientError, setRecipientError] = useState(null);
-  const debouncedRecipient = useDebounce(recipientDisplay, 1000);
+export const useAddress = (initialAddress, ethProvider, network) => {
+  const [addressDisplay, setAddressDisplay] = useState(initialAddress);
+  const [addressValue, setAddressValue] = useState(null);
+  const [addressError, setAddressError] = useState(null);
+  const debouncedAddress = useDebounce(addressDisplay, 1000);
   useEffect(() => {
     (async () => {
-      if (debouncedRecipient === null) return;
-      let newVal = debouncedRecipient;
+      if (debouncedAddress === null) return;
+      let value = debouncedAddress;
       let error;
-      if (debouncedRecipient.startsWith("ethereum:")) {
-        newVal = debouncedRecipient.split(":")[1];
+      if (debouncedAddress.startsWith("ethereum:")) {
+        value = debouncedAddress.split(":")[1];
       }
-      if (network.ensAddress && newVal.endsWith('.eth')) {
-        newVal = await ethProvider.resolveName(newVal);
+      if (network.ensAddress && value.endsWith('.eth')) {
+        value = await ethProvider.resolveName(value);
       }
-      if (newVal === "") {
+      if (value === "") {
         error = "Please provide an address or ens name";
-      } else if (!isHexString(newVal)) {
+      } else if (!isHexString(value)) {
         error = `Invalid hex string`;
-      } else if (arrayify(newVal).length !== 20) {
-        error = `Invalid length: ${newVal.length} (expected 42)`;
+      } else if (arrayify(value).length !== 20) {
+        error = `Invalid length: ${value.length} (expected 42)`;
       }
-      setRecipientValue(error ? undefined : newVal);
-      setRecipientError(error);
+      setAddressValue(error ? undefined : value);
+      setAddressError(error);
     })()
-  }, [debouncedRecipient]);
+  }, [debouncedAddress, ethProvider, network.ensAddress]);
   return [
-    { display: recipientDisplay, value: recipientValue, error: recipientError },
-    setRecipientDisplay,
+    { display: addressDisplay, value: addressValue, error: addressError },
+    setAddressDisplay,
   ];
+}
+
+export const useXpub = (initialXpub) => {
+  const [xpubDisplay, setXpubDisplay] = useState(initialXpub);
+  const [xpubValue, setXpubValue] = useState(null);
+  const [xpubError, setXpubError] = useState(null);
+  const debouncedXpub = useDebounce(xpubDisplay, 1000);
+  useEffect(() => {
+    (async () => {
+      if (debouncedXpub === null) return;
+      const xpubLen = 111;
+      let value = null;
+      let error = null;
+      value = debouncedXpub;
+      if (!value || !value.startsWith("xpub")) {
+        error = `Invalid xpub: should start with "xpub"`;
+      }
+      if (!error && value.length !== xpubLen) {
+        error = `Invalid length: ${value.length} (expected ${xpubLen})`;
+      }
+      setXpubValue(error ? undefined : value);
+      setXpubError(error);
+    })()
+  }, [debouncedXpub]);
+  return [
+    { display: xpubDisplay, value: xpubValue, error: xpubError },
+    setXpubDisplay,
+  ];
+}
+
+export const XpubInput = ({ xpub, setXpub }) => {
+  const [scan, setScan] = useState(false);
+  return (
+    <div>
+      <TextField
+        fullWidth
+        id="outlined"
+        label="Recipient Public Identifier"
+        type="string"
+        value={xpub.display || ""}
+        onChange={evt => setXpub(evt.target.value)}
+        margin="normal"
+        variant="outlined"
+        helperText={xpub.error ? xpub.error : "Ignored for linked payments"}
+        error={xpub.error !== null}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Tooltip disableFocusListener disableTouchListener title="Scan with QR code">
+                <Button
+                  disableTouchRipple
+                  variant="contained"
+                  color="primary"
+                  style={{ color: "#FFF" }}
+                  onClick={() => setScan(true)}
+                >
+                  <QRIcon />
+                </Button>
+              </Tooltip>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Modal
+        id="qrscan"
+        open={scan}
+        onClose={() => setScan(false)}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          position: "absolute",
+          top: "10%",
+          width: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <QRScan handleResult={(res) => {
+          // Extract the xpub from a request link if necessary
+          const i = res.indexOf('=xpub')
+          if (i !== -1) {
+            setXpub(res.substring(i + 1, i + 112));
+          } else {
+            setXpub(res);
+          }
+          setScan(false);
+        }} />
+      </Modal>
+    </div>
+  );
 }
 
 export const AddressInput = ({ address, setAddress }) => {
