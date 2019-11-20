04fabdb7c5c992ae7c8f9ad4d98072143ad66f10
LayneHaber <layne.haber@gmail.com>
2019-11-20 15:04:30 -0800

    pretty print

--- modules/node/src/cfCore/cfCore.repository.ts
+++ modules/node/src/cfCore/cfCore.repository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Like, Repository } from "typeorm";
 
 import { CFCoreRecord } from "./cfCore.entity";
 
-import { CLogger } from "../util";
+import { CLogger, stringify } from "../util";
 
 const logger = new CLogger("CFCoreRecordRepository")
 
@@ -62,7 +62,7 @@ export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
       // if you use anything other than JSON (i.e. a raw string).
       // In some cases, the cf core code is inserting strings as values instead of objects :(
       const record = { path: pair.path, value: { [pair.path]: pair.value } };
-      logger.debug(`Saving record: ${JSON.stringify(record)}`);
+      logger.debug(`Saving record: ${stringify(record, 2)}`);
       await this.save(record);
     }
   }
