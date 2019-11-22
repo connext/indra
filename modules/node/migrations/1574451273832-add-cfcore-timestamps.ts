25a28fafdd14b918fedf9361b1dc4bb4f17e4d6f
LayneHaber <layne.haber@gmail.com>
2019-11-22 13:34:53 -0800

    add default values

--- modules/node/migrations/1574451273832-add-cfcore-timestamps.ts
+++ modules/node/migrations/1574451273832-add-cfcore-timestamps.ts
@@ -4,8 +4,8 @@ export class AddCfcoreTimestamps1574451273832 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`
       ALTER TABLE node_records
-      ADD COLUMN "createdAt" timestamp,
-      ADD COLUMN "updatedAt" timestamp;
+      ADD COLUMN "createdAt" timestamp NOT NULL DEFAULT NOW(),
+      ADD COLUMN "updatedAt" timestamp NOT NULL DEFAULT NOW();
     `);
   }
 
