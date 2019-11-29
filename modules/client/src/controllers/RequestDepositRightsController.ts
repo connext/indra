3cab79246390ab2e97b443338208c07ea6961a30
Rahul <rksethuram9@gmail.com>
2019-11-29 18:12:33 +0100

    Push controller

---
+++ modules/client/src/controllers/RequestDepositRightsController.ts
@@ -0,0 +1,162 @@
+import { Contract } from "ethers";
+import { AddressZero, Zero } from "ethers/constants";
+import { bigNumberify } from "ethers/utils";
+import tokenAbi from "human-standard-token-abi";
+
+import { delayAndThrow, stringify } from "../lib/utils";
+import {
+  BigNumber,
+  CFCoreTypes,
+  CoinBalanceRefundAppStateBigNumber,
+  RejectProposalMessage,
+  RequestDepositRightsParameters,
+  SupportedApplication,
+  SupportedApplications,
+} from "../types";
+import { invalidAddress, validate } from "../validation";
+
+import { AbstractController } from "./AbstractController";
+
+export class RequestDepositRightsController extends AbstractController {
+  public requestDepositRights = async (
+    assetId: string,
+  ): Promise<CFCoreTypes.RequestDepositRightsResult> => {
+    validate(invalidAddress(assetId));
+
+    let multisigBalance: BigNumber;
+    if (assetId === AddressZero) {
+      multisigBalance = await this.connext.ethProvider.getBalance(this.connext.multisigAddress);
+    } else {
+      const token = new Contract(assetId, tokenAbi, this.connext.ethProvider);
+      multisigBalance = await token.balanceOf(this.connext.multisigAddress);
+    }
+
+    const existingBalanceRefundApp = await this.connext.getBalanceRefundApp(assetId);
+    if (existingBalanceRefundApp) {
+      if (
+        bigNumberify(existingBalanceRefundApp.latestState["threshold"]).eq(multisigBalance) &&
+        existingBalanceRefundApp.latestState["recipient"] === this.connext.freeBalanceAddress
+      ) {
+        this.log.info(`Balance refund app is in the correct state, doing nothing`);
+        return {
+          freeBalance: await this.connext.getFreeBalance(assetId),
+          recipient: this.connext.freeBalanceAddress,
+          tokenAddress: assetId,
+        } as any;
+      }
+      this.log.info(`Balance refund app is not in the correct state, uninstalling first`);
+      await this.connext.rescindDepositRights(assetId);
+    }
+
+    // propose the app install
+    this.log.info(`Installing balance refund app`);
+    const err = await this.proposeDepositInstall(assetId);
+    if (err) {
+      throw new Error(err);
+    }
+    const requestDepositRightsResponse = await this.connext.channelRouter.requestDepositRights(
+      assetId,
+    );
+    this.log.info(
+      `requestDepositRightsResponse Response: ${stringify(requestDepositRightsResponse)}`,
+    );
+    this.log.info("Deposit rights gained!");
+
+    this.listener.emit(`indra.client.${this.connext.publicIdentifier}.freeBalanceUpdated`, {
+      freeBalance: await this.connext.getFreeBalance(assetId),
+    });
+    return {
+      freeBalance: await this.connext.getFreeBalance(assetId),
+      recipient: this.connext.freeBalanceAddress,
+      tokenAddress: assetId,
+    };
+  };
+
+  /////////////////////////////////
+  ////// PRIVATE METHODS
+
+  private proposeDepositInstall = async (assetId: string): Promise<string | undefined> => {
+    let boundReject: (msg: RejectProposalMessage) => void;
+
+    const threshold =
+      assetId === AddressZero
+        ? await this.ethProvider.getBalance(this.connext.multisigAddress)
+        : await new Contract(assetId!, tokenAbi, this.ethProvider).functions.balanceOf(
+            this.connext.multisigAddress,
+          );
+
+    const initialState: CoinBalanceRefundAppStateBigNumber = {
+      multisig: this.connext.multisigAddress,
+      recipient: this.connext.freeBalanceAddress,
+      threshold,
+      tokenAddress: assetId,
+    };
+
+    const {
+      actionEncoding,
+      appDefinitionAddress: appDefinition,
+      stateEncoding,
+      outcomeType,
+    } = this.connext.getRegisteredAppDetails(
+      SupportedApplications.CoinBalanceRefundApp as SupportedApplication,
+    );
+
+    const params: CFCoreTypes.ProposeInstallParams = {
+      abiEncodings: {
+        actionEncoding,
+        stateEncoding,
+      },
+      appDefinition,
+      initialState,
+      initiatorDeposit: Zero,
+      initiatorDepositTokenAddress: assetId,
+      outcomeType,
+      proposedToIdentifier: this.connext.nodePublicIdentifier,
+      responderDeposit: Zero,
+      responderDepositTokenAddress: assetId,
+      timeout: Zero,
+    };
+
+    let appId: string;
+    try {
+      await Promise.race([
+        new Promise(async (res: any, rej: any) => {
+          boundReject = this.rejectInstallCoinBalance.bind(null, rej);
+          console.warn(
+            `subscribing to indra.node.${this.connext.nodePublicIdentifier}.proposalAccepted.${this.connext.multisigAddress}`,
+          );
+          await this.connext.messaging.subscribe(
+            `indra.node.${this.connext.nodePublicIdentifier}.proposalAccepted.${this.connext.multisigAddress}`,
+            res,
+          );
+          const { appInstanceId } = await this.connext.proposeInstallApp(params);
+          appId = appInstanceId;
+          console.warn(`waiting for proposal acceptance of ${appInstanceId}`);
+          this.listener.on(CFCoreTypes.EventName.REJECT_INSTALL, boundReject);
+        }),
+        delayAndThrow(15_000, "App install took longer than 15 seconds"),
+      ]);
+      this.log.info(`App was proposed successfully!: ${appId}`);
+      return undefined;
+    } catch (e) {
+      this.log.error(`Error installing app: ${e.toString()}`);
+      return e.message();
+    } finally {
+      this.cleanupInstallListeners(appId, boundReject);
+    }
+  };
+
+  private rejectInstallCoinBalance = (
+    rej: (reason?: string) => void,
+    msg: RejectProposalMessage,
+  ): void => {
+    return rej(`Install failed. Event data: ${stringify(msg)}`);
+  };
+
+  private cleanupInstallListeners = (appId: string, boundReject: any): void => {
+    this.connext.messaging.unsubscribe(
+      `indra.node.${this.connext.nodePublicIdentifier}.install.${appId}`,
+    );
+    this.listener.removeListener(CFCoreTypes.EventName.REJECT_INSTALL_VIRTUAL, boundReject);
+  };
+}
