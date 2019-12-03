764ef542ed5118d8c143130be4dbe7cbc649901c
Bo Henderson <bohende@gmail.com>
2019-12-03 21:29:07 +0530

    cf-types: add proxyBytecode to StateChannelJSON

--- modules/cf-types/src/data-types.ts
+++ modules/cf-types/src/data-types.ts
@@ -93,6 +93,7 @@ export type StateChannelJSON = {
   ][];
   readonly freeBalanceAppInstance: AppInstanceJson | undefined;
   readonly monotonicNumProposedApps: number;
+  readonly proxyBytecode?: string;
 };
 
 export type AppInstanceInfo = {
