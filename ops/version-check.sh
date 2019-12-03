27580bdcfaf1a5a4634a9e73b4ff035eebf3499e
Bo Henderson <bohende@gmail.com>
2019-12-03 15:09:22 +0530

    fix version checker

--- ops/version-check.sh
+++ ops/version-check.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-format='{printf("| %-32s|%8s  ->  %-8s|\n", $1, $3, $4)}'
+function get_outdated {
+  format='{printf("| %-32s|%8s  ->  %-8s|\n", $1, $3, $4)}'
+  npm outdated -S | tail -n +2 | awk '$3 != $4' | awk "$format"
+  npm outdated -D | tail -n +2 | awk '$3 != $4' | awk "$format"
+}
 
 echo "==== Module: project root"
-npm outdated | tail -n +2 | awk '$3 != $4' | awk "$format"
+get_outdated
 echo
 
 cd modules
@@ -15,7 +19,7 @@ do
   fi
   echo "===== Module: $module"
   cd $module
-  npm outdated | tail -n +2 | awk '$3 != $4' | awk "$format"
+  get_outdated
   cd ..
   echo
 done
