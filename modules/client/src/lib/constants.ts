0f46a2444d10cc067bdf54deda5f78aa0d3e877e
Rahul <rksethuram9@gmail.com>
2019-12-04 07:59:58 +0100

    Bump nats timeout

--- modules/client/src/lib/constants.ts
+++ modules/client/src/lib/constants.ts
@@ -4,4 +4,4 @@ export const CF_PATH = "m/44'/60'/0'/25446";
 export const CF_METHOD_TIMEOUT = 90_000;
 
 // shortest timeout
-export const NATS_TIMEOUT = 15_000;
+export const NATS_TIMEOUT = 30_000;
