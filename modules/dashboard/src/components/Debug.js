d083ce1390fbcf048ba13579b2b4005417f497d5
LayneHaber <layne.haber@gmail.com>
2019-12-04 19:54:49 -0800

    prettier

--- modules/dashboard/src/components/Debug.js
+++ modules/dashboard/src/components/Debug.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button, Grid, Typography, withStyles } from "@material-ui/core";
-import { Switch, Route,Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import DebugChannel from "./DebugChannel";
 import DebugNode from "./DebugNode";
-import { Home as HomeIcon} from "@material-ui/icons";
+import { Home as HomeIcon } from "@material-ui/icons";
 
 const styles = {
   top: {
@@ -36,18 +36,18 @@ const styles = {
     fontSize: "24px",
     color: "#002868",
     textDecoration: "none",
-    paddingLeft:"2%",
-    paddingRight:"2%",
+    paddingLeft: "2%",
+    paddingRight: "2%",
   },
 };
 
 function Debug(props) {
   const { classes, messaging, token, prefix } = props;
-  console.log(messaging)
+  console.log(messaging);
   return (
     <Grid className={classes.top} container>
       <Grid className={classes.appBar} container>
-      <Button className={classes.button} component={Link} to={`${prefix}`}>
+        <Button className={classes.button} component={Link} to={`${prefix}`}>
           <HomeIcon className={classes.icon} />
         </Button>
         <Button className={classes.button} component={Link} to={`${prefix}/debug/channel`}>
@@ -56,12 +56,19 @@ function Debug(props) {
         <Button className={classes.button} component={Link} to={`${prefix}/debug/node`}>
           <Typography className={classes.buttonText}>Node</Typography>
         </Button>
-
       </Grid>
-        <Switch>
-          <Route exact path={`${prefix}/debug/channel`} render={props => <DebugChannel {...props} messaging={messaging} token={token} />} />
-          <Route exact path={`${prefix}/debug/node`} render={props => <DebugNode {...props} messaging={messaging} token={token}  />} />
-        </Switch>
+      <Switch>
+        <Route
+          exact
+          path={`${prefix}/debug/channel`}
+          render={props => <DebugChannel {...props} messaging={messaging} token={token} />}
+        />
+        <Route
+          exact
+          path={`${prefix}/debug/node`}
+          render={props => <DebugNode {...props} messaging={messaging} token={token} />}
+        />
+      </Switch>
     </Grid>
   );
 }
