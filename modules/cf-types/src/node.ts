7e000fa6bb7a2f55915f4aee82866a018b8d35c1
LayneHaber <layne.haber@gmail.com>
2019-12-10 13:04:05 -0800

    events enum --> type

--- modules/cf-types/src/node.ts
+++ modules/cf-types/src/node.ts
@@ -117,25 +117,26 @@ export namespace Node {
   }
 
   // SOURCE: https://github.com/counterfactual/monorepo/blob/master/packages/cf.js/API_REFERENCE.md#events
-  export enum EventName {
-    COUNTER_DEPOSIT_CONFIRMED = "counterDepositConfirmed",
-    CREATE_CHANNEL = "createChannelEvent",
-    DEPOSIT_CONFIRMED = "depositConfirmedEvent",
-    DEPOSIT_FAILED = "depositFailed",
-    DEPOSIT_STARTED = "depositStartedEvent",
-    INSTALL = "installEvent",
-    INSTALL_VIRTUAL = "installVirtualEvent",
-    REJECT_INSTALL = "rejectInstallEvent",
-    UNINSTALL = "uninstallEvent",
-    UNINSTALL_VIRTUAL = "uninstallVirtualEvent",
-    UPDATE_STATE = "updateStateEvent",
-    WITHDRAWAL_CONFIRMED = "withdrawalConfirmedEvent",
-    WITHDRAWAL_FAILED = "withdrawalFailed",
-    WITHDRAWAL_STARTED = "withdrawalStartedEvent",
-    PROPOSE_INSTALL = "proposeInstallEvent",
-    PROTOCOL_MESSAGE_EVENT = "protocolMessageEvent",
-    REJECT_INSTALL_VIRTUAL = "rejectInstallEvent"
-  }
+  export const EventNames = {
+    COUNTER_DEPOSIT_CONFIRMED_EVENT: "COUNTER_DEPOSIT_CONFIRMED_EVENT",
+    CREATE_CHANNEL_EVENT: "CREATE_CHANNEL_EVENT",
+    DEPOSIT_CONFIRMED_EVENT: "DEPOSIT_CONFIRMED_EVENT",
+    DEPOSIT_FAILED_EVENT: "DEPOSIT_FAILED_EVENT",
+    DEPOSIT_STARTED_EVENT: "DEPOSIT_STARTED_EVENT",
+    INSTALL_EVENT: "INSTALL_EVENT",
+    INSTALL_VIRTUAL_EVENT: "INSTALL_VIRTUAL_EVENT",
+    REJECT_INSTALL_EVENT: "REJECT_INSTALL_EVENT",
+    UNINSTALL_EVENT: "UNINSTALL_EVENT",
+    UNINSTALL_VIRTUAL_EVENT: "UNINSTALL_VIRTUAL_EVENT",
+    UPDATE_STATE_EVENT: "UPDATE_STATE_EVENT",
+    WITHDRAWAL_CONFIRMED_EVENT: "WITHDRAWAL_CONFIRMED_EVENT",
+    WITHDRAWAL_FAILED_EVENT: "WITHDRAWAL_FAILED_EVENT",
+    WITHDRAWAL_STARTED_EVENT: "WITHDRAWAL_STARTED_EVENT",
+    PROPOSE_INSTALL_EVENT: "PROPOSE_INSTALL_EVENT",
+    PROTOCOL_MESSAGE_EVENT: "PROTOCOL_MESSAGE_EVENT",
+    REJECT_INSTALL_VIRTUAL_EVENT: "REJECT_INSTALL_VIRTUAL_EVENT"
+  };
+  export type EventName = keyof typeof EventNames;
 
   export type CreateChannelParams = {
     owners: string[];
