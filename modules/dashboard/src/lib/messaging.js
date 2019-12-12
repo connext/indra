c4af749f0013f56b38ff1e15791a68ff3f2a1eb4
Bo Henderson <bohende@gmail.com>
2019-12-12 19:58:02 +0530

    ⚡ dashboard: copy/paste re-try-able messaging from client

--- modules/dashboard/src/lib/messaging.js
+++ modules/dashboard/src/lib/messaging.js
@@ -2,7 +2,10 @@ import { MessagingServiceFactory } from "@connext/messaging";
 import uuid from "uuid";
 import { stringify } from "./utils";
 
-const API_TIMEOUT = 30_000;
+const NATS_ATTEMPTS = 90_000;
+const NATS_TIMEOUT = 5_000;
+const guardedSubjects = ["channel", "lock", "transfer"];
+const sendFailed = "Failed to send message";
 
 export default class AdminMessaging {
   constructor(messagingUrl, token, logLevel = 5) {
@@ -10,16 +13,14 @@ export default class AdminMessaging {
       logLevel,
       messagingUrl, // nodeUrl
     });
-    this.service = messagingFactory.createService("messaging");
-    this.connected = false;
+    this.messaging = messagingFactory.createService("messaging");
     this.token = token;
   }
 
   ///////////////////////////////////////
   ////// SETUP FN
   async connect() {
-    const res = await this.service.connect();
-    this.connected = true;
+    const res = await this.messaging.connect();
     return res;
   }
 
@@ -66,41 +67,83 @@ export default class AdminMessaging {
     });
   }
 
+  async getAuthToken() {
+    return new Promise(
+      async (resolve: any, reject: any): Promise<any> => {
+        const nonce = await this.send("auth.getNonce", {
+          address: this.channelRouter.signerAddress,
+        });
+        const sig = await this.channelRouter.signMessage(nonce);
+        const token = `${nonce}:${sig}`;
+        return resolve(token);
+      },
+    );
+  }
+
   ///////////////////////////////////////
   ////// SEND REQUEST
-  async send(subject, data, timeout = API_TIMEOUT) {
-    // assert connected
-    if (!this.connected) {
-      throw new Error(`Call messaging.connect before calling send`);
+
+  async send(subject, data) {
+    let error;
+    for (let attempt = 1; attempt <= NATS_ATTEMPTS; attempt += 1) {
+      const timeout = new Promise((resolve: any): any => setTimeout(resolve, NATS_TIMEOUT));
+      try {
+        return await this.sendAttempt(subject, data);
+      } catch (e) {
+        error = e;
+        if (e.message.startsWith(sendFailed)) {
+          console.warn(
+            `Attempt ${attempt}/${NATS_ATTEMPTS} to send ${subject} failed: ${e.message}`,
+          );
+          await this.messaging.disconnect();
+          await this.messaging.connect();
+          if (attempt + 1 <= NATS_ATTEMPTS) {
+            await timeout; // Wait at least a NATS_TIMEOUT before retrying
+          }
+        } else {
+          throw e;
+        }
+      }
     }
+    throw error;
+  }
 
-    // data is optional
-    console.debug(
-      `Sending request to admin.${subject} ${
-        data ? `with data: ${stringify(data)}` : `without data`
-      }`,
+  async sendAttempt(subject, data) {
+    console.log(
+      `Sending request to ${subject} ${data ? `with data: ${stringify(data)}` : `without data`}`,
     );
-
     const payload = {
-      ...(data || {}),
+      ...data,
       id: uuid.v4(),
-      token: this.token,
     };
-
-    const msg = await this.service.request(`admin.${subject}`, timeout, payload);
-    const error = msg ? (msg.data ? (msg.data.response ? msg.data.response.err : "") : "") : "";
-
+    if (guardedSubjects.includes(subject.split(".")[0])) {
+      this.assertAuthToken();
+      payload.token = await this.token;
+    }
+    let msg;
+    try {
+      msg = await this.messaging.request(subject, NATS_TIMEOUT, payload);
+    } catch (e) {
+      throw new Error(`${sendFailed}: ${e.message}`);
+    }
+    let error = msg ? (msg.data ? (msg.data.response ? msg.data.response.err : "") : "") : "";
+    if (error && error.startsWith("Invalid token")) {
+      console.log(`Auth error, token might have expired. Let's get a fresh token & try again.`);
+      this.token = this.getAuthToken();
+      payload.token = await this.token;
+      msg = await this.messaging.request(subject, NATS_TIMEOUT, payload);
+      error = msg ? (msg.data ? (msg.data.response ? msg.data.response.err : "") : "") : "";
+    }
     if (!msg.data) {
-      this.log.info(`Maybe this message is malformed: ${stringify(msg)}`);
+      console.log(`Maybe this message is malformed: ${stringify(msg)}`);
       return undefined;
     }
-
     const { err, response } = msg.data;
     if (err || error) {
       throw new Error(`Error sending request. Message: ${stringify(msg)}`);
     }
-
     const isEmptyObj = typeof response === "object" && Object.keys(response).length === 0;
     return !response || isEmptyObj ? undefined : response;
   }
+
 }
