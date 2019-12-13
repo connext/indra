7b69703732bfc7026e87c27c21dd0807b46f007c
Pedro Gomes <pedrogomes94@gmail.com>
2019-12-13 19:16:17 +0100

    clean console.log

--- modules/client/src/channelProvider.ts
+++ modules/client/src/channelProvider.ts
@@ -67,21 +67,14 @@ export class ChannelProvider {
     authKey: any,
   ) {
     this.store = store;
-    console.log("[ChannelProvider]", "this.store", "=>", this.store);
     this.wallet = authKey ? new Wallet(authKey) : null;
-    console.log("[ChannelProvider]", "this.wallet", "=>", this.wallet);
     this.connection = connection;
-    console.log("[ChannelProvider]", "this.connection", "=>", this.connection);
     this._config = config;
-    console.log("[ChannelProvider]", "this._config", "=>", this._config);
     this._multisigAddress = config.multisigAddress;
-    console.log("[ChannelProvider]", "this._multisigAddress", "=>", this._multisigAddress);
     this._signerAddress = config.signerAddress;
-    console.log("[ChannelProvider]", "this._signerAddress", "=>", this._signerAddress);
   }
 
   public enable = async (): Promise<ChannelProviderConfig> => {
-    console.log("[ChannelProvider]", "[enable]", "this.config", "=>", this.config);
     return this.config;
   };
 
@@ -90,8 +83,6 @@ export class ChannelProvider {
     params: any = {},
   ): Promise<any> => {
     let result;
-    console.log("[ChannelProvider]", "[send]", "method", "=>", method);
-    console.log("[ChannelProvider]", "[send]", "params", "=>", params);
     switch (method) {
       case NewRpcMethodName.STORE_SET:
         result = await this.set(params.pairs);
@@ -112,54 +103,30 @@ export class ChannelProvider {
         result = await this._send(method as CFCoreTypes.RpcMethodName, params);
         break;
     }
-    console.log("[ChannelProvider]", "[send]", "result", "=>", result);
     return result;
   };
 
   ///////////////////////////////////////////////
   ///// GETTERS / SETTERS
   get config(): ChannelProviderConfig {
-    console.log("[ChannelProvider]", "[get config()]", "this._config", "=>", this._config);
     return this._config;
   }
 
   get multisigAddress(): string | undefined {
     const multisigAddress = this._multisigAddress || this._config.multisigAddress;
-    console.log(
-      "[ChannelProvider]",
-      "[get multisigAddress()]",
-      "multisigAddress",
-      "=>",
-      multisigAddress,
-    );
     return multisigAddress;
   }
 
   set multisigAddress(multisigAddress: string) {
-    console.log(
-      "[ChannelProvider]",
-      "[set multisigAddress()]",
-      "multisigAddress",
-      "=>",
-      multisigAddress,
-    );
     this._config.multisigAddress = multisigAddress;
     this._multisigAddress = multisigAddress;
   }
 
   get signerAddress(): string | undefined {
-    console.log(
-      "[ChannelProvider]",
-      "[get signerAddress()]",
-      "this._signerAddress",
-      "=>",
-      this._signerAddress,
-    );
     return this._signerAddress;
   }
 
   set signerAddress(signerAddress: string) {
-    console.log("[ChannelProvider]", "[set signerAddress()]", "signerAddress", "=>", signerAddress);
     this._signerAddress = signerAddress;
   }
 
