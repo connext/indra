c6ecddc591f9b59574c93224acb95a3cfac7a3af
LayneHaber <layne.haber@gmail.com>
2019-12-17 15:13:09 -0800

    temporary auth fix, need to fix wc channel provider and bump deps

--- modules/client/src/node.ts
+++ modules/client/src/node.ts
@@ -279,8 +279,10 @@ export class NodeApiClient implements INodeApiClient {
         `Must have instantiated a channel provider (ie a signing thing) before setting auth token`,
       );
     }
+    // TODO: fix the channel provider in wallet connect to return a
+    // signer address
     const nonce = await this.send("auth.getNonce", {
-      address: this.channelProvider.signerAddress,
+      address: this.channelProvider.signerAddress || this.channelProvider.config.signerAddress,
     });
     const sig = await this.channelProvider.send(NewRpcMethodName.NODE_AUTH, { message: nonce });
     const token = `${nonce}:${sig}`;
