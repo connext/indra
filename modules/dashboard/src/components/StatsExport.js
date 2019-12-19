a370eb40c06bcd79f3af6a5ab29034db0a9aa5e8
Hunter Hillman <hthillman@Hunters-MacBook-Pro.local>
2019-12-19 13:45:40 -0800

    export transfer data

--- modules/dashboard/src/components/StatsExport.js
+++ modules/dashboard/src/components/StatsExport.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Grid, Typography, styled } from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import { Grid, Typography, styled, Button } from "@material-ui/core";
 
 const TopGrid = styled(Grid)({
   display: "flex",
@@ -9,7 +9,7 @@ const TopGrid = styled(Grid)({
   height: "100%",
   justifyContent: "center",
   alignItems: "center",
-})
+});
 
 const StatTypography = styled(Typography)({
   textAlign: "center",
@@ -17,16 +17,78 @@ const StatTypography = styled(Typography)({
   fontSize: "24px",
   color: "#002868",
   textDecoration: "none",
-})
+});
 
-function StatsExport({ classes }) {
+function StatsExport({ classes, messaging }) {
+
+  const [allTransfers, setAllTransfers] = useState(null);
+
+
+  function convertArrayOfObjectsToCSV(args) {
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+      return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ",";
+    lineDelimiter = args.lineDelimiter || "\n";
+
+    keys = Object.keys(data[0]);
+
+    result = "";
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    data.forEach(function(item) {
+      ctr = 0;
+      keys.forEach(function(key) {
+        if (ctr > 0) result += columnDelimiter;
+
+        result += item[key];
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+
+    return result;
+  }
+
+  function downloadCSV(args) {
+    var data, filename, link;
+    var csv = convertArrayOfObjectsToCSV({
+      data: args.data,
+    });
+    if (csv == null) return;
+
+    filename = args.filename || "export.csv";
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = "data:text/csv;charset=utf-8," + csv;
+    }
+    data = encodeURI(csv);
+
+    link = document.createElement("a");
+    link.setAttribute("href", data);
+    link.setAttribute("download", filename);
+    link.click();
+  }
+
+  async function getDataAndDownload(){
+      if (!messaging) {
+        return;
+      }
+      const res = await messaging.getAllLinkedTransfers();
+      if(res){
+        downloadCSV({ data: res, filename: "transfer-data.csv" })
+      }
+  }
   return (
     <TopGrid container>
-      <StatTypography >Hello</StatTypography>
-      <StatTypography >World</StatTypography>
+      <Button onClick={getDataAndDownload}>Download CSV</Button>
     </TopGrid>
   );
 }
-
 
 export default StatsExport;
