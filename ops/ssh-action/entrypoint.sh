3683751908cb4de0089420025cb60f6e69dd2f86
Bo Henderson <bohende@gmail.com>
2019-12-19 15:29:59 +0530

    👷‍ clean up ssh-action entrypoint

--- ops/ssh-action/entrypoint.sh
+++ ops/ssh-action/entrypoint.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo "ssh-action activated"
-
 HOST="$1"
 SSH_KEY="$2"
 CMD="$3"
 
-echo "executing command \"$CMD\" on host $HOST (home: $HOME)"
-
 SSH_DIR="$HOME/.ssh"
 KEY_FILE="$SSH_DIR/id_rsa"
+KEY_FOOTER='-----END RSA PRIVATE KEY-----'
+KEY_HEADER='-----BEGIN RSA PRIVATE KEY-----'
+
+echo "ssh-action activated!"
+echo "Executing command \"$CMD\" on host $HOST"
 
 mkdir -p $SSH_DIR
 rm -f $KEY_FILE $SSH_DIR/known_hosts
 touch $KEY_FILE $SSH_DIR/known_hosts
-echo "Creating ssh key"
-echo '-----BEGIN RSA PRIVATE KEY-----' >> $KEY_FILE
-echo $SSH_KEY >> $KEY_FILE
-echo '-----END RSA PRIVATE KEY-----' >> $KEY_FILE
-chmod 400 $KEY_FILE
-echo "created ssh key"
-ssh-keygen -lf $KEY_FILE
-echo "key scanning"
-ssh-keyscan -t rsa ${HOST#*@} >> $SSH_DIR/known_hosts
-echo "doing test connection"
-ssh -i $KEY_FILE -o StrictHostKeyChecking=no $HOST hostname
 
-echo "executing cmd for real"
-exec ssh -i $KEY_FILE $HOST "bash -s" <<EOF
+# Env vars strip out newlines so a naively loaded ssh key will be improperly formatted
+# Replace any existing header/footers with manually added ones that include proper newlines
+echo $KEY_HEADER >> $KEY_FILE
+echo $SSH_KEY | sed "s/$KEY_HEADER//" | sed "s/$KEY_FOOTER//" | tr -d '\n ' >> $KEY_FILE
+echo >> $KEY_FILE
+echo $KEY_FOOTER >> $KEY_FILE
+chmod 400 $KEY_FILE
+echo "Loaded ssh key with fingerprint:"
+ssh-keygen -lf $KEY_FILE
+
+exec ssh -i $KEY_FILE -o StrictHostKeyChecking=no $HOST "bash -s" <<EOF
   set -e
   $CMD
   exit
