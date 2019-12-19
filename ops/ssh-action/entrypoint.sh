10fe50c807fb64c8b6977da0cfbdf7ef6ef9c7c0
Bo Henderson <bohende@gmail.com>
2019-12-19 14:55:20 +0530

    bugfix

--- ops/ssh-action/entrypoint.sh
+++ ops/ssh-action/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e -o pipefail
 
+echo "ssh-action activated"
+
 HOST="$1"
 SSH_KEY="$2"
 CMD="$3"
+
+echo "executing command \"$CMD\" on host $HOST (home: $HOME)"
 
 SSH_DIR="$HOME/.ssh"
 KEY_FILE="$SSH_DIR/id_rsa"
@@ -11,14 +15,19 @@ KEY_FILE="$SSH_DIR/id_rsa"
 mkdir -p $SSH_DIR
 rm -f $KEY_FILE $SSH_DIR/known_hosts
 touch $KEY_FILE $SSH_DIR/known_hosts
+echo "Creating ssh key"
 echo '-----BEGIN RSA PRIVATE KEY-----' >> $KEY_FILE
 echo $SSH_KEY >> $KEY_FILE
 echo '-----END RSA PRIVATE KEY-----' >> $KEY_FILE
 chmod 400 $KEY_FILE
+echo "created ssh key"
 ssh-keygen -lf $KEY_FILE
-ssh-keyscan -t rsa $STAGING_DOMAINNAME >> $SSH_DIR/known_hosts
-ssh -i $KEY_FILE -o StrictHostKeyChecking=no ubuntu@$STAGING_DOMAINNAME hostname
+echo "key scanning"
+ssh-keyscan -t rsa ${HOST#*@} >> $SSH_DIR/known_hosts
+echo "doing test connection"
+ssh -i $KEY_FILE -o StrictHostKeyChecking=no $HOST hostname
 
+echo "executing cmd for real"
 exec ssh -i $KEY_FILE $HOST "bash -s" <<EOF
   set -e
   $CMD
