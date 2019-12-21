7c83920be99d036d73c67784e1a2fa6d9574e985
Rahul <rksethuram9@gmail.com>
2019-12-21 12:54:02 +0100

    Deploy contracts before tests start

--- modules/node/src/test/integration/deposit/deposit.spec.ts
+++ modules/node/src/test/integration/deposit/deposit.spec.ts
@@ -3,9 +3,13 @@ import { ClientOptions, IConnextClient } from "@connext/types";
 import { INestApplication, INestMicroservice } from "@nestjs/common";
 import { Transport } from "@nestjs/microservices";
 import { Test } from "@nestjs/testing";
+import { getConnectionToken } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
 
 import { AppModule } from "../../../app.module";
 import { ConfigService } from "../../../config/config.service";
+import { clearDb } from "../../db";
+import { deployContracts } from "../../deployContracts";
 import { MemoryStoreServiceFactory } from "../../store";
 
 describe("Deposits", () => {
@@ -20,6 +24,9 @@ describe("Deposits", () => {
     }).compile();
 
     app = module.createNestApplication();
+    const connection = app.get<Connection>(getConnectionToken());
+    await clearDb(connection);
+
     await app.init();
 
     const config = app.get(ConfigService);
@@ -31,15 +38,20 @@ describe("Deposits", () => {
       transport: Transport.NATS,
     });
     await micro.init();
-  });
+
+    const wallet = config.getEthWallet();
+    const mnemonic = config.getMnemonic();
+    await deployContracts(wallet, mnemonic);
+  }, 90000);
 
   test(`should deposit ETH in the happy case.`, async () => {
     const config = app.get(ConfigService);
+
     const messagingUrl = config.getMessagingConfig().messagingUrl;
     // client setup
     const storeServiceFactory = new MemoryStoreServiceFactory();
 
-    const nodeUrl = typeof messagingUrl === "string" ? messagingUrl : messagingUrl[0]
+    const nodeUrl = typeof messagingUrl === "string" ? messagingUrl : messagingUrl[0];
 
     // client A
     const clientAStore = storeServiceFactory.createStoreService();
@@ -54,7 +66,7 @@ describe("Deposits", () => {
     clientA = await connext.connect(clientAOpts);
     await clientA.isAvailable();
 
-    console.log('clientA: ', clientA);
+    console.log("clientA: ", clientA);
     // console.log('clientA.publicIdentifier: ', clientA.publicIdentifier);
     expect(clientA.freeBalanceAddress).toBeTruthy();
   }, 90000);
