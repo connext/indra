cc9cc7b7f0a5a1ad5ff5257890d40c620f689cb1
Rahul <rksethuram9@gmail.com>
2019-12-21 17:16:58 +0100

    ✅ Add deposit test happy case

---
+++ modules/integration-test/src/deposit/deposit.test.ts
@@ -0,0 +1,41 @@
+import { connect } from "@connext/client";
+import { ClientOptions, IConnextClient } from "@connext/types";
+import { AddressZero } from "ethers/constants";
+
+import { MemoryStoreServiceFactory } from "../util/store";
+
+describe("Deposits", () => {
+  let clientA: IConnextClient;
+  beforeAll(async () => {
+    const storeServiceFactory = new MemoryStoreServiceFactory();
+
+    // client A
+    const clientAStore = storeServiceFactory.createStoreService();
+    const clientAOpts: ClientOptions = {
+      ethProviderUrl: "http://localhost:8545",
+      logLevel: 4,
+      mnemonic:
+        "humble sense shrug young vehicle assault destroy cook property average silent travel",
+      nodeUrl: "nats://localhost:4222",
+      store: clientAStore,
+    };
+    clientA = await connect(clientAOpts);
+
+    // TODO: add client endpoint to get node config, so we can easily have its xpub etc
+
+    await clientA.isAvailable();
+
+    expect(clientA.freeBalanceAddress).toBeTruthy();
+    expect(clientA.publicIdentifier).toBeTruthy();
+  }, 90_000);
+
+  test("client A should deposit ETH", async () => {
+    const depositResult = await clientA.deposit({ amount: "1", assetId: AddressZero });
+    console.log("depositResult: ", depositResult);
+    const freeBalance = await clientA.getFreeBalance(AddressZero);
+
+    expect(freeBalance[clientA.freeBalanceAddress].toNumber()).toEqual(1);
+
+    // TODO: add node endpoint to get free balance and assert it's 0
+  }, 30_000);
+});
