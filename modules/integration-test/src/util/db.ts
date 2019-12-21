93ac2f19cb6ea725167e91ede66547c7f9f8d27c
Rahul <rksethuram9@gmail.com>
2019-12-21 21:51:08 +0100

    🗃 DB clear

---
+++ modules/integration-test/src/util/db.ts
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+
+const client = new Client({
+  database: "indra",
+  host: "localhost",
+  password: "indra",
+  port: 5432,
+  user: "indra",
+});
+
+client.connect();
+
+export const clearDb = async (): Promise<void> => {
+  console.log(`Clearing database`);
+  await client.query("truncate table app_registry cascade;");
+  await client.query("truncate table channel cascade;");
+  await client.query("truncate table channel_payment_profiles_payment_profile cascade;");
+  await client.query("truncate table linked_transfer cascade;");
+  await client.query("truncate table node_records cascade;");
+  await client.query("truncate table onchain_transaction cascade;");
+  await client.query("truncate table payment_profile cascade;");
+  await client.query("truncate table peer_to_peer_transfer cascade;");
+  console.log(`Cleared database`);
+};
