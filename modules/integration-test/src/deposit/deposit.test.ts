551ea033bdb93059481f02e92f525a5cdf1f0d00
Rahul <rksethuram9@gmail.com>
2019-12-22 12:56:42 +0100

    🎨 Clean up test a lot

--- modules/integration-test/src/deposit/deposit.test.ts
+++ modules/integration-test/src/deposit/deposit.test.ts
@@ -1,46 +1,41 @@
-import { connect } from "@connext/client";
-import { ClientOptions, IConnextClient } from "@connext/types";
+import { xkeyKthAddress } from "@connext/cf-core";
+import { IConnextClient } from "@connext/types";
 import { AddressZero } from "ethers/constants";
 
+import { createClient } from "../util/client";
+import { FUNDED_MNEMONICS } from "../util/constants";
 import { clearDb } from "../util/db";
-import { MemoryStoreServiceFactory } from "../util/store";
 
 describe("Deposits", () => {
   let clientA: IConnextClient;
-  beforeAll(async () => {
-    // TODO: clearDb doesnt work between test runs because it deletes necessary things
-    // such as app registry. need to snapshot the db instead.
-    // await clearDb();
 
-    const storeServiceFactory = new MemoryStoreServiceFactory();
-
-    // client A
-    const clientAStore = storeServiceFactory.createStoreService();
-    const clientAOpts: ClientOptions = {
-      ethProviderUrl: "http://localhost:8545",
-      logLevel: 4,
-      mnemonic:
-        "humble sense shrug young vehicle assault destroy cook property average silent travel",
-      nodeUrl: "nats://localhost:4222",
-      store: clientAStore,
-    };
-    clientA = await connect(clientAOpts);
-
-    // TODO: add client endpoint to get node config, so we can easily have its xpub etc
-
-    await clientA.isAvailable();
-
-    expect(clientA.freeBalanceAddress).toBeTruthy();
-    expect(clientA.publicIdentifier).toBeTruthy();
+  beforeEach(async () => {
+    // TODO: try to snapshot db instead
+    await clearDb();
+    clientA = await createClient(FUNDED_MNEMONICS[0]);
   }, 90_000);
 
   test("client A should deposit ETH", async () => {
-    const depositResult = await clientA.deposit({ amount: "1", assetId: AddressZero });
-    console.log("depositResult: ", depositResult);
+    await clientA.deposit({ amount: "1", assetId: AddressZero });
     const freeBalance = await clientA.getFreeBalance(AddressZero);
 
-    expect(freeBalance[clientA.freeBalanceAddress].toNumber()).toEqual(1);
+    const nodeFreeBalanceAddress = xkeyKthAddress(clientA.config.nodePublicIdentifier);
+    expect(freeBalance[clientA.freeBalanceAddress]).toBeBigNumberEq(1);
+    expect(freeBalance[nodeFreeBalanceAddress]).toBeBigNumberEq(0);
 
-    // TODO: add node endpoint to get free balance and assert it's 0
-  }, 30_000);
+    // TODO: assert node's version of free balance also?
+  });
+
+  test("client A should deposit tokens", async () => {
+    const tokenAddress = clientA.config.contractAddresses.Token;
+
+    await clientA.deposit({ amount: "1", assetId: tokenAddress });
+    const freeBalance = await clientA.getFreeBalance(tokenAddress);
+
+    const nodeFreeBalanceAddress = xkeyKthAddress(clientA.config.nodePublicIdentifier);
+    expect(freeBalance[clientA.freeBalanceAddress]).toBeBigNumberEq(1);
+    expect(freeBalance[nodeFreeBalanceAddress]).toBeBigNumberEq(0);
+
+    // TODO: assert node's version of free balance also?
+  });
 });
