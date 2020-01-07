822752aad56af00d7068f4ee9c32db5aabb67dff
Bo Henderson <bohende@gmail.com>
2020-01-07 16:14:06 +0530

    🐛 misc tinkering that'll hopefully fix stuff

--- ops/test-integration.sh
+++ ops/test-integration.sh
@@ -2,22 +2,33 @@
 set -e
 
 version="${1:-latest}"
+name="indra_test_runner"
+
+# Kill the service when this script exits
+function cleanup {
+  echo "Stopping test runner.."
+  docker container stop $name || true
+  echo "Done!"
+  echo "Node logs:"
+  docker service logs --raw --tail 200 indra_node
+}
+trap cleanup EXIT
+
 
 # Damn I forget where I copy/pasted this witchcraft from, yikes.
 # It's supposed to find out whether we're calling this script from a shell & can print stuff
 # Or whether it's running in the background of another script and can't attach to a screen
 test -t 0 -a -t 1 -a -t 2 && interactive="--interactive"
 
-network="indra_test_runner"
-docker network create --attachable $network 2> /dev/null || true
+docker network create --attachable $name 2> /dev/null || true
 
-exec docker run \
+docker run \
   --env="INDRA_CLIENT_LOG_LEVEL=$LOG_LEVEL" \
   --env="INDRA_ETH_RPC_URL=$ETH_RPC_URL" \
   --env="INDRA_NODE_URL=$NODE_URL" \
   $interactive \
-  --name="indra_test_runner" \
-  --network="$network" \
+  --name="$name" \
+  --network="$name" \
   --rm \
   --tty \
-  indra_test_runner:$version "$@"
+  $name:$version "$@"
