45abbd18274ee5d0e422fe46710fc47331dd7349
Rahul Sethuram <rahul@connext.network>
2020-01-07 08:30:44 -0800

    Add extra matcher

--- modules/test-runner/src/setup.ts
+++ modules/test-runner/src/setup.ts
@@ -1,18 +1,32 @@
+import { BigNumber, BigNumberish } from "ethers/utils";
+
 expect.extend({
-  toBeBigNumberEq(
-    received,
-    equalTo,
-  ) {
+  toBeBigNumberEq(received: BigNumber, equalTo: BigNumberish): any {
     const pass = received.eq(equalTo);
     if (pass) {
       return {
-        message: () =>
+        message: (): string =>
           `expected ${received.toString()} not to be equal to ${equalTo.toString()}`,
         pass: true,
       };
     }
     return {
-      message: () => `expected ${received.toString()} to be equal to ${equalTo.toString()}`,
+      message: (): string => `expected ${received.toString()} to be equal to ${equalTo.toString()}`,
+      pass: false,
+    };
+  },
+  toBeBigNumberGte(received: BigNumber, equalTo: BigNumberish): any {
+    const pass = received.gte(equalTo);
+    if (pass) {
+      return {
+        message: (): string =>
+          `expected ${received.toString()} not to be greater than or equal to ${equalTo.toString()}`,
+        pass: true,
+      };
+    }
+    return {
+      message: (): string =>
+        `expected ${received.toString()} to be greater than or equal to ${equalTo.toString()}`,
       pass: false,
     };
   },
--- modules/test-runner/src/typings/index.d.ts
+++ modules/test-runner/src/typings/index.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace jest {
     interface Matchers<R, T> {
       toBeBigNumberEq(equalTo: BigNumberish): R;
+      toBeBigNumberGte(greaterThanOrEqualTo: BigNumberish): R;
     }
   }
 }
