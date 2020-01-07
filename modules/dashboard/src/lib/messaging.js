e7562b74f52d2276bd10b9f6a4f9fa5509f67cec
LayneHaber <layne.haber@gmail.com>
2020-01-06 19:41:18 -0700

    fix todo

--- modules/dashboard/src/lib/messaging.js
+++ modules/dashboard/src/lib/messaging.js
@@ -1,13 +1,11 @@
 import { MessagingServiceFactory } from "@connext/messaging";
+import { CF_PATH } from "@connext/types";
 import uuid from "uuid";
 import { Wallet } from "ethers";
 import { arrayify } from "ethers/utils";
 import { fromExtendedKey, fromMnemonic } from "ethers/utils/hdnode";
 
 import { stringify } from "./utils";
-
-// TODO: import from connext/types
-const CF_PATH = "m/44'/60'/0'/25446";
 
 const NATS_ATTEMPTS = 2;
 const NATS_TIMEOUT = 90_000;
