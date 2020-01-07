b029c7f4f81d1f8374d85115c1f4833a6281c99c
Bo Henderson <bohende@gmail.com>
2020-01-07 15:37:31 +0530

    🐛 re-add network to test-runner

--- ops/test-integration.sh
+++ ops/test-integration.sh
@@ -8,12 +8,16 @@ version="${1:-latest}"
 # Or whether it's running in the background of another script and can't attach to a screen
 test -t 0 -a -t 1 -a -t 2 && interactive="--interactive"
 
+network="indra_test_runner"
+docker network create --attachable $network 2> /dev/null || true
+
 exec docker run \
   --env="INDRA_CLIENT_LOG_LEVEL=$LOG_LEVEL" \
   --env="INDRA_ETH_RPC_URL=$ETH_RPC_URL" \
   --env="INDRA_NODE_URL=$NODE_URL" \
   $interactive \
   --name="indra_test_runner" \
+  --network="$network" \
   --rm \
   --tty \
   indra_test_runner:$version "$@"
