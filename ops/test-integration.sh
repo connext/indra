ff7825a1cd2ffb6451dbb09716dd54be79f9fefa
Bo Henderson <bohende@gmail.com>
2020-01-07 15:18:24 +0530

    🐛 re-add interactive flag

--- ops/test-integration.sh
+++ ops/test-integration.sh
@@ -3,11 +3,16 @@ set -e
 
 version="${1:-latest}"
 
+# Damn I forget where I copy/pasted this witchcraft from, yikes.
+# It's supposed to find out whether we're calling this script from a shell & can print stuff
+# Or whether it's running in the background of another script and can't attach to a screen
+test -t 0 -a -t 1 -a -t 2 && interactive="--interactive"
+
 exec docker run \
   --env="INDRA_CLIENT_LOG_LEVEL=$LOG_LEVEL" \
   --env="INDRA_ETH_RPC_URL=$ETH_RPC_URL" \
   --env="INDRA_NODE_URL=$NODE_URL" \
-  --interactive \
+  $interactive \
   --name="indra_test_runner" \
   --rm \
   --tty \
