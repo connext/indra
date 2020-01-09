4d13a6b18576bdbdc40282e333a625b1a4869018
Arjun <arjunbhuptani@gmail.com>
2020-01-09 16:47:30 +0400

    wip walletconenct setup

--- modules/test-runner/src/channelProvider/channelProvider.test.ts
+++ modules/test-runner/src/channelProvider/channelProvider.test.ts
@@ -1,18 +1,22 @@
 import { createClient } from "../util/client";
 import { IConnextClient } from "@connext/types";
 import { xkeyKthAddress } from "@connext/cf-core";
+import WalletConnectChannelProvider from "@walletconnect/channel-provider";
 
 describe("ChannelProvider", () => {
   let clientA: IConnextClient;
   let tokenAddress: string;
   let nodeFreeBalanceAddress: string;
   let nodePublicIdentifier: string;
+  let channelProvider: WalletConnectChannelProvider;
 
   beforeEach(async () => {
     clientA = await createClient();
     tokenAddress = clientA.config.contractAddresses.Token;
     nodePublicIdentifier = clientA.config.nodePublicIdentifier;
     nodeFreeBalanceAddress = xkeyKthAddress(nodePublicIdentifier);
+    channelProvider = new WalletConnectChannelProvider({ qrcode: false });
+    channelProvider.enable();
   }, 90_000);
 
   test("Happy case: client A1 can be instantiated with a channelProvider generated from client A", async () => {
