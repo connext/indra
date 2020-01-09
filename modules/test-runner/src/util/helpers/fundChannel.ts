04c0154e86c7d0b139eef2ecdb2107d08b0a3cf4
LayneHaber <layne.haber@gmail.com>
2020-01-08 21:28:47 -0700

    use once

--- modules/test-runner/src/util/helpers/fundChannel.ts
+++ modules/test-runner/src/util/helpers/fundChannel.ts
@@ -10,7 +10,7 @@ export const fundChannel = async (
   const prevFreeBalance = await client.getFreeBalance();
   await new Promise(async resolve => {
     // TODO: should add `once` to top level client
-    client.on("DEPOSIT_CONFIRMED_EVENT", async () => {
+    client.once("DEPOSIT_CONFIRMED_EVENT", async () => {
       const freeBalance = await client.getFreeBalance(assetId);
       // verify free balance increased as expected
       const expected = prevFreeBalance[client.freeBalanceAddress].add(parseEther(amount));
