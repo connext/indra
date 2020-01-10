e0fb164968406a366d024c99b9e1bd547a1b54f6
Rahul Sethuram <rahul@connext.network>
2020-01-10 15:27:26 -0800

    Add token test case

--- modules/test-runner/src/transfer/asyncTransfer.test.ts
+++ modules/test-runner/src/transfer/asyncTransfer.test.ts
@@ -77,4 +77,61 @@ describe("Async Transfers", () => {
       preTransferFreeBalanceEthNodeB.sub(transferAmount),
     );
   });
+
+  test("happy case: client A transfers tokens to client B through node", async () => {
+    const transferAmount = parseEther("1");
+    await clientA.deposit({ amount: transferAmount.toString(), assetId: tokenAddress });
+    await clientB.requestCollateral(tokenAddress);
+
+    const {
+      [clientA.freeBalanceAddress]: preTransferFreeBalanceClientA,
+      [nodeFreeBalanceAddress]: preTransferFreeBalanceNodeA,
+    } = await clientA.getFreeBalance(tokenAddress);
+
+    const {
+      [clientB.freeBalanceAddress]: preTransferFreeBalanceClientB,
+      [nodeFreeBalanceAddress]: preTransferFreeBalanceNodeB,
+    } = await clientB.getFreeBalance(tokenAddress);
+
+    expect(preTransferFreeBalanceClientA).toBeBigNumberEq(transferAmount);
+    expect(preTransferFreeBalanceNodeA).toBeBigNumberEq(0);
+
+    expect(preTransferFreeBalanceClientB).toBeBigNumberEq(0);
+    expect(preTransferFreeBalanceNodeB).toBeBigNumberGte(transferAmount);
+
+    const senderDone = new Promise((res: any): any => clientA.on("UNINSTALL_EVENT", res));
+    const recipientDone = new Promise((res: any): any =>
+      clientB.on("RECIEVE_TRANSFER_FINISHED_EVENT", res),
+    );
+
+    await clientA.transfer({
+      amount: transferAmount.toString(),
+      assetId: tokenAddress,
+      meta: { hello: "world" },
+      recipient: clientB.publicIdentifier,
+    });
+
+    await recipientDone;
+
+    // TODO: this doesn't work
+    // await senderDone;
+
+    const {
+      [clientA.freeBalanceAddress]: postTransferFreeBalanceClientA,
+      [nodeFreeBalanceAddress]: postTransferFreeBalanceNodeA,
+    } = await clientA.getFreeBalance(tokenAddress);
+
+    const {
+      [clientB.freeBalanceAddress]: postTransferFreeBalanceClientB,
+      [nodeFreeBalanceAddress]: postTransferFreeBalanceNodeB,
+    } = await clientB.getFreeBalance(tokenAddress);
+
+    expect(postTransferFreeBalanceClientA).toBeBigNumberEq(0);
+    // expect(postTransferFreeBalanceNodeA).toBeBigNumberEq(transferAmount);
+
+    expect(postTransferFreeBalanceClientB).toBeBigNumberEq(transferAmount);
+    expect(postTransferFreeBalanceNodeB).toBeBigNumberEq(
+      preTransferFreeBalanceNodeB.sub(transferAmount),
+    );
+  });
 });
