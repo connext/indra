9bbfbbab0e86d52bf21260b8c561807c640bdaa7
Bo Henderson <bohende@gmail.com>
2020-01-10 12:48:41 +0530

    ➕ client: add eth-crypto and test cross-compatibility

--- modules/client/src/lib/crypto.spec.ts
+++ modules/client/src/lib/crypto.spec.ts
@@ -1,16 +1,48 @@
+import * as EthCrypto from "eth-crypto";
 import { Wallet } from "ethers";
 import { computePublicKey } from "ethers/utils";
 
 import { decryptWithPrivateKey, encryptWithPublicKey } from "./crypto";
 
 describe("crypto", () => {
-  test("we should be able to decrypt the thing that's encrypted", async () => {
+  test("we should be able to decrypt newly encrypted things", async () => {
     const prvKey = Wallet.createRandom().privateKey;
     const pubKey = computePublicKey(prvKey);
     const message = "Hello World!";
     const encrypted = await encryptWithPublicKey(pubKey, message);
     const decrypted = await decryptWithPrivateKey(prvKey, encrypted);
     console.log(`Decryped message: ${decrypted}`);
+    expect(message).toEqual(decrypted);
+  });
+
+  test("we should be able to decrypt messages that eth-crypto encrypted", async () => {
+    const prvKey = Wallet.createRandom().privateKey;
+    const pubKey = computePublicKey(prvKey);
+    const message = "Hello World!";
+    const encrypted = await EthCrypto.encryptWithPublicKey(pubKey, message);
+    const encryptedMessage = EthCrypto.cipher.stringify(encrypted);
+    const decrypted = await decryptWithPrivateKey(prvKey, encryptedMessage);
+    console.log(`Decryped message: ${decrypted}`);
+    expect(message).toEqual(decrypted);
+  });
+
+  test("we should be able to decrypt messages that previous versions encrypted", async () => {
+    const mnemonic = "trophy chimney shove high merry type ready weasel trouble join hobby quick";
+    const message = "0x7c728ba4ad2f5d2184ab5dd36b232764b1e3f9e29a311a5ef494373f5450df12";
+    const encryptedMessage =
+      "0f" +
+      "c09f11f955bd9b4aa4122758d04c3003e2696d406176a9d56f757b3b978b7997" +
+      "0106d265b80f0a586439430c91a5349ab1a1520d08c9181f9304e169cc6350f1" +
+      "6fd440a3b548577aee91312056f9dccefe028b6bd1af2f2c65b561d6b9a6901f" +
+      "5fac024238df89781e607e2ae612e6cfd20b3c95f5c0097af1d103c25520dca9" +
+      "7c3756c82ba8629e14c28b15cba208451ae577c2540c0bcd1afbf94db23dd25a";
+    const prvKey = Wallet.fromMnemonic(mnemonic).privateKey;
+    const pubKey = computePublicKey(prvKey);
+    const encrypted = await encryptWithPublicKey(pubKey, message);
+    const decrypted = await decryptWithPrivateKey(prvKey, encrypted);
+    console.log(`Encrypted message: ${decrypted}`);
+    console.log(`Decryped message: ${decrypted}`);
+    expect(encryptedMessage).toEqual(encrypted);
     expect(message).toEqual(decrypted);
   });
 });
--- modules/client/src/lib/crypto.ts
+++ modules/client/src/lib/crypto.ts
@@ -1,4 +1,5 @@
 import { decrypt, encrypt } from "eccrypto";
+import { arrayify } from "ethers/utils";
 import { publicKeyConvert } from "secp256k1";
 
 type Encrypted = {
@@ -8,140 +9,26 @@ type Encrypted = {
   mac: string;
 };
 
-function removeTrailing0x(str: string): string {
-  if (str.startsWith("0x")) {
-    return str.substring(2);
-  }
-  return str;
-}
+const no0x = (hex: string): string => hex.replace(/^0x/, "");
 
-function addTrailing0x(str: string): string {
-  if (!str.startsWith("0x")) {
-    return `0x${str}`;
-  }
-  return str;
-}
-
-function compress(startsWith04: string): string {
-  // add trailing 04 if not done before
-  const testBuffer = Buffer.from(startsWith04, "hex");
-  if (testBuffer.length === 64) {
-    startsWith04 = `04${startsWith04}`; // tslint:ignore-line:no-parameter-reassignment
-  }
+const compress = (pubKey: string): string => {
+  const startsWith04 =
+    Buffer.from(pubKey.replace(/^0x/, ""), "hex").length === 64
+      ? `04${pubKey.replace(/^0x/, "")}`
+      : pubKey.replace(/^0x/, "");
   return publicKeyConvert(Buffer.from(startsWith04, "hex"), true).toString("hex");
-}
-
-function decompress(startsWith02Or03: string): string {
-  // if already decompressed an not has trailing 04
-  const testBuffer = Buffer.from(startsWith02Or03, "hex");
-  if (testBuffer.length === 64) {
-    startsWith02Or03 = `04${startsWith02Or03}`; // tslint:ignore-line:no-parameter-reassignment
-  }
-  let decompressed = publicKeyConvert(Buffer.from(startsWith02Or03, "hex"), false).toString("hex");
-  // remove trailing 04
-  decompressed = decompressed.substring(2);
-  return decompressed;
-}
-
-function stringify(cipher: Encrypted): string {
-  if (typeof cipher === "string") {
-    return cipher;
-  }
-
-  // use compressed key because it's smaller
-  const compressedKey = compress(cipher.ephemPublicKey);
-
-  const ret = Buffer.concat([
-    Buffer.from(cipher.iv, "hex"), // 16bit
-    Buffer.from(compressedKey, "hex"), // 33bit
-    Buffer.from(cipher.mac, "hex"), // 32bit
-    Buffer.from(cipher.ciphertext, "hex"), // var bit
-  ]);
-
-  return ret.toString("hex");
-}
-
-function parse(str: string): Encrypted {
-  if (typeof str !== "string") {
-    return str;
-  }
-
-  const buf = Buffer.from(str, "hex");
-
-  const ret = {
-    ciphertext: buf.toString("hex", 81, buf.length),
-    ephemPublicKey: buf.toString("hex", 16, 49),
-    iv: buf.toString("hex", 0, 16),
-    mac: buf.toString("hex", 49, 81),
-  };
-
-  // decompress publicKey
-  ret.ephemPublicKey = `04${decompress(ret.ephemPublicKey)}`;
-
-  return ret;
-}
-
-async function innerEncryptWithPublicKey(publicKey: string, message: string): Promise<Encrypted> {
-  // ensure its an uncompressed publicKey
-  publicKey = decompress(publicKey); // tslint:ignore-line:no-parameter-reassignment
-
-  // re-add the compression-flag
-  const pubString = `04${publicKey}`;
-
-  const encryptedBuffers = await encrypt(Buffer.from(pubString, "hex"), Buffer.from(message));
-
-  const encrypted = {
-    ciphertext: encryptedBuffers.ciphertext.toString("hex"),
-    ephemPublicKey: encryptedBuffers.ephemPublicKey.toString("hex"),
-    iv: encryptedBuffers.iv.toString("hex"),
-    mac: encryptedBuffers.mac.toString("hex"),
-  };
-  return encrypted;
-}
-
-async function innerDecryptWithPrivateKey(
-  privateKey: string,
-  encrypted: Encrypted,
-): Promise<string> {
-  if (typeof encrypted === "string") {
-    encrypted = parse(encrypted); // tslint:ignore-line:no-parameter-reassignment
-  }
-
-  // remove trailing '0x' from privateKey
-  const twoStripped = removeTrailing0x(privateKey);
-
-  const encryptedBuffer = {
-    ciphertext: Buffer.from(encrypted.ciphertext, "hex"),
-    ephemPublicKey: Buffer.from(encrypted.ephemPublicKey, "hex"),
-    iv: Buffer.from(encrypted.iv, "hex"),
-    mac: Buffer.from(encrypted.mac, "hex"),
-  };
-
-  const decryptedBuffer = await decrypt(Buffer.from(twoStripped, "hex"), encryptedBuffer);
-
-  return decryptedBuffer.toString();
-}
-
-export const encryptWithPublicKey = async (publicKey: string, message: string): Promise<string> => {
-  return stringify(await innerEncryptWithPublicKey(publicKey, message));
 };
 
-export const decryptWithPrivateKey = async (
-  privateKey: string,
-  message: string,
-): Promise<string> => {
-  return await innerDecryptWithPrivateKey(privateKey, parse(message));
+const decompress = (pubKey: string): string => {
+  const prefixed = Buffer.from(pubKey, "hex").length === 64 ? `04${pubKey}` : pubKey;
+  return publicKeyConvert(Buffer.from(prefixed.replace(/^0x/, ""), "hex"), false)
+    .toString("hex")
+    .substring(2);
 };
 
-// METHODS ABOVE CREATED BY PEDRO (BUT UTILIZING NEW INTERFACE CREATED BY BO)
-// METHODS BELOW CLEANED UP (& MAYBE BROKEN) BY BO
-
-/*
 export const encryptWithPublicKey = async (publicKey: string, message: string): Promise<string> => {
-  const encryptedBuffers = await encrypt(
-    Buffer.from(`04${decompress(publicKey)}`, "hex"),
-    Buffer.from(message),
-  );
+  const key = `04${decompress(publicKey)}`;
+  const encryptedBuffers = await encrypt(Buffer.from(key, "hex"), Buffer.from(message));
   const encrypted: Encrypted = {
     ciphertext: encryptedBuffers.ciphertext.toString("hex"),
     ephemPublicKey: encryptedBuffers.ephemPublicKey.toString("hex"),
@@ -178,4 +65,3 @@ export const decryptWithPrivateKey = async (
   const decryptedBuffer = await decrypt(Buffer.from(twoStripped, "hex"), encryptedBuffer);
   return decryptedBuffer.toString();
 };
-*/
