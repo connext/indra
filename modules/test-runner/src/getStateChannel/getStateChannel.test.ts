438133e45b95ec99d7746d31e746bfdddab52382
Arjun <arjunbhuptani@gmail.com>
2020-01-11 00:50:17 +0400

    most of wrong multisig test

--- modules/test-runner/src/getStateChannel/getStateChannel.test.ts
+++ modules/test-runner/src/getStateChannel/getStateChannel.test.ts
@@ -1,5 +1,5 @@
 import { StateChannel, xkeyKthAddress } from "@connext/cf-core";
-import { IConnextClient, StateChannelJSON, ClientOptions, Store } from "@connext/types";
+import { ClientOptions, IConnextClient, StateChannelJSON, Store, StorePair } from "@connext/types";
 import { AddressZero } from "ethers/constants";
 import { parseEther } from "ethers/utils";
 import { createClient, getStore } from "../util";
@@ -32,11 +32,18 @@ describe("Get State Channel", () => {
     );
   });
 
-  test("Store contains state channel on wrong multisig address", async () => {
-    // How do we test this?
+  test.only("Store contains state channel on wrong multisig address", async () => {
+    const store: Store = getStore();
+    const path: `${this.storeKeyPrefix}/${DB_NAMESPACE_CHANNEL}/${stateChannel.multisigAddress}`;
+    const value: StateChannel = await store.get(path);
+
+    const pair: StorePair[] = [{ path, value }];
+    store.set(pair);
+
+    await expect(clientA.getStateChannel()).rejects.toThrowError("something");
   });
 
-  test.only("Store contains multiple state channels", async () => {
+  test("Store contains multiple state channels", async () => {
     // Get store from clientA
     const store: Store = getStore();
     const opts: any = { store };
@@ -46,6 +53,7 @@ describe("Get State Channel", () => {
     await clientB.deposit({ amount: parseEther("0.01").toString(), assetId: AddressZero });
     await clientB.requestCollateral(tokenAddress);
 
+    // Now check both exist in the same store
     const stateChannelA: StateChannelJSON = (await clientA.getStateChannel()).data;
     const stateChannelB: StateChannelJSON = (await clientB.getStateChannel()).data;
     expect(stateChannelA.multisigAddress).toBe(clientA.multisigAddress);
