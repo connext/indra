b86fd1af72014f929ab6045ab39fe6732bbe851f
Pedro Gomes <pedrogomes94@gmail.com>
2020-01-11 12:34:04 +0100

    moar logs

--- modules/test-runner/src/channelProvider/channelProvider.test.ts
+++ modules/test-runner/src/channelProvider/channelProvider.test.ts
@@ -12,28 +12,46 @@ describe("ChannelProvider", () => {
   let channelProvider: ChannelProvider;
 
   beforeEach(async () => {
+    console.log("[beforeEach]", "===========>", true);
+
     clientA = await createClient();
+    console.log("[beforeEach]", "clientA", clientA);
+
     tokenAddress = clientA.config.contractAddresses.Token;
     console.log("[beforeEach]", "tokenAddress", tokenAddress);
+
     nodePublicIdentifier = clientA.config.nodePublicIdentifier;
     console.log("[beforeEach]", "nodePublicIdentifier", nodePublicIdentifier);
+
     nodeFreeBalanceAddress = xkeyKthAddress(nodePublicIdentifier);
     console.log("[beforeEach]", "nodeFreeBalanceAddress", nodeFreeBalanceAddress);
+
     channelProvider = await createChannelProvider(clientA);
+    console.log("[beforeEach]", "channelProvider", channelProvider);
   }, 90_000);
 
   // tslint:disable-next-line:max-line-length
   test("Happy case: client A1 can be instantiated with a channelProvider generated from client A", async () => {
+    console.log(" ====> Starting test with clientA", clientA);
+    console.log(" ====> Starting test with clientB", clientB);
+    console.log(" ====> Starting test with tokenAddress", tokenAddress);
+    console.log(" ====> Starting test with nodeFreeBalanceAddress", nodeFreeBalanceAddress);
+    console.log(" ====> Starting test with nodePublicIdentifier", nodePublicIdentifier);
     console.log(" ====> Starting test with channelProvider", channelProvider);
 
     clientB = await createRemoteClient(channelProvider);
 
+    console.log("[TEST]", "clientB", clientB);
+
     // tslint:disable-next-line:variable-name
     const _tokenAddress = clientB.config.contractAddresses.Token;
+    console.log("[TEST]", "_tokenAddress", _tokenAddress);
     // tslint:disable-next-line:variable-name
     const _nodePublicIdentifier = clientB.config.nodePublicIdentifier;
+    console.log("[TEST]", "_nodePublicIdentifier", _nodePublicIdentifier);
     // tslint:disable-next-line:variable-name
     const _nodeFreeBalanceAddress = xkeyKthAddress(nodePublicIdentifier);
+    console.log("[TEST]", "_nodeFreeBalanceAddress", _nodeFreeBalanceAddress);
 
     expect(_tokenAddress).toBe(tokenAddress);
     expect(_nodePublicIdentifier).toBe(nodeFreeBalanceAddress);
--- modules/test-runner/src/util/client.ts
+++ modules/test-runner/src/util/client.ts
@@ -1,6 +1,7 @@
 import { connect } from "@connext/client";
 import { ClientOptions, IConnextClient } from "@connext/types";
 import { Contract, Wallet } from "ethers";
+import { JsonRpcProvider } from "ethers/providers";
 import { parseEther } from "ethers/utils";
 import tokenAbi from "human-standard-token-abi";
 
@@ -15,8 +16,11 @@ export const createClient = async (
   mnemonic: string = Wallet.createRandom().mnemonic,
 ): Promise<IConnextClient> => {
   const storeServiceFactory = new MemoryStoreServiceFactory();
+  console.log("createClient", "storeServiceFactory", storeServiceFactory);
 
   const clientStore = storeServiceFactory.createStoreService();
+  console.log("createClient", "clientStore", clientStore);
+
   const clientOpts: ClientOptions = {
     ethProviderUrl: env.ethProviderUrl,
     logLevel: env.logLevel,
@@ -24,24 +28,32 @@ export const createClient = async (
     nodeUrl: env.nodeUrl,
     store: clientStore,
   };
-  const client = await connect(clientOpts);
+  console.log("createClient", "clientOpts", clientOpts);
 
+  const client = await connect(clientOpts);
+  console.log("createClient", "client", client);
   // TODO: add client endpoint to get node config, so we can easily have its xpub etc
 
   await client.isAvailable();
+  console.log("createClient", "isAvailable", true);
 
   const ethTx = await wallet.sendTransaction({
     to: client.signerAddress,
     value: parseEther("0.1"),
   });
+  console.log("createClient", "ethTx", ethTx);
+
   const token = new Contract(client.config.contractAddresses.Token, tokenAbi, wallet);
+  console.log("createClient", "token", token);
+
   const tokenTx = await token.functions.transfer(client.signerAddress, parseEther("10"));
+  console.log("createClient", "tokenTx", tokenTx);
 
   await Promise.all([ethTx.wait(), tokenTx.wait()]);
+  console.log("createClient", "Promise.all", true);
 
   expect(client.freeBalanceAddress).toBeTruthy();
   expect(client.publicIdentifier).toBeTruthy();
-
   return client;
 };
 
@@ -53,10 +65,13 @@ export const createRemoteClient = async (
     ethProviderUrl: env.ethProviderUrl,
     logLevel: env.logLevel,
   };
+  console.log("createRemoteClient", "clientOpts", clientOpts);
 
   const client = await connect(clientOpts);
+  console.log("createRemoteClient", "client", client);
 
   await client.isAvailable();
+  console.log("createRemoteClient", "isAvailable", true);
 
   expect(client.freeBalanceAddress).toBeTruthy();
   expect(client.publicIdentifier).toBeTruthy();
