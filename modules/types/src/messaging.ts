3f594491a9ead58765e7fd50abe9f08901df2799
Pedro Gomes <pedrogomes94@gmail.com>
2020-01-13 20:00:49 +0100

    move InternalClientOptions and NodeInitializationParameters to types modules

---
+++ modules/types/src/messaging.ts
@@ -0,0 +1,25 @@
+import { CFCoreTypes } from "./cf";
+
+export interface MessagingConfig {
+  clusterId?: string;
+  messagingUrl: string | string[];
+  token?: string;
+  logLevel: number;
+}
+
+export interface IMessagingService extends CFCoreTypes.IMessagingService {
+  connect(): Promise<void>;
+  disconnect(): Promise<void>;
+  flush(): Promise<void>;
+  onReceive(subject: string, callback: (msg: CFCoreTypes.NodeMessage) => void): Promise<void>;
+  publish(subject: string, data: any): Promise<void>;
+  request(
+    subject: string,
+    timeout: number,
+    data: object,
+    callback?: (response: any) => any,
+  ): Promise<any>;
+  send(to: string, msg: CFCoreTypes.NodeMessage): Promise<void>;
+  subscribe(subject: string, callback: (msg: CFCoreTypes.NodeMessage) => void): Promise<void>;
+  unsubscribe(subject: string): Promise<void>;
+}
