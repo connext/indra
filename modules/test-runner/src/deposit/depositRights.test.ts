f905753e6118cd2c2127f11b630254432655faa7
Rahul Sethuram <rahul@connext.network>
2020-01-13 12:41:20 -0800

    Help work on test

--- modules/test-runner/src/deposit/depositRights.test.ts
+++ modules/test-runner/src/deposit/depositRights.test.ts
@@ -13,12 +13,15 @@ describe("Deposits", () => {
   // tslint:disable-next-line:max-line-length
   test.only("happy case: client should request deposit rights and deposit ETH", async (done: jest.DoneCallback) => {
     await clientA.requestDepositRights({ assetId: AddressZero });
-    await clientA.rescindDepositRights({ assetId: AddressZero });
     const { [clientA.freeBalanceAddress]: preDeposit } = await clientA.getFreeBalance(AddressZero);
     expect(preDeposit).toBeBigNumberEq(One);
 
     // listen for tx to multisig address
+    const balance = await ethProvider.getBalance(clientA.multisigAddress);
+    console.log('balance: ', balance);
     ethProvider.on(clientA.multisigAddress, async () => {
+      const balance = await ethProvider.getBalance(clientA.multisigAddress);
+      console.log('balance: ', balance);
       await clientA.rescindDepositRights({ assetId: AddressZero });
       const { [clientA.freeBalanceAddress]: postDeposit } = await clientA.getFreeBalance(
         AddressZero,
