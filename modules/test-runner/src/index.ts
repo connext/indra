e3462f4bb07924e4ac3081e67374602921e0b3b6
LayneHaber <layne.haber@gmail.com>
2020-01-13 18:04:46 -0800

    update index

--- modules/test-runner/src/index.ts
+++ modules/test-runner/src/index.ts
@@ -1,5 +1,8 @@
+import "./createChannel/createChannel.test";
 import "./deposit/collateral.test";
 import "./deposit/deposit.test";
+import "./getStateChannel/getStateChannel.test";
+import "./restoreState/restoreState.test";
 import "./swap/swap.test";
 import "./transfer/asyncTransfer.test";
 import "./withdraw/withdraw.test";
