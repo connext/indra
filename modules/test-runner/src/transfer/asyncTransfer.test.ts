a6e9f2edeeddaeba254638bbd202d2fc6f0324c6
LayneHaber <layne.haber@gmail.com>
2020-01-14 12:44:03 -0800

    low hanging fruit tests

--- modules/test-runner/src/transfer/asyncTransfer.test.ts
+++ modules/test-runner/src/transfer/asyncTransfer.test.ts
@@ -1,9 +1,18 @@
 import { xkeyKthAddress } from "@connext/cf-core";
 import { IConnextClient } from "@connext/types";
+import { Wallet } from "ethers";
 import { AddressZero } from "ethers/constants";
-import { parseEther } from "ethers/utils";
+import { HDNode, hexlify, parseEther, randomBytes } from "ethers/utils";
 
-import { createClient } from "../util/client";
+import {
+  asyncTransferAsset,
+  createClient,
+  ETH_AMOUNT_LG,
+  ETH_AMOUNT_SM,
+  fundChannel,
+  ZERO_ONE,
+  ZERO_ZERO_ONE,
+} from "../util";
 
 describe("Async Transfers", () => {
   let clientA: IConnextClient;
@@ -22,170 +31,131 @@ describe("Async Transfers", () => {
   }, 90_000);
 
   test("happy case: client A transfers eth to client B through node", async () => {
-    const transferAmount = parseEther("0.01");
-    await clientA.deposit({ amount: transferAmount.toString(), assetId: AddressZero });
-    await clientB.requestCollateral(AddressZero);
+    const transferAmount = ETH_AMOUNT_SM;
+    const assetId = AddressZero;
+    await clientA.deposit({ amount: transferAmount.toString(), assetId });
+    await clientB.requestCollateral(assetId);
 
-    const {
-      [clientA.freeBalanceAddress]: preTransferFreeBalanceEthClientA,
-      [nodeFreeBalanceAddress]: preTransferFreeBalanceEthNodeA,
-    } = await clientA.getFreeBalance(AddressZero);
-
-    const {
-      [clientB.freeBalanceAddress]: preTransferFreeBalanceEthClientB,
-      [nodeFreeBalanceAddress]: preTransferFreeBalanceEthNodeB,
-    } = await clientB.getFreeBalance(AddressZero);
-
-    expect(preTransferFreeBalanceEthClientA).toBeBigNumberEq(transferAmount);
-    expect(preTransferFreeBalanceEthNodeA).toBeBigNumberEq(0);
-
-    expect(preTransferFreeBalanceEthClientB).toBeBigNumberEq(0);
-    expect(preTransferFreeBalanceEthNodeB).toBeBigNumberGte(transferAmount);
-
-    let paymentId;
-    await new Promise(async resolve => {
-      let count = 0;
-      clientA.once("UNINSTALL_EVENT", async () => {
-        count += 1;
-        if (count === 2) {
-          resolve();
-        }
-      });
-
-      clientB.once("RECIEVE_TRANSFER_FINISHED_EVENT", async () => {
-        count += 1;
-        if (count === 2) {
-          resolve();
-        }
-      });
-
-      const { paymentId: senderPaymentId, preImage: linkedPreImage } = await clientA.transfer({
-        amount: transferAmount.toString(),
-        assetId: AddressZero,
-        meta: { hello: "world" },
-        recipient: clientB.publicIdentifier,
-      });
-      paymentId = senderPaymentId;
-    });
-    expect((await clientB.getAppInstances()).length).toEqual(0);
-    expect((await clientA.getAppInstances()).length).toEqual(0);
-
-    const {
-      [clientA.freeBalanceAddress]: postTransferFreeBalanceEthClientA,
-      [nodeFreeBalanceAddress]: postTransferFreeBalanceEthNodeA,
-    } = await clientA.getFreeBalance(AddressZero);
-
-    const {
-      [clientB.freeBalanceAddress]: postTransferFreeBalanceEthClientB,
-      [nodeFreeBalanceAddress]: postTransferFreeBalanceEthNodeB,
-    } = await clientB.getFreeBalance(AddressZero);
-
-    expect(postTransferFreeBalanceEthClientA).toBeBigNumberEq(0);
-    expect(postTransferFreeBalanceEthNodeA).toBeBigNumberEq(transferAmount);
-
-    expect(postTransferFreeBalanceEthClientB).toBeBigNumberEq(transferAmount);
-    expect(postTransferFreeBalanceEthNodeB).toBeBigNumberEq(
-      preTransferFreeBalanceEthNodeB.sub(transferAmount),
-    );
-
-    // verify payment
-    const paymentA = await clientA.getLinkedTransfer(paymentId);
-    const paymentB = await clientB.getLinkedTransfer(paymentId);
-    expect(paymentA).toMatchObject({
-      paymentId,
-      assetId: AddressZero,
-      amount: transferAmount.toString(),
-      status: "RECLAIMED",
-      type: "LINKED",
-      senderPublicIdentifier: clientA.publicIdentifier,
-      receiverPublicIdentifier: clientB.publicIdentifier,
-      meta: { hello: "world" },
-    });
-    expect(paymentB).toMatchObject(paymentA);
+    await asyncTransferAsset(clientA, clientB, transferAmount, assetId, nodeFreeBalanceAddress);
   });
 
   test("happy case: client A transfers tokens to client B through node", async () => {
-    const transferAmount = parseEther("1");
-    await clientA.deposit({ amount: transferAmount.toString(), assetId: tokenAddress });
+    const transferAmount = ETH_AMOUNT_LG;
+    const assetId = tokenAddress;
+    await clientA.deposit({ amount: transferAmount.toString(), assetId });
+    await clientB.requestCollateral(assetId);
+
+    await asyncTransferAsset(clientA, clientB, transferAmount, assetId, nodeFreeBalanceAddress);
+  });
+
+  test("Bot A tries to transfer a negative amount", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+    // verify collateral
     await clientB.requestCollateral(tokenAddress);
 
-    const {
-      [clientA.freeBalanceAddress]: preTransferFreeBalanceClientA,
-      [nodeFreeBalanceAddress]: preTransferFreeBalanceNodeA,
-    } = await clientA.getFreeBalance(tokenAddress);
-
-    const {
-      [clientB.freeBalanceAddress]: preTransferFreeBalanceClientB,
-      [nodeFreeBalanceAddress]: preTransferFreeBalanceNodeB,
-    } = await clientB.getFreeBalance(tokenAddress);
-
-    expect(preTransferFreeBalanceClientA).toBeBigNumberEq(transferAmount);
-    expect(preTransferFreeBalanceNodeA).toBeBigNumberEq(0);
-
-    expect(preTransferFreeBalanceClientB).toBeBigNumberEq(0);
-    expect(preTransferFreeBalanceNodeB).toBeBigNumberGte(transferAmount);
-
-    let paymentId;
-    await new Promise(async resolve => {
-      let count = 0;
-      clientA.once("UNINSTALL_EVENT", async () => {
-        console.error(`Caught sender uninstall event!!!!!`);
-        count += 1;
-        if (count === 2) {
-          console.error(`resolving promise!`);
-          resolve();
-        }
-      });
-
-      clientB.once("RECIEVE_TRANSFER_FINISHED_EVENT", async () => {
-        count += 1;
-        if (count === 2) {
-          resolve();
-        }
-      });
-
-      const { paymentId: senderPaymentId } = await clientA.transfer({
-        amount: transferAmount.toString(),
+    await expect(
+      clientA.transfer({
+        amount: ETH_AMOUNT_SM.mul(-1).toString(),
         assetId: tokenAddress,
-        meta: { hello: "world" },
         recipient: clientB.publicIdentifier,
-      });
-      paymentId = senderPaymentId;
-    });
-    expect((await clientB.getAppInstances()).length).toEqual(0);
-    expect((await clientA.getAppInstances()).length).toEqual(0);
+      }),
+    ).rejects.toThrowError("Value -10000000000000000 is negative");
+  });
 
-    const {
-      [clientA.freeBalanceAddress]: postTransferFreeBalanceClientA,
-      [nodeFreeBalanceAddress]: postTransferFreeBalanceNodeA,
-    } = await clientA.getFreeBalance(tokenAddress);
+  test("Bot A tries to transfer with an invalid token address", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
 
-    const {
-      [clientB.freeBalanceAddress]: postTransferFreeBalanceClientB,
-      [nodeFreeBalanceAddress]: postTransferFreeBalanceNodeB,
-    } = await clientB.getFreeBalance(tokenAddress);
-
-    expect(postTransferFreeBalanceClientA).toBeBigNumberEq(0);
-    expect(postTransferFreeBalanceNodeA).toBeBigNumberEq(transferAmount);
-
-    expect(postTransferFreeBalanceClientB).toBeBigNumberEq(transferAmount);
-    expect(postTransferFreeBalanceNodeB).toBeBigNumberEq(
-      preTransferFreeBalanceNodeB.sub(transferAmount),
+    await expect(
+      clientA.transfer({
+        amount: ETH_AMOUNT_SM.toString(),
+        assetId: "0xabc",
+        recipient: clientB.publicIdentifier,
+      }),
+    ).rejects.toThrowError(
+      `Value "0xabc" is not a valid eth address, Value (${ETH_AMOUNT_SM.toString()}) is not less than or equal to 0`,
     );
+    // NOTE: will also include a `Value (..) is not less than or equal to 0
+    // because it will not be able to fetch the free balance of the assetId
+  });
 
-    // verify payment
-    const paymentA = await clientA.getLinkedTransfer(paymentId);
-    const paymentB = await clientB.getLinkedTransfer(paymentId);
-    expect(paymentA).toMatchObject({
-      paymentId,
-      assetId: tokenAddress,
-      amount: transferAmount.toString(),
-      status: "RECLAIMED",
-      type: "LINKED",
-      senderPublicIdentifier: clientA.publicIdentifier,
-      receiverPublicIdentifier: clientB.publicIdentifier,
-      meta: { hello: "world" },
-    });
-    expect(paymentB).toMatchObject(paymentA);
+  // TODO: need to add free balance of the asset in question to get to the
+  // hub error
+  test.skip("Bot A tries to transfer with an unsupported (but not invalid) token address", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+
+    await expect(
+      clientA.transfer({
+        amount: ETH_AMOUNT_SM.toString(),
+        assetId: Wallet.createRandom().address,
+        recipient: clientB.publicIdentifier,
+      }),
+    ).rejects.toThrowError(`test`);
+  });
+
+  test("Bot A tries to transfer with invalid recipient xpub", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+
+    await expect(
+      clientA.transfer({
+        amount: ETH_AMOUNT_SM.toString(),
+        assetId: tokenAddress,
+        recipient: `nope`,
+      }),
+    ).rejects.toThrowError(`Value \"nope\" must start with \"xpub\"`);
+  });
+
+  // tslint:disable-next-line: max-line-length
+  test("Bot A tries to transfer an amount greater than they have in their free balance", async () => {
+    await expect(
+      clientA.transfer({
+        amount: parseEther(ZERO_ZERO_ONE).toString(),
+        assetId: tokenAddress,
+        recipient: clientB.publicIdentifier,
+      }),
+    ).rejects.toThrowError(
+      `Value (${parseEther(ZERO_ZERO_ONE).toString()}) is not less than or equal to 0`,
+    );
+  });
+
+  test("Bot A tries to transfer with a paymentId that is not 32 bytes", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+
+    await expect(
+      clientA.conditionalTransfer({
+        amount: ETH_AMOUNT_SM.toString(),
+        assetId: tokenAddress,
+        conditionType: "LINKED_TRANSFER_TO_RECIPIENT",
+        paymentId: "nope",
+        preImage: hexlify(randomBytes(32)),
+        recipient: clientB.publicIdentifier,
+      }),
+    ).rejects.toThrowError(`Value \"nope\" is not a valid hex string`);
+  });
+
+  test("Bot A tries to transfer with a preimage that is not 32 bytes", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+
+    await expect(
+      clientA.conditionalTransfer({
+        amount: parseEther(ZERO_ONE).toString(),
+        assetId: tokenAddress,
+        conditionType: "LINKED_TRANSFER_TO_RECIPIENT",
+        paymentId: hexlify(randomBytes(32)),
+        preImage: "nope",
+        recipient: clientB.publicIdentifier,
+      }),
+    ).rejects.toThrowError(`Value \"nope\" is not a valid hex string`);
+  });
+
+  test("Bot A proposes a transfer to an xpub that doesn’t have a channel", async () => {
+    await fundChannel(clientA, ZERO_ZERO_ONE, tokenAddress);
+
+    await expect(
+      clientA.transfer({
+        amount: parseEther(ZERO_ZERO_ONE).toString(),
+        assetId: tokenAddress,
+        recipient: HDNode.fromMnemonic(Wallet.createRandom().mnemonic).neuter().extendedKey,
+      }),
+    ).rejects.toThrowError(`No channel exists for recipientPublicIdentifier`);
   });
 });
