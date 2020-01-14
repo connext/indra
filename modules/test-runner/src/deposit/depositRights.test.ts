0235dbe8f0f2847c4f40c57fad44c997880e1407
Rahul Sethuram <rahul@connext.network>
2020-01-13 16:03:24 -0800

    ✅ Fix asserts

--- modules/test-runner/src/deposit/depositRights.test.ts
+++ modules/test-runner/src/deposit/depositRights.test.ts
@@ -1,5 +1,5 @@
 import { IConnextClient } from "@connext/types";
-import { AddressZero, One } from "ethers/constants";
+import { AddressZero, One, Zero } from "ethers/constants";
 
 import { createClient, ethProvider, ethWallet } from "../util";
 
@@ -11,22 +11,22 @@ describe("Deposits", () => {
   }, 90_000);
 
   // tslint:disable-next-line:max-line-length
-  test.only("happy case: client should request deposit rights and deposit ETH", async (done: jest.DoneCallback) => {
+  test("happy case: client should request deposit rights and deposit ETH", async (done: jest.DoneCallback) => {
     await clientA.requestDepositRights({ assetId: AddressZero });
     const { [clientA.freeBalanceAddress]: preDeposit } = await clientA.getFreeBalance(AddressZero);
-    expect(preDeposit).toBeBigNumberEq(One);
+    expect(preDeposit).toBeBigNumberEq(0);
 
     // listen for tx to multisig address
     const balance = await ethProvider.getBalance(clientA.multisigAddress);
-    console.log('balance: ', balance);
+    expect(balance).toBeBigNumberEq(0);
     ethProvider.on(clientA.multisigAddress, async () => {
       const balance = await ethProvider.getBalance(clientA.multisigAddress);
-      console.log('balance: ', balance);
+      expect(balance).toBeBigNumberEq(1);
       await clientA.rescindDepositRights({ assetId: AddressZero });
       const { [clientA.freeBalanceAddress]: postDeposit } = await clientA.getFreeBalance(
         AddressZero,
       );
-      expect(postDeposit).toBeBigNumberEq(One);
+      expect(postDeposit).toBeBigNumberEq(1);
       done();
     });
     await ethWallet.sendTransaction({ to: clientA.multisigAddress, value: One });
