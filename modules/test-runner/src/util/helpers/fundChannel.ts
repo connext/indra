1eea9e8cea756f7e6c0e7ee8c9cf8f36af818463
LayneHaber <layne.haber@gmail.com>
2020-01-15 15:21:22 -0800

    reject on deposit failure

--- modules/test-runner/src/util/helpers/fundChannel.ts
+++ modules/test-runner/src/util/helpers/fundChannel.ts
@@ -10,13 +10,16 @@ export const fundChannel = async (
   assetId: string = AddressZero,
 ): Promise<void> => {
   const prevFreeBalance = await client.getFreeBalance(assetId);
-  await new Promise(async resolve => {
+  await new Promise(async (resolve, reject) => {
     client.once("DEPOSIT_CONFIRMED_EVENT", async () => {
       const freeBalance = await client.getFreeBalance(assetId);
       // verify free balance increased as expected
       const expected = prevFreeBalance[client.freeBalanceAddress].add(amount);
       expect(freeBalance[client.freeBalanceAddress].toString()).to.be.eq(expected.toString());
       resolve();
+    });
+    client.once("DEPOSIT_FAILED_EVENT", async (msg: any) => {
+      reject(new Error(JSON.stringify(msg)));
     });
 
     await client.deposit({ amount: amount.toString(), assetId });
