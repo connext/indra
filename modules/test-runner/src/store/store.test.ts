91709bf7a3456c06eaae7e047b876b0525634d7d
LayneHaber <layne.haber@gmail.com>
2020-01-17 18:21:43 -0800

    both tests passing

--- modules/test-runner/src/store/store.test.ts
+++ modules/test-runner/src/store/store.test.ts
@@ -1,22 +1,41 @@
-import { ConnextStore, MemoryStorage } from "@connext/store";
+import {
+  ConnextStore,
+  PATH_CHANNEL,
+  PATH_PROPOSED_APP_INSTANCE_ID,
+  StorePair,
+} from "@connext/store";
 
-import { setAndGet } from "../util";
+import { createStore, expect, setAndGet } from "../util";
 
-describe("Store", () => {
-  let memoryStorage: MemoryStorage;
+describe.only("Store", () => {
+  let memoryStorage: Storage;
   let store: ConnextStore;
 
   beforeEach(async () => {
-    memoryStorage = new MemoryStorage();
-    store = new ConnextStore(memoryStorage);
+    const { store: testStore, storage } = createStore("memorystorage");
+    memoryStorage = storage as Storage;
+    store = testStore;
   }, 90_000);
 
   it("happy case: set & get the same path consecutively", async () => {
-    await setAndGet(store);
+    for (const _ of Array(5)) {
+      await setAndGet(store);
+    }
   });
 
   it("happy case: get partial matches when available", async () => {
     // TODO: happy case: get partial matches when available
+    const names = [PATH_PROPOSED_APP_INSTANCE_ID, PATH_CHANNEL];
+    const keyspace = `partial`;
+    for (const name of names) {
+      const pair: StorePair = {
+        path: `${name}/${keyspace}`,
+        value: { ilyk: "tests" },
+      };
+      await setAndGet(store, pair);
+      const retrieved = await store.get(name);
+      expect(retrieved).to.deep.equal({ [keyspace]: pair.value });
+    }
   });
 
   it("happy case: reset the whole store state", async () => {
