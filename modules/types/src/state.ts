97c5eba2ff4c5c052bb2ee394236a75233771576
Bo Henderson <bohende@gmail.com>
2020-01-20 15:08:19 +0530

    ✨ init CriticalAddresses type

--- modules/types/src/state.ts
+++ modules/types/src/state.ts
@@ -1,6 +1,15 @@
 import { AppInstanceProposal, AppInstanceJson } from "./app";
 import { SingleAssetTwoPartyIntermediaryAgreement } from "./contracts";
 
+// Contract addresses that must be provided to withdraw funds from a channel
+// Losing track to a channel's critical addresses means losing access to the funds in that channel
+// Each channel must track it's own critical addresses because there's no
+//   guarantee that these addresses will be the same across different channels
+export type CriticalAddresses = {
+  proxyFactory: string;
+  multisigMastercopy: string;
+}
+
 export type StateChannelJSON = {
   readonly multisigAddress: string;
   readonly proxyFactoryAddress: string;
