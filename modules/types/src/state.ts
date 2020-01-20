fea11295edd21cd2918d97edf00138f074ed8ae1
Bo Henderson <bohende@gmail.com>
2020-01-20 16:09:17 +0530

    🚚 rename new type to CriticalStateChannelAddresses

--- modules/types/src/state.ts
+++ modules/types/src/state.ts
@@ -2,17 +2,17 @@ import { AppInstanceProposal, AppInstanceJson } from "./app";
 import { SingleAssetTwoPartyIntermediaryAgreement } from "./contracts";
 
 // Contract addresses that must be provided to withdraw funds from a channel
-// Losing track to a channel's critical addresses means losing access to the funds in that channel
+// Losing track of a critical address means losing access to the funds in that channel
 // Each channel must track it's own critical addresses because there's no
 //   guarantee that these addresses will be the same across different channels
-export type CriticalAddresses = {
+export type CriticalStateChannelAddresses = {
   proxyFactory: string;
   multisigMastercopy: string;
 }
 
 export type StateChannelJSON = {
-  readonly multisigAddress: string;
-  readonly proxyFactoryAddress: string;
+  readonly multisigAddress: string; // TODO: remove & replace w getter fn?
+  readonly addresses: CriticalStateChannelAddresses;
   readonly userNeuteredExtendedKeys: string[];
   readonly proposedAppInstances: [string, AppInstanceProposal][];
   readonly appInstances: [string, AppInstanceJson][];
