e4eec0cf59df8a5d752df8b673b6947c05fa9124
Bo Henderson <bohende@gmail.com>
2020-01-22 21:19:00 +0530

    ✅ more error handling & logs around race condition

--- modules/test-runner/src/util/nats.ts
+++ modules/test-runner/src/util/nats.ts
@@ -12,6 +12,8 @@ export const createOrRetrieveNatsConnection = async (): Promise<Client> => {
   try {
     natsConnection = await connect({ servers: [env.nodeUrl] });
   } catch (e) {
+    console.error(e);
+    console.error(`Attempting to create a nats connection again..`);
     // Try one more time in case the first attempt timed out
     natsConnection = await connect({ servers: [env.nodeUrl] });
   }
