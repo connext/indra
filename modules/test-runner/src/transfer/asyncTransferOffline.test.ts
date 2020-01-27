605002de8ce3263f0f86b3f969f6910a8bdf4b3c
LayneHaber <layne.haber@gmail.com>
2020-01-27 14:19:31 -0700

    first async transfer test passing

---
+++ modules/test-runner/src/transfer/asyncTransferOffline.test.ts
@@ -0,0 +1,120 @@
+import { IConnextClient, DefaultApp } from "@connext/types";
+import * as lolex from "lolex";
+
+import {
+  expect,
+  cleanupMessaging,
+  createClientWithMessagingLimits,
+  TOKEN_AMOUNT,
+  fundChannel,
+  requestCollateral,
+  fastForwardDuringCall,
+  asyncTransferAsset,
+  TOKEN_AMOUNT_SM,
+} from "../util";
+import { BigNumber } from "ethers/utils";
+
+describe.only("Async transfer offline tests", () => {
+  let clock: any;
+  let senderClient: IConnextClient;
+  let receiverClient: IConnextClient;
+  let tokenAddress: string;
+
+  const fundForTransfers = async (
+    amount: BigNumber = TOKEN_AMOUNT,
+    assetId?: string,
+  ): Promise<void> => {
+    // make sure the tokenAddress is set
+    tokenAddress = senderClient.config.contractAddresses.Token;
+    await fundChannel(senderClient, amount, assetId || tokenAddress);
+    await requestCollateral(receiverClient, assetId || tokenAddress);
+  };
+
+  beforeEach(async () => {
+    // create the clock
+    clock = lolex.install({
+      shouldAdvanceTime: true,
+      advanceTimeDelta: 1,
+      now: Date.now(),
+    });
+  });
+
+  it("sender successfully installs transfer, goes offline before sending paymentId or preimage, then comes online and has the pending installed transfer", async () => {
+    /**
+     * Means that the node will not know the recipient or encrypted preimage,
+     * the transfer to recipient is essentially installed as an unclaimable
+     * (i.e. preImage lost) linked transfer without a specified recipient.
+     *
+     * Avoid `transfer.set-recipient.${this.userPublicIdentifier}` endpoint
+     * with the hub. The hub should clean these up during disputes
+     */
+
+    // create the sender client and receiver clients
+    senderClient = await createClientWithMessagingLimits({
+      forbiddenSubjects: [`transfer.set-recipient`],
+    });
+    receiverClient = await createClientWithMessagingLimits();
+
+    // fund the channels
+    await fundForTransfers();
+
+    // make the transfer call, should fail when sending info to node, but
+    // will retry. fast forward through NATS_TIMEOUT
+    await fastForwardDuringCall(
+      89_000,
+      () => asyncTransferAsset(senderClient, receiverClient, TOKEN_AMOUNT_SM, tokenAddress),
+      clock,
+      `Subject is in forbidden URLs, refusing to publish data`,
+    );
+
+    // make sure that the app is installed with the hub/sender
+    const simpleTransferApp = senderClient.appRegistry.filter(
+      (app: DefaultApp) => app.name === "SimpleLinkedTransferApp",
+    )[0];
+    const getLinkedApp = async (client: IConnextClient, onlyOne: boolean = true): Promise<any> => {
+      const linkedApps = (await client.getAppInstances()).filter(
+        app => app.appInterface.addr === simpleTransferApp.appDefinitionAddress,
+      );
+      // make sure the state is correct
+      if (onlyOne) {
+        expect(linkedApps.length).to.be.equal(1);
+        return linkedApps[0];
+      }
+      expect(linkedApps).to.be.ok;
+      return linkedApps;
+    };
+    const senderLinkedApp = await getLinkedApp(senderClient);
+    const { paymentId } = senderLinkedApp.latestState as any;
+    const transfer = await senderClient.getLinkedTransfer(paymentId);
+    // verify the saved transfer information
+    expect(transfer).to.containSubset({
+      amount: TOKEN_AMOUNT_SM.toString(),
+      recipientPublicIdentifier: null,
+      paymentId,
+      senderPublicIdentifier: senderClient.publicIdentifier,
+      status: "PENDING",
+      type: "LINKED",
+    });
+    const receiverLinkedApp = await getLinkedApp(receiverClient, false);
+    expect(receiverLinkedApp.length).to.equal(0);
+  });
+
+  it("sender successfully installs transfer, goes offline before sending paymentId/preimage, and stays offline", async () => {});
+
+  it("sender installs transfer successfully, receiver proposes install but node is offline", async () => {});
+
+  it("sender installs transfer successfully, receiver installs successfully, but node is offline", async () => {});
+
+  it("sender installs transfer successfully, receiver install succesfully, takesAction, then goes offline", async () => {});
+
+  it("sender installs, receiver installs, takesAction, then uninstalls. Node tries to take action with sender but sender is offline but then comes online later (node needs to know sender is back online and clean up pending transfers)", async () => {});
+
+  it("sender installs, receiver installs, takesAction, then uninstalls. Node takes action, then uninstalls with sender but sender goes offline during uninstall and comes back on later", async () => {});
+
+  afterEach(async () => {
+    await cleanupMessaging();
+    if (clock) {
+      clock.reset();
+    }
+  });
+});
