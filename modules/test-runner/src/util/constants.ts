cc6fb57f6680ad5ef1a8b14aa56f7a395f29a070
LayneHaber <layne.haber@gmail.com>
2020-01-27 14:42:59 -0700

    add error messages

--- modules/test-runner/src/util/constants.ts
+++ modules/test-runner/src/util/constants.ts
@@ -6,8 +6,12 @@ export const FUNDED_MNEMONICS = [
 ];
 
 export const WRONG_ADDRESS = "0xdeadbeef";
+
+// error messages
 export const APP_PROTOCOL_TOO_LONG = (protocol: string): string =>
   `App ${protocol} took longer than 90 seconds`;
+export const MESSAGE_FAILED_TO_SEND = (reason?: string): string =>
+  `Failed to send message: ${reason}`;
 
 export const NEGATIVE_ONE = "-1";
 export const NEGATIVE_ZERO_ZERO_ONE = "-0.01";
