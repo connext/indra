9b957bae1a2ceaaa61cd3d2c6f4d5881f1a65537
LayneHaber <layne.haber@gmail.com>
2020-01-29 00:13:19 -0700

    all tests passing

--- modules/test-runner/src/transfer/asyncTransferOffline.test.ts
+++ modules/test-runner/src/transfer/asyncTransferOffline.test.ts
@@ -1,7 +1,13 @@
-import { IConnextClient, DefaultApp } from "@connext/types";
+import {
+  IConnextClient,
+  DefaultApp,
+  RECIEVE_TRANSFER_FINISHED_EVENT,
+  UPDATE_STATE_EVENT,
+} from "@connext/types";
 import * as lolex from "lolex";
 
 import {
+  APP_PROTOCOL_TOO_LONG,
   expect,
   cleanupMessaging,
   createClientWithMessagingLimits,
@@ -13,6 +19,12 @@ import {
   TOKEN_AMOUNT_SM,
   MESSAGE_FAILED_TO_SEND,
   FORBIDDEN_SUBJECT,
+  INSTALL_SUPPORTED_APP_COUNT_RECEIVED,
+  PROPOSE_INSTALL_SUPPORTED_APP_COUNT_RECEIVED,
+  getMessaging,
+  getOpts,
+  createClient,
+  getStore,
 } from "../util";
 import { BigNumber } from "ethers/utils";
 
@@ -113,7 +125,7 @@ describe.only("Async transfer offline tests", () => {
     expect(receiverLinkedApp.length).to.equal(0);
   });
 
-  it.only("sender successfully installs transfer, goes offline before sending paymentId/preimage, and stays offline", async () => {
+  it("sender successfully installs transfer, goes offline before sending paymentId/preimage, and stays offline", async () => {
     /**
      * Will have a transfer saved on the hub, but nothing sent to recipient.
      *
@@ -156,15 +168,203 @@ describe.only("Async transfer offline tests", () => {
     await verifyTransfer(receiverClient, { ...expectedTransfer, status: "REDEEMED" });
   });
 
-  it("sender installs transfer successfully, receiver proposes install but node is offline", async () => {});
+  it("sender installs transfer successfully, receiver proposes install but node is offline", async () => {
+    /**
+     * Should get timeout errors.
+     *
+     * Client calls `resolve` on node, node will install and propose, client
+     * will take action with recipient.
+     */
+    // create the sender client and receiver clients + fund
+    senderClient = await createClientWithMessagingLimits();
+    // 2 app proposals expected (coin balance x1, transfer)
+    const expectedProposalsReceived = 2 * PROPOSE_INSTALL_SUPPORTED_APP_COUNT_RECEIVED;
+    receiverClient = await createClientWithMessagingLimits({
+      ceiling: { sent: expectedProposalsReceived },
+      protocol: "propose",
+    });
+    await fundForTransfers();
 
-  it("sender installs transfer successfully, receiver installs successfully, but node is offline", async () => {});
+    // make the transfer call, should timeout in propose protocol
+    await fastForwardDuringCall(
+      89_000,
+      () => asyncTransferAsset(senderClient, receiverClient, TOKEN_AMOUNT_SM, tokenAddress),
+      clock,
+      `Failed to send message: Request timed out`,
+      [3000, 6000],
+    );
+  });
 
-  it("sender installs transfer successfully, receiver install succesfully, takesAction, then goes offline", async () => {});
+  it("sender installs transfer successfully, receiver installs successfully, but node is offline", async () => {
+    /**
+     * Should get timeout errors
+     */
+    // create the sender client and receiver clients + fund
+    senderClient = await createClientWithMessagingLimits();
+    // 2 app proposals expected (coin balance x1, transfer)
+    const expectedInstallsReceived = 2 * INSTALL_SUPPORTED_APP_COUNT_RECEIVED;
+    receiverClient = await createClientWithMessagingLimits({
+      ceiling: { received: expectedInstallsReceived },
+      protocol: "install",
+    });
+    await fundForTransfers();
 
-  it("sender installs, receiver installs, takesAction, then uninstalls. Node tries to take action with sender but sender is offline but then comes online later (node needs to know sender is back online and clean up pending transfers)", async () => {});
+    // make the transfer call, should timeout in propose protocol
+    await fastForwardDuringCall(
+      89_000,
+      () => asyncTransferAsset(senderClient, receiverClient, TOKEN_AMOUNT_SM, tokenAddress),
+      clock,
+      `Failed to send message: Request timed out`,
+      [3000, 6000],
+    );
+  });
 
-  it("sender installs, receiver installs, takesAction, then uninstalls. Node takes action, then uninstalls with sender but sender goes offline during uninstall and comes back on later", async () => {});
+  it("sender installs transfer successfully, receiver install succesfully, takesAction, then goes offline", async () => {
+    /**
+     * Receiver will not uninstall app, but this is not a true protocol, so the
+     * app should be removed from the receivers store anyways
+     */
+    // create the sender client and receiver clients + fund
+    senderClient = await createClientWithMessagingLimits();
+    // 2 app proposals expected (coin balance x1, transfer)
+    receiverClient = await createClientWithMessagingLimits({
+      ceiling: { received: 0 },
+      protocol: "takeAction",
+    });
+    await fundForTransfers();
+
+    // make the transfer call, should timeout in propose protocol
+    await fastForwardDuringCall(
+      89_000,
+      () => asyncTransferAsset(senderClient, receiverClient, TOKEN_AMOUNT_SM, tokenAddress),
+      clock,
+      APP_PROTOCOL_TOO_LONG("takeAction"),
+    );
+  });
+
+  it("sender installs, receiver installs, takesAction, then uninstalls. Node tries to take action with sender but sender is offline but then comes online later", async function() {
+    this.timeout(130_000);
+    /**
+     * Expected behavior: sender should still have app (with money owed to
+     * them) installed in the channel when they come back online
+     *
+     * Ideally, the node takes action +  uninstalls these apps on `connect`,
+     * and money is returned to the hubs channel (redeemed payment)
+     */
+    // create the sender client and receiver clients + fund
+    senderClient = await createClientWithMessagingLimits();
+    receiverClient = await createClientWithMessagingLimits();
+    await fundForTransfers();
+
+    // transfer from the sender to the receiver, then take the
+    // sender offline
+    const received = new Promise(resolve =>
+      receiverClient.once(RECIEVE_TRANSFER_FINISHED_EVENT, () => {
+        resolve();
+      }),
+    );
+    const { paymentId } = await senderClient.transfer({
+      amount: TOKEN_AMOUNT_SM.toString(),
+      assetId: tokenAddress,
+      recipient: receiverClient.publicIdentifier,
+    });
+    // immediately take sender offline
+    const messaging = getMessaging(senderClient.publicIdentifier);
+    await messaging!.disconnect();
+    // wait for transfer to finish
+    await received;
+    // fast forward 5 min
+    clock.tick(60_000 * 5);
+    // verify transfer
+    const expected = {
+      amount: TOKEN_AMOUNT_SM.toString(),
+      receiverPublicIdentifier: receiverClient.publicIdentifier,
+      paymentId,
+      senderPublicIdentifier: senderClient.publicIdentifier,
+      status: "REDEEMED",
+      type: "LINKED",
+      assetId: tokenAddress,
+    };
+    await verifyTransfer(receiverClient, expected);
+
+    // reconnect the sender
+    const { mnemonic } = getOpts(senderClient.publicIdentifier);
+    const reconnected = await createClient({
+      mnemonic,
+      store: getStore(senderClient.publicIdentifier),
+    });
+    expect(reconnected.publicIdentifier).to.be.equal(senderClient.publicIdentifier);
+    expect(reconnected.multisigAddress).to.be.equal(senderClient.multisigAddress);
+    expect(reconnected.freeBalanceAddress).to.be.equal(senderClient.freeBalanceAddress);
+    // make sure the transfer is properly reclaimed
+    await verifyTransfer(reconnected, { ...expected, status: "RECLAIMED" });
+  });
+
+  it("sender installs, receiver installs, takesAction, then uninstalls. Node tries to take action, with sender but sender is offline but then comes online later", async function() {
+    this.timeout(200_000);
+    /**
+     * Expected behavior: sender should still have app (with money owed to
+     * them) installed in the channel when they come back online
+     *
+     * Ideally, the node takes action +  uninstalls these apps on `connect`,
+     * and money is returned to the hubs channel (redeemed payment)
+     */
+    // create the sender client and receiver clients + fund
+    senderClient = await createClientWithMessagingLimits({
+      ceiling: { sent: 1 }, // for deposit app
+      protocol: "uninstall",
+    });
+    receiverClient = await createClientWithMessagingLimits();
+    await fundForTransfers();
+
+    // transfer from the sender to the receiver, then take the
+    // sender offline
+    const received = new Promise((resolve: Function) =>
+      receiverClient.once(RECIEVE_TRANSFER_FINISHED_EVENT, () => {
+        resolve();
+      }),
+    );
+    // disconnect messaging on take action event
+    const actionTaken = new Promise((resolve: Function) => {
+      senderClient.once(UPDATE_STATE_EVENT, async () => {
+        await received;
+        const messaging = getMessaging(senderClient.publicIdentifier);
+        await messaging!.disconnect();
+        // fast forward 5 min
+        clock.tick(60_000 * 5);
+        resolve();
+      });
+    });
+
+    const { paymentId } = await senderClient.transfer({
+      amount: TOKEN_AMOUNT_SM.toString(),
+      assetId: tokenAddress,
+      recipient: receiverClient.publicIdentifier,
+    });
+    // wait for transfer to finish + messaging to be disconnected
+    await actionTaken;
+    // verify transfer
+    const expected = {
+      amount: TOKEN_AMOUNT_SM.toString(),
+      receiverPublicIdentifier: receiverClient.publicIdentifier,
+      paymentId,
+      senderPublicIdentifier: senderClient.publicIdentifier,
+      status: "REDEEMED",
+      type: "LINKED",
+    };
+    await verifyTransfer(receiverClient, expected);
+
+    // reconnect the sender
+    const { mnemonic } = getOpts(senderClient.publicIdentifier);
+    const reconnected = await createClient({
+      mnemonic,
+    });
+    expect(reconnected.publicIdentifier).to.be.equal(senderClient.publicIdentifier);
+    expect(reconnected.multisigAddress).to.be.equal(senderClient.multisigAddress);
+    expect(reconnected.freeBalanceAddress).to.be.equal(senderClient.freeBalanceAddress);
+    // make sure the transfer is properly reclaimed
+    await verifyTransfer(reconnected, { ...expected, status: "RECLAIMED" });
+  });
 
   afterEach(async () => {
     await cleanupMessaging();
