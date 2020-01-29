be8f744f54de606c67c2eed404bf4d850a668f3d
LayneHaber <layne.haber@gmail.com>
2020-01-29 00:12:12 -0700

    add export of type

--- modules/client/src/types.ts
+++ modules/client/src/types.ts
@@ -49,6 +49,7 @@ export {
   KeyGen,
   LinkedTransferParameters,
   LinkedTransferResponse,
+  LINKED_TRANSFER_TO_RECIPIENT,
   LinkedTransferToRecipientParameters,
   LinkedTransferToRecipientResponse,
   makeChecksum,
