71c5719032bd3287dce782be5d6114c12673c3fc
Rahul Sethuram <rahul@connext.network>
2020-01-30 13:35:34 +0100

    Fix test

--- modules/client/src/lib/bn.spec.ts
+++ modules/client/src/lib/bn.spec.ts
@@ -1,13 +1,12 @@
-import { bigNumberify } from "ethers/utils";
-import { calculateExchange } from "../../../test-runner/src/util";
+import { calculateExchange } from "../types";
 import { One } from "ethers/constants";
 
 describe("calculateExchange", () => {
-  it("should work with small amounts", () => {
+  it.only("should work with small amounts", () => {
     const toExchange = One;
     const swapRate = "185.56";
     const ret = calculateExchange(toExchange, swapRate);
-    const expected = bigNumberify(1 / 185.56);
-    expect(ret.toString()).toBe(expected.toString());
+    const expected = "185";
+    expect(ret.toString()).toBe(expected);
   });
 });
