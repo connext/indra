e65320059c1bbe609b52a1093533af4520481aff
LayneHaber <layne.haber@gmail.com>
2020-01-29 20:43:26 -0700

    tests passing

--- modules/test-runner/src/withdraw/withdrawOffline.test.ts
+++ modules/test-runner/src/withdraw/withdrawOffline.test.ts
@@ -1,14 +1,26 @@
 import { IConnextClient } from "@connext/types";
 import { BigNumber } from "ethers/utils";
-import { AddressZero } from "ethers/constants";
+import { AddressZero, Zero } from "ethers/constants";
 import * as lolex from "lolex";
 import {
-  ETH_AMOUNT_MD,
   ClientTestMessagingInputOpts,
-  createClientWithMessagingLimits,
-  fundChannel,
   cleanupMessaging,
+  createClientWithMessagingLimits,
+  ETH_AMOUNT_SM,
+  expect,
+  fastForwardDuringCall,
+  fundChannel,
+  withdrawFromChannel,
+  ZERO_ZERO_ZERO_FIVE_ETH,
+  FORBIDDEN_SUBJECT,
+  getStore,
+  getOpts,
+  createClient,
+  ethProvider,
 } from "../util";
+import { utils } from "@connext/client";
+
+const { withdrawalKey } = utils;
 
 describe("Withdraw offline tests", () => {
   let clock: any;
@@ -19,7 +31,7 @@ describe("Withdraw offline tests", () => {
 
   const createAndFundChannel = async (
     messagingConfig: Partial<ClientTestMessagingInputOpts> = {},
-    amount: BigNumber = ETH_AMOUNT_MD,
+    amount: BigNumber = ETH_AMOUNT_SM,
     assetId: string = AddressZero,
   ): Promise<IConnextClient> => {
     // make sure the tokenAddress is set
@@ -38,16 +50,71 @@ describe("Withdraw offline tests", () => {
   });
 
   it("client proposes withdrawal but doesn't receive a response from node", async () => {
-    client = createAndFundChannel({
-      ceiling: { received: },
-      protocol: "withdraw"
+    client = await createAndFundChannel({
+      ceiling: { received: 0 },
+      protocol: "withdraw",
     });
 
+    await fastForwardDuringCall(
+      89_000,
+      () => withdrawFromChannel(client, ZERO_ZERO_ZERO_FIVE_ETH, AddressZero),
+      clock,
+      `timed out after 90s waiting for counterparty reply in withdraw`,
+    );
   });
 
-  it("client proposes withdrawal and then goes offline before node responds", async () => {});
+  it("client proposes withdrawal and then goes offline before node responds", async () => {
+    client = await createAndFundChannel({
+      ceiling: { sent: 1 },
+      protocol: "withdraw",
+    });
 
-  it("client proposes a node submitted withdrawal but node is offline for one message (commitment should be written to store and retried)", async () => {});
+    await fastForwardDuringCall(
+      89_000,
+      () => withdrawFromChannel(client, ZERO_ZERO_ZERO_FIVE_ETH, AddressZero),
+      clock,
+      `timed out after 90s waiting for counterparty reply in withdraw`,
+    );
+  });
+
+  it("client proposes a node submitted withdrawal but node is offline for one message (commitment should be written to store and retried)", async () => {
+    client = await createAndFundChannel({
+      forbiddenSubjects: ["channel.withdraw"],
+    });
+
+    // make call, should fail
+    await fastForwardDuringCall(
+      89_000,
+      () => withdrawFromChannel(client, ZERO_ZERO_ZERO_FIVE_ETH, AddressZero),
+      clock,
+      FORBIDDEN_SUBJECT,
+    );
+
+    // make sure withdrawal is in the store
+    const store = getStore(client.publicIdentifier);
+    const { tx, retry } = await store.get(withdrawalKey(client.publicIdentifier));
+    expect(tx).to.be.ok;
+    expect(tx.to).to.be.equal(client.multisigAddress);
+    expect(tx.value).equal(Zero); // amt transferred in internal tx
+    expect(retry).to.be.equal(0);
+
+    // restart the client
+    const { mnemonic } = getOpts(client.publicIdentifier);
+    const reconnected = await createClient({ mnemonic, store });
+    expect(reconnected.publicIdentifier).to.be.equal(client.publicIdentifier);
+    expect(reconnected.multisigAddress).to.be.equal(client.multisigAddress);
+    expect(reconnected.freeBalanceAddress).to.be.equal(client.freeBalanceAddress);
+
+    await new Promise((resolve: Function) => {
+      ethProvider.once(client.multisigAddress, async () => {
+        resolve();
+      });
+    });
+
+    // make sure the withdrawal has been handled
+    const resubmitted = await store.get(withdrawalKey(client.publicIdentifier));
+    expect(resubmitted).to.not.be.ok;
+  });
 
   afterEach(async () => {
     await cleanupMessaging();
