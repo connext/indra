c34a6c72de8a8425b5945182e2ae9ff3b431a35d
LayneHaber <layne.haber@gmail.com>
2020-01-29 17:58:13 -0700

    dont use config file

--- modules/test-runner/ops/entry.sh
+++ modules/test-runner/ops/entry.sh
@@ -37,9 +37,9 @@ if [[ $1 == "--watch" ]]
 then
   webpack --watch --config ops/webpack.config.js &
   sleep 5 # give webpack a sec to finish the first watch-mode build
-  mocha --config ".mocharc.json" --watch $bundle
+  mocha --timeout 60000 --bail --check-leaks --watch $bundle
 else
-  mocha --config ".mocharc.json" --exit $bundle
+  mocha --timeout 60000 --bail --check-leaks --exit $bundle
 fi
 
 rm -rf $STORE_DIR
