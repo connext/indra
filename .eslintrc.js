659657447e343bc2a1d32ac8d749d1f088704fe9
Rahul Sethuram <rahul@connext.network>
2020-02-03 14:03:11 +0100

    🔧   Consolidate linter and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   rules: {
     "@typescript-eslint/no-unused-expressions": "off",
-    "comma-dangle": ["error", "only-multiline"],
-    indent: ["error", 2],
+    "comma-dangle": ["error", "always-multiline"],
     "max-len": ["warn", { code: 120, ignoreTemplateLiterals: true }],
     "no-async-promise-executor": "off",
     "no-undef": ["error"],
-    "no-unused-vars": ["error"],
     "no-var": ["error"],
     "object-curly-spacing": ["error", "always"],
     quotes: ["error", "double", { allowTemplateLiterals: true }],
