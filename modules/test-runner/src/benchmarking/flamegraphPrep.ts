09cab884b8b2d6874b0e4e4c96efc4f17cb78f10
Arjun <arjunbhuptani@gmail.com>
2020-02-05 21:32:26 +0400

    flamegraph attempt #2 (still doesn't work)

--- modules/test-runner/src/benchmarking/flamegraphPrep.ts
+++ modules/test-runner/src/benchmarking/flamegraphPrep.ts
@@ -1,14 +1,26 @@
 import { createClient, AssetOptions, ETH_AMOUNT_SM, fundChannel, requestCollateral } from "../util";
 import { IConnextClient } from "@connext/types";
 import { AddressZero } from "ethers/constants";
+import { ConnextStore, FileStorage } from "@connext/store";
+import { connect } from "@connext/client";
 
 export let clientA: IConnextClient;
 export let clientB: IConnextClient;
 
 export default async () => {
     console.log("Setting up clients")
-    clientA = await createClient();
-    clientB = await createClient();
+    const clientA = await connect({
+        mnemonic: "harsh cancel view follow approve digital tool cram physical easily lend cinnamon betray scene round",
+        nodeUrl: "nats://localhost:4222",
+        ethProviderUrl: "http://localhost:8545",
+        store: new ConnextStore(new FileStorage())
+    })
+    const clientB = await connect({
+        mnemonic: "mom shrimp way ripple gravity scene eyebrow topic enlist apple analyst shell obscure midnight buddy", 
+        nodeUrl: "nats://localhost:4222",
+        ethProviderUrl: "http://localhost:8545",
+        store: new ConnextStore(new FileStorage())
+    })
 
     const transfer: AssetOptions = { amount: ETH_AMOUNT_SM, assetId: AddressZero };
     console.log("Funding channel")
