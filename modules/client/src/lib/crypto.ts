7ac6218ae862cfe7b984fee0f029d9ff722d6050
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-10 21:26:46 +0100

    fix publicKey compression

--- modules/client/src/lib/crypto.ts
+++ modules/client/src/lib/crypto.ts
@@ -1,5 +1,4 @@
 import { decrypt, encrypt } from "eccrypto-js";
-import { publicKeyConvert } from "secp256k1";
 
 type Encrypted = {
   ciphertext: string;
@@ -8,24 +7,8 @@ type Encrypted = {
   mac: string;
 };
 
-const compress = (pubKey: string): string => {
-  const startsWith04 =
-    Buffer.from(pubKey.replace(/^0x/, ""), "hex").length === 64
-      ? `04${pubKey.replace(/^0x/, "")}`
-      : pubKey.replace(/^0x/, "");
-  return publicKeyConvert(Buffer.from(startsWith04, "hex"), true).toString("hex");
-};
-
-const decompress = (pubKey: string): string => {
-  const prefixed = Buffer.from(pubKey, "hex").length === 64 ? `04${pubKey}` : pubKey;
-  return publicKeyConvert(Buffer.from(prefixed.replace(/^0x/, ""), "hex"), false)
-    .toString("hex")
-    .substring(2);
-};
-
 export const encryptWithPublicKey = async (publicKey: string, message: string): Promise<string> => {
-  const key = `04${decompress(publicKey)}`;
-  const encryptedBuffers = await encrypt(Buffer.from(key, "hex"), Buffer.from(message));
+  const encryptedBuffers = await encrypt(Buffer.from(publicKey, "hex"), Buffer.from(message));
   const encrypted: Encrypted = {
     ciphertext: encryptedBuffers.ciphertext.toString("hex"),
     ephemPublicKey: encryptedBuffers.ephemPublicKey.toString("hex"),
@@ -35,20 +18,17 @@ export const encryptWithPublicKey = async (publicKey: string, message: string): 
   // console.log(`Encrypted data: ${JSON.stringify(encrypted, null, 2)}`);
   return Buffer.concat([
     Buffer.from(encrypted.iv, "hex"), // 16bit
-    Buffer.from(compress(encrypted.ephemPublicKey), "hex"), // 33bit
+    Buffer.from(encrypted.ephemPublicKey, "hex"), // 33bit
     Buffer.from(encrypted.mac, "hex"), // 32bit
     Buffer.from(encrypted.ciphertext, "hex"), // var bit
   ]).toString("hex");
 };
 
-export const decryptWithPrivateKey = async (
-  privateKey: string,
-  message: string,
-): Promise<string> => {
+export const decryptWithPrivateKey = async (privateKey: string, message: string): Promise<string> => {
   const buf = Buffer.from(message, "hex");
   const encrypted = {
     ciphertext: buf.toString("hex", 81, buf.length),
-    ephemPublicKey: `04${decompress(buf.toString("hex", 16, 49))}`,
+    ephemPublicKey: buf.toString("hex", 16, 49),
     iv: buf.toString("hex", 0, 16),
     mac: buf.toString("hex", 49, 81),
   };
