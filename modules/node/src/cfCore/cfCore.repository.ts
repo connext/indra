cb0c882cff307ad8e99d72052e4098752085b477
Rahul Sethuram <rahul@connext.network>
2020-02-12 21:30:49 +0100

    Simplify node store implementation

--- modules/node/src/cfCore/cfCore.repository.ts
+++ modules/node/src/cfCore/cfCore.repository.ts
@@ -17,30 +17,14 @@ export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
   async get(path: string): Promise<StringKeyValue | string | undefined> {
     // logger.log(`Getting path from store: ${path}`);
     let res: any;
-    // FIXME: this queries for all channels or proposed app instances, which
-    // are nested under the respective keywords, hence the 'like' keyword
-    // Action item: this hack won't be needed when a more robust schema around
-    // node records is implemented
-    if (path.endsWith("channel") || path.endsWith("appInstanceIdToProposedAppInstance")) {
+    if (path.endsWith("channel")) {
       res = await this.createQueryBuilder("node_records")
         .where("node_records.path like :path", { path: `%${path}%` })
         .getMany();
-      const nestedRecords = res.map((record: CFCoreRecord) => {
-        const existingKey = Object.keys(record.value)[0];
-        const leafKey = existingKey.split("/").pop()!;
-        const nestedValue = record.value[existingKey];
-        delete record.value[existingKey];
-        record.value[leafKey] = nestedValue;
-        return record.value;
-      });
       const records = {};
-      nestedRecords.forEach((record: any): void => {
-        const key = Object.keys(record)[0];
-        const value = Object.values(record)[0];
-        // FIXME: the store implementation (firebase) that the cf core used in the
-        // very first implementation of the store assumed that values which are
-        // null wouldn't contain key entries in the returned object so we have to
-        // explicitly remove these when Postgres correctly returns even null values
+      res.forEach((record: CFCoreRecord): void => {
+        const key = record.value["multisigAddress"];
+        const value = record.value;
         if (value !== null) {
           records[key] = value;
         }
@@ -53,21 +37,14 @@ export class CFCoreRecordRepository extends Repository<CFCoreRecord> {
       return undefined;
     }
     // logger.debug(`Got value: ${stringify(res.value[path])}`);
-    return res.value[path];
+    return res.value;
   }
 
   async set(pairs: { path: string; value: any }[]): Promise<void> {
     for (const pair of pairs) {
-      // Wrapping the value into an object is necessary for Postgres bc the JSON column breaks
-      // if you use anything other than JSON (i.e. a raw string).
-      // In some cases, the cf core code is inserting strings as values instead of objects :(
-      const record = { path: pair.path, value: { [pair.path]: pair.value } };
+      const record = { path: pair.path, value: pair.value };
       // logger.debug(`Saving record: ${stringify(record)}`);
       await this.save(record);
     }
-  }
-
-  async findRecordsForRestore(multisigAddress: string): Promise<CFCoreRecord[]> {
-    return await this.find({ path: Like(`%${multisigAddress}`) });
   }
 }
