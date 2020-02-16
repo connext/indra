4dac86355e11c0e2c517c5ede3154e86669b91be
Rahul Sethuram <rahul@connext.network>
2020-02-16 16:29:38 +0100

    🗃  Fix migration for name change

--- modules/node/migrations/1581796200880-rebalancing-profile.ts
+++ modules/node/migrations/1581796200880-rebalancing-profile.ts
@@ -8,17 +8,19 @@ export class RebalancingProfile1581796200880 implements MigrationInterface {
       ALTER TABLE "payment_profile" ADD COLUMN "lowerBoundReclaim" text NOT NULL DEFAULT '0'::text;
       ALTER TABLE "payment_profile" ADD COLUMN "upperBoundReclaim" text NOT NULL DEFAULT '0'::text;
       ALTER TABLE "payment_profile" RENAME TO "rebalance_profile";
+      ALTER TABLE "channel_payment_profiles_payment_profile" RENAME COLUMN "paymentProfileId" TO "rebalanceProfileId";
+      ALTER TABLE "channel_payment_profiles_payment_profile" RENAME TO "channel_rebalance_profiles_rebalance_profile";
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(``);
     await queryRunner.query(`
       ALTER TABLE "rebalance_profile" RENAME TO "payment_profile";
       ALTER TABLE "payment_profile" RENAME COLUMN "lowerBoundCollateralize" TO "minimumMaintainedCollateral";
       ALTER TABLE "payment_profile" RENAME COLUMN "upperBoundCollateralize" TO "amountToCollateralize";
       ALTER TABLE "payment_profile" DROP COLUMN "lowerBoundReclaim";
       ALTER TABLE "payment_profile" DROP COLUMN "upperBoundReclaim";
+      ALTER TABLE "channel_rebalance_profiles_rebalance_profile" RENAME TO "channel_payment_profiles_payment_profile";
     `);
   }
 }
