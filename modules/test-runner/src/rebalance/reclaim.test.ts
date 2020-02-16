655ab6513333022a006c3fb66e9d87db744b96f0
Rahul Sethuram <rahul@connext.network>
2020-02-16 16:38:20 +0100

    Add a test for token reclaim

--- modules/test-runner/src/rebalance/reclaim.test.ts
+++ modules/test-runner/src/rebalance/reclaim.test.ts
@@ -36,7 +36,7 @@ describe("Reclaim", () => {
     nats.close();
   });
 
-  it.only("happy case: node should reclaim ETH with async transfer", async () => {
+  it("happy case: node should reclaim ETH with async transfer", async () => {
     const REBALANCE_PROFILE = {
       assetId: AddressZero,
       lowerBoundCollateralize: "5",
@@ -71,7 +71,45 @@ describe("Reclaim", () => {
     ).to.be.ok;
   });
 
-  it.skip("happy case: node should reclaim tokens after async transfer", async () => {});
+  it("happy case: node should reclaim tokens after async transfer", async () => {
+    const REBALANCE_PROFILE = {
+      assetId: tokenAddress,
+      lowerBoundCollateralize: "5",
+      upperBoundCollateralize: "10",
+      // eslint-disable-next-line sort-keys
+      lowerBoundReclaim: "20",
+      upperBoundReclaim: "30",
+    };
+
+    // set rebalancing profile to reclaim collateral
+    await addRebalanceProfile(nats, clientA, REBALANCE_PROFILE);
+
+    // deposit client
+    await fundChannel(clientA, bigNumberify(REBALANCE_PROFILE.upperBoundReclaim).add(Two), tokenAddress);
+    await clientB.requestCollateral(AddressZero);
+
+    // transfer to node to get node over upper bound reclaim
+    // first transfer gets to upper bound
+    await asyncTransferAsset(
+      clientA,
+      clientB,
+      bigNumberify(REBALANCE_PROFILE.upperBoundReclaim).add(One),
+      tokenAddress,
+    );
+    // second transfer triggers reclaim
+    // verify that node reclaims until lower bound reclaim
+    await new Promise(async res => {
+      await nats.subscribe(`indra.node.${clientA.nodePublicIdentifier}.reclaim.${clientA.multisigAddress}`, res);
+      clientA.transfer({ amount: One.toString(), assetId: tokenAddress, recipient: clientB.publicIdentifier });
+    });
+
+    const freeBalancePost = await clientA.getFreeBalance(tokenAddress);
+    // expect this could be checked pre or post the rest of the transfer, so try to pre-emptively avoid race conditions
+    expect(
+      freeBalancePost[nodeFreeBalanceAddress].gte(bigNumberify(REBALANCE_PROFILE.lowerBoundReclaim)) &&
+        freeBalancePost[nodeFreeBalanceAddress].lte(bigNumberify(REBALANCE_PROFILE.lowerBoundReclaim).add(One)),
+    ).to.be.ok;
+  });
 
   it.skip("happy case: node should reclaim ETH after linked transfer", async () => {});
 
