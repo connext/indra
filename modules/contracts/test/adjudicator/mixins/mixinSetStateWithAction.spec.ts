8eca5042443db1b93f9bd74ae0c62f1f204c24fa
LayneHaber <layne.haber@gmail.com>
2020-02-19 15:24:48 -0700

    misnomer fixed

--- modules/contracts/test/adjudicator/mixins/mixinSetStateWithAction.spec.ts
+++ modules/contracts/test/adjudicator/mixins/mixinSetStateWithAction.spec.ts
@@ -39,7 +39,7 @@ const bob =
   // 0xb37e49bFC97A948617bF3B63BC6942BB15285715
   new Wallet("0x4ccac8b1e81fb18a98bbaf29b9bfe307885561f71b76bd4680d7aec9d0ddfcfd");
 
-describe("MixinSetState.sol", () => {
+describe("MixinSetStateWithAction.sol", () => {
   const provider = buidler.provider;
   const encodedState = encodeAppState(getAppWithActionState());
   const encodedAction = encodeAppAction(getIncrementCounterAction());
