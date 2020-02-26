8877d4cd034f7691067958cbc5cb882355d16d7f
LayneHaber <layne.haber@gmail.com>
2020-02-25 20:23:28 -0700

    rename

--- modules/cf-core/src/ethereum/index.ts
+++ modules/cf-core/src/ethereum/index.ts
@@ -1,11 +1,11 @@
-import { ConditionalTransaction } from "./conditional-transaction-commitment";
+import { ConditionalTransactionCommitment } from "./conditional-transaction-commitment";
 import { SetStateCommitment } from "./set-state-commitment";
 import { SetupCommitment } from "./setup-commitment";
 import { WithdrawERC20Commitment } from "./withdraw-erc20-commitment";
 import { WithdrawETHCommitment } from "./withdraw-eth-commitment";
 
 export {
-  ConditionalTransaction,
+  ConditionalTransactionCommitment,
   SetStateCommitment,
   SetupCommitment,
   WithdrawETHCommitment,
