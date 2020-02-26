410c4bc930d6d5d5e384c1fb309fad63fc3b2d00
LayneHaber <layne.haber@gmail.com>
2020-02-25 21:23:44 -0700

    add entities for all commitment types

--- modules/node/src/commitment/commitment.repository.ts
+++ modules/node/src/commitment/commitment.repository.ts
@@ -1,10 +1,19 @@
 import { EntityRepository, Repository } from "typeorm";
-import { Commitment, CommitmentType } from "./commitment.entity";
-import { ProtocolTypes } from "@connext/types";
+import {
+  CommitmentType,
+  SetStateCommitmentEntity,
+  ConditionalTransactionCommitmentEntity,
+  WithdrawCommitment,
+} from "./commitment.entity";
+import {
+  ConditionalTransactionCommitmentJSON,
+  ProtocolTypes,
+  SetStateCommitmentJSON,
+} from "@connext/types";
 
-@EntityRepository(Commitment)
-export class CommitmentRepository extends Repository<Commitment> {
-  findByMultisigAddress(multisigAddress: string): Promise<Commitment> {
+@EntityRepository(WithdrawCommitment)
+export class WithdrawCommitmentRepository extends Repository<WithdrawCommitment> {
+  findByMultisigAddress(multisigAddress: string): Promise<WithdrawCommitment> {
     return this.findOne({
       where: {
         multisigAddress,
@@ -12,7 +21,7 @@ export class CommitmentRepository extends Repository<Commitment> {
     });
   }
 
-  findByCommitmentHash(commitmentHash: string): Promise<Commitment> {
+  findByCommitmentHash(commitmentHash: string): Promise<WithdrawCommitment> {
     return this.findOne({
       where: {
         commitmentHash,
@@ -35,27 +44,107 @@ export class CommitmentRepository extends Repository<Commitment> {
     multisigAddress: string,
     commitment: ProtocolTypes.MinimalTransaction,
   ): Promise<void> {
-    const commitmentEnt = new Commitment();
-    commitmentEnt.type = CommitmentType.COMMITMENT;
+    const commitmentEnt = new WithdrawCommitment();
+    commitmentEnt.type = CommitmentType.WITHDRAWAL;
     commitmentEnt.multisigAddress = multisigAddress;
     commitmentEnt.data = commitment;
     this.save(commitmentEnt);
   }
+}
 
-  async getCommitment(commitmentHash: string): Promise<ProtocolTypes.MinimalTransaction> {
-    const commitment = await this.findByCommitmentHash(commitmentHash);
+@EntityRepository(ConditionalTransactionCommitmentEntity)
+export class ConditionalTransactionCommitmentRepository extends Repository<
+  ConditionalTransactionCommitmentEntity
+> {
+  findByAppIdentityHash(
+    appIdentityHash: string,
+  ): Promise<ConditionalTransactionCommitmentEntity | undefined> {
+    return this.findOne({
+      where: {
+        appIdentityHash,
+      },
+    });
+  }
+
+  findByMultisigAddress(
+    multisigAddress: string,
+  ): Promise<ConditionalTransactionCommitmentEntity | undefined> {
+    return this.findOne({
+      where: {
+        multisigAddress,
+      },
+    });
+  }
+
+  async getConditionalTransactionCommitment(
+    appIdentityHash: string,
+  ): Promise<ConditionalTransactionCommitmentJSON | undefined> {
+    const commitment = await this.findByAppIdentityHash(appIdentityHash);
     if (!commitment) {
       return undefined;
     }
-
-    return commitment.data as ProtocolTypes.MinimalTransaction;
+    return commitment as ConditionalTransactionCommitmentJSON;
   }
 
-  async saveCommitment(commitmentHash: string, commitment: any[]): Promise<void> {
-    const commitmentEnt = new Commitment();
-    commitmentEnt.type = CommitmentType.COMMITMENT;
-    commitmentEnt.commitmentHash = commitmentHash;
-    commitmentEnt.data = commitment;
-    this.save(commitmentEnt);
+  async saveConditionalTransactionCommitment(
+    appIdentityHash: string,
+    commitment: ConditionalTransactionCommitmentJSON,
+  ): Promise<void> {
+    const commitmentEntity = new ConditionalTransactionCommitmentEntity();
+    commitmentEntity.type = CommitmentType.CONDITIONAL;
+    commitmentEntity.appIdentityHash = appIdentityHash;
+    commitmentEntity.freeBalanceAppIdentityHash = commitment.freeBalanceAppIdentityHash;
+    commitmentEntity.interpreterAddr = commitment.interpreterAddr;
+    commitmentEntity.interpreterParams = commitment.interpreterParams;
+    commitmentEntity.multisigAddress = commitment.multisigAddress;
+    commitmentEntity.multisigOwners = commitment.multisigOwners;
+    commitmentEntity.networkContext = commitment.networkContext;
+    commitmentEntity.signatures = commitment.signatures;
+    this.save(commitmentEntity);
+  }
+}
+
+@EntityRepository(SetStateCommitmentEntity)
+export class SetStateCommitmentRepository extends Repository<SetStateCommitmentEntity> {
+  findByAppIdentityHash(appIdentityHash: string): Promise<SetStateCommitmentEntity | undefined> {
+    return this.findOne({
+      where: {
+        appIdentityHash,
+      },
+    });
+  }
+
+  findByAppStateHash(appStateHash: string): Promise<SetStateCommitmentEntity | undefined> {
+    return this.findOne({
+      where: {
+        appStateHash,
+      },
+    });
+  }
+
+  async getLatestSetStateCommitment(
+    appIdentityHash: string,
+  ): Promise<SetStateCommitmentJSON | undefined> {
+    const commitment = await this.findByAppIdentityHash(appIdentityHash);
+    if (!commitment) {
+      return undefined;
+    }
+    return commitment as SetStateCommitmentJSON;
+  }
+
+  async saveLatestSetStateCommitment(
+    appIdentityHash: string,
+    commitment: SetStateCommitmentJSON,
+  ): Promise<void> {
+    const commitmentEntity = new SetStateCommitmentEntity();
+    commitmentEntity.type = CommitmentType.SET_STATE;
+    commitmentEntity.appIdentityHash = appIdentityHash;
+    commitmentEntity.appIdentity = commitment.appIdentity;
+    commitmentEntity.appStateHash = commitment.appStateHash;
+    commitmentEntity.challengeRegistryAddress = commitment.challengeRegistryAddress;
+    commitmentEntity.signatures = commitment.signatures;
+    commitmentEntity.timeout = commitment.timeout;
+    commitmentEntity.versionNumber = commitment.versionNumber;
+    this.save(commitmentEntity);
   }
 }
