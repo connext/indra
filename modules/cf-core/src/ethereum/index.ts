d9beb2073141c88e1371149b9f459d9e3fcc4f9c
LayneHaber <layne.haber@gmail.com>
2020-02-26 23:30:57 -0700

    export multisig commitment

--- modules/cf-core/src/ethereum/index.ts
+++ modules/cf-core/src/ethereum/index.ts
@@ -1,4 +1,5 @@
 import { ConditionalTransactionCommitment } from "./conditional-transaction-commitment";
+import { MultisigCommitment } from "./multisig-commitment";
 import { SetStateCommitment } from "./set-state-commitment";
 import { SetupCommitment } from "./setup-commitment";
 import { WithdrawERC20Commitment } from "./withdraw-erc20-commitment";
@@ -6,6 +7,7 @@ import { WithdrawETHCommitment } from "./withdraw-eth-commitment";
 
 export {
   ConditionalTransactionCommitment,
+  MultisigCommitment,
   SetStateCommitment,
   SetupCommitment,
   WithdrawETHCommitment,
