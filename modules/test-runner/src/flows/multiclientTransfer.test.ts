df268d190ee6df990dca20eb9d65d56eebb94aab
Rahul Sethuram <rahul@connext.network>
2020-03-02 13:27:18 +0100

    Skip test by default

--- modules/test-runner/src/flows/multiclientTransfer.test.ts
+++ modules/test-runner/src/flows/multiclientTransfer.test.ts
@@ -2,7 +2,7 @@ import { IConnextClient, ReceiveTransferFinishedEventData } from "@connext/types
 import { Client } from "ts-nats";
 import { before, after } from "mocha";
 import { createClient, fundChannel, connectNats, closeNats } from "../util";
-import { parseEther } from "ethers/utils";
+import { parseEther, bigNumberify } from "ethers/utils";
 
 describe("Full Flow: Transfer", () => {
   let gateway: IConnextClient;
@@ -27,54 +27,55 @@ describe("Full Flow: Transfer", () => {
     await gateway.messaging.disconnect();
     await indexerA.messaging.disconnect();
     await indexerB.messaging.disconnect();
-    await clientD.messaging.disconnect();
   });
 
   after(() => {
     closeNats();
   });
 
-  it.only("Clients transfer assets between themselves", async function(done) {
+  it.skip("Clients transfer assets between themselves", async function() {
     this.timeout(300_000);
-    await fundChannel(gateway, parseEther("100"));
-    gateway.on(
-      "RECEIVE_TRANSFER_FINISHED_EVENT",
-      async (data: ReceiveTransferFinishedEventData) => {
-        const freeBalance = await gateway.getFreeBalance();
-        if (freeBalance[gateway.freeBalanceAddress]) {
-          done();
-        }
-        console.log("gateway received transfer: ", data);
-        await gateway.transfer({
-          amount: data.amount,
-          recipient: data.sender,
-        });
-      },
-    );
+    return new Promise(async res => {
+      await fundChannel(gateway, bigNumberify(100));
+      gateway.on(
+        "RECEIVE_TRANSFER_FINISHED_EVENT",
+        async (data: ReceiveTransferFinishedEventData) => {
+          const freeBalance = await gateway.getFreeBalance();
+          if (freeBalance[gateway.freeBalanceAddress].isZero()) {
+            res();
+          }
+          console.log("gateway received transfer: ", data);
+          await gateway.transfer({
+            amount: data.amount,
+            recipient: data.sender,
+          });
+        },
+      );
 
-    indexerA.on(
-      "RECEIVE_TRANSFER_FINISHED_EVENT",
-      async (data: ReceiveTransferFinishedEventData) => {
-        console.log("indexerA received transfer: ", data);
-        await indexerA.transfer({
-          amount: data.amount,
-          recipient: data.sender,
-        });
-      },
-    );
+      indexerA.on(
+        "RECEIVE_TRANSFER_FINISHED_EVENT",
+        async (data: ReceiveTransferFinishedEventData) => {
+          console.log("indexerA received transfer: ", data);
+          await indexerA.transfer({
+            amount: data.amount,
+            recipient: data.sender,
+          });
+        },
+      );
 
-    indexerB.on(
-      "RECEIVE_TRANSFER_FINISHED_EVENT",
-      async (data: ReceiveTransferFinishedEventData) => {
-        console.log("indexerB received transfer: ", data);
-        await indexerB.transfer({
-          amount: data.amount,
-          recipient: data.sender,
-        });
-      },
-    );
+      indexerB.on(
+        "RECEIVE_TRANSFER_FINISHED_EVENT",
+        async (data: ReceiveTransferFinishedEventData) => {
+          console.log("indexerB received transfer: ", data);
+          await indexerB.transfer({
+            amount: data.amount,
+            recipient: data.sender,
+          });
+        },
+      );
 
-    await gateway.transfer({ amount: "1", recipient: indexerA.publicIdentifier });
-    await gateway.transfer({ amount: "1", recipient: indexerB.publicIdentifier });
+      await gateway.transfer({ amount: "1", recipient: indexerA.publicIdentifier });
+      await gateway.transfer({ amount: "1", recipient: indexerB.publicIdentifier });
+    });
   });
 });
