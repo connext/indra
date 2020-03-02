daeb9ef00171d3d22026540d53a43a2742acc089
Arjun <arjunbhuptani@gmail.com>
2020-03-02 14:10:01 +0400

    clean up auth

--- modules/node/src/auth/auth.service.ts
+++ modules/node/src/auth/auth.service.ts
@@ -1,16 +1,15 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { arrayify, hexlify, randomBytes, verifyMessage, isHexString } from "ethers/utils";
+import { Injectable } from "@nestjs/common";
+import { arrayify, hexlify, randomBytes, verifyMessage } from "ethers/utils";
 import { fromExtendedKey } from "ethers/utils/hdnode";
 
 import { ChannelRepository } from "../channel/channel.repository";
 import { LoggerService } from "../logger/logger.service";
+import { ConfigService } from "../config/config.service";
 
-import { isValidHex, isXpub, isEthAddress } from "../util";
+import { isXpub } from "../util";
 import { MessagingAuthService } from "@connext/messaging";
 
-// TODO: integrate JWT token
-
-const logger = new Logger("AuthService");
+const logger = new LoggerService("AuthService");
 const nonceLen = 16;
 const nonceTTL = 24 * 60 * 60 * 1000; // 1 day
 
@@ -24,6 +23,7 @@ export class AuthService {
   constructor(
     private readonly channelRepo: ChannelRepository,
     private readonly messagingAuthSerivice: MessagingAuthService,
+    private readonly configService: ConfigService,
   ) {}
 
   // FIXME-- fix this client api contract error...
@@ -67,44 +67,11 @@ export class AuthService {
       // },
     };
 
-    // TODO -- ARJUN: Use admin XPUB from env vars
-    // if (isAdmin) {
-    //   do permissions stuff...
-    // }
+    // if(userPublicIdentifier = config.)
 
     const jwt = this.messagingAuthSerivice.vend(userPublicIdentifier, nonceTTL, permissions);
     logger.debug(``);
     return jwt;
-  }
-
-  useAdminToken(callback: any): any {
-    // get token from subject
-    return async (subject: string, data: { token: string }): Promise<string> => {
-      // // verify token is admin token
-      const { token } = data;
-      if (token !== process.env.INDRA_ADMIN_TOKEN) {
-        return this.badToken(`Unrecognized admin token: ${token}.`);
-      }
-      return callback(data);
-    };
-  }
-
-  useAdminTokenWithPublicIdentifier(callback: any): any {
-    // get token from subject
-    return async (subject: string, data: { token: string }): Promise<string> => {
-      // // verify token is admin token
-      const { token } = data;
-      if (token !== process.env.INDRA_ADMIN_TOKEN) {
-        return this.badToken(`Unrecognized admin token: ${token}.`);
-      }
-
-      // Get & validate xpub from subject
-      const xpub = subject.split(".").pop(); // last item of subscription is xpub
-      if (!xpub || !isXpub(xpub)) {
-        return this.badSubject(`Subject's last item isn't a valid xpub: ${subject}`);
-      }
-      return callback(xpub, data);
-    };
   }
 
   parseXpub(callback: any): any {
@@ -112,7 +79,7 @@ export class AuthService {
       // Get & validate xpub from subject
       const xpub = subject.split(".")[0]; // first item of subscription is xpub
       if (!xpub || !isXpub(xpub)) {
-        return this.badSubject(`Subject's first item isn't a valid xpub: ${subject}`);
+        throw new Error(`Subject's first item isn't a valid xpub: ${subject}`)
       }
       return callback(xpub, data);
     };
