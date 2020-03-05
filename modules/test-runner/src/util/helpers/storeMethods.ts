796ca085b47312a752b7681b864684b6e307dc35
LayneHaber <layne.haber@gmail.com>
2020-03-04 21:34:03 -0700

    comment out mocks, should be taken care of by webpack eventually

--- modules/test-runner/src/util/helpers/storeMethods.ts
+++ modules/test-runner/src/util/helpers/storeMethods.ts
@@ -23,7 +23,7 @@ import {
   FILESTORAGE,
 } from "@connext/types";
 import { BigNumber } from "ethers/utils";
-import MockAsyncStorage from "mock-async-storage";
+// import MockAsyncStorage from "mock-async-storage";
 import uuid from "uuid";
 
 import { expect } from "../";
@@ -110,106 +110,106 @@ export async function testAsyncStorageKey(
   expect(keys[0]).to.equal(asyncStorageKey);
 }
 
-export class MockAsyncWrappedStorage implements WrappedStorage {
-  private asyncStorage: IAsyncStorage;
-  private data: AsyncStorageData = {};
-  private initializing: boolean = false;
-  private initCallbacks: InitCallback[] = [];
+// export class MockAsyncWrappedStorage implements WrappedStorage {
+//   private asyncStorage: IAsyncStorage;
+//   private data: AsyncStorageData = {};
+//   private initializing: boolean = false;
+//   private initCallbacks: InitCallback[] = [];
 
-  constructor(
-    private readonly prefix: string = DEFAULT_STORE_PREFIX,
-    private readonly separator: string = DEFAULT_STORE_SEPARATOR,
-    private readonly asyncStorageKey: string = DEFAULT_ASYNC_STORAGE_KEY,
-  ) {
-    this.asyncStorage = new MockAsyncStorage();
-    this.loadData();
-  }
+//   constructor(
+//     private readonly prefix: string = DEFAULT_STORE_PREFIX,
+//     private readonly separator: string = DEFAULT_STORE_SEPARATOR,
+//     private readonly asyncStorageKey: string = DEFAULT_ASYNC_STORAGE_KEY,
+//   ) {
+//     this.asyncStorage = new MockAsyncStorage();
+//     this.loadData();
+//   }
 
-  loadData(): Promise<AsyncStorageData> {
-    return new Promise(
-      async (resolve, reject): Promise<void> => {
-        if (this.initializing) {
-          // @ts-ignore
-          this.onInit((cb: InitCallback) => resolve(cb));
-        } else {
-          try {
-            this.initializing = true;
-            this.data = await this.fetch();
-            this.initializing = false;
-            resolve(this.data);
-            this.triggerInit(this.data);
-          } catch (e) {
-            this.initializing = false;
-            reject(e);
-          }
-        }
-      },
-    );
-  }
+//   loadData(): Promise<AsyncStorageData> {
+//     return new Promise(
+//       async (resolve, reject): Promise<void> => {
+//         if (this.initializing) {
+//           // @ts-ignore
+//           this.onInit((cb: InitCallback) => resolve(cb));
+//         } else {
+//           try {
+//             this.initializing = true;
+//             this.data = await this.fetch();
+//             this.initializing = false;
+//             resolve(this.data);
+//             this.triggerInit(this.data);
+//           } catch (e) {
+//             this.initializing = false;
+//             reject(e);
+//           }
+//         }
+//       },
+//     );
+//   }
 
-  onInit(callback: InitCallback): void {
-    this.initCallbacks.push(callback);
-  }
+//   onInit(callback: InitCallback): void {
+//     this.initCallbacks.push(callback);
+//   }
 
-  triggerInit(data: AsyncStorageData): void {
-    if (this.initCallbacks && this.initCallbacks.length) {
-      this.initCallbacks.forEach((callback: InitCallback) => callback(data));
-    }
-  }
+//   triggerInit(data: AsyncStorageData): void {
+//     if (this.initCallbacks && this.initCallbacks.length) {
+//       this.initCallbacks.forEach((callback: InitCallback) => callback(data));
+//     }
+//   }
 
-  async getItem(key: string) {
-    await this.loadData();
-    const result = this.data[`${this.prefix}${this.separator}${key}`] || undefined;
-    return result as any;
-  }
+//   async getItem(key: string) {
+//     await this.loadData();
+//     const result = this.data[`${this.prefix}${this.separator}${key}`] || undefined;
+//     return result as any;
+//   }
 
-  async setItem(key: string, value: string): Promise<void> {
-    await this.loadData();
-    this.data[`${this.prefix}${this.separator}${key}`] = value;
-    await this.persist();
-  }
+//   async setItem(key: string, value: string): Promise<void> {
+//     await this.loadData();
+//     this.data[`${this.prefix}${this.separator}${key}`] = value;
+//     await this.persist();
+//   }
 
-  async removeItem(key: string): Promise<void> {
-    await this.loadData();
-    delete this.data[`${this.prefix}${this.separator}${key}`];
-    await this.persist();
-  }
+//   async removeItem(key: string): Promise<void> {
+//     await this.loadData();
+//     delete this.data[`${this.prefix}${this.separator}${key}`];
+//     await this.persist();
+//   }
 
-  async persist(): Promise<void> {
-    await this.asyncStorage.setItem(this.asyncStorageKey, safeJsonStringify(this.data));
-  }
+//   async persist(): Promise<void> {
+//     await this.asyncStorage.setItem(this.asyncStorageKey, safeJsonStringify(this.data));
+//   }
 
-  async fetch(): Promise<AsyncStorageData> {
-    const data = await this.asyncStorage.getItem(this.asyncStorageKey);
-    return safeJsonParse(data) || {};
-  }
+//   async fetch(): Promise<AsyncStorageData> {
+//     const data = await this.asyncStorage.getItem(this.asyncStorageKey);
+//     return safeJsonParse(data) || {};
+//   }
 
-  async getKeys(): Promise<string[]> {
-    const relevantKeys = Object.keys(this.data).filter(key => key.startsWith(this.prefix));
-    return relevantKeys.map(key => key.split(`${this.prefix}${this.separator}`)[1]);
-  }
+//   async getKeys(): Promise<string[]> {
+//     const relevantKeys = Object.keys(this.data).filter(key => key.startsWith(this.prefix));
+//     return relevantKeys.map(key => key.split(`${this.prefix}${this.separator}`)[1]);
+//   }
 
-  async getEntries(): Promise<[string, any][]> {
-    return Object.entries(this.data).filter(([name, _]) => name.startsWith(this.prefix));
-  }
+//   async getEntries(): Promise<[string, any][]> {
+//     return Object.entries(this.data).filter(([name, _]) => name.startsWith(this.prefix));
+//   }
 
-  clear(): Promise<void> {
-    return this.asyncStorage.removeItem(this.asyncStorageKey);
-  }
+//   clear(): Promise<void> {
+//     return this.asyncStorage.removeItem(this.asyncStorageKey);
+//   }
 
-  restore(): Promise<void> {
-    return this.clear();
-  }
+//   restore(): Promise<void> {
+//     return this.clear();
+//   }
 
-  joinWithSeparator(...args: string[]): string {
-    let str = "";
-    args.forEach(arg => {
-      // dont add separator to last one
-      if (args.indexOf(arg) === args.length - 1) {
-        return;
-      }
-      str.concat(arg, this.separator);
-    });
-    return str;
-  }
-}
+//   joinWithSeparator(...args: string[]): string {
+//     let str = "";
+//     args.forEach(arg => {
+//       // dont add separator to last one
+//       if (args.indexOf(arg) === args.length - 1) {
+//         return;
+//       }
+//       str.concat(arg, this.separator);
+//     });
+//     return str;
+//   }
+// }
