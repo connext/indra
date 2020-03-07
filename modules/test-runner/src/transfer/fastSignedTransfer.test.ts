cd98e5109eb7ec7c5cac147280b02e9d79b9c47f
Rahul Sethuram <rahul@connext.network>
2020-03-07 22:39:37 +0100

    Fix test

--- modules/test-runner/src/transfer/fastSignedTransfer.test.ts
+++ modules/test-runner/src/transfer/fastSignedTransfer.test.ts
@@ -1,14 +1,74 @@
-import { createClient } from "../util";
-import { IConnextClient } from "@connext/types";
+import {
+  IConnextClient,
+  FastSignedTransferParameters,
+  FAST_SIGNED_TRANSFER,
+  FastSignedTransferAppStateBigNumber,
+  FastSignedTransfer,
+  bigNumberifyObj,
+  CoinTransfer,
+} from "@connext/types";
+import { hexlify, randomBytes, bigNumberify, BigNumber, hexZeroPad } from "ethers/utils";
+import { Wallet } from "ethers";
+import { AddressZero, One, HashZero, Zero } from "ethers/constants";
+
+import { createClient, fundChannel, expect } from "../util";
+import { xkeyKthAddress } from "@connext/cf-core";
 
 describe("Fast Signed Transfer", () => {
   let clientA: IConnextClient;
+  let clientB: IConnextClient;
 
   beforeEach(async () => {
     clientA = await createClient();
+    clientB = await createClient();
   });
 
   afterEach(async () => {
     await clientA.messaging.disconnect();
+    await clientB.messaging.disconnect();
+  });
+
+  it("Should send a fast signed transfer", async () => {
+    const paymentId = hexlify(randomBytes(32));
+    const signerWallet = Wallet.createRandom();
+    const signerAddress = await signerWallet.getAddress();
+
+    const initialChannelBalance = bigNumberify(10);
+    const transferAmount = One;
+
+    await fundChannel(clientA, initialChannelBalance);
+    const { transferAppInstanceId }: any = await clientA.conditionalTransfer({
+      amount: transferAmount.toString(),
+      conditionType: FAST_SIGNED_TRANSFER,
+      paymentId,
+      recipient: clientB.publicIdentifier,
+      signer: signerAddress,
+      assetId: AddressZero,
+      meta: { foo: "bar" },
+    } as FastSignedTransferParameters);
+
+    const transferApp = await clientA.getAppInstanceDetails(transferAppInstanceId);
+    expect(transferApp).to.be.ok;
+    const transferAppState = transferApp.appInstance
+      .latestState as FastSignedTransferAppStateBigNumber;
+
+    const coinTransfers: CoinTransfer<BigNumber>[] = transferAppState.coinTransfers.map(
+      bigNumberifyObj,
+    );
+    expect(coinTransfers[0][0]).eq(clientA.freeBalanceAddress);
+    expect(coinTransfers[0][1]).eq(initialChannelBalance.sub(transferAmount));
+    expect(coinTransfers[1][0]).eq(xkeyKthAddress(clientA.nodePublicIdentifier));
+    expect(coinTransfers[1][1]).eq(Zero);
+
+    // locked payments contains transfer info
+    const transfers = transferAppState.lockedPayments;
+    expect(transfers.length).to.eq(1);
+    const [transfer] = transfers;
+    expect(transfer[0]).to.eq(clientB.publicIdentifier);
+    expect(transfer[1]).to.eq(transferAmount);
+    expect(transfer[2]).to.eq(signerAddress);
+    expect(transfer[3]).to.eq(paymentId);
+    expect(transfer[4]).to.eq(HashZero);
+    expect(transfer[5]).to.eq(hexZeroPad(HashZero, 65));
   });
 });
