9f2d2030352b0a1214abb33c4934c5d60c7e10ba
LayneHaber <layne.haber@gmail.com>
2020-03-08 20:28:42 -0700

    add relations to commitment tables

--- modules/node/migrations/1583618773094-update-channel-table.ts
+++ modules/node/migrations/1583618773094-update-channel-table.ts
@@ -8,8 +8,30 @@ export class UpdateChannelTable1583618773094 implements MigrationInterface {
       ALTER TABLE "channel"
         ADD COLUMN "schemaVersion" integer NOT NULL,
         ADD COLUMN "addresses" json NOT NULL,
-        ADD COLUMN "singleAssetTwoPartyIntermediaryAgreements" json array NOT NULL,
+        ADD COLUMN "singleAssetTwoPartyIntermediaryAgreements" json NOT NULL,
         ADD COLUMN "monotonicNumProposedApps" integer NOT NULL;
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "conditional_transaction_commitment" 
+        ADD CONSTRAINT "PK_5bb6fd3cc74b86faf8bfee876dd"
+        FOREIGN KEY ("appIdentityHash")
+        REFERENCES "app_instance" ("identityHash");
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "set_state_commitment" 
+        ADD CONSTRAINT "PK_5bb6fd3cc74b86faf8bfff873ee"
+        FOREIGN KEY ("appIdentityHash")
+        REFERENCES "app_instance" ("identityHash");
+    `);
+
+    await queryRunner.query(`
+      ALTER TABLE "withdraw_commitment"
+        ADD COLUMN "channelId" integer NOT NULL,
+        ADD CONSTRAINT "PK_5bb6fd3cc74b86faf8bfbb679ee"
+        FOREIGN KEY ("channelId")
+        REFERENCES "channel" ("id");
     `);
   }
 
