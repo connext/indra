cd6fcb8b01f50c2ebf443549b7ab866a3b2308f4
Arjun <arjunbhuptani@gmail.com>
2020-03-09 18:33:25 +0400

    Move withdraw types into new file

---
+++ modules/types/src/apps/WithdrawApp.ts
@@ -0,0 +1,40 @@
+import { CoinTransfer } from "../";
+import { BigNumber } from "ethers/utils";
+import { singleAssetTwoPartyCoinTransferEncoding } from "../contracts";
+import { Address } from "../basic";
+import { TransactionResponse } from "ethers/providers";
+
+// Contract types
+export type WithdrawAppState<T = string> = {
+  transfers: CoinTransfer<T>[];
+  signatures: string[];
+  signers: string[];
+  data: string;
+  finalized: boolean;
+}
+export type WithdrawAppStateBigNumber = WithdrawAppState<BigNumber>;
+export type WithdrawAppAction = {
+  signature: string;
+}
+
+// Input/output
+export type WithdrawParameters<T = string> = {
+  amount: T;
+  assetId?: string; // if not provided, will default to 0x0 (Eth)
+  recipient?: Address; // if not provided, will default to signer addr
+};
+export type WithdrawParametersBigNumber = WithdrawParameters<BigNumber>;
+
+export type WithdrawResponse = {
+  transaction: TransactionResponse;
+}
+
+export const WithdrawAppStateEncoding = `tuple(
+  ${singleAssetTwoPartyCoinTransferEncoding} transfers,
+  bytes[2] signatures,
+  address[2] signers,
+  bytes32 data,
+  bool finalized
+)`;
+
+export const WithdrawAppActionEncoding = `tuple(bytes signature)`;
--- modules/types/src/inputs.ts
+++ modules/types/src/inputs.ts
@@ -29,13 +29,6 @@ export type RescindDepositRightsParameters = RequestDepositRightsParameters;
 
 export type RescindDepositRightsResponse = ProtocolTypes.DepositResult;
 
-////// Withdraw types
-export type WithdrawParameters<T = string> = DepositParameters<T> & {
-  userSubmitted?: boolean;
-  recipient?: Address; // if not provided, will default to signer addr
-};
-export type WithdrawParametersBigNumber = WithdrawParameters<BigNumber>;
-
 ////// Generic transfer types
 export type TransferParameters<T = string> = DepositParameters<T> & {
   recipient: Address;
