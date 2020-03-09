8b6e85c8fbb69becbf5b1a1997aea5345f3b49c9
Rahul Sethuram <rahul@connext.network>
2020-03-09 10:52:50 +0100

    Remove array of payments in app

--- modules/contracts/contracts/apps/FastSignedTransferApp.sol
+++ modules/contracts/contracts/apps/FastSignedTransferApp.sol
@@ -23,25 +23,29 @@ contract FastSignedTransferApp is CounterfactualApp {
         REJECT
     }
 
-    struct Payment {
+    struct AppState {
+        // transfer metadata
         string recipientXpub; // Not checked in app, but is part of the state for intermediaries to use
         uint256 amount;
         address signer;
         // This needs to be unique to each payment - the entropy is used to ensure that
         // intermediaries can't steal money by replaying state.
         bytes32 paymentId;
-        bytes32 data;
-        bytes signature;
-    }
 
-    struct AppState {
-        Payment[] lockedPayments;
+        // app metadata
         LibOutcome.CoinTransfer[2] coinTransfers; // balances
         uint256 turnNum;
     }
 
     struct Action {
-        Payment[] newLockedPayments;
+        // transfer metadata
+        string recipientXpub;
+        uint256 amount;
+        address signer;
+        bytes32 paymentId;
+        bytes32 data;
+        bytes signature;
+
         ActionType actionType;
     }
 
@@ -64,6 +68,10 @@ contract FastSignedTransferApp is CounterfactualApp {
         returns (bytes memory)
     {
         AppState memory state = abi.decode(encodedState, (AppState));
+        // return non-unlocked payment to sender
+        if (state.paymentId != bytes32(0)) {
+            state.coinTransfers[0].amount = state.coinTransfers[0].amount.add(state.amount);
+        }
         return abi.encode(state.coinTransfers);
     }
 
@@ -108,20 +116,21 @@ contract FastSignedTransferApp is CounterfactualApp {
         returns (AppState memory)
     {
         require(state.turnNum % 2 == 0, "Only senders can create locked payments.");
-        for (uint i = 0; i < action.newLockedPayments.length; i++) {
-            require(
-                action.newLockedPayments[i].paymentId != bytes32(0), "paymentId cannot be empty bytes"
-            );
-            require(
-                (find(state.lockedPayments, action.newLockedPayments[i].paymentId)).paymentId == bytes32(0), "Locked payment with this paymentId already exists."
-            );
-            require(action.newLockedPayments[i].amount <= state.coinTransfers[0].amount, "Insufficient balance for new locked payment");
-            require(action.newLockedPayments[i].data == bytes32(0), "Data field must be empty bytes");
 
-            // Reduce sender's balance by locked payment amount and then insert into state lockedPayments array
-            state.coinTransfers[0].amount = state.coinTransfers[0].amount.sub(action.newLockedPayments[i].amount);
-            state.lockedPayments = insert(state.lockedPayments, action.newLockedPayments[i]);
-        }
+        require(state.paymentId == bytes32(0), "Transfer data is not empty");
+
+        require(
+            action.amount <= state.coinTransfers[0].amount,
+            "Insufficient balance for new locked transfer"
+        );
+        require (action.paymentId != bytes32(0), "paymentId cannot be empty bytes");
+        require(action.data == bytes32(0), "Data field must be empty bytes");
+
+        state.recipientXpub = action.recipientXpub;
+        state.amount = action.amount;
+        state.signer = action.signer;
+        state.paymentId = action.paymentId;
+
         return state;
     }
 
@@ -133,27 +142,25 @@ contract FastSignedTransferApp is CounterfactualApp {
         pure
         returns (AppState memory)
     {
-        require(state.turnNum % 2 == 1, "Only receivers can unlock payments.");
-        for (uint i = 0; i < action.newLockedPayments.length; i++) {
-            require(
-                action.newLockedPayments[i].paymentId != bytes32(0),
-                "paymentId cannot be empty bytes"
-            );
-            require(
-                (find(state.lockedPayments, action.newLockedPayments[i].paymentId)).paymentId != bytes32(0),
-                "No locked payment with that paymentId exists"
-            );
+        require(state.turnNum % 2 == 1, "Only receivers can unlock transfers.");
 
-            Payment memory lockedPayment = find(state.lockedPayments, action.newLockedPayments[i].paymentId);
-            // TODO any possibility of collision?
-            bytes32 rawHash = keccak256(abi.encodePacked(action.newLockedPayments[i].data, lockedPayment.paymentId));
-            require(lockedPayment.signer == rawHash.recover(action.newLockedPayments[i].signature), "Incorrect signer recovered from signature");
+        require(
+            action.paymentId != bytes32(0),
+            "paymentId cannot be empty bytes"
+        );
+        require(
+            state.paymentId == action.paymentId,
+            "No locked transfer with that paymentId exists"
+        );
 
-            // Add balances to transfers
-            state.coinTransfers[1].amount = state.coinTransfers[1].amount.add(lockedPayment.amount);
-            state.lockedPayments = remove(state.lockedPayments, action.newLockedPayments[i].paymentId);
-        }
-        return state;
+        // TODO any possibility of collision?
+        bytes32 rawHash = keccak256(abi.encodePacked(action.data, state.paymentId));
+        require(state.signer == rawHash.recover(action.signature), "Incorrect signer recovered from signature");
+
+        // Add balances to transfers
+        state.coinTransfers[1].amount = state.coinTransfers[1].amount.add(state.amount);
+        AppState newState = removeTransfer(state);
+        return newState;
     }
 
     function doReject(
@@ -165,71 +172,21 @@ contract FastSignedTransferApp is CounterfactualApp {
         returns (AppState memory)
     {
         require(state.turnNum % 2 == 1, "Only receivers can reject payments.");
-        for (uint8 i = 0; i < action.newLockedPayments.length; i++) {
-            require(
-                action.newLockedPayments[i].paymentId != bytes32(0),
-                "paymentId cannot be 0"
-            );
-            state.lockedPayments = remove(state.lockedPayments, action.newLockedPayments[i].paymentId);
-        }
-        return state;
+        AppState newState = removeTransfer(state);
+        return newState;
     }
 
-    function insert(
-        Payment[] memory lockedPayments,
-        Payment memory newLockedPayment
+    function removeTransfer(
+        AppState memory state
     )
         internal
         pure
-        returns (Payment[] memory)
+        returns (AppState memory)
     {
-        // Can't dynamically size arrays in-memory in solidity. Must copy to new array.
-        Payment[] memory newLockedPayments = new Payment[](lockedPayments.length+1); // TODO is this the right pattern?
-        for (uint i = 0; i <= lockedPayments.length; i++) {
-            if (i == lockedPayments.length) {
-                newLockedPayments[i] = newLockedPayment;
-            } else {
-                newLockedPayments[i] = lockedPayments[i];
-            }
-        }
-        return newLockedPayments;
-    }
-
-    function remove(
-        Payment[] memory lockedPayments,
-        bytes32 paymentId
-    )
-        internal
-        pure
-        returns (Payment[] memory)
-    {
-        uint j = 0;
-        Payment[] memory newLockedPayments = new Payment[](lockedPayments.length-1);
-        for (uint i = 0; i < lockedPayments.length; i++) {
-            // If the element should stay, write it to a new array
-            if (lockedPayments[i].paymentId != paymentId) {
-                newLockedPayments[j] = lockedPayments[i];
-                j++;
-            }
-        }
-        return newLockedPayments;
-    }
-
-    function find(
-        Payment[] memory lockedPayments,
-        bytes32 paymentId
-    )
-        internal
-        pure
-        returns (Payment memory)
-    {
-        Payment memory element;
-        for (uint i = 0; i < lockedPayments.length; i++) {
-            if (lockedPayments[i].paymentId == paymentId) {
-                element = lockedPayments[i];
-                break;
-            }
-        }
-        return element;
+        state.amount = 0;
+        state.signer = address(0);
+        state.paymentId = bytes32(0);
+        state.data = bytes32(0);
+        state.signature = [];
     }
 }
