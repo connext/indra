0941b9ab8c9636018df14dd2078eef7cfd9120f6
Rahul Sethuram <rahul@connext.network>
2020-03-10 10:25:35 +0100

    View table to migration

--- modules/node/migrations/1583682931763-fast-signed-transfer.ts
+++ modules/node/migrations/1583682931763-fast-signed-transfer.ts
@@ -30,6 +30,39 @@ export class fastSignedTransfer1583682931763 implements MigrationInterface {
       )`,
       undefined,
     );
+    await queryRunner.query(
+      `DROP VIEW "transfer";
+      CREATE OR REPLACE VIEW "transfer" AS
+      SELECT
+        "fast_signed_transfer"."paymentId" as "paymentId",
+        "fast_signed_transfer"."amount" as "amount",
+        "fast_signed_transfer"."assetId" as "assetId",
+        "sender_channel"."userPublicIdentifier" as "senderPublicIdentifier",
+        "fast_signed_transfer"."createdAt" as "createdAt",
+        "fast_signed_transfer"."meta" as "meta",
+        "fast_signed_transfer"."recipientPublicIdentifier" as "receiverPublicIdentifier",
+        "fast_signed_transfer"."status"::TEXT as "status",
+        'FAST_SIGNED' AS "type"
+      FROM fast_signed_transfer
+        LEFT JOIN channel receiver_channel ON receiver_channel.id = fast_signed_transfer."receiverChannelId"
+        LEFT JOIN channel sender_channel ON sender_channel.id = fast_signed_transfer."senderChannelId";
+      UNION ALL
+      SELECT
+        "linked_transfer"."paymentId" as "paymentId",
+        "linked_transfer"."amount" as "amount",
+        "linked_transfer"."assetId" as "assetId",
+        "sender_channel"."userPublicIdentifier" as "senderPublicIdentifier",
+        "linked_transfer"."createdAt" as "createdAt",
+        "linked_transfer"."meta" as "meta",
+        "linked_transfer"."recipientPublicIdentifier" as "receiverPublicIdentifier",
+        "linked_transfer"."status"::TEXT as "status",
+        'LINKED' AS "type"
+      FROM linked_transfer
+        LEFT JOIN channel receiver_channel ON receiver_channel.id = linked_transfer."receiverChannelId"
+        LEFT JOIN channel sender_channel ON sender_channel.id = linked_transfer."senderChannelId";
+      `,
+      undefined,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
@@ -44,5 +77,38 @@ export class fastSignedTransfer1583682931763 implements MigrationInterface {
 
     await queryRunner.query(`DROP TABLE "fast_signed_transfer"`, undefined);
     await queryRunner.query(`DROP TYPE "fast_signed_transfer_status_enum"`, undefined);
+
+    await queryRunner.query(`
+      DROP VIEW transfer;
+
+      CREATE OR REPLACE VIEW transfer AS
+      SELECT
+        "peer_to_peer_transfer"."id"::TEXT as "paymentId",
+        "peer_to_peer_transfer"."amount" as "amount",
+        "peer_to_peer_transfer"."assetId" as "assetId",
+        "sender_channel"."userPublicIdentifier" as "senderPublicIdentifier",
+        "receiver_channel"."userPublicIdentifier" as "receiverPublicIdentifier",
+        "peer_to_peer_transfer"."createdAt" as "createdAt",
+        "peer_to_peer_transfer"."meta" as "meta",
+        "peer_to_peer_transfer"."status"::TEXT as "status",
+        'P2P' AS "type"
+      FROM peer_to_peer_transfer
+        LEFT JOIN channel receiver_channel ON receiver_channel.id = peer_to_peer_transfer."receiverChannelId"
+        LEFT JOIN channel sender_channel ON sender_channel.id = peer_to_peer_transfer."senderChannelId"
+      UNION ALL
+      SELECT
+        "linked_transfer"."paymentId" as "paymentId",
+        "linked_transfer"."amount" as "amount",
+        "linked_transfer"."assetId" as "assetId",
+        "sender_channel"."userPublicIdentifier" as "senderPublicIdentifier",
+        "receiver_channel"."userPublicIdentifier" as "receiverPublicIdentifier",
+        "linked_transfer"."createdAt" as "createdAt",
+        "linked_transfer"."meta" as "meta",
+        "linked_transfer"."status"::TEXT as "status",
+        'LINKED' AS "type"
+      FROM linked_transfer
+        LEFT JOIN channel receiver_channel ON receiver_channel.id = linked_transfer."receiverChannelId"
+        LEFT JOIN channel sender_channel ON sender_channel.id = linked_transfer."senderChannelId";
+    `);
   }
 }
