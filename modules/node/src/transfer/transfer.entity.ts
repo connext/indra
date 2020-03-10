190ca3a335df5aa3a2a16528af6543ef390db19b
Rahul Sethuram <rahul@connext.network>
2020-03-10 10:26:21 +0100

    Add fast-signed transfers to view

--- modules/node/src/transfer/transfer.entity.ts
+++ modules/node/src/transfer/transfer.entity.ts
@@ -3,23 +3,24 @@ import { ViewColumn, ViewEntity } from "typeorm";
 export enum TransferType {
   P2P = "P2P",
   LINKED = "LINKED",
+  FAST_SIGNED = "FAST_SIGNED",
 }
 
 @ViewEntity({
   expression: `
   SELECT
-    "peer_to_peer_transfer"."id"::TEXT as "paymentId",
-    "peer_to_peer_transfer"."amount" as "amount",
-    "peer_to_peer_transfer"."assetId" as "assetId",
+    "fast_signed_transfer"."paymentId" as "paymentId",
+    "fast_signed_transfer"."amount" as "amount",
+    "fast_signed_transfer"."assetId" as "assetId",
     "sender_channel"."userPublicIdentifier" as "senderPublicIdentifier",
-    "receiver_channel"."userPublicIdentifier" as "receiverPublicIdentifier",
-    "peer_to_peer_transfer"."createdAt" as "createdAt",
-    "peer_to_peer_transfer"."meta" as "meta",
-    "peer_to_peer_transfer"."status"::TEXT as "status",
-    '${TransferType.P2P}' AS "type"
-  FROM peer_to_peer_transfer
-    LEFT JOIN channel receiver_channel ON receiver_channel.id = peer_to_peer_transfer."receiverChannelId"
-    LEFT JOIN channel sender_channel ON sender_channel.id = peer_to_peer_transfer."senderChannelId"
+    "fast_signed_transfer"."createdAt" as "createdAt",
+    "fast_signed_transfer"."meta" as "meta",
+    "fast_signed_transfer"."recipientPublicIdentifier" as "receiverPublicIdentifier",
+    "fast_signed_transfer"."status"::TEXT as "status",
+    '${TransferType.FAST_SIGNED}' AS "type"
+  FROM fast_signed_transfer
+    LEFT JOIN channel receiver_channel ON receiver_channel.id = fast_signed_transfer."receiverChannelId"
+    LEFT JOIN channel sender_channel ON sender_channel.id = fast_signed_transfer."senderChannelId";
   UNION ALL
   SELECT
     "linked_transfer"."paymentId" as "paymentId",
