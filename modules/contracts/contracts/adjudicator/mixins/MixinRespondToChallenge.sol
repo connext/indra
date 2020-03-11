a7e6cd7731ad6db14a59da3e57e5869bebf826e6
Heiko Fisch <heiko.fisch@gmail.com>
2020-03-11 18:06:21 +0100

    Fix `respondToChallenge`

--- modules/contracts/contracts/adjudicator/mixins/MixinRespondToChallenge.sol
+++ modules/contracts/contracts/adjudicator/mixins/MixinRespondToChallenge.sol
@@ -12,54 +12,79 @@ contract MixinRespondToChallenge is LibStateChannelApp, LibDispute, LibAppCaller
     /// @notice Respond to a challenge with a valid action
     /// @param appIdentity an AppIdentity object pointing to the app for which there is a challenge to progress
     /// @param appState The ABI encoded latest signed application state
-    /// @param action The ABI encoded action the submitter wishes to take
-    /// @param actionSignature A bytes string of a single signature by the address of the
-    /// participant for which it is their turn to take the submitted `action`
-    /// @dev This function is only callable when the application has an open challenge
+    /// @param action The ABI encoded action the submitter wishes to take and
+    /// a single signature by the address of the participant for which it is their turn
+    /// to take the submitted `action`
     function respondToChallenge(
         AppIdentity memory appIdentity,
         bytes memory appState,
-        bytes memory action,
-        bytes memory actionSignature
+        SignedAction memory action
     )
         public
     {
-
         bytes32 identityHash = appIdentityToHash(appIdentity);
 
         AppChallenge storage challenge = appChallenges[identityHash];
 
         require(
-            (
-                challenge.status == ChallengeStatus.FINALIZES_AFTER_DEADLINE
-            ) && challenge.finalizesAt >= block.number,
-            "respondToChallenge called on app not in FINALIZES_AFTER_DEADLINE state"
+            isProgressable(challenge, appIdentity.defaultTimeout),
+            "respondToChallenge called on app not in a progressable state"
         );
 
         require(
-            keccak256(appState) == challenge.appStateHash,
+            appStateToHash(appState) == challenge.appStateHash,
             "Tried to progress a challenge with non-agreed upon app"
         );
 
+        require(
+            correctKeySignedTheAction(
+                appIdentity,
+                challenge,
+                appState,
+                action
+            ),
+            "respondToChallenge called with action signed by incorrect turn taker"
+        );
+
+        // This should throw an error if reverts
+        bytes memory newState = applyAction(
+            appIdentity.appDefinition,
+            appState,
+            action.encodedAction
+        );
+
+        challenge.status = ChallengeStatus.IN_ONCHAIN_PROGRESSION;
+        challenge.latestSubmitter = msg.sender;
+        challenge.appStateHash = appStateToHash(newState);
+        challenge.versionNumber++;
+        challenge.finalizesAt = block.number.add(appIdentity.defaultTimeout);
+    }
+
+    function correctKeySignedTheAction(
+        AppIdentity memory appIdentity,
+        AppChallenge memory challenge,
+        bytes memory appState,
+        SignedAction memory action
+    )
+        private
+        pure
+        returns (bool)
+    {
         address turnTaker = getTurnTaker(
             appIdentity.appDefinition,
             appIdentity.participants,
             appState
         );
 
-        require(
-            turnTaker == keccak256(action).recover(actionSignature),
-            "Action must have been signed by correct turn taker"
+        bytes32 actionHash = computeActionHash(
+            turnTaker,
+            challenge.appStateHash,
+            action.encodedAction,
+            challenge.versionNumber
         );
 
-        // This should throw an error if reverts
-        applyAction(
-            appIdentity.appDefinition,
-            appState,
-            action
-        );
+        address signer = actionHash.recover(action.signature);
 
-        delete appChallenges[identityHash];
-
+        return turnTaker == signer;
     }
 }
