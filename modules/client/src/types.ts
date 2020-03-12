62619a429780701a9f25876443b0ba1b8de93add
LayneHaber <layne.haber@gmail.com>
2020-03-11 19:18:56 -0700

    fix client build

--- modules/client/src/types.ts
+++ modules/client/src/types.ts
@@ -9,7 +9,7 @@ import {
   INodeApiClient,
   KeyGen,
   Network,
-  Store,
+  IStoreService,
 } from "@connext/types";
 
 // This type is only ever used inside the client,
@@ -24,7 +24,7 @@ export type InternalClientOptions = {
   messaging: IMessagingService;
   network: Network;
   node: INodeApiClient;
-  store: Store;
+  store: IStoreService;
   token: Contract;
   xpub: string;
 };
