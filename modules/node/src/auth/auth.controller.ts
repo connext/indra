59988714d779e829f6de7a4cae0b7835497e466c
Rahul Sethuram <rahul@connext.network>
2020-03-13 13:00:47 +0100

    Throw proper exception

--- modules/node/src/auth/auth.controller.ts
+++ modules/node/src/auth/auth.controller.ts
@@ -1,20 +1,29 @@
-import { Controller, Get, Post, Body, Param } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, BadRequestException } from "@nestjs/common";
+
+import { LoggerService } from "../logger/logger.service";
 
 import { VerifyNonceDto } from "./auth.dto";
 import { AuthService } from "./auth.service";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly log: LoggerService) {
+    this.log.setContext("AuthController");
+  }
 
-  @Get("nonce/:userPublicIdentifier")
-  async getNonce(@Param() userPublicIdentifier: string): Promise<string> {
+  @Get(":userPublicIdentifier")
+  async getNonce(@Param("userPublicIdentifier") userPublicIdentifier: string): Promise<string> {
     return this.authService.getNonce(userPublicIdentifier);
   }
 
-  @Post("nonce")
+  @Post("")
   async verifyNonce(@Body() verifyNonceDto: VerifyNonceDto): Promise<string> {
     let { sig, userPublicIdentifier, adminToken } = verifyNonceDto;
-    return this.authService.verifyAndVend(sig, userPublicIdentifier, adminToken);
+    try {
+      return this.authService.verifyAndVend(sig, userPublicIdentifier, adminToken);
+    } catch (e) {
+      this.log.error(e);
+      throw new BadRequestException(`Signature not verified: ${e.toString()}`);
+    }
   }
 }
