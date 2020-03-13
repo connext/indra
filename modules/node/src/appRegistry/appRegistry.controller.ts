dc090a03ab33ca3e224d1ac42bcbf6dcec5eb6d2
Rahul Sethuram <rahul@connext.network>
2020-03-13 13:00:25 +0100

    Get app registry by HTTP

--- modules/node/src/appRegistry/appRegistry.controller.ts
+++ modules/node/src/appRegistry/appRegistry.controller.ts
@@ -1,5 +1,4 @@
-import { Controller } from "@nestjs/common";
-import { MessagePattern } from "@nestjs/microservices";
+import { Controller, Get } from "@nestjs/common";
 
 import { AppRegistry } from "./appRegistry.entity";
 import { AppRegistryRepository } from "./appRegistry.repository";
@@ -8,19 +7,8 @@ import { AppRegistryRepository } from "./appRegistry.repository";
 export class AppRegistryController {
   constructor(private readonly appRegistryRepository: AppRegistryRepository) {}
 
-  @MessagePattern("app-registry")
-  async getNats(
-    data: { name?: string; chainId?: number; appDefinitionAddress?: string } | undefined,
-  ): Promise<AppRegistry[]> {
-    if (data && data.chainId && data.name) {
-      return [await this.appRegistryRepository.findByNameAndNetwork(data.name, data.chainId)];
-    }
-
-    if (data && data.appDefinitionAddress) {
-      return [
-        await this.appRegistryRepository.findByAppDefinitionAddress(data.appDefinitionAddress),
-      ];
-    }
-    return await this.appRegistryRepository.find();
+  @Get()
+  async get(): Promise<AppRegistry[]> {
+    return this.appRegistryRepository.find();
   }
 }
