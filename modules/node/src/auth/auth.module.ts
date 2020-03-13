3684b2c475143266b8225f02b2cff68a20cbb131
Rahul Sethuram <rahul@connext.network>
2020-03-13 13:00:56 +0100

    Export controller

--- modules/node/src/auth/auth.module.ts
+++ modules/node/src/auth/auth.module.ts
@@ -7,8 +7,10 @@ import { ConfigModule } from "../config/config.module";
 
 import { AuthService } from "./auth.service";
 import { messagingAuthProviderFactory } from "./auth.provider";
+import { AuthController } from "./auth.controller";
 
 @Module({
+  controllers: [AuthController],
   exports: [AuthService, messagingAuthProviderFactory],
   imports: [LoggerModule, TypeOrmModule.forFeature([ChannelRepository]), ConfigModule],
   providers: [AuthService, messagingAuthProviderFactory],
