669463f0efc3491c9b88bd82205a4b38e4a3fee3
LayneHaber <layne.haber@gmail.com>
2020-03-18 12:14:20 -0700

    create example index for hardcoded values

--- modules/test-runner/src/store/migrate.test.ts
+++ modules/test-runner/src/store/migrate.test.ts
@@ -5,8 +5,14 @@ import { Client as NatsClient } from "ts-nats";
 import SQL from "sql-template-strings";
 
 import { expect, getDbClient, connectNats, env } from "../util";
+import {
+  CHANNEL_VALUE_VO_2,
+  CHANNEL_KEY_VO_2,
+  CHANNEL_VALUE_VO_1,
+  CHANNEL_KEY_VO_1,
+} from "./examples";
 
-describe("Store Migrations", () => {
+describe.only("Store Migrations", () => {
   let dbClient: DBClient;
   let nats: NatsClient;
 
@@ -19,115 +25,12 @@ describe("Store Migrations", () => {
     await dbClient.query("truncate table node_records cascade;");
   });
 
-  it.only("node can migrate from v0 to v1", async () => {
-    const oldChannelKey1 =
-      "INDRA_NODE_CF_CORE/xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6/channel/0x8C0A6Fee57539DCF1e2F8414ded4C3692742f994";
-    const oldChannelValue1 = {
-      "INDRA_NODE_CF_CORE/xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6/channel/0x8C0A6Fee57539DCF1e2F8414ded4C3692742f994": {
-        multisigAddress: "0x8C0A6Fee57539DCF1e2F8414ded4C3692742f994",
-        userNeuteredExtendedKeys: [
-          "xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6",
-          "xpub6EahuqCg3qVXgL1oh33w6gfUYrKh4gBoMacUa8oJvRpzg1inb9aZ73fS1iAP2pKrRf3pkbmTSyuybLhLhYXVVPF9VvVbgjCeaQcfi8M5qpV",
-        ],
-        appInstances: [] as [string, AppInstanceJson][],
-        freeBalanceAppInstance: {
-          participants: [
-            "0xadFA28e08Fb7427cfE0e02b47632151FC549eDd9",
-            "0xDA3CCBa9F3e3a9fE7D0Ed9F699Ca2BEF78Ba7A6c",
-          ],
-          defaultTimeout: 172800,
-          appInterface: {
-            addr: "0xde8d1288e2c7eC3e0b7279F8395b87A996Cc02f4",
-            stateEncoding:
-              "\n  tuple(\n    address[] tokenAddresses,\n    tuple(\n      address to,\n      uint256 amount\n    )[][] balances,\n    bytes32[] activeApps\n  )\n",
-          },
-          appSeqNo: 0,
-          latestState: {
-            activeApps: [],
-            tokenAddresses: ["0x0000000000000000000000000000000000000000"],
-            balances: [
-              [
-                { to: "0xadFA28e08Fb7427cfE0e02b47632151FC549eDd9", amount: { _hex: "0x00" } },
-                { to: "0xDA3CCBa9F3e3a9fE7D0Ed9F699Ca2BEF78Ba7A6c", amount: { _hex: "0x00" } },
-              ],
-            ],
-          },
-          latestVersionNumber: 0,
-          latestTimeout: 172800,
-          outcomeType: "MULTI_ASSET_MULTI_PARTY_COIN_TRANSFER",
-          identityHash: "0x278399c996e94f465e79ad006c5766b53f9cb61715943f9c35ea595ae3dd3611",
-        },
-        monotonicNumProposedApps: 1,
-        singleAssetTwoPartyIntermediaryAgreements: [],
-        createdAt: 1569188857018,
-        proxyFactoryAddress: "0x8eb543b35DE94B0E636402C7cA32947b22853eDF",
-        addresses: {
-          proxyFactory: "0x8eb543b35DE94B0E636402C7cA32947b22853eDF",
-          multisigMastercopy: "0xe54f4EBeCE507477dFb86FA226394bdbA0b85d66",
-        },
-      },
-    };
+  it("node can migrate from v0 to v1", async () => {
+    const oldChannelKey1 = CHANNEL_KEY_VO_1;
+    const oldChannelValue1 = CHANNEL_VALUE_VO_1;
 
-    const oldChannelKey2 =
-      "INDRA_NODE_CF_CORE/xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6/channel/0x9E746946146Da59D4d0daBfEA159ed791FB42FD1";
-    const oldChannelValue2 = {
-      "INDRA_NODE_CF_CORE/xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6/channel/0x9E746946146Da59D4d0daBfEA159ed791FB42FD1": {
-        multisigAddress: "0x9E746946146Da59D4d0daBfEA159ed791FB42FD1",
-        userNeuteredExtendedKeys: [
-          "xpub6E3tjd9js7QMrBtYo7f157D7MwauL6MWdLzKekFaRBb3bvaQnUPjHKJcdNhiqSjhmwa6TcTjV1wSDTgvz52To2ZjhGMiQFbYie2N2LZpNx6",
-          "xpub6E37ACnYHvmaHmkHsWrLKGc3ibkBppwhhBptWfv3dRW4t72LQtuo8eDegvqqwPtf2agT6FXJBfSHPxokbvaBqNeZS1FPkEvKjHVc77p5s9e",
-        ],
-        appInstances: [] as [string, AppInstanceJson][],
-        freeBalanceAppInstance: {
-          participants: [
-            "0xD5f9bDb0A387F42B9109110c6fEe1864737E4274",
-            "0xDA3CCBa9F3e3a9fE7D0Ed9F699Ca2BEF78Ba7A6c",
-          ],
-          defaultTimeout: 172800,
-          appInterface: {
-            addr: "0xde8d1288e2c7eC3e0b7279F8395b87A996Cc02f4",
-            stateEncoding:
-              "\n  tuple(\n    address[] tokenAddresses,\n    tuple(\n      address to,\n      uint256 amount\n    )[][] balances,\n    bytes32[] activeApps\n  )\n",
-          },
-          appSeqNo: 0,
-          latestState: {
-            activeApps: [],
-            tokenAddresses: [
-              "0x0000000000000000000000000000000000000000",
-              "0xFab46E002BbF0b4509813474841E0716E6730136",
-            ],
-            balances: [
-              [
-                { to: "0xD5f9bDb0A387F42B9109110c6fEe1864737E4274", amount: { _hex: "0x00" } },
-                { to: "0xDA3CCBa9F3e3a9fE7D0Ed9F699Ca2BEF78Ba7A6c", amount: { _hex: "0x00" } },
-              ],
-              [
-                {
-                  to: "0xDA3CCBa9F3e3a9fE7D0Ed9F699Ca2BEF78Ba7A6c",
-                  amount: { _hex: "0x68155a43676e0000" },
-                },
-                {
-                  to: "0xD5f9bDb0A387F42B9109110c6fEe1864737E4274",
-                  amount: { _hex: "0x22b1c8c1227a0000" },
-                },
-              ],
-            ],
-          },
-          latestVersionNumber: 24,
-          latestTimeout: 172800,
-          outcomeType: "MULTI_ASSET_MULTI_PARTY_COIN_TRANSFER",
-          identityHash: "0xc70a8518557ac136f60c643485d96e774e54bf9a026e6e68ad6a5b15dfbc811e",
-        },
-        monotonicNumProposedApps: 5,
-        singleAssetTwoPartyIntermediaryAgreements: [],
-        createdAt: 1569070400584,
-        proxyFactoryAddress: "0x8eb543b35DE94B0E636402C7cA32947b22853eDF",
-        addresses: {
-          proxyFactory: "0x8eb543b35DE94B0E636402C7cA32947b22853eDF",
-          multisigMastercopy: "0xe54f4EBeCE507477dFb86FA226394bdbA0b85d66",
-        },
-      },
-    };
+    const oldChannelKey2 = CHANNEL_KEY_VO_2;
+    const oldChannelValue2 = CHANNEL_VALUE_VO_2;
 
     // insert old records
     await dbClient.query(SQL`
