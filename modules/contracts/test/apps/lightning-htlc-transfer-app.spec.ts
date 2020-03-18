9c1cd6cb47d0a3273411be2f1b399da3c8194d7b
Bo Henderson <bohende@gmail.com>
2020-03-18 11:18:44 +0530

    🔀 properly merge contracts

--- modules/contracts/test/apps/lightning-htlc-transfer-app.spec.ts
+++ modules/contracts/test/apps/lightning-htlc-transfer-app.spec.ts
@@ -1,14 +1,13 @@
 /* global before */
 import { waffle as buidler } from "@nomiclabs/buidler";
 import {
-  SolidityValueType,
   CoinTransfer,
-  singleAssetTwoPartyCoinTransferEncoding,
+  HashLockTransferAppAction,
+  HashLockTransferAppActionEncoding,
   HashLockTransferAppState,
   HashLockTransferAppStateEncoding,
-  HashLockTransferAppActionEncoding,
-  HashLockTransferAppStateBigNumber,
-  HashLockTransferAppAction,
+  singleAssetTwoPartyCoinTransferEncoding,
+  SolidityValueType,
 } from "@connext/types";
 import chai from "chai";
 import * as waffle from "ethereum-waffle";
@@ -33,11 +32,11 @@ function mkHash(prefix: string = "0xa"): string {
 const decodeTransfers = (encodedAppState: string): CoinTransfer[] =>
   defaultAbiCoder.decode([singleAssetTwoPartyCoinTransferEncoding], encodedAppState)[0];
 
-const decodeAppState = (encodedAppState: string): HashLockTransferAppStateBigNumber =>
+const decodeAppState = (encodedAppState: string): HashLockTransferAppState =>
   defaultAbiCoder.decode([HashLockTransferAppStateEncoding], encodedAppState)[0];
 
 const encodeAppState = (
-  state: HashLockTransferAppStateBigNumber,
+  state: HashLockTransferAppState,
   onlyCoinTransfers: boolean = false,
 ): string => {
   if (!onlyCoinTransfers)
@@ -61,9 +60,9 @@ describe("LightningHTLCTransferApp", () => {
   let transferAmount: BigNumber;
   let preImage: string;
   let lockHash: string;
-  let preState: HashLockTransferAppStateBigNumber;
+  let preState: HashLockTransferAppState;
 
-  async function computeOutcome(state: HashLockTransferAppStateBigNumber): Promise<string> {
+  async function computeOutcome(state: HashLockTransferAppState): Promise<string> {
     return await lightningHTLCTransferApp.functions.computeOutcome(encodeAppState(state));
   }
 
@@ -76,7 +75,7 @@ describe("LightningHTLCTransferApp", () => {
 
   async function validateOutcome(
     encodedTransfers: string,
-    postState: HashLockTransferAppStateBigNumber,
+    postState: HashLockTransferAppState,
   ) {
     const decoded = decodeTransfers(encodedTransfers);
     expect(encodedTransfers).to.eq(encodeAppState(postState, true));
@@ -122,7 +121,7 @@ describe("LightningHTLCTransferApp", () => {
       let ret = await applyAction(preState, action);
       const afterActionState = decodeAppState(ret);
 
-      const expectedPostState: HashLockTransferAppStateBigNumber = {
+      const expectedPostState: HashLockTransferAppState = {
         coinTransfers: [
           {
             amount: Zero,
@@ -151,7 +150,7 @@ describe("LightningHTLCTransferApp", () => {
       let ret = await applyAction(preState, action);
       const afterActionState = decodeAppState(ret);
 
-      const expectedPostState: HashLockTransferAppStateBigNumber = {
+      const expectedPostState: HashLockTransferAppState = {
         coinTransfers: [
           {
             amount: transferAmount,
@@ -181,7 +180,7 @@ describe("LightningHTLCTransferApp", () => {
       const afterActionState = decodeAppState(ret);
       expect(afterActionState.finalized).to.be.true;
 
-      const modifiedPostState: HashLockTransferAppStateBigNumber = {
+      const modifiedPostState: HashLockTransferAppState = {
         coinTransfers: [
           {
             amount: transferAmount,
--- modules/contracts/test/funding/withdraw-app.spec.ts
+++ modules/contracts/test/funding/withdraw-app.spec.ts
@@ -1,12 +1,19 @@
 /* global before */
 import { waffle as buidler } from "@nomiclabs/buidler";
-import { WithdrawAppState, WithdrawAppAction, CoinTransfer, singleAssetTwoPartyCoinTransferEncoding, WithdrawAppStateEncoding, WithdrawAppActionEncoding } from "@connext/types";
+import {
+  CoinTransfer,
+  singleAssetTwoPartyCoinTransferEncoding,
+  WithdrawAppAction,
+  WithdrawAppActionEncoding,
+  WithdrawAppState,
+  WithdrawAppStateEncoding,
+} from "@connext/types";
 import chai from "chai";
 import * as waffle from "ethereum-waffle";
 import { Contract, Wallet } from "ethers";
 import { BigNumber, defaultAbiCoder, joinSignature, SigningKey } from "ethers/utils";
 
-import WithdrawApp from "../../build/WithdrawApp.json"
+import WithdrawApp from "../../build/WithdrawApp.json";
 import { Zero, HashZero } from "ethers/constants";
 
 const { expect } = chai;
@@ -15,14 +22,14 @@ function mkHash(prefix: string = "0xa"): string {
   return prefix.padEnd(66, "0");
 }
 
-const decodeTransfers = (encodedTransfers: string): CoinTransfer<string>[] =>
+const decodeTransfers = (encodedTransfers: string): CoinTransfer[] =>
   defaultAbiCoder.decode([singleAssetTwoPartyCoinTransferEncoding], encodedTransfers)[0];
 
-const decodeAppState = (encodedAppState: string): WithdrawAppState<string> =>
+const decodeAppState = (encodedAppState: string): WithdrawAppState =>
   defaultAbiCoder.decode([WithdrawAppStateEncoding], encodedAppState)[0];
 
 const encodeAppState = (
-  state: WithdrawAppState<string>,
+  state: WithdrawAppState,
   onlyCoinTransfers: boolean = false,
 ): string => {
   if (!onlyCoinTransfers) return defaultAbiCoder.encode([WithdrawAppStateEncoding], [state]);
@@ -31,40 +38,50 @@ const encodeAppState = (
 
 const encodeAppAction = (state: WithdrawAppAction): string => {
   return defaultAbiCoder.encode([WithdrawAppActionEncoding], [state]);
-}
+};
 
 describe("WithdrawApp", async () => {
   let provider = buidler.provider;
-  const wallet = (await provider.getWallets())[0];
-  const withdrawApp: Contract = await waffle.deployContract(wallet, WithdrawApp);
-  let withdrawerWallet = Wallet.createRandom();
-  let counterpartyWallet = Wallet.createRandom();
+  let wallet: Wallet;
+  let withdrawerWallet: Wallet;
+  let counterpartyWallet: Wallet;
+  let withdrawApp: Contract;
+  let withdrawerSigningKey: SigningKey;
+  let counterpartySigningKey: SigningKey;
 
   const amount = new BigNumber(10000);
   const data = mkHash("0xa"); // TODO: test this with real withdrawal commitment hash?
-  const withdrawerSigningKey = new SigningKey(withdrawerWallet.privateKey);
-  const counterpartySigningKey = new SigningKey(counterpartyWallet.privateKey);
+
+  before(async () => {
+    provider = buidler.provider;
+    wallet = (await provider.getWallets())[0];
+    withdrawApp = await waffle.deployContract(wallet, WithdrawApp);
+    withdrawerWallet = Wallet.createRandom();
+    counterpartyWallet = Wallet.createRandom();
+    withdrawerSigningKey = new SigningKey(withdrawerWallet.privateKey);
+    counterpartySigningKey = new SigningKey(counterpartyWallet.privateKey);
+  });
   
-  const computeOutcome = async (state: WithdrawAppState<string>): Promise<string> => {
+  const computeOutcome = async (state: WithdrawAppState): Promise<string> => {
     return await withdrawApp.functions.computeOutcome(encodeAppState(state));
-  }
-  
+  };
+
   const applyAction = async (state: any, action: WithdrawAppAction): Promise<string> => {
     return await withdrawApp.functions.applyAction(
       encodeAppState(state),
       encodeAppAction(action),
     );
-  }
+  };
 
-  const createInitialState = (): WithdrawAppState<string> => {
+  const createInitialState = (): WithdrawAppState => {
     return {
       transfers: [
         {
-          amount: amount.toString(),
+          amount,
           to: withdrawerWallet.address,
         },
         {
-          amount: Zero.toString(),
+          amount: Zero,
           to: counterpartyWallet.address,
         },
       ],
@@ -73,18 +90,15 @@ describe("WithdrawApp", async () => {
       data,
       finalized: false,
     };
-  }
+  };
 
   const createAction = (): WithdrawAppAction => {
     return {
-      signature: joinSignature(counterpartySigningKey.signDigest(data))
+      signature: joinSignature(counterpartySigningKey.signDigest(data)),
     };
-  }
+  };
 
-  beforeEach(async () => {
-  });
-
-  describe("It zeroes withdrawer balance if state is finalized (w/ valid signatures)", async () => {
+  it("It zeroes withdrawer balance if state is finalized (w/ valid signatures)", async () => {
     let initialState = createInitialState();
     let action = createAction();
 
@@ -97,12 +111,12 @@ describe("WithdrawApp", async () => {
     const decoded = decodeTransfers(ret);
 
     expect(decoded[0].to).eq(initialState.transfers[0].to);
-    expect(decoded[0].amount).eq(Zero.toString());
+    expect(decoded[0].amount).eq(Zero);
     expect(decoded[1].to).eq(initialState.transfers[1].to);
     expect(decoded[1].amount).eq(Zero);
-  })
+  });
 
-  describe("It cancels the withdrawal if state is not finalized", async () => {
+  it("It cancels the withdrawal if state is not finalized", async () => {
     let initialState = createInitialState();
 
     // Compute outcome without taking action
@@ -112,10 +126,10 @@ describe("WithdrawApp", async () => {
     expect(decoded[0].to).eq(initialState.transfers[0].to);
     expect(decoded[0].amount).eq(initialState.transfers[0].amount);
     expect(decoded[1].to).eq(initialState.transfers[1].to);
-    expect(decoded[1].amount).eq(Zero.toString());
-  })
+    expect(decoded[1].amount).eq(Zero);
+  });
 
-  describe("It reverts the action if state is finalized", async () => {
+  it("It reverts the action if state is finalized", async () => {
     let initialState = createInitialState();
     let action = createAction();
 
@@ -124,22 +138,22 @@ describe("WithdrawApp", async () => {
     expect(afterActionState.signatures[1]).to.eq(action.signature);
     expect(afterActionState.finalized).to.be.true;
 
-    await expect(applyAction(afterActionState, action)).revertedWith("cannot take action on a finalized state")
-  })
+    await expect(applyAction(afterActionState, action)).revertedWith("cannot take action on a finalized state");
+  });
 
-  describe("It reverts the action if withdrawer signature is invalid", async () => {
+  it("It reverts the action if withdrawer signature is invalid", async () => {
     let initialState = createInitialState();
     let action = createAction();
 
     initialState.signatures[0] = mkHash("0x0");
-    await expect(applyAction(initialState, action)).revertedWith("invalid withdrawer signature")
-  })
+    await expect(applyAction(initialState, action)).revertedWith("invalid withdrawer signature");
+  });
 
-  describe("It reverts the action if counterparty signature is invalid", async () => {
+  it("It reverts the action if counterparty signature is invalid", async () => {
     let initialState = createInitialState();
     let action = createAction();
 
     action.signature = HashZero;
-    await expect(applyAction(initialState, action)).revertedWith("invalid counterparty signature")
-  })
+    await expect(applyAction(initialState, action)).revertedWith("invalid counterparty signature");
+  });
 });
