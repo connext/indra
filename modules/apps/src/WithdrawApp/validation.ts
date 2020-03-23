98f3d611c6cbbb0eaa187816a1a9933eca61536b
Bo Henderson <bohende@gmail.com>
2020-03-23 10:17:08 +0530

    🔀 properly merge apps

--- modules/apps/src/WithdrawApp/validation.ts
+++ modules/apps/src/WithdrawApp/validation.ts
@@ -1,14 +1,5 @@
 import { xkeyKthAddress } from "@connext/cf-core";
-<<<<<<< HEAD
-import { MethodParams, WithdrawAppState } from "@connext/types";
-=======
-import {
-  CFCoreTypes,
-  CoinTransferBigNumber,
-  bigNumberifyObj,
-  WithdrawAppState,
-} from "@connext/types";
->>>>>>> nats-messaging-refactor
+import { bigNumberifyJson, MethodParams, WithdrawAppState } from "@connext/types";
 
 import { unidirectionalCoinTransferValidation } from "../shared";
 import { recoverAddress } from "ethers/utils";
@@ -20,23 +11,11 @@ export const validateWithdrawApp = (
   responderPublicIdentifier: string,
 ) => {
   const { responderDeposit, initiatorDeposit } = params;
-  const initialState = params.initialState as WithdrawAppState;
+  const initialState = bigNumberifyJson(params.initialState) as WithdrawAppState;
 
   const initiatorFreeBalanceAddress = xkeyKthAddress(initiatorPublicIdentifier);
   const responderFreeBalanceAddress = xkeyKthAddress(responderPublicIdentifier);
 
-<<<<<<< HEAD
-=======
-  const initialState: WithdrawAppState<BigNumber> = convertWithrawAppState(
-    "bignumber",
-    initialStateBadType,
-  );
-
-  initialState.transfers = initialState.transfers.map((transfer: CoinTransferBigNumber) =>
-    bigNumberifyObj(transfer),
-  ) as any;
-
->>>>>>> nats-messaging-refactor
   const initiatorTransfer = initialState.transfers[0];
   const responderTransfer = initialState.transfers[1];
 
@@ -47,31 +26,17 @@ export const validateWithdrawApp = (
     responderTransfer,
   );
 
-<<<<<<< HEAD
-  if(initialState.finalized) {
-    throw new Error(
-      `Cannot install a withdraw app with a finalized state. State: ${initialState}`,
-    );
-  }
-
-  if(initialState.signatures[1] !== HashZero) {
-=======
   if (initialState.finalized) {
     throw new Error(`Cannot install a withdraw app with a finalized state. State: ${initialState}`);
   }
 
   if (initialState.signatures[1] !== HashZero) {
->>>>>>> nats-messaging-refactor
     throw new Error(
       `Cannot install a withdraw app with a populated signatures[1] field. Signatures[1]: ${initialState.signatures[1]}`,
     );
   }
 
-<<<<<<< HEAD
-  if(
-=======
   if (
->>>>>>> nats-messaging-refactor
     initialState.signers[0] !== initiatorFreeBalanceAddress ||
     initialState.signers[1] !== responderFreeBalanceAddress
   ) {
@@ -80,11 +45,7 @@ export const validateWithdrawApp = (
     );
   }
 
-<<<<<<< HEAD
-  if(!initialState.transfers[1].amount.eq(Zero)) {
-=======
   if (!initialState.transfers[1].amount.eq(Zero)) {
->>>>>>> nats-messaging-refactor
     throw new Error(
       `Cannot install a withdraw app with nonzero recipient amount. ${initialState.transfers[1].amount.toString()}`,
     );
@@ -92,17 +53,10 @@ export const validateWithdrawApp = (
 
   let recovered = recoverAddress(initialState.data, initialState.signatures[0]);
 
-<<<<<<< HEAD
-  if(recovered !== initialState.signers[0]) {
-    throw new Error(
-      `Cannot install withdraw app - incorrect signer recovered from initiator sig on data. 
-       Recovered: ${recovered}, Expected: ${initialState.signers[0]}`,
-=======
   if (recovered !== initialState.signers[0]) {
     throw new Error(
       `Cannot install withdraw app - incorrect signer recovered from initiator sig on data. 
          Recovered: ${recovered}, Expected: ${initialState.signers[0]}`,
->>>>>>> nats-messaging-refactor
     );
   }
 };
