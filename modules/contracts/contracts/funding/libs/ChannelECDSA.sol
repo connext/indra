d55bfa49fd11e2d5045f67b6ff070d50dcdac86c
Pedro Gomes <pedrogomes94@gmail.com>
2020-03-24 12:53:14 +0100

    add ChannelECDSA.sol

---
+++ modules/contracts/contracts/funding/libs/ChannelECDSA.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.11;
+pragma experimental "ABIEncoderV2";
+
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+
+library ChannelECDSA {
+  function hashChannelMessage(bytes memory message) internal pure returns (bytes32) {
+      // 32 is the length in bytes of hash,
+      // enforced by the type signature above
+      return keccak256(abi.encodePacked("\x19Channel Signed Message:\n", message.length, message));
+  }
+
+  function verifyChannelMessage(bytes memory message, bytes memory signature) {
+    return recover(hashChannelMessage(message), signature)
+  }
+} 
