d3d328b629b4b5edb3caf10618976e3115976c13
LayneHaber <layne.haber@gmail.com>
2020-03-29 15:00:46 -0700

    update adjudicator types

--- modules/types/src/contracts/adjudicator.ts
+++ modules/types/src/contracts/adjudicator.ts
@@ -1,31 +1,34 @@
 import { Address, HexString } from "../basic";
+import { BigNumber } from "ethers/utils";
 
 ////////////////////////////////////////
 // keep synced w contracts/adjudicator/libs/LibStateChannelApp.sol
 
 // The status of a challenge in the ChallengeRegistry
-enum ChallengeStatus {
+export const enum ChallengeStatus {
   NO_CHALLENGE = 0,
-  FINALIZES_AFTER_DEADLINE = 1,
-  EXPLICITLY_FINALIZED = 2,
+  IN_DISPUTE = 1,
+  IN_ONCHAIN_PROGRESSION = 2,
+  EXPLICITLY_FINALIZED = 3,
 }
 
 // A minimal structure that uniquely identifies a single instance of an App
-export type AppIdentity = {
-  channelNonce: number;
+export type AppIdentity<T = string> = {
+  channelNonce: T;
   participants: Address[];
   appDefinition: Address;
-  defaultTimeout: number;
+  defaultTimeout: T;
 };
+export type AppIdentityBigNumber = AppIdentity<BigNumber>
 
 // A structure representing the state of a CounterfactualApp instance from the POV of the blockchain
 // NOTE: AppChallenge is the overall state of a channelized app instance,
 // appStateHash is the hash of a state specific to the CounterfactualApp (e.g. chess position)
-export type AppChallenge = {
+export type AppChallenge<T = string> = {
   latestSubmitter: Address;
   appStateHash: HexString;
-  challengeCounter: number;
-  versionNumber: number;
-  finalizesAt: number;
+  versionNumber: T;
+  finalizesAt: T;
   status: ChallengeStatus;
 };
+export type AppChallengeBigNumber = AppChallenge<BigNumber>
