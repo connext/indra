e5281660d0cd7d00daca284566f6c54791428802
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-03-30 20:32:34 +0400

    Deposit app contract

---
+++ modules/contracts/contracts/funding/default-apps/DepositApp.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.5.11;
+pragma experimental "ABIEncoderV2";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../adjudicator/interfaces/CounterfactualApp.sol";
+import "../state-deposit-holders/MinimumViableMultisig.sol";
+import "../libs/LibOutcome.sol";
+
+
+/// @title Deposit App
+/// @notice This contract allows a user to trustlessly deposit into a channel
+///         by attributing the difference in value of multisig to the depositor
+
+///         THIS CONTRACT WILL ONLY WORK FOR 2-PARTY CHANNELS!
+contract DepositApp is CounterfactualApp {
+
+    address constant CONVENTION_FOR_ETH_TOKEN_ADDRESS = address(0x0);
+
+    struct AppState {
+        LibOutcome.CoinTransfer[2] coinTransfers; // both amounts should be 0 in initial state
+        address payable multisigAddress;
+        address assetId;
+        uint256 startingTotalAmountWithdrawn;
+        uint256 startingMultisigBalance;
+    }
+
+    function computeOutcome(bytes calldata encodedState)
+        external
+        view
+        returns (bytes memory)
+    {
+        AppState memory state = abi.decode(encodedState, (AppState));
+
+        uint256 endingTotalAmountWithdrawn = MinimumViableMultisig(state.multisigAddress).totalAmountWithdrawn(state.assetId);
+        uint256 endingMultisigBalance;
+
+        if (state.assetId == CONVENTION_FOR_ETH_TOKEN_ADDRESS) {
+            endingMultisigBalance = state.multisigAddress.balance;
+        } else {
+            endingMultisigBalance = ERC20(state.assetId).balanceOf(state.multisigAddress);
+        }
+
+        return abi.encode(
+            LibOutcome.CoinTransfer[2]([
+                LibOutcome.CoinTransfer(
+                    state.coinTransfers[0].to,
+                    // NOTE: deliberately do NOT use safemath here. For more info, see: TODO
+                    (endingMultisigBalance - state.startingMultisigBalance) +
+                    (endingTotalAmountWithdrawn - state.startingTotalAmountWithdrawn)
+                ),
+                LibOutcome.CoinTransfer(
+                    state.coinTransfers[1].to,
+                    0
+                )
+            ])
+        );
+    }
+}
