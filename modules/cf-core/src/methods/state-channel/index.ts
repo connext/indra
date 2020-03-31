d659fc7283ba2ca8ec0f17ea83d845b729dd4960
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-04-01 00:16:17 +0400

    Clean up cfcore legacy deposit

--- modules/cf-core/src/methods/state-channel/index.ts
+++ modules/cf-core/src/methods/state-channel/index.ts
@@ -1,8 +1,5 @@
 export { CreateChannelController } from "./create";
 export { DeployStateDepositController } from "./deploy-state-deposit-holder";
-export { DepositController } from "./deposit";
 export { GetStateChannelController } from "./get";
 export { GetStateDepositHolderAddressController } from "./get-state-deposit-holder-address";
 export { GetAllChannelAddressesController } from "./get-all-addresses";
-export { RequestDepositRightsController } from "./request-deposit-rights";
-export { RescindDepositRightsController } from "./rescind-deposit-rights";
