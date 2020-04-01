2fae18b8f6ec40203fb109e4b7a6f1f319603eaf
Bo Henderson <bohende@gmail.com>
2020-04-01 15:10:28 +0530

    ⭕️ remove .only from test-runner

--- modules/test-runner/src/withdraw/withdraw.test.ts
+++ modules/test-runner/src/withdraw/withdraw.test.ts
@@ -27,7 +27,7 @@ describe("Withdrawal", () => {
     tokenAddress = client.config.contractAddresses.Token;
   });
 
-  it.only("happy case: client successfully withdraws eth and node submits the tx", async () => {
+  it("happy case: client successfully withdraws eth and node submits the tx", async () => {
     await fundChannel(client, ZERO_ZERO_TWO_ETH);
     console.log(`FUNDED!`);
     // withdraw
