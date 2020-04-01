fd1f02bb3e994a28f4cffa1106e49cafebd72cf7
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-04-01 15:03:17 +0400

    Export deposit service

---
+++ modules/node/src/deposit/deposit.module.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { AppRegistryRepository } from "../appRegistry/appRegistry.repository";
+import { CFCoreModule } from "../cfCore/cfCore.module";
+import { ConfigModule } from "../config/config.module";
+import { LoggerModule } from "../logger/logger.module";
+import { OnchainTransactionModule } from "../onchainTransactions/onchainTransaction.module";
+import { OnchainTransactionRepository } from "../onchainTransactions/onchainTransaction.repository";
+import { ChannelRepository } from "../channel/channel.repository";
+
+import { DepositService } from "./deposit.service";
+
+@Module({
+    controllers: [],
+    exports: [DepositService],
+    imports: [
+        CFCoreModule,
+        ConfigModule,
+        OnchainTransactionModule,
+        LoggerModule,
+        TypeOrmModule.forFeature([
+        OnchainTransactionRepository,
+        AppRegistryRepository,
+        ChannelRepository,
+        ]),
+    ],
+    providers: [DepositService],
+})
+export class DepositModule {}
