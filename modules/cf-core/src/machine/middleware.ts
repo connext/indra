94319b794f1a532ace647b3d2cc671b48527b22c
LayneHaber <layne.haber@gmail.com>
2020-04-03 11:05:52 -0700

    allow for validation to be optional

--- modules/cf-core/src/machine/middleware.ts
+++ modules/cf-core/src/machine/middleware.ts
@@ -19,8 +19,13 @@ export class MiddlewareContainer {
   public async run(opCode: Opcode, args: any[]) {
     const middleware = this.middlewares[opCode][0];
 
+    // it is okay for middleware to be undefined iff
+    // it is the validation opcode
     if (typeof middleware === "undefined") {
-      throw new Error(`Attempted to run middleware for opcode ${opCode} but none existed`);
+      if (opCode !== Opcode.OP_VALIDATE) {
+        throw new Error(`Attempted to run middleware for opcode ${opCode} but none existed`);
+      }
+      return;
     }
 
     return middleware(args);
