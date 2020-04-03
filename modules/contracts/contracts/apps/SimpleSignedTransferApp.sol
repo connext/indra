78b0a3f7554ddcf7964f892df4adb84fa8d075b8
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-03 19:10:51 +0200

    fix test-contracts

--- modules/contracts/contracts/apps/SimpleSignedTransferApp.sol
+++ modules/contracts/contracts/apps/SimpleSignedTransferApp.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.11;
 pragma experimental "ABIEncoderV2";
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "../../shared/libs/LibChannelCrypto.sol";
+import "../shared/libs/LibChannelCrypto.sol";
 import "../adjudicator/interfaces/CounterfactualApp.sol";
 import "../funding/libs/LibOutcome.sol";
 
