ab4f2d16063f38cac36059516b7f465bad6ee7b2
Rahul Sethuram <rahul@connext.network>
2020-04-05 20:03:34 +0200

    Remove .only

--- modules/test-runner/src/transfer/signedTransfer.test.ts
+++ modules/test-runner/src/transfer/signedTransfer.test.ts
@@ -307,7 +307,7 @@ describe("Signed Transfers", () => {
     ).to.eventually.be.rejectedWith(/VM Exception while processing transaction/);
   });
 
-  it.only("Experimental: Average latency of 5 signed transfers with Eth", async () => {
+  it("Experimental: Average latency of 5 signed transfers with Eth", async () => {
     let runTime: number[] = [];
     let sum = 0;
     const numberOfRuns = 5;
