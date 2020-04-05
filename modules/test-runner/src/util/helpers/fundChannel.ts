f7c655554088143d436b2e0c7cf09b80c3486326
LayneHaber <layne.haber@gmail.com>
2020-04-04 22:34:07 -0700

    rework requestCollateral for no deposit events from counterparty

--- modules/test-runner/src/util/helpers/fundChannel.ts
+++ modules/test-runner/src/util/helpers/fundChannel.ts
@@ -1,5 +1,4 @@
-import { xkeyKthAddress } from "@connext/cf-core";
-import { IConnextClient, EventNames } from "@connext/types";
+import { IConnextClient, EventNames, delay } from "@connext/types";
 import { AddressZero } from "ethers/constants";
 import { BigNumber } from "ethers/utils";
 
@@ -42,33 +41,35 @@ export const fundChannel = async (
 export const requestCollateral = async (
   client: IConnextClient,
   assetId: string = AddressZero,
+  enforce: boolean = false,
 ): Promise<void> => {
   const log = new Logger("RequestCollateral", env.logLevel);
-  const nodeFreeBalanceAddress = xkeyKthAddress(client.nodePublicIdentifier);
-  const prevFreeBalance = await client.getFreeBalance(assetId);
-  await new Promise(async (resolve, reject) => {
-    client.once(EventNames.DEPOSIT_CONFIRMED_EVENT, async data => {
-      const freeBalance = await client.getFreeBalance(assetId);
-      // verify free balance increased as expected
-      expect(freeBalance[nodeFreeBalanceAddress]).to.be.above(
-        prevFreeBalance[nodeFreeBalanceAddress],
-      );
-      log.info(`Got deposit confirmed event, helper wrapper is returning`);
-      resolve();
-    });
-    client.once(EventNames.DEPOSIT_FAILED_EVENT, async (msg: any) => {
-      reject(new Error(JSON.stringify(msg)));
-    });
+  log.debug(`client.requestCollateral() called`);
+  const start = Date.now();
+  await client.requestCollateral(assetId);
+  log.info(`client.requestCollateral() returned in ${Date.now() - start}`);
+  const preCollateralBal = await client.getFreeBalance(assetId);
 
-    try {
-      log.debug(`client.requestCollateral() called`);
-      const start = Date.now();
-      await client.requestCollateral(assetId);
-      log.info(`client.requestCollateral() returned in ${Date.now() - start}`);
-    } catch (e) {
-      return reject(new Error(e.stack || e.message));
+  return new Promise(async (resolve, reject) => {
+    log.debug(`client.requestCollateral() called`);
+    const start = Date.now();
+    await client.requestCollateral(assetId);
+    log.info(`client.requestCollateral() returned in ${Date.now() - start}`);
+    let currCollateralBal = await client.getFreeBalance(assetId);
+    while (
+      enforce &&
+      currCollateralBal[
+        client.nodeFreeBalanceAddress
+      ].lte(preCollateralBal[client.nodeFreeBalanceAddress]) &&
+      Date.now() - start > 5_000 // wait 5s
+    ) {
+      await delay(500);
+      currCollateralBal = await client.getFreeBalance(assetId);
     }
+    if (Date.now() - start > 5_000 && enforce) {
+      reject(`No collateral received after 5s in channel ${client.multisigAddress}`);
+      return;
+    }
+    resolve();
   });
-
-  return;
 };
