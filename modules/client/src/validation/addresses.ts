1f003568e321dd543578344bb4395e8f79bbf078
LayneHaber <layne.haber@gmail.com>
2020-04-09 15:20:40 -0700

    user deposits working

--- modules/client/src/validation/addresses.ts
+++ modules/client/src/validation/addresses.ts
@@ -1,4 +1,5 @@
 import { arrayify, getAddress, isHexString } from "ethers/utils";
+import { parsePublicIdentifier } from "@connext/types";
 
 export const isValidAddress = (address: any): boolean =>
   typeof address === "string" && isHexString(address) && arrayify(address).length === 20;
@@ -11,3 +12,25 @@ export const invalidAddress = (value: string): string | undefined => {
     return e.message;
   }
 };
+
+export const invalidIdentifier = (identifier: string): string | undefined => {
+  let parsed;
+  let errors: string[] = [];
+  try {
+    parsed = parsePublicIdentifier(identifier);
+  } catch (e) {
+    errors.push(`Identifier is invalid: ${identifier}`);
+    return errors.toString();
+  }
+  if (!parsed.chainId || typeof parsed.chainId !== "number") {
+    errors.push(`Identifier has invalid chainId: ${identifier}`);
+  }
+  if (!parsed.namespace || typeof parsed.namespace !== "string") {
+    errors.push(`Identifier has invalid namespace: ${identifier}`);
+  }
+  if (!parsed.address || invalidAddress(parsed.address)) {
+    errors.push(`Identifier has invalid address: ${identifier}`);
+  }
+  return errors.length === 0 ? undefined : errors.toString();
+};
+
