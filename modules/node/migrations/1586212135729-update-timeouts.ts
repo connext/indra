86aa3fda61766b851c965a31bbd6eaee8920e4f7
Rahul Sethuram <rahul@connext.network>
2020-04-12 16:17:45 +0200

    Fix migration issues

--- modules/node/migrations/1586212135729-update-timeouts.ts
+++ modules/node/migrations/1586212135729-update-timeouts.ts
@@ -4,11 +4,11 @@ export class updateTimeouts1586212135729 implements MigrationInterface {
     name = "updateTimeouts1586212135729"
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "set_state_commitment" ADD COLUMN "stateTimeout" text NOT NULL`, undefined);
+        await queryRunner.query(`ALTER TABLE "set_state_commitment" ADD COLUMN "stateTimeout" text`, undefined);
         await queryRunner.query(`ALTER TABLE "set_state_commitment" DROP COLUMN "timeout"`, undefined);
   
-        await queryRunner.query(`ALTER TABLE "app_instance" ADD "defaultTimeout" text NOT NULL`, undefined);
-        await queryRunner.query(`ALTER TABLE "app_instance" ADD "stateTimeout" text NOT NULL`, undefined);
+        await queryRunner.query(`ALTER TABLE "app_instance" ADD "defaultTimeout" text NOT NULL DEFAULT '0x0'`, undefined);
+        await queryRunner.query(`ALTER TABLE "app_instance" ADD "stateTimeout" text`, undefined);
         await queryRunner.query(`ALTER TABLE "app_instance" DROP COLUMN "latestTimeout"`, undefined);
         await queryRunner.query(`ALTER TABLE "app_instance" DROP COLUMN "timeout"`, undefined);
     }
