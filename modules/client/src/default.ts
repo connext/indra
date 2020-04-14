0810644108b88d744d4a925f33059da1d527a829
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-14 16:28:56 +0200

    update default helpers

--- modules/client/src/default.ts
+++ modules/client/src/default.ts
@@ -1,26 +1,33 @@
 import { ConnextStore } from "@connext/store";
-import { ClientOptions, StoreTypes } from "@connext/types";
+import { ClientOptions, StoreTypes, IClientStore } from "@connext/types";
+import { getRandomPrivateKey, removeUndefinedFields } from "@connext/utils";
 
-export const getDefaultStore = (opts: Partial<ClientOptions>) => {
-  const storeType = (opts && opts.storeType) ? opts.storeType : StoreTypes.LocalStorage;
-  const backupService = (opts && opts.backupService) ? opts.backupService : null;
-  return new ConnextStore(storeType, { backupService });
+const CONNEXT_DEFAULT_SIGNER_KEY = "CONNEXT_DEFAULT_SIGNER";
+
+const getGeneratedSigner = (): string => {
+  if (typeof window === "undefined" || typeof window.localStorage === undefined) {
+    return getRandomPrivateKey();
+  }
+  let signer: string = window.localStorage.getItem(CONNEXT_DEFAULT_SIGNER_KEY);
+  if (!signer) {
+    signer = getRandomPrivateKey();
+    window.localStorage.setItem(CONNEXT_DEFAULT_SIGNER_KEY, signer);
+  }
+  return signer;
 };
 
-export const getDefaultOptions = async (
-  network: string,
-  opts?: Partial<ClientOptions>,
-): Promise<ClientOptions> => {
+const getUrlOptions = (network: string): { ethProviderUrl: string; nodeUrl: string } => {
   let urlOptions;
 
   if (network.toLowerCase() === "localhost") {
     urlOptions = { ethProviderUrl: `http://localhost:8545`, nodeUrl: `http://localhost:8080` };
   } else {
-    const baseUrl = network.toLowerCase() === "mainnet"
-      ? "indra.connext.network/api"
-      : network.toLowerCase() === "rinkeby"
-      ? "rinkeby.indra.connext.network/api"
-      : null;
+    const baseUrl =
+      network.toLowerCase() === "mainnet"
+        ? "indra.connext.network/api"
+        : network.toLowerCase() === "rinkeby"
+        ? "rinkeby.indra.connext.network/api"
+        : null;
     if (!baseUrl) {
       throw new Error(`Provided network (${network.toLowerCase()}) is not supported`);
     }
@@ -29,17 +36,51 @@ export const getDefaultOptions = async (
       nodeUrl: `https://${baseUrl}`,
     };
   }
+  return urlOptions;
+};
 
-  if (!opts) {
-    return ({
-      store: new ConnextStore(StoreTypes.LocalStorage),
-      ...urlOptions,
-    });
+const getOverrideOptions = (_opts?: Partial<ClientOptions>): Partial<ClientOptions> | undefined => {
+  if (!_opts) {
+    return undefined;
   }
+  const opts = removeUndefinedFields(_opts);
+  if (Object.keys(opts)) {
+    return undefined;
+  }
+  return opts;
+};
 
-  return ({
-    store: opts.store || getDefaultStore(opts),
+export const getDefaultStore = (opts: Partial<ClientOptions>): IClientStore => {
+  const storeType = opts && opts.storeType ? opts.storeType : StoreTypes.LocalStorage;
+  const backupService = opts && opts.backupService ? opts.backupService : null;
+  return new ConnextStore(storeType, { backupService });
+};
+
+export const getDefaultOptions = async (
+  network: string,
+  _opts?: Partial<ClientOptions>,
+): Promise<ClientOptions> => {
+  const urlOptions = getUrlOptions(network);
+
+  const opts = getOverrideOptions(_opts);
+
+  const store = opts
+    ? opts.store || getDefaultStore(opts)
+    : new ConnextStore(StoreTypes.LocalStorage);
+
+  const signer = opts.signer
+    ? opts.signer
+    : network.toLowerCase() !== "mainnet"
+    ? getGeneratedSigner()
+    : undefined;
+
+  if (!signer) {
+    throw new Error("Signer required for Mainnet");
+  }
+  return {
+    signer,
+    store,
     ...urlOptions,
     ...opts,
-  });
+  };
 };
