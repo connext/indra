1364b5058f3fe724c22a3be7278ba1962122e61c
LayneHaber <layne.haber@gmail.com>
2020-04-20 15:36:48 -0700

    move to utils

--- modules/utils/src/identifiers.ts
+++ modules/utils/src/identifiers.ts
@@ -1,5 +1,5 @@
-import { AssetId } from "@connext/types";
-import { getAddress } from "ethers/utils";
+import { AssetId, AppIdentity } from "@connext/types";
+import { getAddress, keccak256, solidityPack } from "ethers/utils";
 
 ////////////////////////////////////////
 // AssetId
@@ -8,3 +8,17 @@ import { getAddress } from "ethers/utils";
 export const getAddressFromAssetId = (assetId: AssetId): string =>
   getAddress(assetId);
 
+export function appIdentityToHash(appIdentity: AppIdentity): string {
+  return keccak256(
+    solidityPack(
+      ["address", "uint256", "bytes32", "address", "uint256"],
+      [
+        appIdentity.multisigAddress,
+        appIdentity.channelNonce,
+        keccak256(solidityPack(["address[]"], [appIdentity.participants])),
+        appIdentity.appDefinition,
+        appIdentity.defaultTimeout,
+      ],
+    ),
+  );
+}
