a7bce8a3ddd71c47c7fff4bbe2fb0f0a50bb3684
LayneHaber <layne.haber@gmail.com>
2020-04-20 10:47:52 -0700

    reorganize utils

---
+++ modules/watcher/test/utils/assertions.ts
@@ -0,0 +1,10 @@
+import { waffleChai } from "@ethereum-waffle/chai";
+import { use } from "chai";
+/////////////////////////////
+//// Assertions
+
+use(require("chai-as-promised"));
+use(require("chai-subset"));
+use(waffleChai);
+
+export { expect } from "chai";
