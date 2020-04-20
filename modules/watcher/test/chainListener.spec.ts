1e0da9ee669c8f5a4e0ea30a10d2b6a2780919a1
LayneHaber <layne.haber@gmail.com>
2020-04-20 10:48:45 -0700

    set and progress tests passing

--- modules/watcher/test/chainListener.spec.ts
+++ modules/watcher/test/chainListener.spec.ts
@@ -1,25 +1,33 @@
 import { expect } from "chai";
-import { Contract } from "ethers";
-import { JsonRpcProvider, ChallengeUpdatedContractEvent, ChallengeStatus, NetworkContext } from "@connext/types";
-import { randomState, stateToHash, setupContext } from "./utils";
-import { nullLogger, toBN } from "@connext/utils";
+import { Contract, Wallet } from "ethers";
+import {
+  JsonRpcProvider,
+  ChallengeUpdatedContractEvent,
+  ChallengeStatus,
+  NetworkContext,
+  StateProgressedContractEvent,
+} from "@connext/types";
+import { stateToHash, setupContext, AppWithCounterClass, ActionType } from "./utils";
+import { nullLogger, toBN, ChannelSigner, computeAppChallengeHash } from "@connext/utils";
 import { ChainListener } from "../src";
 import { beforeEach } from "mocha";
+import { Zero, One } from "ethers/constants";
 
 describe("ChainListener", () => {
-
   let challengeRegistry: Contract;
   let provider: JsonRpcProvider;
   let chainListener: ChainListener;
-  let setState: any;
-  let appInstance: any;
+  let setAndProgressState: any;
+  let appInstance: AppWithCounterClass;
+  let channelResponder: Wallet;
 
   beforeEach(async () => {
     const context = await setupContext();
     challengeRegistry = context["challengeRegistry"];
     provider = context["provider"];
-    setState = context["setState"];
+    setAndProgressState = context["setAndProgressState"];
     appInstance = context["appInstance"];
+    channelResponder = context["channelResponder"];
 
     chainListener = new ChainListener(
       provider,
@@ -28,33 +36,83 @@ describe("ChainListener", () => {
     );
   });
 
-  it("should parse ChallengeUpdated events properly when enabled", async () => {
+  it("should parse ChallengeUpdated + StateProgressed events properly when enabled", async () => {
     await chainListener.enable();
 
     const versionNumber = toBN(3);
-    const state = randomState();
-    const timeout = toBN(4);
+    const state = {
+      counter: Zero,
+    };
+    const action = {
+      actionType: ActionType.SUBMIT_COUNTER_INCREMENT,
+      increment: toBN(1),
+    };
+    const timeout = Zero;
 
+    let statesUpdated: ChallengeUpdatedContractEvent[] = [];
     // trigger `ChallengeUpdated` event
-    const [emitted, tx] = await Promise.all([
+    const [states, progressed, tx] = await Promise.all([
       new Promise(async resolve => {
-        chainListener.once("ChallengeUpdated", async (data: ChallengeUpdatedContractEvent) => {
-          return resolve(data);
+        chainListener.on("ChallengeUpdated", async (data: ChallengeUpdatedContractEvent) => {
+          statesUpdated.push(data);
+          if (statesUpdated.length >= 2) {
+            return resolve(
+              statesUpdated.sort((a, b) => a.versionNumber.toNumber() - b.versionNumber.toNumber()),
+            );
+          }
         });
       }),
       new Promise(async resolve => {
-        const tx = await setState(versionNumber, timeout, state);
-        await tx.wait();
-        return resolve(tx);
+        chainListener.once("StateProgressed", async (data: StateProgressedContractEvent) => {
+          return resolve(data);
+        });
+      }),
+      new Promise(async (resolve, reject) => {
+        try {
+          const tx = await setAndProgressState(versionNumber, state, action);
+          await tx.wait();
+          return resolve(tx);
+        } catch (e) {
+          return reject(e.stack || e.message);
+        }
       }),
     ]);
+    ////// verification
+    // tx
     expect(tx).to.be.ok;
-    expect(emitted).to.containSubset({
+    // first state from "setState"
+    expect((states as ChallengeUpdatedContractEvent[])[0]).to.containSubset({
       identityHash: appInstance.identityHash,
       status: ChallengeStatus.IN_DISPUTE,
-      appStateHash: stateToHash(state),
+      appStateHash: stateToHash(AppWithCounterClass.encodeState(state)),
       versionNumber,
       finalizesAt: timeout.add(await provider.getBlockNumber()),
+    });
+    // final state from "applyAction"
+    const finalState = AppWithCounterClass.encodeState({
+      counter: state.counter.add(action.increment),
+    });
+    expect((states as ChallengeUpdatedContractEvent[])[1]).to.containSubset({
+      identityHash: appInstance.identityHash,
+      status: ChallengeStatus.IN_ONCHAIN_PROGRESSION,
+      appStateHash: stateToHash(finalState),
+      versionNumber: versionNumber.add(1),
+      finalizesAt: appInstance.defaultTimeout.add(await provider.getBlockNumber()),
+    });
+    // applied action
+    const turnTaker = new ChannelSigner(channelResponder.privateKey);
+    const digest = computeAppChallengeHash(
+      appInstance.identityHash,
+      stateToHash(finalState),
+      versionNumber.add(One),
+      Zero,
+    );
+    expect(progressed).to.containSubset({
+      identityHash: appInstance.identityHash,
+      action: AppWithCounterClass.encodeAction(action),
+      versionNumber: versionNumber.add(One),
+      turnTaker: turnTaker.address,
+      signature: await turnTaker.signMessage(digest),
     });
   });
 });
