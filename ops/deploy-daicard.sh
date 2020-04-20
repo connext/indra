1a5133489892c3ef78a45965f12ebfe70770ec33
Bo Henderson <bohende@gmail.com>
2020-04-20 11:07:59 +0530

    📨 upgrade daicard proxy

--- ops/deploy-daicard.sh
+++ ops/deploy-daicard.sh
@@ -2,7 +2,7 @@
 set -e
 
 daicard="$1"
-indra="https://${2#*://}"
+indra="$2"
 ssh_key="$HOME/.ssh/connext-aws"
 user=ubuntu
 registry="docker.io/connextproject"
@@ -26,29 +26,32 @@ if [[ -z "$indra" ]]
 then echo "Please provide hostname of upstream indra server to deploy as second argument." && exit
 fi
 
-
 if [[ ! -f "$ssh_key" ]]
 then echo "To deploy, you need to have an ssh key at: $ssh_key" && exit
 fi
 
+indra_url="https://${2#*://}"
+
 echo "Deploying a proxy to: $daicard"
-echo "Requests will be forwarded to: $indra"
+echo "Requests will be forwarded to: $indra_url"
 
 echo;echo "Building & pushing latest version of the daicard proxy"
-make daicard-proxy
+make proxy-daicard
 docker tag daicard_proxy $registry/daicard_proxy
 docker push $registry/daicard_proxy
 if [[ "$?" != "0" ]]
 then echo "Make sure you're logged into docker & have push permissions: docker login" && exit
 fi
 
+exit
+
 echo;echo "Copying start script to $daicard"
 scp -i $ssh_key ops/start-daicard.sh $user@$daicard:/home/$user/start-daicard.sh
 
-echo;echo "Running start script: bash start-daicard.sh $daicard $indra"
+echo;echo "Running start script: bash start-daicard.sh $daicard $indra_url"
 ssh -i $ssh_key $user@$daicard "bash -c '
   docker service rm daicard_proxy
-  bash start-daicard.sh $daicard $indra
+  bash start-daicard.sh $daicard $indra_url
 '"
 
 echo;echo "Changes should be live, check out $daicard"
