77ac5751a951e91c362e5fe89c1ff7129674715d
LayneHaber <layne.haber@gmail.com>
2020-04-20 21:33:31 -0700

    add contract method implementations + stubs

--- modules/watcher/src/watcher.ts
+++ modules/watcher/src/watcher.ts
@@ -1,4 +1,4 @@
-import { JsonRpcProvider, TransactionResponse } from "ethers/providers";
+import { ChallengeRegistry } from "@connext/contracts";
 import {
   NetworkContext,
   ILoggerService,
@@ -24,11 +24,13 @@ import {
   SetStateCommitment,
   ConditionalTransactionCommitment,
 } from "@connext/utils";
-import { ChainListener } from "./chainListener";
+import { JsonRpcProvider, TransactionResponse } from "ethers/providers";
 import EventEmitter from "eventemitter3";
 import { Contract } from "ethers";
-import { ChallengeRegistry } from "@connext/contracts";
-import { Interface, defaultAbiCoder } from "ethers/utils";
+import { Interface, BigNumber, defaultAbiCoder } from "ethers/utils";
+
+import { ChainListener } from "./chainListener";
+import { ChallengeEvents, StateProgressed, ChallengeUpdated } from "../../types/dist/src";
 
 /**
  * Watchers will watch for contract events and respond to disputes on behalf
@@ -135,6 +137,7 @@ export class Watcher implements IWatcher {
     // register listener for any future events
     this.log.debug(`Enabling listener`);
     await this.listener.enable();
+    await this.registerListeners();
 
     this.enabled = true;
     this.log.info(`Watcher enabled`);
@@ -191,16 +194,40 @@ export class Watcher implements IWatcher {
     throw new Error("Method not implemented");
   };
 
-  private assertSignerCanSendTransactions = (): void => {
-    if (!this.signer.provider) {
-      throw new Error(`Signer cannot send transactions without an attached provider`);
-    }
+  // processes + responds to any events caught for challenges we did not
+  // intiate/are unaware of
+  private respondToDispute = async (blockNumber: number): Promise<void> => {
+    throw new Error("Method not implemented");
+  };
+
+  // should advance (call `setOutcome`, `progressState`, etc.) any active
+  // disputes whos dispute period elapses
+  private advanceDisputes = async (): Promise<void> => {
+    throw new Error("Method not implemented");
+  };
+
+  // should check every block for challenges that should be advanced,
+  // and respond to any listener emitted chain events
+  private registerListeners = async (): Promise<void> => {
+    this.listener.on(ChallengeEvents[ChallengeUpdated], async () => {
+      throw new Error("Watcher callback not implemented");
+    });
+    this.listener.on(ChallengeEvents[StateProgressed], async () => {
+      throw new Error("Watcher callback not implemented");
+    });
+    this.provider.on("block", async (blockNumber: number) => {
+      await this.advanceDisputes();
+    });
   };
 
   //////// Private contract methods
-  private setState = async (appIdentityHash: string): Promise<TransactionResponse> => {
+  private setState = async (
+    appIdentityHash: string,
+    versionNumber: BigNumber,
+  ): Promise<TransactionResponse> => {
+    const jsons = await this.store.getSetStateCommitments(appIdentityHash);
     const commitment = SetStateCommitment.fromJson(
-      await this.store.getSetStateCommitment(appIdentityHash),
+      jsons.filter(x => x.versionNumber.eq(versionNumber))[0],
     );
     return this.sendContractTransaction(await commitment.getSignedTransaction());
   };
@@ -209,7 +236,7 @@ export class Watcher implements IWatcher {
     appIdentityHash: string,
   ): Promise<TransactionResponse> => {
     const commitment = ConditionalTransactionCommitment.fromJson(
-      await this.store.getSetStateCommitment(appIdentityHash),
+      await this.store.getConditionalTransactionCommitment(appIdentityHash),
     );
     return this.sendContractTransaction(commitment.getTransactionDetails());
   };
@@ -218,18 +245,81 @@ export class Watcher implements IWatcher {
     appIdentityHash: string,
     multisigAddress: string,
   ): Promise<TransactionResponse> => {
-    throw new Error("Method not implemented");
+    const jsons = await this.store.getSetStateCommitments(appIdentityHash);
+    const commitments = jsons
+      .sort((a, b) => a.versionNumber.sub(b.versionNumber))
+      .map(SetStateCommitment.fromJson);
+
+    const app = await this.store.getAppInstance(appIdentityHash);
+    const action = defaultAbiCoder.encode([app.appInterface.actionEncoding!], [app.latestAction]);
+    const state = defaultAbiCoder.encode([app.appInterface.stateEncoding], [app.latestState]);
+
+    const tx = {
+      to: this.challengeRegistry.address,
+      value: 0,
+      data: new Interface(ChallengeRegistry.abi).functions.progressState.encode([
+        this.getAppIdentity(app, multisigAddress),
+        await commitments[0].getSignedAppChallengeUpdate(),
+        await commitments[1].getSignedAppChallengeUpdate(),
+        state,
+        action,
+      ]),
+    };
+    return this.sendContractTransaction(tx);
   };
 
   private setAndProgressState = async (
     appIdentityHash: string,
     multisigAddress: string,
   ): Promise<TransactionResponse> => {
-    throw new Error("Method not implemented");
+    const jsons = await this.store.getSetStateCommitments(appIdentityHash);
+    const commitments = jsons
+      .sort((a, b) => a.versionNumber.sub(b.versionNumber))
+      .map(SetStateCommitment.fromJson);
+
+    const app = await this.store.getAppInstance(appIdentityHash);
+    const action = defaultAbiCoder.encode([app.appInterface.actionEncoding!], [app.latestAction]);
+    const state = defaultAbiCoder.encode([app.appInterface.stateEncoding], [app.latestState]);
+    const tx = {
+      to: this.challengeRegistry.address,
+      value: 0,
+      data: new Interface(ChallengeRegistry.abi).functions.setAndProgressState.encode([
+        this.getAppIdentity(app, multisigAddress),
+        await commitments[0].getSignedAppChallengeUpdate(),
+        await commitments[1].getSignedAppChallengeUpdate(),
+        state,
+        action,
+      ]),
+    };
+    return this.sendContractTransaction(tx);
   };
 
-  private setOutcome = async (): Promise<TransactionResponse> => {
-    throw new Error("Method not implemented");
+  private setOutcome = async (
+    appIdentityHash: string,
+    multisigAddress: string,
+  ): Promise<TransactionResponse> => {
+    const challenge = await this.store.getAppChallenge(appIdentityHash);
+    if (!challenge) {
+      throw new Error(`No record of challenge found, cannot set outcome`);
+    }
+    const app = await this.store.getAppInstance(appIdentityHash);
+    // FIXME: assumes that the `app` in the store will be updated
+    // from state transitions that result from the game being played out
+    // onchain. currently the watcher service CANNOT do this because the
+    // service does not have access to "update" store methods
+    if (!challenge.versionNumber.eq(app.versionNumber)) {
+      throw new Error(`App is not up to date with onchain challenges, cannot setOutcome`);
+    }
+    const state = defaultAbiCoder.encode([app.appInterface.stateEncoding], [app.latestState]);
+    const tx = {
+      to: this.challengeRegistry.address,
+      value: 0,
+      data: new Interface(ChallengeRegistry.abi).functions.setOutcome.encode([
+        this.getAppIdentity(app, multisigAddress),
+        state,
+      ]),
+    };
+    return this.sendContractTransaction(tx);
   };
 
   private cancelChallenge = async (
@@ -263,6 +353,7 @@ export class Watcher implements IWatcher {
     };
   };
 
+  //////// Private helper functions
   private sendContractTransaction = async (
     transaction: MinimalTransaction,
   ): Promise<TransactionResponse> => {
@@ -276,6 +367,12 @@ export class Watcher implements IWatcher {
     await tx.wait();
     this.log.debug(`Successfully sent transaction ${tx.hash}`);
     return tx;
+  };
+
+  private assertSignerCanSendTransactions = (): void => {
+    if (!this.signer.provider) {
+      throw new Error(`Signer cannot send transactions without an attached provider`);
+    }
   };
 
   private getAppIdentity = (app: AppInstanceJson, multisigAddress: string): AppIdentity => {
