f3ca6833b821c6fd21afa34cff2d5dd0646c5bf6
LayneHaber <layne.haber@gmail.com>
2020-04-21 14:28:13 -0700

    add event entities

---
+++ modules/node/src/challengeUpdatedEvent/challengeUpdatedEvent.entity.ts
@@ -0,0 +1,68 @@
+import {
+  PrimaryGeneratedColumn,
+  Entity,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from "typeorm";
+
+import { Challenge } from "../challenge/challenge.entity";
+import { AppName, Bytes32, ChallengeUpdatedEventPayload, ChallengeStatus } from "@connext/types";
+import { BigNumber } from "ethers/utils";
+import { toBN } from "@connext/utils";
+import { IsKeccak256Hash } from "../validate";
+
+export const entityToChallengeUpdatedPayload = (
+  item: ChallengeUpdatedEvent | undefined,
+): ChallengeUpdatedEventPayload | undefined => {
+  if (!item) {
+    return undefined;
+  }
+  const { challenge, appStateHash, versionNumber, finalizesAt, status } = item;
+  return {
+    appStateHash,
+    versionNumber,
+    finalizesAt,
+    status,
+    identityHash: challenge.app.identityHash,
+  };
+};
+
+@Entity()
+export class ChallengeUpdatedEvent<T extends AppName = any> {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne((type: any) => Challenge)
+  challenge!: Challenge<T>;
+
+  @Column("text")
+  @IsKeccak256Hash()
+  appStateHash!: Bytes32;
+
+  @Column("text", {
+    transformer: {
+      from: (value: string): BigNumber => toBN(value),
+      to: (value: BigNumber): string => value.toString(),
+    },
+  })
+  versionNumber!: BigNumber;
+
+  @Column("text", {
+    transformer: {
+      from: (value: string): BigNumber => toBN(value),
+      to: (value: BigNumber): string => value.toString(),
+    },
+  })
+  finalizesAt!: BigNumber;
+
+  @Column({ type: "enum", enum: ChallengeStatus })
+  status: ChallengeStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ modules/node/src/stateProgressedEvent/stateProgressedEvent.entity.ts
@@ -0,0 +1,73 @@
+import {
+  PrimaryGeneratedColumn,
+  Entity,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+} from "typeorm";
+
+import { Challenge } from "../challenge/challenge.entity";
+import { StateProgressedEventPayload, AppName, AppActions, Address } from "@connext/types";
+import { IsEthAddress } from "src/validate";
+import { BigNumber, defaultAbiCoder } from "ethers/utils";
+import { toBN } from "@connext/utils";
+
+export const entityToStateProgressedEventPayload = (
+  item: StateProgressedEvent | undefined,
+): StateProgressedEventPayload | undefined => {
+  if (!item) {
+    return undefined;
+  }
+  const { challenge, action, versionNumber, timeout, turnTaker, signature } = item;
+  const encodedAction = defaultAbiCoder.encode([challenge.app.actionEncoding!], [action]);
+  return {
+    action: encodedAction,
+    versionNumber,
+    timeout,
+    turnTaker,
+    signature,
+    identityHash: challenge.app.identityHash,
+  };
+};
+
+@Entity()
+export class StateProgressedEvent<T extends AppName = any> {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne((type: any) => Challenge)
+  challenge!: Challenge<T>;
+
+  @Column("jsonb")
+  action!: AppActions[T];
+
+  @Column("text", {
+    transformer: {
+      from: (value: string): BigNumber => toBN(value),
+      to: (value: BigNumber): string => value.toString(),
+    },
+  })
+  versionNumber!: BigNumber;
+
+  @Column("text", {
+    transformer: {
+      from: (value: string): BigNumber => toBN(value),
+      to: (value: BigNumber): string => value.toString(),
+    },
+  })
+  timeout!: BigNumber;
+
+  @Column("text")
+  @IsEthAddress()
+  turnTaker!: Address;
+
+  @Column("text")
+  signature!: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
