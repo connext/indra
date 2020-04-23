45865b44a02cc96b350eb9d5bf0e8a514f64d71f
bohendo <bohende@gmail.com>
2020-04-23 11:04:33 +0530

    🔏 use hardcoded oracle type in dev-mode

--- ops/start-dev.sh
+++ ops/start-dev.sh
@@ -63,7 +63,7 @@ else
 fi
 
 token_address="`echo $eth_contract_addresses | jq '.["'"$chainId"'"].Token.address' | tr -d '"'`"
-allowed_swaps='[{"from":"'"$token_address"'","to":"0x0000000000000000000000000000000000000000","priceOracleType":"UNISWAP"},{"from":"0x0000000000000000000000000000000000000000","to":"'"$token_address"'","priceOracleType":"UNISWAP"}]'
+allowed_swaps='[{"from":"'"$token_address"'","to":"0x0000000000000000000000000000000000000000","priceOracleType":"HARDCODED"},{"from":"0x0000000000000000000000000000000000000000","to":"'"$token_address"'","priceOracleType":"HARDCODED"}]'
 
 if [[ -z "$chainId" ]]
 then echo "Failed to fetch chainId from provider ${INDRA_ETH_PROVIDER}" && exit 1;
