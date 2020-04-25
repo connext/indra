d6fcbb7b01a5f1ee9679a5877fc2c69bd0172588
LayneHaber <layne.haber@gmail.com>
2020-04-24 19:26:09 -0700

    constant

--- modules/watcher/test/utils/utils.ts
+++ modules/watcher/test/utils/utils.ts
@@ -6,3 +6,10 @@ import { hexlify, keccak256, randomBytes } from "ethers/utils";
 export const randomState = (numBytes: number = 64) => hexlify(randomBytes(numBytes));
 
 export const stateToHash = (state: string) => keccak256(state);
+
+/////////////////////////////
+//// Constants
+
+// ProxyFactory.createProxy uses assembly `call` so we can't estimate
+// gas needed, so we hard-code this number to ensure the tx completes
+export const CREATE_PROXY_AND_SETUP_GAS = 1e6;
