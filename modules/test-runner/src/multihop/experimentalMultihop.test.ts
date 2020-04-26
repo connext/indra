bc81f57420a7c981722d138f827789e204207b19
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-04-27 02:49:38 +0400

    flesh out the tests

--- modules/test-runner/src/multihop/experimentalMultihop.test.ts
+++ modules/test-runner/src/multihop/experimentalMultihop.test.ts
@@ -1,29 +1,85 @@
-import { IConnextClient } from "@connext/types";
-import { createClient } from "../util";
-import { stringify } from "@connext/utils";
-import axios, { AxiosResponse } from "axios";
+import { IConnextClient, ConditionalTransferTypes, PublicParams, ClientOptions } from "@connext/types";
+import { createClient, fundChannel, ETH_AMOUNT_MD, ETH_AMOUNT_SM, env } from "../util";
+import { stringify, getRandomBytes32, ConsoleLogger } from "@connext/utils";
+import axios from "axios";
+import { soliditySha256 } from "ethers/utils";
+import { providers } from "ethers";
 
 describe.only("Experimental multihop tests", () => {
   let clientA: IConnextClient;
   let clientB: IConnextClient;
+  let clientC: IConnextClient;
   const nodeBUrl = "http://172.17.0.1:8081";
+  const provider = new providers.JsonRpcProvider(env.ethProviderUrl);
+  const log = new ConsoleLogger("test", 4)
 
-  beforeEach(async () => {
+  before(async () => {
     clientA = await createClient();
-  });
-
-  it("clientB can connect to nodeB", async () => {
-    console.log("here");
-    clientB = await createClient({
+    clientB = await createClient();
+    clientC = await createClient({
       nodeUrl: nodeBUrl,
     });
+    console.log(`ClientA: ${clientA.publicIdentifier}`)
+    console.log(`NodeA: ${clientA.nodeIdentifier}`)
+    console.log(`NodeB: ${clientC.nodeIdentifier}`)
+    console.log(`ClientC: ${clientC.publicIdentifier}`)
+    await fundChannel(clientA, ETH_AMOUNT_MD)
   });
 
-  it("nodeA can create a channel with nodeB", async () => {
-    console.log("here");
-    const verifyResponse = await axios.post(`${nodeBUrl}/admin/nodetonode`, {
-      userIdentifier: clientA.nodeIdentifier
-    });
-    console.log(stringify(verifyResponse));
-  });
+  it("can create a  nodeA to nodeB channel", async () => {
+    let nodeANodeBMultisig: string;
+    try {
+       nodeANodeBMultisig = await axios.post(`${nodeBUrl}/admin/nodetonode`, {
+        userIdentifier: clientA.nodeIdentifier
+      });
+    } catch (e) {
+      nodeANodeBMultisig = "";
+    }
+    console.log(`NodeToNode Multisig: ${nodeANodeBMultisig}`)
+  })
+
+  it.skip("clientA can transfer funds to clientB over nodeA", async() => {
+    const preImage = getRandomBytes32();
+    const timelock = ((await provider.getBlockNumber()) + 5000).toString();
+    const lockHash = soliditySha256(["bytes32"], [preImage]);
+    const waitForInstall = new Promise(resolve => {
+      clientB.on("CONDITIONAL_TRANSFER_CREATED_EVENT", data => {
+        resolve();
+      })
+    })
+    const transferInstallRet = await clientA.conditionalTransfer({
+      amount: ETH_AMOUNT_SM.toString(),
+      conditionType: ConditionalTransferTypes.HashLockTransfer,
+      lockHash,
+      timelock,
+      meta: { foo: "bar", sender: clientA.publicIdentifier },
+      recipient: clientB.publicIdentifier,
+    } as PublicParams.HashLockTransfer)
+    await waitForInstall;
+  })
+
+  it("clientA can transfer funds to clientC over both nodeA and nodeB", async () => {
+    const preImage = getRandomBytes32();
+    const timelock = ((await provider.getBlockNumber()) + 5000).toString();
+    const lockHash = soliditySha256(["bytes32"], [preImage]);
+    const waitForInstall = new Promise(resolve => {
+      clientC.on("CONDITIONAL_TRANSFER_CREATED_EVENT", data => {
+        console.log(stringify(data))
+        resolve();
+      })
+    })
+    const transferInstallRet = await clientA.conditionalTransfer({
+      amount: ETH_AMOUNT_SM.toString(),
+      conditionType: ConditionalTransferTypes.HashLockTransfer,
+      lockHash,
+      timelock,
+      meta: { 
+        path: [clientA.nodeIdentifier, clientC.nodeIdentifier, clientC.publicIdentifier], 
+        sender: clientA.publicIdentifier,
+      },
+      recipient: clientC.publicIdentifier,
+    } as PublicParams.HashLockTransfer)
+    console.log(stringify(transferInstallRet))
+    await waitForInstall;
+  })
 });
