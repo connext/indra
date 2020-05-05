17cd344285f90101ada65bf6952b6a7b01b55922
bohendo <bohende@gmail.com>
2020-05-05 10:27:27 +0530

    📑 explicitly export what we need from store's index

--- modules/store/src/index.ts
+++ modules/store/src/index.ts
@@ -1,5 +1,20 @@
-export * from "./connextStore";
-export * from "./pisaClient";
-export * from "./wrappers";
-export * from "./helpers";
-export * from "./constants";
+export { ConnextStore } from "./connextStore";
+export { PisaClientBackupAPI } from "./pisaClient";
+export {
+  WrappedAsyncStorage,
+  FileStorage,
+  KeyValueStorage,
+  WrappedLocalStorage,
+  WrappedMemoryStorage,
+  WrappedPostgresStorage,
+} from "./wrappers";
+export {
+  getDirectoryFiles,
+  isDirectory,
+} from "./helpers";
+export {
+  DEFAULT_FILE_STORAGE_EXT,
+  DEFAULT_STORE_PREFIX,
+  PATH_PROPOSED_APP_IDENTITY_HASH,
+  STORE_KEY,
+} from "./constants";
