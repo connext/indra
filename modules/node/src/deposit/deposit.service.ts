08f03937acb70491aaa2f246b949e6b20b712554
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-05-09 23:48:29 +0400

    Add setting and waiting for active deposits

--- modules/node/src/deposit/deposit.service.ts
+++ modules/node/src/deposit/deposit.service.ts
@@ -20,6 +20,7 @@ import { ConfigService } from "../config/config.service";
 import { LoggerService } from "../logger/logger.service";
 import { OnchainTransactionService } from "../onchainTransactions/onchainTransaction.service";
 import { AppRegistryRepository } from "../appRegistry/appRegistry.repository";
+import { ChannelRepository } from "../channel/channel.repository";
 import {
   OnchainTransaction,
   TransactionReason,
@@ -33,6 +34,7 @@ export class DepositService {
     private readonly onchainTransactionService: OnchainTransactionService,
     private readonly log: LoggerService,
     private readonly appRegistryRepository: AppRegistryRepository,
+    private readonly channelRepository: ChannelRepository
   ) {
     this.log.setContext("DepositService");
   }
@@ -40,7 +42,7 @@ export class DepositService {
   async deposit(
     channel: Channel,
     amount: BigNumber,
-    tokenAddress: string,
+    assetId: string,
   ): Promise<TransactionReceipt> {
     // don't allow deposit if user's balance refund app is installed
     const depositRegistry = await this.appRegistryRepository.findByNameAndNetwork(
@@ -50,31 +52,40 @@ export class DepositService {
     const depositApp = channel.appInstances.filter(
       app =>
         app.appDefinition === depositRegistry.appDefinitionAddress &&
-        app.latestState.assetId === tokenAddress,
+        app.latestState.assetId === assetId,
     )[0];
     this.log.debug(`Found deposit app: ${stringify(depositApp)}`);
     if (depositApp && depositApp.latestState.transfers[0].to === channel.userIdentifier) {
       throw new Error(
-        `Cannot deposit, user has deposit app installed for asset ${tokenAddress}, app: ${depositApp.identityHash}`,
+        `Cannot deposit, user has deposit app installed for asset ${assetId}, app: ${depositApp.identityHash}`,
       );
     }
+
+    // don't allow deposit if an active deposit is in process
+    if (channel.activeCollateralizations[assetId]) {
+      this.log.warn(`Collateral request is in flight for ${assetId}, waiting for transaction`);
+      await this.waitForActiveDeposit(channel.userIdentifier);
+    }
+
+    await this.channelRepository.setInflightCollateralization(channel, assetId, false);
 
     let appIdentityHash;
     if (!depositApp) {
       this.log.info(`Requesting deposit rights before depositing`);
-      appIdentityHash = await this.requestDepositRights(channel, tokenAddress);
+      appIdentityHash = await this.requestDepositRights(channel, assetId);
     }
     // deposit app for asset id with node as initiator is already installed
     // send deposit to chain
     let receipt;
     try {
-      const tx = await this.sendDepositToChain(channel, amount, tokenAddress);
+      const tx = await this.sendDepositToChain(channel, amount, assetId);
       receipt = await tx.wait();
     } catch (e) {
-      throw new Error(e.stack || e.message);
+      throw e;
     } finally {
       await this.rescindDepositRights(appIdentityHash || depositApp.identityHash);
     }
+    await this.channelRepository.setInflightCollateralization(channel, assetId, true);
     return receipt;
   }
 
@@ -102,6 +113,37 @@ export class DepositService {
       return undefined;
     }
     return tx;
+  }
+
+  private async waitForActiveDeposit(
+    userId: string,
+  ): Promise<TransactionReceipt> {
+    this.log.info(`Collateralization in flight for user ${userId}, waiting`)
+    const ethProvider = this.configService.getEthProvider();
+    const startingBlock = await ethProvider.getBlockNumber();
+    // register listener
+    const depositReceipt: TransactionReceipt = await new Promise(async resolve => {
+      const BLOCKS_TO_WAIT = 5;
+      ethProvider.on("block", async (blockNumber: number) => {
+        if (blockNumber - startingBlock > BLOCKS_TO_WAIT) {
+          return resolve(undefined);
+        }
+        const { transactions } = await ethProvider.getBlock(blockNumber);
+        for (const hash of transactions) {
+          const tx = await this.findByHash(hash);
+          if (
+            tx &&
+            tx.channel.userIdentifier === userId &&
+            tx.from === (await this.configService.getSignerAddress())
+          ) {
+            this.log.info(`Found deposit transaction: ${hash}`);
+            return resolve(await ethProvider.getTransactionReceipt(hash));
+          }
+        }
+      });
+    });
+    this.log.info(`Done waiting for collateralization in flight. DepositReceipt: ${depositReceipt}`)
+    return depositReceipt;
   }
 
   private async sendDepositToChain(
