f9a191104d24f79bb2e39323f2854a9cd12387f8
LayneHaber <layne.haber@gmail.com>
2020-05-12 13:16:41 -0700

    fix tests

--- modules/test-runner/src/util/ethprovider.ts
+++ modules/test-runner/src/util/ethprovider.ts
@@ -1,13 +1,28 @@
 import { Contract, Wallet } from "ethers";
 import { AddressZero } from "ethers/constants";
 import { JsonRpcProvider } from "ethers/providers";
-import { BigNumber, BigNumberish } from "ethers/utils";
+import { BigNumber, BigNumberish, parseEther } from "ethers/utils";
 import abi from "human-standard-token-abi";
 
 import { env } from "./env";
+import { stringify } from "@connext/utils";
+import { ERC20, addressBook } from "@connext/contracts";
 
 export const ethProvider = new JsonRpcProvider(env.ethProviderUrl);
-export const ethWallet = Wallet.fromMnemonic(env.mnemonic).connect(ethProvider);
+export const sugarDaddy = Wallet.fromMnemonic(env.mnemonic).connect(ethProvider);
+export const ethWallet = Wallet.createRandom().connect(ethProvider);
+
+export const fundEthWallet = async () => {
+  const ethFunding = await sugarDaddy.sendTransaction({
+    to: ethWallet.address,
+    value: parseEther("50"),
+  });
+  await ethFunding.wait();
+  const tokenContract = new Contract(addressBook[4447].Token.address, ERC20.abi, sugarDaddy);
+  const tx = await tokenContract.functions.transfer(ethWallet.address, parseEther("100"));
+  await tx.wait();
+  return;
+};
 
 /**
  * EVM snapshot, returns hex string of snapshot ID.
@@ -29,13 +44,32 @@ export const sendOnchainValue = async (
   value: BigNumberish,
   assetId: string = AddressZero,
 ): Promise<void> => {
-  if (assetId === AddressZero) {
-    const tx = await ethWallet.sendTransaction({ to, value });
-    await tx.wait()
-  } else {
-    const tokenContract = new Contract(assetId, abi, ethWallet);
-    const tx = await tokenContract.functions.transfer(to, value);
-    await tx.wait()
+  const nonceErr = "the tx doesn't have the correct nonce";
+  const retries = 3;
+  for (let i = 0; i < retries; i++) {
+    const nonce = await ethWallet.getTransactionCount();
+    try {
+      if (assetId === AddressZero) {
+        const tx = await ethWallet.sendTransaction({
+          to,
+          value,
+          nonce,
+        });
+        await tx.wait();
+        return;
+      } else {
+        const tokenContract = new Contract(assetId, abi, ethWallet);
+        const tx = await tokenContract.functions.transfer(to, value, { nonce });
+        await tx.wait();
+        return;
+      }
+    } catch (e) {
+      console.log("***** caught error!", stringify(e), e.message);
+      if (e.message.includes(nonceErr)) {
+        continue;
+      }
+      throw e;
+    }
   }
 };
 
