fbe89651eb25e1ee7bbc47506e0bfa1c95f3e13e
Rahul Sethuram <rahul@connext.network>
2020-05-13 17:27:39 +0200

    ⚡️ Multisig to lock

--- modules/cf-core/src/methods/app-instance/reject-install.ts
+++ modules/cf-core/src/methods/app-instance/reject-install.ts
@@ -18,8 +18,14 @@ export class RejectInstallController extends NodeController {
     params: MethodParams.RejectInstall,
   ): Promise<string[]> {
     const { appIdentityHash } = params;
+    const { store } = requestHandler;
 
-    return [appIdentityHash];
+    const stateChannel = await store.getStateChannelByAppIdentityHash(appIdentityHash);
+    if (!stateChannel) {
+      throw new Error(NO_STATE_CHANNEL_FOR_APP_IDENTITY_HASH(appIdentityHash));
+    }
+
+    return [appIdentityHash, stateChannel.multisigAddress];
   }
 
   @jsonRpcMethod(MethodNames.chan_rejectInstall)
