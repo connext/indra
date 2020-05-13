38af3a211a56e93998e1ae376dcff9cad6e2726f
Rahul Sethuram <rahul@connext.network>
2020-05-13 12:16:38 +0200

    Add new messaging service to simulate disconnects

---
+++ modules/cf-core/src/testing/services/memory-messaging-service-limits.ts
@@ -0,0 +1,65 @@
+import { IMessagingService, Message, ProtocolName } from "@connext/types";
+import { EventEmitter } from "events";
+import { stringify } from "@connext/utils";
+
+export type MessagingLimit = { to: string; limit: number };
+export type MessagingLimitAndCount = MessagingLimit & { count: number };
+
+export class MemoryMessagingServiceWithLimits implements IMessagingService {
+  public eventEmitter: EventEmitter;
+  private connected: boolean = true;
+  private messagesSent = 0;
+
+  constructor(
+    eventEmitter: EventEmitter = new EventEmitter(),
+    private readonly messagesToSend: number = 0,
+    private readonly protocol?: ProtocolName,
+  ) {
+    this.messagesToSend = messagesToSend;
+    this.eventEmitter = eventEmitter;
+  }
+
+  async send(to: string, msg: Message): Promise<void> {
+    if (!this.connected) {
+      console.log(`Messaging service disconnected, not responding to message: ${stringify(msg)}`);
+      return;
+    }
+
+    this.eventEmitter.emit(to, msg);
+    if (this.protocol && msg.data.protocol === this.protocol) {
+      console.log(`SENDING ${msg.data.protocol} MESSAGE ${stringify(msg)} to ${to}`);
+      this.messagesSent += 1;
+      if (this.messagesSent >= this.messagesToSend) {
+        console.log(`Disconnecting after ${this.messagesSent} messages sent`);
+        await this.disconnect();
+      }
+    }
+  }
+
+  async onReceive(address: string, callback: (msg: Message) => void) {
+    this.eventEmitter.on(address, (msg) => {
+      if (!this.connected) {
+        console.log(`Messaging service disconnected, not responding to message: ${stringify(msg)}`);
+        return;
+      }
+      callback(msg);
+    });
+  }
+
+  async connect() {
+    this.connected = true;
+  }
+  async disconnect() {
+    this.connected = false;
+  }
+  async flush() {}
+  async publish(subject: string, data: any) {}
+  async request(
+    subject: string,
+    timeout: number,
+    data: object,
+    callback?: (response: any) => any,
+  ) {}
+  async subscribe(subject: string, callback: (msg: Message) => void) {}
+  async unsubscribe(subject: string) {}
+}
