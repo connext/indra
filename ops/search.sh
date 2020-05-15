1dec60570747e22e6e942a1880df9f666e8d5434
bohendo <bohende@gmail.com>
2020-05-15 15:28:04 +0530

    🔎 init search helper util

---
+++ ops/search.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+target="$1";
+shift;
+
+if [[ -z "$target" ]]
+then echo "One arg required: bash ops/search.sh <target>" && exit 1
+fi
+
+grep "$@" --exclude=*.swp --exclude=*.pdf --color=auto -r "$target" modules/*/src modules/*/ops
