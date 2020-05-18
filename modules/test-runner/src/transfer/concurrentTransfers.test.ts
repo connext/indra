af6202603ffe5c94e3a40875febda89357529187
Rahul Sethuram <rahul@connext.network>
2020-05-18 21:33:32 +0200

    Skip concurrent test for CI/CD

--- modules/test-runner/src/transfer/concurrentTransfers.test.ts
+++ modules/test-runner/src/transfer/concurrentTransfers.test.ts
@@ -18,27 +18,30 @@ describe("Concurrent transfers", async () => {
   let indexerB: IConnextClient;
   let subgraphChannels;
 
-  before(async() => {
+  before(async () => {
     // let wallet = Wallet.fromMnemonic(
     //   "favorite plunge fatigue crucial decorate bottom hour veteran embark gravity devote business",
     // );
     // privateKey = wallet.privateKey;
-  
+
     channel = await createClient({
       // signer: privateKey,
-      loggerService: new ColorfulLogger("Client", 1, true, 'Gateway'),
+      loggerService: new ColorfulLogger("Client", 1, true, "Gateway"),
     });
     indexerA = await createClient();
     indexerB = await createClient();
-  
+
     console.log("Signer address:", channel.signerAddress);
-  
+
     console.log("Deposit into state channel");
     await fundChannel(channel, DEPOSIT_AMOUNT, AddressZero);
-    const balance: BigNumber = (await channel.getFreeBalance())[channel.signerAddress]
+    const balance: BigNumber = (await channel.getFreeBalance())[channel.signerAddress];
     console.log("Free balance:", balance.toString());
-    console.log("Total # of payments possible: ", balance.div(utils.parseEther("0.00001")).toString());
-  
+    console.log(
+      "Total # of payments possible: ",
+      balance.div(utils.parseEther("0.00001")).toString(),
+    );
+
     subgraphChannels = [
       {
         signer: indexerA.signerAddress,
@@ -49,25 +52,19 @@ describe("Concurrent transfers", async () => {
         publicIdentifier: indexerB.publicIdentifier,
       },
     ];
-  })
+  });
 
-  it.only("Can handle many concurrent transfers", async function() {
-    this.timeout(0) // disable timeout
-    let count = 0
-    indexerA.on(
-      "CONDITIONAL_TRANSFER_CREATED_EVENT",
-      (data) => {
-        count++
-        console.log(`${data.paymentId} Payment created: ${count}`);
-      },
-    );
-    indexerB.on(
-      "CONDITIONAL_TRANSFER_CREATED_EVENT",
-      (data) => {
-        count++
-        console.log(`${data.paymentId} Payment created: ${count}`);
-      },
-    );
+  it.skip("Can handle many concurrent transfers", async function () {
+    this.timeout(0); // disable timeout
+    let count = 0;
+    indexerA.on("CONDITIONAL_TRANSFER_CREATED_EVENT", (data) => {
+      count++;
+      console.log(`${data.paymentId} Payment created: ${count}`);
+    });
+    indexerB.on("CONDITIONAL_TRANSFER_CREATED_EVENT", (data) => {
+      count++;
+      console.log(`${data.paymentId} Payment created: ${count}`);
+    });
     let queue = new PQueue({ concurrency: 10 });
     let sendLoop = async () => {
       while (true) {
