11a9599ca73fb7f301e920ca81321e8aa3feab41
LayneHaber <layne.haber@gmail.com>
2020-05-19 10:27:19 -0700

    remove .only

--- modules/test-runner/src/withdraw/withdraw.test.ts
+++ modules/test-runner/src/withdraw/withdraw.test.ts
@@ -22,14 +22,13 @@ import {
   requestCollateral,
   getNatsClient,
 } from "../util";
-import { toBN } from "@connext/utils";
 import { addRebalanceProfile } from "../util/helpers/rebalanceProfile";
 import { Client } from "ts-nats";
 
 // TODO: multiple withdrawal tests are skipped because there are issues where
 // the TX is sent before the client can subscribe. need to fix by possibly increasing block
 // time
-describe.only("Withdrawal", () => {
+describe("Withdrawal", () => {
   let client: IConnextClient;
   let tokenAddress: string;
   let nats: Client;
