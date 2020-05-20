129365b611c77343e4e1beaf826a29af350e71fd
Rahul Sethuram <rahul@connext.network>
2020-05-20 19:38:41 +0200

    Change error message expectation

--- modules/test-runner/src/withdraw/withdrawOffline.test.ts
+++ modules/test-runner/src/withdraw/withdrawOffline.test.ts
@@ -101,7 +101,7 @@ describe("Withdraw offline tests", () => {
 
     await expect(
       withdrawFromChannel(client, ZERO_ZERO_ZERO_FIVE_ETH, AddressZero),
-    ).to.be.rejectedWith(`proposal took longer than ${CF_METHOD_TIMEOUT / 1000} seconds`);
+    ).to.be.rejectedWith(`timed out after 10s waiting for counterparty`);
 
     await recreateClientAndRetryWithdraw(client, store, {
       amount: ZERO_ZERO_ZERO_FIVE_ETH,
