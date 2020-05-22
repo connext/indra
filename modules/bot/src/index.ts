4f25f89df7534d1d2df8035d7391984b8245af16
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-05-22 17:04:15 +0400

    Correctly setup ops

--- modules/bot/src/index.ts
+++ modules/bot/src/index.ts
@@ -1,5 +1,4 @@
 require("yargs")
-  .command(require("./commands/sender").default)
-  .command(require("./commands/receiver").default)
+  .command(require("./commands/agent").default)
   .demandCommand(1, "Choose a command from the above list")
   .help().argv;
