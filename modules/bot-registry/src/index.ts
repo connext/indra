744aed6a717a4d246982fc73dca3aa5a65ad3848
Rahul Sethuram <rahul@connext.network>
2020-05-22 15:24:56 +0200

    Clear on startup

--- modules/bot-registry/src/index.ts
+++ modules/bot-registry/src/index.ts
@@ -36,6 +36,7 @@ app.delete("/", async (req, res) => {
 
 const start = async () => {
   await init();
+  await clear();
   app.listen(port, () => console.log(`Bot registry listening at http://localhost:${port}`));
 };
 start();
