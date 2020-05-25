c23d118ba24e82585b87cefb3ef06886b3f1b7ac
Pedro Gomes <pedrogomes94@gmail.com>
2020-05-25 17:09:10 +0200

    fix test-utils

--- modules/utils/src/attestations.spec.ts
+++ modules/utils/src/attestations.spec.ts
@@ -5,21 +5,26 @@ import * as bs58 from "bs58";
 
 describe("Attestations", () => {
   test("attestations are correct", async () => {
-    let mnemonic = "coyote tattoo slush ball cluster culture bleak news when action cover effort";
+    const mnemonic = "coyote tattoo slush ball cluster culture bleak news when action cover effort";
 
-    let receipt = {
+    const receipt = {
       requestCID: "0xd902c18a1b3590a3d2a8ae4439db376764fda153ca077e339d0427bf776bd463",
       responseCID: "0xbe0b5ae5f598fdf631133571d59ef16b443b2fe02e35ca2cb807158069009db9",
       subgraphID: hexlify(bs58.decode("QmTXzATwNfgGVukV1fX2T6xw9f6LAYRVWpsdXyRWzUR2H9").slice(2)),
     };
 
-    let signer = Wallet.fromMnemonic(mnemonic);
-    let attestation = await signReceipt(
+    const signer = Wallet.fromMnemonic(mnemonic);
+    const signature = await signReceipt(
       receipt,
       1,
       "0x0000000000000000000000000000000000000000",
       signer.privateKey,
     );
+
+    const attestation = {
+      ...receipt,
+      signature,
+    };
 
     expect(attestation).toStrictEqual({
       requestCID: receipt.requestCID,
