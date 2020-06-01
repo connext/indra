3904f33e0c4ff645f35587aa55cdb052ddb192bd
bohendo <bohende@gmail.com>
2020-06-01 15:48:34 +0530

    🔥 rm bot mnemonics & old copy of migrate script

--- modules/contracts/src.ts/constants.ts
+++ modules/contracts/src.ts/constants.ts
@@ -1,10 +1,5 @@
 import { Options } from "yargs";
 
-export const botMnemonics = [
-  "humble sense shrug young vehicle assault destroy cook property average silent travel",
-  "roof traffic soul urge tenant credit protect conduct enable animal cinnamon adult",
-];
-
 export const classicProviders = ["https://www.ethercluster.com/etc"];
 
 export const defaults = {
