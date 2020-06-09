63390ff87b4b69866b7afc1fb3a3553cd28dd4d7
LayneHaber <layne.haber@gmail.com>
2020-06-09 11:07:29 -0700

    fix sender unopened test

--- cypress/tests/daicard.js
+++ cypress/tests/daicard.js
@@ -68,7 +68,7 @@ describe("Daicard", () => {
         my.burnCard();
         my.deposit(depositEth).then((tokensDeposited) => {
           my.pay(recipient.publicId, payTokens);
-          my.restoreMnemonic(recipient.mnemonic.phrase);
+          my.restoreMnemonic(recipient.mnemonic);
           cy.resolve(my.getChannelTokenBalance).should("contain", payTokens);
         });
       });
