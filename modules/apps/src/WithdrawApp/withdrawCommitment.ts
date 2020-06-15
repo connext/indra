568603da7ee4d6e0843cec87bc5256b61c1536ed
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-06-15 15:27:28 +0400

    Fix build errors

--- modules/apps/src/WithdrawApp/withdrawCommitment.ts
+++ modules/apps/src/WithdrawApp/withdrawCommitment.ts
@@ -11,7 +11,6 @@ import { utils } from "ethers";
 
 const { defaultAbiCoder, Interface } = utils;
 
-const iface = new Interface(ConditionalTransactionDelegateTarget.abi);
 export class WithdrawCommitment extends MultisigCommitment {
   public constructor(
     public readonly contractAddresses: ContractAddresses,
@@ -28,17 +27,19 @@ export class WithdrawCommitment extends MultisigCommitment {
   public getTransactionDetails(): MultisigTransaction {
     const encodedOutcome: string = defaultAbiCoder.encode(
       [singleAssetSinglePartyCoinTransferEncoding],
-      [{ to: this.recipient, amount: this.amount }]
+      [{ to: this.recipient, amount: this.amount }],
     );
     const encodedParams: string = defaultAbiCoder.encode(
       [singleAssetTwoPartyCoinTransferInterpreterParamsEncoding],
-      [{ limit: this.amount, tokenAddress: this.assetId }]
-    )
+      [{ limit: this.amount, tokenAddress: this.assetId }],
+    );
 
     return {
       to: this.contractAddresses.ConditionalTransactionDelegateTarget,
       value: 0,
-      data: iface.functions.executeWithdraw.encode([
+      data: new Interface(
+        ConditionalTransactionDelegateTarget.abi,
+      ).encodeFunctionData("executeWithdraw", [
         this.contractAddresses.WithdrawInterpreter,
         this.nonce,
         encodedOutcome,
