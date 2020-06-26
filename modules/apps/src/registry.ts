55b67e6582490dd28b45e7acfc7c91658f646d5c
LayneHaber <layne.haber@gmail.com>
2020-06-25 18:53:40 -0700

    add app to registry

--- modules/apps/src/registry.ts
+++ modules/apps/src/registry.ts
@@ -3,12 +3,14 @@ import { DepositAppRegistryInfo } from "./DepositApp";
 import { HashLockTransferAppRegistryInfo } from "./HashLockTransferApp";
 import { SimpleLinkedTransferAppRegistryInfo } from "./SimpleLinkedTransferApp";
 import { SimpleSignedTransferAppRegistryInfo } from "./SimpleSignedTransferApp";
+import { GraphSignedTransferAppRegistryInfo } from "./GraphSignedTransferApp";
 import { SimpleTwoPartySwapAppRegistryInfo } from "./SimpleTwoPartySwapApp";
 import { WithdrawAppRegistryInfo } from "./WithdrawApp";
 
 export const AppRegistry: AppRegistryType = [
   SimpleLinkedTransferAppRegistryInfo,
   SimpleSignedTransferAppRegistryInfo,
+  GraphSignedTransferAppRegistryInfo,
   SimpleTwoPartySwapAppRegistryInfo,
   WithdrawAppRegistryInfo,
   HashLockTransferAppRegistryInfo,
