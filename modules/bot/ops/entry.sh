0a066d1d747c0d82609b4143b06543c35a409e45
LayneHaber <layne.haber@gmail.com>
2020-06-29 17:10:01 -0700

    remove unused vars

--- modules/bot/ops/entry.sh
+++ modules/bot/ops/entry.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-INDRA_ETH_RPC_URL="${INDRA_ETH_RPC_URL:-http://172.17.0.1:3000/api/ethprovider}"
-INDRA_NODE_URL="${INDRA_NODE_URL:-http://172.17.0.1:3000/api}"
-INDRA_NATS_URL="${INDRA_NATS_URL:-nats://172.17.0.1:4222}"
-BOT_REGISTRY_URL="${BOT_REGISTRY_URL:-http://172.17.0.1:3333}"
-
 echo "Bot registry container launched!"
 
 function finish {
