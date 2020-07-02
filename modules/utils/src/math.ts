b1cec6580fe291ef5443ba2d9e5c40c37d6f3b2a
Pedro Gomes <pedrogomes94@gmail.com>
2020-07-02 20:32:04 +0200

    update math.ts

--- modules/utils/src/math.ts
+++ modules/utils/src/math.ts
@@ -1,19 +1,15 @@
 import { DecString } from "@connext/types";
-import { BigNumber, constants, utils } from "ethers";
-
-import { toBN } from "./bigNumbers";
+import { BigNumber, BigNumberish, constants } from "ethers";
 
 const { Zero, MaxUint256 } = constants;
-const { parseEther, formatEther } = utils;
 
-export const toWad = (n: any) => parseEther(n.toString());
+export const toWad = (amount: BigNumberish, decimals = 18): BigNumber => {
+  return BigNumber.from(amount).mul(BigNumber.from("10").pow(decimals));
+};
 
-export const fromWad = formatEther;
-
-export const weiToToken = (wei: any, tokenPerEth: any) =>
-  toBN(formatEther(toWad(tokenPerEth).mul(wei)).replace(/\.[0-9]*$/, ``));
-
-export const tokenToWei = (token: any, tokenPerEth: any) => toWad(token).div(toWad(tokenPerEth));
+export const fromWad = (wad: BigNumberish, decimals = 18): BigNumber => {
+  return BigNumber.from(wad).div(BigNumber.from("10").pow(decimals));
+};
 
 export const maxBN = (lobn: any) =>
   lobn.reduce((max: any, current: any) => (max.gt(current) ? max : current), Zero);
@@ -21,10 +17,20 @@ export const maxBN = (lobn: any) =>
 export const minBN = (lobn: any) =>
   lobn.reduce((min: any, current: any) => (min.lt(current) ? min : current), MaxUint256);
 
-export const inverse = (bn: any) => formatEther(toWad(toWad(`1`)).div(toWad(bn)));
+export const inverse = (bn: any) => fromWad(toWad(toWad(`1`)).div(toWad(bn)));
 
-export const calculateExchange = (amount: BigNumber, swapRate: DecString): BigNumber => {
-  const [integer, fractional] = swapRate.split(".");
-  const safeSwapRate = [integer, (fractional || "0").substring(0, 18)].join(".");
-  return BigNumber.from(formatEther(amount.mul(parseEther(safeSwapRate))).replace(/\.[0-9]*$/, ""));
+export const toFixed = (value: string, decimals = 18) => {
+  const [integer, fractional] = value.split(".");
+  return [integer, (fractional || "0").substring(0, decimals)].join(".");
+};
+
+export const calculateExchange = (
+  inputAmount: BigNumber,
+  swapRate: DecString,
+  inputDecimals = 18,
+  outputDecimals = 18,
+): BigNumber => {
+  const swapRateWad = toWad(toFixed(swapRate));
+  const outputAmount = fromWad(toWad(fromWad(inputAmount, inputDecimals)).mul(swapRateWad));
+  return fromWad(toWad(outputAmount, outputDecimals));
 };
