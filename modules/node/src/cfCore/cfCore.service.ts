65c7886c478cc63b72d1c97a6f5dcacbffb8b534
LayneHaber <layne.haber@gmail.com>
2020-07-21 13:39:50 -0700

    retry multisig deployment on known errors

--- modules/node/src/cfCore/cfCore.service.ts
+++ modules/node/src/cfCore/cfCore.service.ts
@@ -40,6 +40,7 @@ import { CFCoreProviderId, MessagingProviderId, TIMEOUT_BUFFER } from "../consta
 import { Channel } from "../channel/channel.entity";
 
 import { CFCoreRecordRepository } from "./cfCore.repository";
+import { MAX_RETRIES, KNOWN_ERRORS } from "../onchainTransactions/onchainTransaction.service";
 
 const { Zero } = constants;
 
@@ -143,9 +144,30 @@ export class CFCoreService {
       } as MethodParams.DeployStateDepositHolder,
     };
     this.logCfCoreMethodStart(MethodNames.chan_deployStateDepositHolder, params.parameters);
-    const deployRes = await this.cfCore.rpcRouter.dispatch(params);
-    this.logCfCoreMethodResult(MethodNames.chan_deployStateDepositHolder, deployRes.result.result);
-    return deployRes.result.result as MethodResults.DeployStateDepositHolder;
+    const errors: { [k: number]: string } = [];
+    for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+      try {
+        const deployRes = await this.cfCore.rpcRouter.dispatch(params);
+        this.logCfCoreMethodResult(
+          MethodNames.chan_deployStateDepositHolder,
+          deployRes.result.result,
+        );
+        return deployRes.result.result as MethodResults.DeployStateDepositHolder;
+      } catch (e) {
+        errors[attempt] = e.message;
+        const knownErr = KNOWN_ERRORS.find((err) => e.message.includes(err));
+        if (!knownErr) {
+          this.log.error(
+            `Failed to deploy multisig with unknown error: ${e.message}. Should use onchain transaction service for this`,
+          );
+          throw new Error(e.stack || e.message);
+        }
+        this.log.warn(
+          `Sending transaction attempt ${attempt}/${MAX_RETRIES} failed: ${e.message}. Retrying multisig deployment. Use onchain tx service.`,
+        );
+      }
+    }
+    throw new Error(`Failed to deploy multisig (errors indexed by attempt): ${stringify(errors)}`);
   }
 
   async createWithdrawCommitment(
@@ -477,13 +499,11 @@ export class CFCoreService {
 
   async onModuleInit() {
     this.appRegistryMap = new Map();
-    this.configService.getSupportedChains().forEach(chainId => {
+    this.configService.getSupportedChains().forEach((chainId) => {
       const contractAddresses = this.configService.getContractAddresses(chainId);
-      RegistryOfApps.forEach(app => {
+      RegistryOfApps.forEach((app) => {
         const appDefinitionAddress = contractAddresses[app.name];
-        this.log.info(
-          `Creating ${app.name} app on chain ${chainId}: ${appDefinitionAddress}`,
-        );
+        this.log.info(`Creating ${app.name} app on chain ${chainId}: ${appDefinitionAddress}`);
         // set both name and app definition as keys for better lookup
         this.appRegistryMap.set(appDefinitionAddress, {
           actionEncoding: app.actionEncoding,
