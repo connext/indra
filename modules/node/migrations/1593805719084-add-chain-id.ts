ace636d0cfd442c0450e82998ecd80dbd1036a87
Rahul Sethuram <rahul@connext.network>
2020-07-21 18:49:43 +0200

    Fix migration

--- modules/node/migrations/1593805719084-add-chain-id.ts
+++ modules/node/migrations/1593805719084-add-chain-id.ts
@@ -4,7 +4,12 @@ export class addChainId1593805719084 implements MigrationInterface {
   name = "addChainId1593805719084";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "channel" ADD "chainId" integer NOT NULL`, undefined);
+    await queryRunner.query(`ALTER TABLE "channel" ADD "chainId" integer`, undefined);
+    await queryRunner.query(`UPDATE "channel" SET "chainId" = 0`, undefined);
+    await queryRunner.query(
+      `ALTER TABLE "channel" ALTER COLUMN "chainId" integer SET NOT NULL`,
+      undefined,
+    );
     await queryRunner.query(
       `ALTER TABLE "onchain_transaction" ALTER COLUMN "gasUsed" SET NOT NULL`,
       undefined,
