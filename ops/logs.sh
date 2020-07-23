129963484df829df7826165e0c663b1c5fde0d4a
bohendo <bohende@gmail.com>
2020-07-23 17:46:48 +0530

    🥩 beef up ops/log

--- ops/logs.sh
+++ ops/logs.sh
@@ -7,12 +7,35 @@ project="`cat $root/package.json | grep '"name":' | head -n 1 | cut -d '"' -f 4`
 name=$1
 shift
 
-if [[ "$name" =~ [0-9]+ ]]
-then
-  docker container logs --tail 100 --follow "${project}_testnet_${name}" $@
-else
-  docker service ps --no-trunc "${project}_$name"
-  sleep 1
-  docker service logs --raw --tail 100 --follow "${project}_$name" $@
-fi
+service_id="`docker service ls --format '{{.ID}} {{.Name}}' |\
+  cut -d "." -f 1 |\
+  grep "$name" |\
+  sort |\
+  head -n 1 |\
+  cut -d " " -f 1
+`"
 
+if [[ -n "$service_id" ]]
+then
+
+  docker service ps --no-trunc $service_id
+  sleep 0.5
+  docker service logs --tail 100 --follow $service_id
+
+else
+  echo "No running service names match: $name"
+
+  container_id="`docker container ls --filter 'status=running' --format '{{.ID}} {{.Names}}' |\
+    cut -d "." -f 1 |\
+    grep "$name" |\
+    sort |\
+    head -n 1 |\
+    cut -d " " -f 1
+  `"
+
+  if [[ -z "$container_id" ]]
+  then echo "No running container names match: $name"
+  else docker container logs --tail 100 --follow $container_id
+  fi
+
+fi
