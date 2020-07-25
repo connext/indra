3c546dff76ae4dfb11c4e578f37b5f1ea4729123
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-07-25 18:09:29 +0400

    Repeated payment test

--- modules/contracts/src.ts/tests/apps/graph-multi-transfer-app.spec.ts
+++ modules/contracts/src.ts/tests/apps/graph-multi-transfer-app.spec.ts
@@ -48,9 +48,7 @@ describe("GraphMultiTransferApp", () => {
   let wallet: Wallet;
   let graphMultiTransferApp: Contract;
 
-  async function computeOutcome(
-    state: GraphMultiTransferAppState,
-  ): Promise<CoinTransfer[]> {
+  async function computeOutcome(state: GraphMultiTransferAppState): Promise<CoinTransfer[]> {
     let ret = await graphMultiTransferApp.computeOutcome(encodeAppState(state));
     return keyify(state.coinTransfers, decodeTransfers(ret));
   }
@@ -192,31 +190,82 @@ describe("GraphMultiTransferApp", () => {
     ).deploy();
   });
 
-  describe("update state", () => {
-    it.only("happy case: sender creates transfer, receiver unlocks, sender finalizes, computesOutcome", async () => {
-      const receipt = getTestGraphReceiptToSign();
-      const state0 = await getInitialState(receipt);
+  it("sender creates transfer, receiver unlocks, sender finalizes, computesOutcome", async () => {
+    const receipt = getTestGraphReceiptToSign();
+    const state0 = await getInitialState(receipt);
 
-      const action0 = await getCreateAction(receipt);
-      const state1 = await applyAction(state0, action0);
-      await validateAction(state0, state1, action0);
+    const action0 = await getCreateAction(receipt);
+    const state1 = await applyAction(state0, action0);
+    await validateAction(state0, state1, action0);
 
-      const signature = await signGraphReceiptMessage(
+    const signature = await signGraphReceiptMessage(
+      receipt,
+      state0.chainId,
+      state0.verifyingContract,
+      wallet.privateKey,
+    );
+    const action1 = await getUnlockAction(receipt, signature);
+    const state2 = await applyAction(state1, action1);
+    await validateAction(state1, state2, action1);
+
+    const action2 = await getFinalizeAction();
+    const state3 = await applyAction(state2, action2);
+    await validateAction(state2, state3, action2);
+
+    const outcome = await computeOutcome(state3);
+    await validateOutcome(outcome, state3);
+  });
+
+  it("can repeatedly create and unlock payments", async () => {
+    const iterations = 5;
+    const receipt = getTestGraphReceiptToSign();
+    const initialState = await getInitialState(receipt);
+
+    let latestState: GraphMultiTransferAppState;
+    let prevState = initialState;
+
+    for (let i = 0; i < iterations; i++) {
+      // Create
+      const createAction = await getCreateAction(receipt);
+      latestState = await applyAction(prevState, createAction);
+      await validateAction(prevState, latestState, createAction);
+      prevState = latestState;
+
+      // Unlock
+      const signature: string = await signGraphReceiptMessage(
         receipt,
-        state0.chainId,
-        state0.verifyingContract,
+        await wallet.getChainId(),
+        prevState.verifyingContract,
         wallet.privateKey,
       );
-      const action1 = await getUnlockAction(receipt, signature);
-      const state2 = await applyAction(state1, action1);
-      await validateAction(state1, state2, action1);
+      const unlockAction = await getUnlockAction(receipt, signature);
+      latestState = await applyAction(prevState, unlockAction);
+      await validateAction(prevState, latestState, unlockAction);
+      prevState = latestState;
+    }
 
-      const action2 = await getFinalizeAction();
-      const state3 = await applyAction(state2, action2);
-      await validateAction(state2, state3, action2);
-
-      const outcome = await computeOutcome(state3);
-      await validateOutcome(outcome, state3);
-    });
+    const outcome = await computeOutcome(latestState!);
+    expect(outcome[0].amount).to.eq(
+      initialState.coinTransfers[0].amount.sub(BigNumber.from(10).mul(iterations)),
+    );
+    expect(outcome[1].amount).to.eq(
+      initialState.coinTransfers[1].amount.add(BigNumber.from(10).mul(iterations)),
+    );
   });
+
+  /*
+        Test cases:
+        - Cannot take action when state is finalized
+        - Create:
+            - Cannot call with odd turnNum
+            - Cannot create for more value than you have
+        - Unlock:
+            - Cannot call with even turnNum
+            - Fails if incorrect signature
+            - Does not update balances if cancelled
+        - Finalize:
+            - Can finalize on evens
+            - Can finalize on odds
+        - Can get the correct turntaker
+    */
 });
