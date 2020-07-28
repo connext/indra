5e4eb48bf69e0d26273389f052af833fc2371118
LayneHaber <layne.haber@gmail.com>
2020-07-27 21:42:46 -0700

    up timeout for tests

--- modules/bot/src/e2e.ts
+++ modules/bot/src/e2e.ts
@@ -43,8 +43,8 @@ export const command = {
     const startEthBalance = await sugarDaddy.getBalance();
 
     // sugarDaddy grants each bot some funds to start with
-    const TRANSFER_AMT = parseEther("0.001");
-    const DEPOSIT_AMT = parseEther("0.01"); // Note: max amount in signer address is 1 eth
+    const TRANSFER_AMT = parseEther("0.0001");
+    const DEPOSIT_AMT = parseEther("0.001"); // Note: max amount in signer address is 1 eth
 
     // Abort if sugarDaddy doesn't have enough ETH to fund all the bots
     if (startEthBalance.lt(DEPOSIT_AMT.mul(2))) {
@@ -59,21 +59,23 @@ export const command = {
 
     // SETUP SENDER + FUND
 
-    let signer = new ChannelSigner(privateKey, env.ethProviderUrl);
     const sender = await connect({
       ...env,
-      signer,
+      signer: new ChannelSigner(privateKey, env.ethProviderUrl),
       loggerService: new ColorfulLogger("Sender", argv.logLevel, true),
       store: getMemoryStore(),
     });
 
     const bot = new Wallet(privateKey, ethProvider);
     if ((await bot.getBalance()).lt(DEPOSIT_AMT.div(Two))) {
-      console.log(`Sending ${DEPOSIT_AMT} ETH to sender: ${abrv(bot.address)}`);
-      await sugarDaddy.sendTransaction({
-        to: bot.address,
+      console.log(`Sending ${DEPOSIT_AMT} ETH to sender: ${abrv(sender.signerAddress)}`);
+      const tx = await sugarDaddy.sendTransaction({
+        to: sender.signerAddress,
         value: DEPOSIT_AMT,
       });
+      console.log(`Tx sent: ${tx.hash}`);
+      await tx.wait();
+      console.log(`Tx mined on ${tx.chainId}`);
     }
 
     console.log(`Sender:
@@ -97,10 +99,9 @@ export const command = {
 
     privateKey = getRandomPrivateKey();
 
-    signer = new ChannelSigner(privateKey, env.ethProviderUrl);
     const receiver = await connect({
       ...env,
-      signer,
+      signer: new ChannelSigner(privateKey, env.ethProviderUrl),
       loggerService: new ColorfulLogger("Receiver", argv.logLevel, true),
       store: getMemoryStore(),
     });
@@ -123,7 +124,7 @@ export const command = {
     console.log("Receiver started.");
 
     // SENDER DEPOSIT
-    console.log(`Sender depositing`);
+    console.log(`Sender depositing ETH (onchain balance: ${formatEther(await bot.getBalance())})`);
     await senderAgent.depositIfNeeded(TRANSFER_AMT, TRANSFER_AMT);
     console.log(`Deposit complete`);
 
@@ -135,12 +136,24 @@ export const command = {
 
     // SENDER TRANSFER TO RECEIVER
     console.log(`Starting transfer`);
+    const ONE_MINUTE = 60_000;
     const receiverUnlocked = receiver.waitFor(
       EventNames.CONDITIONAL_TRANSFER_UNLOCKED_EVENT,
-      60_000,
+      ONE_MINUTE,
     );
-    const senderUnlocked = sender.waitFor(EventNames.CONDITIONAL_TRANSFER_UNLOCKED_EVENT, 60_000);
-    await senderAgent.pay(receiver.publicIdentifier, receiver.signerAddress, TRANSFER_AMT);
+    const senderUnlocked = sender.waitFor(
+      EventNames.CONDITIONAL_TRANSFER_UNLOCKED_EVENT,
+      ONE_MINUTE,
+    );
+    await senderAgent.pay(
+      receiver.publicIdentifier,
+      receiver.signerAddress,
+      TRANSFER_AMT,
+      undefined,
+      undefined,
+      undefined,
+      ONE_MINUTE,
+    );
     await receiverUnlocked;
     await senderUnlocked;
     console.log(`Transfer complete`);
