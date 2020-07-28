ac461062341a8c618c4a0c227d04d0dce04b47e1
bohendo <bohende@gmail.com>
2020-07-28 16:29:27 +0530

    🐛 [node] keep webpack in dev-mode

--- modules/node/ops/webpack.config.js
+++ modules/node/ops/webpack.config.js
@@ -2,7 +2,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   target: "node",
 
   context: path.join(__dirname, ".."),
