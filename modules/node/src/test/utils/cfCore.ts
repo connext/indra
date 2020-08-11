a4b88fc5aba3b3227d0dc81976bf3f2af9fb0885
LayneHaber <layne.haber@gmail.com>
2020-08-10 19:29:30 -0600

    use 1339 default chain

--- modules/node/src/test/utils/cfCore.ts
+++ modules/node/src/test/utils/cfCore.ts
@@ -177,7 +177,7 @@ export const createStoredAppChallenge = (
     versionNumber: One,
     finalizesAt: Zero,
     status: StoredAppChallengeStatus.IN_DISPUTE,
-    chainId: 1337,
+    chainId: 1339,
     ...overrides,
   };
 };
@@ -192,7 +192,7 @@ export const createStateProgressedEventPayload = (
     timeout: Zero,
     turnTaker: getRandomAddress(),
     signature: getRandomAddress(),
-    chainId: 1337,
+    chainId: 1339,
     ...overrides,
   };
 };
@@ -206,7 +206,7 @@ export const createChallengeUpdatedEventPayload = (
     versionNumber: One,
     finalizesAt: Zero,
     status: ChallengeStatus.IN_DISPUTE,
-    chainId: 1337,
+    chainId: 1339,
     ...overrides,
   };
 };
