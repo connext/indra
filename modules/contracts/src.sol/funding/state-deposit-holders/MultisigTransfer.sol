1909351888a17d8a4dbd9de6d5a842f6f681bfa1
Rahul Sethuram <rahul@connext.network>
2020-08-14 11:05:47 +0200

    🥅 Update error message

--- modules/contracts/src.sol/funding/state-deposit-holders/MultisigTransfer.sol
+++ modules/contracts/src.sol/funding/state-deposit-holders/MultisigTransfer.sol
@@ -40,7 +40,7 @@ contract MultisigTransfer is MultisigData {
     function safeTransfer(address token, address to, uint value) internal {
         // bytes4(keccak256(bytes('transfer(address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'MultisigTransfer: TRANSFER_FAILED');
     }
 
 }
