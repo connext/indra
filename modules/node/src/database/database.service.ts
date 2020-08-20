52151e6742c003cd3d15f6e34424c131350781bd
LayneHaber <layne.haber@gmail.com>
2020-08-20 15:22:59 -0600

    add migration

--- modules/node/src/database/database.service.ts
+++ modules/node/src/database/database.service.ts
@@ -70,6 +70,7 @@ import { removeAppCommitments1595210814094 } from "../../migrations/159521081409
 import { updateTxEnum1595439120210 } from "../../migrations/1595439120210-update-tx-enum";
 import { addAppIdTx1596488084652 } from "../../migrations/1596488084652-add-app-id-tx";
 import { transactionAppFlag1596924706697 } from "../../migrations/1596924706697-transaction-app-flag";
+import { storeLatestAction1597957131361 } from "../../migrations/1597957131361-store-latest-action";
 
 export const entities = [
   AppInstance,
@@ -139,6 +140,7 @@ export const migrations = [
   updateTxEnum1595439120210,
   addAppIdTx1596488084652,
   transactionAppFlag1596924706697,
+  storeLatestAction1597957131361,
 ];
 
 @Injectable()
